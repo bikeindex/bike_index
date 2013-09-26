67405f9b28d94157d9de15808a8cb7198f111c01
sethherr <seth.william.herr@gmail.com>
2013-09-26 13:12:29 -0500

    Remove kickstarter in dev so that it doesn't put up error message about https. remove api_url from manufacturers, because it's making something invalid.

--- app/serializers/manufacturer_index_serializer.rb
+++ app/serializers/manufacturer_index_serializer.rb
@@ -1,10 +1,4 @@
 class ManufacturerIndexSerializer < ActiveModel::Serializer
   attributes :slug,
-    :name,
-    :api_url
-
-  def api_url
-    api_v1_manufacturer_url(object)
-  end
-
+    :name
 end
