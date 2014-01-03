bf92709fa74c8f2d2a2df6b77c0c66acd69519c6
sethherr <seth.william.herr@gmail.com>
2014-01-03 08:40:22 -0600

    WIP

--- app/controllers/api/v1/bikes_controller.rb
+++ app/controllers/api/v1/bikes_controller.rb
@@ -1,13 +1,37 @@
 module Api
   module V1
     class BikesController < ApiV1Controller
+      before_filter :current_user, :cors_preflight_check
+      after_filter :cors_set_access_control_headers
 
+      
       def index
         respond_with BikeSearcher.new(params).find_bikes
       end
 
       def show
         respond_with Bike.find(params[:id])
+      end
+
+      # For all responses in this controller, return the CORS access control headers.
+      def cors_set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        # headers['Access-Control-Max-Age'] = "1728000"
+      end
+      # If this is a preflight OPTIONS request, then short-circuit the
+      # request, return only the necessary headers and return an empty
+      # text/plain.
+      def cors_preflight_check
+        if request.method == :options
+          headers['Access-Control-Allow-Origin'] = '*'
+          headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+          headers['Access-Control-Allow-Headers'] = '*'
+          headers['Access-Control-Max-Age'] = '1728000'
+          render :text => '', :content_type => 'text/plain'
+        end
       end
     
     end
