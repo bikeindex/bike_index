6dcdfa9ba803d0fefe07d5d4a19a80010ec2cf77
sethherr <seth.william.herr@gmail.com>
2014-01-14 13:12:25 -0600

    wheel size is not unique

--- app/models/wheel_size.rb
+++ app/models/wheel_size.rb
@@ -1,7 +1,7 @@
 class WheelSize < ActiveRecord::Base
   attr_accessible :name, :priority, :description, :iso_bsd
   validates_presence_of :name, :priority, :description, :iso_bsd
-  validates_uniqueness_of :name, :description, :iso_bsd
+  validates_uniqueness_of :description, :iso_bsd
   has_many :bikes
 
   default_scope order(:iso_bsd)
--- spec/models/wheel_size_spec.rb
+++ spec/models/wheel_size_spec.rb
@@ -6,7 +6,6 @@ describe WheelSize do
     it { should validate_presence_of :priority }
     it { should validate_presence_of :description }
     it { should validate_presence_of :iso_bsd }
-    it { should validate_uniqueness_of :name }  
     it { should validate_uniqueness_of :description }
     it { should validate_uniqueness_of :iso_bsd }
   end
