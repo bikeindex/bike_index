b1073c91f23515cb887520ba4defab104dd0f166
sethherr <seth.william.herr@gmail.com>
2014-01-20 15:52:38 -0600

    cors on things that need cors. Duh.

--- app/controllers/api/v1/frame_materials_controller.rb
+++ app/controllers/api/v1/frame_materials_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module V1
     class FrameMaterialsController < ApiV1Controller
+      before_filter :cors_preflight_check
+      after_filter :cors_set_access_control_headers
+
       def index
         respond_with FrameMaterial.all
       end
