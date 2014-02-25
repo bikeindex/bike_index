de76a92207a61c43157488234947c0788f98f57e
sethherr <seth.william.herr@gmail.com>
2014-02-25 11:04:10 -0600

    updated dumper config

--- config/initializers/dumper.rb
+++ config/initializers/dumper.rb
@@ -1,3 +1,3 @@
-if Rails.env.production?
-  Dumper::Agent.start(:app_key => ENV['DUMPER_KEY'])
+Dumper::Agent.start_if(:app_key => ENV['DUMPER_KEY']) do
+  Rails.env.production? && dumper_enabled_host?
 end
