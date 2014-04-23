0963761a7b90c20e7c39a0dec47f3c07829d1af2
sethherr <seth.william.herr@gmail.com>
2014-04-22 19:06:18 -0500

    remove stupid comment

--- config/initializers/AdminRestriction.rb
+++ config/initializers/AdminRestriction.rb
@@ -3,9 +3,5 @@ class AdminRestriction
     user_id = request.env['rack.session'][:user_id]
     user = User.find_by_id(user_id)
     return user && user.superuser?
-    # unless current_user.present? and current_user.superuser?
-    #   flash[:error] = "Gotta be an admin. Srys"
-    #   redirect_to user_home_url(:subdomain => false) and return
-    # end
   end
 end
