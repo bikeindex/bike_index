c4dc8dc61bc979e7a825695fa8db923ee97e7ba7
sethherr <seth.william.herr@gmail.com>
2014-05-03 14:45:50 -0500

    updated admin bikes view

--- app/controllers/admin/bikes_controller.rb
+++ app/controllers/admin/bikes_controller.rb
@@ -8,7 +8,7 @@ class Admin::BikesController < Admin::BaseController
     if params[:email]
       @bikes = Bike.admin_text_search(params[:email])
     else 
-      @bikes = Bike.scoped.limit(100)
+      @bikes = Bike.all.order("created_at desc").limit(100)
     end
   end
 
