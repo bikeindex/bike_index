9878d8d7cfdefd70c9d32e632ca147cddcaa6c18
sethherr <seth.william.herr@gmail.com>
2014-06-14 09:55:06 -0500

    remove extra bit

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,6 +1,6 @@
 module UrlHelper
   def with_subdomain(subdomain)
-  	default_url_options :host => "bikeindex.org"
+  	# default_url_options :host => "bikeindex.org"
     subdomain = (subdomain || "")
     subdomain += "." unless subdomain.empty?
     [subdomain, request.domain].join
