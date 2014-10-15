0e71a1a22cf992f312c6ebf5738b70973320ee2d
Jamie Wright <jamie@brilliantfantastic.com>
2014-10-15 07:32:42 -0400

    Test rendering of a localized view. This includes a test view for a spanish version of the serials view. This needs to be updated and the test updated as well.

--- spec/controllers/info_controller_spec.rb
+++ spec/controllers/info_controller_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe InfoController do
 
-  describe :about do 
-    before do 
+  describe :about do
+    before do
       get :about
     end
     it { should respond_with(:success) }
     it { should render_template(:about) }
   end
 
-  describe :protect_your_bike do 
-    before do 
+  describe :protect_your_bike do
+    before do
       get :protect_your_bike
     end
     it { should respond_with(:success) }
     it { should render_template(:protect_your_bike) }
   end
 
-  describe :where do 
-    before do 
+  describe :where do
+    before do
       FactoryGirl.create(:country, iso: "US")
       get :where
     end
@@ -27,72 +27,85 @@ describe InfoController do
     it { should render_template(:where)}
   end
 
-  describe :roadmap do 
-    before do 
+  describe :roadmap do
+    before do
       get :roadmap
     end
     it { should respond_with(:success) }
     it { should render_template(:roadmap)}
   end
 
-  describe :security do 
-    before do 
+  describe :security do
+    before do
       get :security
     end
     it { should respond_with(:success) }
     it { should render_template(:security)}
   end
 
-  describe :serials do 
-    before do 
-      get :serials
+  describe :serials do
+    context 'for english speakers' do
+      before { get :serials }
+      it { should respond_with(:success) }
+      it { should render_template(:serials)}
     end
-    it { should respond_with(:success) }
-    it { should render_template(:serials)}
+
+    context 'for the spanish speakers' do
+      render_views
+
+      before do
+        I18n.locale = :es
+        get :serials
+      end
+
+      it 'renders the spanish localized template' do
+        expect(response.body).to match /Holla/im
+      end
+    end
   end
 
-  describe :stolen_bikes do 
-    before do 
+  describe :stolen_bikes do
+    before do
       get :stolen_bikes
     end
     it { should respond_with(:success) }
     it { should render_template(:stolen_bikes)}
   end
 
-  describe :privacy do 
-    before do 
+  describe :privacy do
+    before do
       get :privacy
     end
     it { should respond_with(:success) }
     it { should render_template(:privacy)}
   end
 
-  describe :terms do 
-    before do 
+  describe :terms do
+    before do
       get :terms
     end
     it { should respond_with(:success) }
     it { should render_template(:terms)}
   end
 
-  describe :vendor_terms do 
-    before do 
+  describe :vendor_terms do
+    before do
       get :vendor_terms
     end
     it { should respond_with(:success) }
     it { should render_template(:vendor_terms)}
   end
 
-  describe :resources do 
-    before do 
+  describe :resources do
+    before do
       get :resources
     end
     it { should respond_with(:success) }
     it { should render_template(:resources)}
   end
 
-  describe :spokecard do 
-    before do 
+  describe :spokecard do
+    before do
       get :spokecard
     end
     it { should respond_with(:success) }
