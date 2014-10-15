81287d72f51a4922bc18e20869947ce06f6bd3bf
Jamie Wright <jamie@brilliantfantastic.com>
2014-10-15 07:31:02 -0400

    Remove depreciation warning by setting the enforce_available_locales to false since we don't care at this point

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module Bikeindex
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.enforce_available_locales = false
     # config.i18n.default_locale = :de
 
     # Configure the default encoding used in templates for Ruby 1.9.
