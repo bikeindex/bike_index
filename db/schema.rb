52b219c5c2141dec98af69291192e1a09d2168a7
sethherr <seth.william.herr@gmail.com>
2014-12-09 16:21:25 -0800

    reset schema to actual database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141208210347) do
+ActiveRecord::Schema.define(:version => 20141110174307) do
 
   create_table "ads", :force => true do |t|
     t.string   "title"
@@ -574,18 +574,6 @@ ActiveRecord::Schema.define(:version => 20141208210347) do
   add_index "stolen_records", ["bike_id"], :name => "index_stolen_records_on_bike_id"
   add_index "stolen_records", ["latitude", "longitude"], :name => "index_stolen_records_on_latitude_and_longitude"
 
-  create_table "subscriptions", :force => true do |t|
-    t.integer  "user_id"
-    t.boolean  "is_current",        :default => true
-    t.string   "strip_customer_id"
-    t.string   "stripe_plan_id"
-    t.datetime "end_date"
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
-  end
-
-  add_index "subscriptions", ["user_id"], :name => "index_subscriptions_on_user_id"
-
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
@@ -618,8 +606,6 @@ ActiveRecord::Schema.define(:version => 20141208210347) do
     t.boolean  "can_invite"
     t.boolean  "can_send_many_stolen_notifications", :default => false, :null => false
     t.string   "auth_token"
-    t.boolean  "make_subscription_request",          :default => false, :null => false
-    t.boolean  "is_subscribed",                      :default => false, :null => false
   end
 
   add_index "users", ["password_reset_token"], :name => "index_users_on_password_reset_token"
