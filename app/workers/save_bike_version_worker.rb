3c96b16b9d51d80f8f63ad344fb63644c513247d
sethherr <seth.william.herr@gmail.com>
2015-03-24 15:04:06 -0500

    fix broken spec

--- app/workers/save_bike_version_worker.rb
+++ app/workers/save_bike_version_worker.rb
@@ -4,10 +4,13 @@ class SaveBikeVersionWorker
   sidekiq_options backtrace: true
 
   def perform(bike_id)
-    bike = Bike.unscoped.find(bike_id)
-    bike_json = BikeV2ShowSerializer.new(bike, root: false).to_json.gsub("'", '\'')
-    File.open("#{ENV['VERSIONER_LOCATION']}/bikes/#{bike_id}.json", 'w+') {|f| f.write(bike_json) }
-    `ruby #{ENV['VERSIONER_LOCATION']}/versioner.rb -l '#{ENV['VERSIONER_LOCATION']}'  -i #{bike_id}`
+    if ENV['VERSIONER_LOCATION'].present?
+      bike = Bike.unscoped.find(bike_id)
+      bike_json = BikeV2ShowSerializer.new(bike, root: false).as_json
+      out = JSON.pretty_generate(bike_json)
+      File.open("#{ENV['VERSIONER_LOCATION']}/bikes/#{bike_id}.json", 'w') {|f| f.write(out) }
+      puts `ruby #{ENV['VERSIONER_LOCATION']}/versioner.rb -l '#{ENV['VERSIONER_LOCATION']}' -i #{bike_id}`
+    end
   end
 
 end
