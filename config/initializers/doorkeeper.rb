d5be6979ff30cb0274286c099e7f076693688790
sethherr <seth.william.herr@gmail.com>
2015-03-24 08:21:18 -0500

    explicit doorkeeper force ssl

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -63,6 +63,12 @@ Doorkeeper.configure do
   # (Similar behaviour: https://developers.google.com/accounts/docs/OAuth2InstalledApp#choosingredirecturi)
   #
   # native_redirect_uri 'urn:ietf:wg:oauth:2.0:oob'
+  
+  # Forces the usage of the HTTPS protocol in non-native redirect uris (enabled
+  # by default in non-development environments). OAuth2 delegates security in
+  # communication to the HTTPS protocol so it is wise to keep this enabled.
+  #
+  force_ssl_in_redirect_uri Rails.env.production?
 
   # Specify what grant flows are enabled in array of Strings. The valid
   # strings and the flows they enable are:
