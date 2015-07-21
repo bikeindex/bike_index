0ed8137821e89d01280d9d87475cf22dd44a9066
sethherr <seth.william.herr@gmail.com>
2015-07-21 09:53:43 -0500

    remove comment

--- lib/integrations/stolen_twitterbot_integration.rb
+++ lib/integrations/stolen_twitterbot_integration.rb
@@ -6,19 +6,6 @@ class StolenTwitterbotIntegration
     HTTParty.post(ENV['STOLEN_TWITTERBOT_URL'],
       body: options.to_json,
       headers: { 'Content-Type' => 'application/json' } )
-
-    # begin
-    #   uri = URI("http://bikebook.io#{method}")
-    #   uri.query = URI.encode_www_form(query)
-    #   res = Net::HTTP.get_response(uri)
-    # rescue
-    #   return nil
-    # end
-    # return nil unless res.is_a?(Net::HTTPSuccess)
-
-    # response = JSON.parse(res.body)
-    # return response if response.kind_of?(Array)
-    # response.with_indifferent_access     
   end
-  
+
 end
