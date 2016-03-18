0ac3239df9a0f9110dabcc79ac322892a5e1258e
sethherr <seth.william.herr@gmail.com>
2016-03-17 23:28:50 -0500

    what

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -10,37 +10,53 @@ describe Admin::UsersController do
       }.should raise_error(ActionController::RoutingError)
     end
     it "shows the edit page if the user exists" do 
-      @admin = FactoryGirl.create(:admin)
-      @user = FactoryGirl.create(:user)
-      set_current_user(@admin)
-      get :edit, id: @user.username
+      admin = FactoryGirl.create(:admin)
+      user = FactoryGirl.create(:user)
+      set_current_user(admin)
+      get :edit, id: user.username
       response.should render_template :edit
     end
   end
 
   describe :update do 
-    it "updates all the things that can be edited" do 
-      @admin = FactoryGirl.create(:admin)
-      @user = FactoryGirl.create(:user, confirmed: false)
-      set_current_user(@admin)
-      post :update, id: @user.username, :user =>{
-        name: "New Name",
-        email: "newemail@example.com",
-        confirmed: true,
-        superuser: true,
-        is_content_admin: true,
-        can_invite: true,
-        can_send_many_stolen_notifications: true,
-        banned: true
-      }
-      @user.reload.name.should eq("New Name")
-      @user.email.should eq("newemail@example.com")
-      @user.confirmed.should be_true
-      @user.superuser.should be_true
-      @user.can_invite.should be_true
-      @user.can_send_many_stolen_notifications.should be_true
-      @user.banned.should be_true
+    context 'non developer' do
+      it "updates all the things that can be edited" do 
+        admin = FactoryGirl.create(:admin)
+        user = FactoryGirl.create(:user, confirmed: false)
+        set_current_user(admin)
+        post :update, id: user.username, :user =>{
+          name: "New Name",
+          email: "newemailexample.com",
+          confirmed: true,
+          superuser: true,
+          developer: true,
+          is_content_admin: true,
+          can_invite: true,
+          can_send_many_stolen_notifications: true,
+          banned: true
+        }
+        user.reload.name.should eq("New Name")
+        user.email.should eq("newemailexample.com")
+        user.confirmed.should be_true
+        user.superuser.should be_true
+        user.developer.should be_false
+        user.can_invite.should be_true
+        user.can_send_many_stolen_notifications.should be_true
+        user.banned.should be_true
+      end
     end
+    context 'developer' do
+      it 'updates developer' do
+        admin = FactoryGirl.create(:admin)
+        admin.developer = true
+        admin.save
+        user = FactoryGirl.create(:user, confirmed: false)
+        set_current_user(admin)
+        post :update, id: user.username, user: { developer: true }
+        user.developer.should be_true
+      end
+    end
+
   end
 
 
