b61262dba233b9fff61b7e0144097476ac066370
sethherr <seth.william.herr@gmail.com>
2016-04-14 09:56:30 -0500

    Content menu update
    Also, at some point RSpec stopped auto rendering views. Turned that back on now

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe :nav_link do
+  describe :active_link do
     context 'without a class' do
       it 'returns the link active if it ought to be' do
         view.stub(:current_page?).and_return(true)
         generated = '<a href="http://bikeindex.org" class=" active">Bike Index about</a>'
-        expect(helper.nav_link('Bike Index about', 'http://bikeindex.org')).to eq generated
+        expect(helper.active_link('Bike Index about', 'http://bikeindex.org')).to eq generated
       end
     end
     context 'match controller true' do
@@ -14,7 +14,7 @@ describe ApplicationHelper do
       before { allow(helper).to receive(:request).and_return(request) }
       it 'returns the link active if it is a bikes page' do
         generated = '<a href="' + new_bike_url + '" class="seeeeeeee active">Bike Index bikes page</a>'
-        result = helper.nav_link('Bike Index bikes page', new_bike_url, match_controller: true, class_name: 'seeeeeeee')
+        result = helper.active_link('Bike Index bikes page', new_bike_url, match_controller: true, class_name: 'seeeeeeee')
         expect(result).to eq generated
       end
     end
@@ -22,8 +22,15 @@ describe ApplicationHelper do
       it 'returns the link active if it ought to be' do
         view.stub(:current_page?).and_return(true)
         generated = '<a href="http://bikeindex.org" class="nav-party-link active">Bike Index about</a>'
-        result = helper.nav_link('Bike Index about', 'http://bikeindex.org', class_name: 'nav-party-link')
+        result = helper.active_link('Bike Index about', 'http://bikeindex.org', class_name: 'nav-party-link')
         expect(result).to eq generated
+      end
+    end
+    context 'organization_invitation' do
+      it 'returns link, active if it ought to be' do
+        view.stub(:controller_name).and_return('organization_invitations')
+        generated = '<a href="/invitations" class="">Invitations</a>'
+        helper.active_link('Invitations', '/invitations').should eq(generated)
       end
     end
   end
@@ -35,28 +42,27 @@ describe ApplicationHelper do
     end
   end
 
-  describe :current_page_form_well? do
-    context 'bikes new' do
-      it 'returns link, active if it ought to be' do
-        view.stub(:controller_name).and_return('bikes')
-        view.stub(:action_name).and_return('new')
-        helper.current_page_form_well?.should be_true
+  describe 'content_page_type' do
+    context 'info controller' do
+      it 'returns info active_page' do
+        view.stub(:controller_name).and_return('info')
+        view.stub(:action_name).and_return('dev_and_design')
+        expect(helper.content_page_type).to eq 'dev_and_design'
       end
     end
-    context 'bikes show' do
-      it 'returns link, active if it ought to be' do
-        view.stub(:controller_name).and_return('bikes')
-        view.stub(:action_name).and_return('show')
-        helper.current_page_form_well?.should be_false
+    context 'news controller' do
+      it 'returns news index' do
+        view.stub(:controller_name).and_return('news')
+        view.stub(:action_name).and_return('index')
+        expect(helper.content_page_type).to eq 'news'
       end
     end
-  end
-
-  describe :admin_nav_link do
-    it 'returns link, active if it ought to be' do
-      view.stub(:controller_name).and_return('organization_invitations')
-      generated = '<a href="/invitations" class="">Invitations</a>'
-      helper.nav_link('Invitations', '/invitations').should eq(generated)
+    context 'bikes controller' do
+      let(:request) { double('request', url: new_bike_url) }
+      before { allow(helper).to receive(:request).and_return(request) }
+      it 'returns nil for non-info pages' do
+        expect(helper.content_page_type).to be_nil
+      end
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
+  config.render_views
   
   config.before :suite do
     DatabaseCleaner.clean
