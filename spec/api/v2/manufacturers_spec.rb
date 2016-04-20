8aefc00f3b906fc45c2c6501e4c9761cbed0a9be
sethherr <seth.william.herr@gmail.com>
2016-04-20 10:42:06 -0500

    update manufacturer spec

--- spec/api/v2/manufacturers_spec.rb
+++ spec/api/v2/manufacturers_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Manufacturers API V2' do
   describe 'root' do 
-    it "responds on index with pagination" do
+    it 'responds on index with pagination' do
       manufacturer = FactoryGirl.create(:manufacturer)
       FactoryGirl.create(:manufacturer)
       get '/api/v2/manufacturers?per_page=1'
@@ -21,39 +21,39 @@ describe 'Manufacturers API V2' do
     before :all do 
       @manufacturer = FactoryGirl.create(:manufacturer)
     end
-    it "returns one with from an id" do
+    it 'returns one with from an id' do
       get "/api/v2/manufacturers/#{@manufacturer.id}"
       result = response.body
       expect(response.code).to eq('200')
       expect(JSON.parse(result)['manufacturer']['id']).to eq(@manufacturer.id)
     end
 
-    it "responds with missing and cors headers" do 
-      get "/api/v2/manufacturers/10000"
+    it 'responds with missing and cors headers' do 
+      get '/api/v2/manufacturers/10000'
       expect(response.code).to eq('404')
-      expect(JSON(response.body)["error"].present?).to be_truthy
+      expect(JSON(response.body)['error'].present?).to be_truthy
       expect(response.headers['Access-Control-Allow-Origin']).to eq('*')
       expect(response.headers['Access-Control-Request-Method']).to eq('*')
       expect(response.headers['Content-Type'].match('json')).to be_present
     end
 
-    it "returns one from a name" do 
+    it 'returns one from a name' do 
       # THIS FAILS when we don't create a manufacturer in this block,
       # I've got no idea why
       manufacturer = FactoryGirl.create(:manufacturer, name: 'awesome')
-      get "/api/v2/manufacturers/awesome"
+      get '/api/v2/manufacturers/awesome'
       result = response.body
       expect(response.code).to eq('200')
       expect(JSON.parse(result)['manufacturer']['id']).to eq(@manufacturer.id)
     end
   end
 
-  describe "JUST CRAZY 404" do 
-    it "responds with missing and cors headers" do 
-      get "/api/v2/manufacturersdddd"
+  describe 'JUST CRAZY 404' do 
+    it 'responds with missing and cors headers' do 
+      get '/api/v2/manufacturersdddd'
       # pp JSON.parse(response.body)
       expect(response.code).to eq('404')
-      expect(JSON(response.body)["error"].present?).to be_truthy
+      expect(JSON(response.body)['error'].present?).to be_truthy
       expect(response.headers['Access-Control-Allow-Origin']).to eq('*')
       expect(response.headers['Access-Control-Request-Method']).to eq('*')
       expect(response.headers['Content-Type'].match('json')).to be_present
