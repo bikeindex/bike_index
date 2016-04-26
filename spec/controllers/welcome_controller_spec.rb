de3d6d1b36f2a5bd8ff24481bceb06b18dcc011e
sethherr <seth.william.herr@gmail.com>
2016-04-26 16:29:55 -0500

    revisions of stuff

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,47 +2,37 @@ require 'spec_helper'
 
 describe WelcomeController do
   describe 'index' do
-    before do
+    it 'renders' do
       get :index
+      expect(response.status).to eq(200)
+      expect(response).to render_template(:index)
+      expect(response).to render_with_layout('application_updated')
+      expect(flash).to_not be_present
     end
-    it { is_expected.to respond_with(:success) }
-    it { is_expected.to render_template(:index) }
-    it { is_expected.not_to set_flash }
   end
 
   describe 'goodbye' do
-    before do
+    it 'renders' do
       get :goodbye
+      expect(response.status).to eq(200)
+      expect(response).to render_template(:index)
+      expect(response).to render_with_layout('application_updated')
+      expect(flash).to_not be_present
     end
-    it { is_expected.to respond_with(:success) }
-    it { is_expected.to render_template(:goodbye) }
   end
 
   describe 'user_home' do
-    describe 'user not present' do
-      before do
+    context 'user not present' do
+      it 'redirects' do
         get :user_home
+        expect(response.status).to eq(200)
+        expect(response).to redirect_to(new_user_url)
+        expect(flash).to be_present
       end
-      it { is_expected.to respond_with(:redirect) }
-      it { is_expected.to redirect_to(new_user_url) }
-      it { is_expected.not_to set_flash }
     end
 
-    describe 'when user is present' do
-      before do
-        user = FactoryGirl.create(:user)
-        FactoryGirl.create(:ownership, user_id: user.id, current: true)
-        set_current_user(user)
-        get :user_home
-      end
-
-      it { is_expected.not_to set_flash }
-      it { is_expected.to respond_with(:success) }
-      it { is_expected.to render_template(:user_home) }
-    end
-
-    describe 'user things should be assigned' do
-      it 'sends the bikes and the locks' do
+    context 'when user is present' do
+      it 'renders and user things are assigned' do
         user = FactoryGirl.create(:user)
         ownership = FactoryGirl.create(:ownership, user_id: user.id, current: true)
         lock = FactoryGirl.create(:lock, user: user)
