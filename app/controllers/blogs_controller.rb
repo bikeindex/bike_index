b4f63937264706af39b53810678885df50e9ae25
Lucas Andrade <lucasandradeunb@gmail.com>
2016-05-02 13:59:15 -0300

    Application_controller is almost done (call miss the assert method)

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,10 +8,18 @@
 
 class BlogsController < ApplicationController
 
+  # Name: index
+  # Explication:
+  # Paramts:
+  # Return:
   def index
     redirect_to news_index_url
   end
   
+  # Name: show
+  # Explication:
+  # Paramts:
+  # Return:
   def show
     redirect_to news_url
   end
