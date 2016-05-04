be758e3e8cd0ed5b77b9148603b5294388d137b6
timlkelly <timlkelly@gmail.com>
2016-05-04 11:47:21 -0500

    create tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -146,6 +146,17 @@ describe ApplicationHelper do
         end
       end
     end
+    context 'stolen' do
+      before { allow(view).to receive(:controller_name) { 'stolen'} }
+       %w(index).each do |action|
+        context action do
+          it 'returns nil' do
+            allow(view).to receive(:action_name) { action }
+            expect(helper.current_page_skeleton).to eq 'content_skeleton'
+          end
+        end
+      end
+    end
   end
 
   describe 'content_page_type' do
