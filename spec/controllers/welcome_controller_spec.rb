cee965b413ac22442177fcbab86d7f5c8e37a65f
sethherr <seth.william.herr@gmail.com>
2016-05-06 13:26:41 -0500

    do not break if no bikes

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -31,25 +31,35 @@ describe WelcomeController do
 
     context 'when user is present' do
       let(:user) { FactoryGirl.create(:user) }
-      let(:ownership) { FactoryGirl.create(:ownership, user_id: user.id, current: true) }
-      let(:bike) { ownership.bike }
-      let(:bike_2) { FactoryGirl.create(:bike) }
-      let(:lock) { FactoryGirl.create(:lock, user: user) }
-      before do
-        allow_any_instance_of(User).to receive(:bikes) { [bike, bike_2] }
-        allow_any_instance_of(User).to receive(:locks) { [lock] }
-        set_current_user(user)
+      context 'without anything' do
+        it 'renders' do
+          set_current_user(user)
+          get :user_home
+          expect(response.status).to eq(200)
+          expect(response).to render_template('user_home')
+          expect(response).to render_with_layout('application_revised')
+        end
       end
-      it 'renders and user things are assigned' do
-        allow(controller).to receive(:revised_layout_enabled?) { true }
-        get :user_home, per_page: 1
-        expect(response.status).to eq(200)
-        expect(response).to render_template('user_home')
-        expect(response).to render_with_layout('application_revised')
-        expect(assigns(:bikes).count).to eq 1
-        expect(assigns(:per_page).to_s).to eq '1'
-        expect(assigns(:bikes).first).to eq(bike)
-        expect(assigns(:locks).first).to eq(lock)
+      context 'with stuff' do
+        let(:ownership) { FactoryGirl.create(:ownership, user_id: user.id, current: true) }
+        let(:bike) { ownership.bike }
+        let(:bike_2) { FactoryGirl.create(:bike) }
+        let(:lock) { FactoryGirl.create(:lock, user: user) }
+        before do
+          allow_any_instance_of(User).to receive(:bikes) { [bike, bike_2] }
+          allow_any_instance_of(User).to receive(:locks) { [lock] }
+          set_current_user(user)
+        end
+        it 'renders and user things are assigned' do
+          get :user_home, per_page: 1
+          expect(response.status).to eq(200)
+          expect(response).to render_template('user_home')
+          expect(response).to render_with_layout('application_revised')
+          expect(assigns(:bikes).count).to eq 1
+          expect(assigns(:per_page).to_s).to eq '1'
+          expect(assigns(:bikes).first).to eq(bike)
+          expect(assigns(:locks).first).to eq(lock)
+        end
       end
     end
   end
