b7106e0dfa6998906c75afdfb10a72e33181171d
Lucas <lucasandradeunb@gmail.com>
2016-06-08 23:39:20 -0300

    Add some description to controllers

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -9,22 +9,42 @@
 class ErrorsController < ApplicationController
   respond_to :html, :xml, :json
  
+=begin    
+  Name: bad_request
+  Explication: just a rrender error page
+=end
   def bad_request
     render status: 400
   end
 
+=begin    
+  Name: bad_request
+  Explication: just a rrender error page
+=end
   def not_found
     render status: 404
   end
 
+=begin    
+  Name: bad_request
+  Explication: just a rrender error page
+=end
   def unprocessable_entity
     render status: 422
   end
 
+=begin    
+  Name: bad_request
+  Explication: just a rrender error page
+=end
   def server_error
     render status: 500
   end
 
+=begin    
+  Name: bad_request
+  Explication: just a rrender error page
+=end
   def unauthorized
     render status: 401
   end
--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -20,6 +20,10 @@ class InfoController < ApplicationController
   def about
   end
   
+
+  # Explication: set a variable 
+  # Params: Organization.shown_on_map
+  # Return: @organizations
   def where
     @organizations = Organization.shown_on_map
   end
@@ -30,14 +34,17 @@ class InfoController < ApplicationController
   def protect_your_bike
   end
 
+# Explication: render a layout with one condition
   def privacy
     render layout: 'legal' unless revised_layout_enabled?
   end
 
+# Explication: render a layout with one condition
   def terms
     render layout: 'legal' unless revised_layout_enabled?
   end
 
+# Explication: render a layout with one condition
   def vendor_terms
     render layout: 'legal' unless revised_layout_enabled?
   end
@@ -48,11 +55,18 @@ class InfoController < ApplicationController
   def image_resources
   end
 
+=begin
+  Name: suport_the_index
+  Explication: get a text and render in layout
+  Params: text
+  Return: render layout
+=end 
   def support_the_index
     @pageTitle = 'Support the Bike Index'
     render layout: (revised_layout_enabled? ? 'application_revised' : 'application_updated')
   end
 
+# Explication: reditect so support
   def support_the_bike_index
     redirect_to support_the_index_url
   end
@@ -60,12 +74,19 @@ class InfoController < ApplicationController
   def dev_and_design
   end
 
+# Explication: reditect so stolen bike page
   def how_not_to_buy_stolen
     redirect_to 'https://files.bikeindex.org/stored/dont_buy_stolen.pdf'
   end
 
 protected
 
+=begin
+  Name: set_activeSection
+  Explication: set active text with some condition
+  Params: text
+  Return: @activeSection
+=end 
   def set_activeSection
     resources = %w(serials resources protect_your_bike image_resources)
     if resources.include? action_name
