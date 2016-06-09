e12b27deb82c32037a66d7685bcad859fb3083cb
Lucas <lucasandradeunb@gmail.com>
2016-06-08 23:44:22 -0300

    add more details of controllers methods

--- app/controllers/integrations_controller.rb
+++ app/controllers/integrations_controller.rb
@@ -10,6 +10,11 @@
 class IntegrationsController < ApplicationController
   include Sessionable
  
+=begin
+  Explication: create integration for user
+  Params: user params and integration
+  Return: sign_in_and_redirect
+=end 
   def create
     @integration = Integration.new
     @integration.access_token = request.env['omniauth.auth']['credentials']['token']
@@ -23,6 +28,11 @@ class IntegrationsController < ApplicationController
     end
   end
 
+=begin
+  Explication: control of creation integration for user
+  Params: integration
+  Return: redirect_to new_session_url and return
+=end 
   def integrations_controller_creation_error
     provider_name = request.env['omniauth.auth']['provider']
     flash[:notice] = "There was a problem authenticating you with #{provider_name}. Please try To sign in a different way."
