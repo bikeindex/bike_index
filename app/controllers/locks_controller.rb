1d6b94ebbbf3c592d3c239f536f19663a0857b9d
Lucas <lucasandradeunb@gmail.com>
2016-06-09 08:22:38 -0300

    add more comments to controllers

--- app/controllers/locks_controller.rb
+++ app/controllers/locks_controller.rb
@@ -10,6 +10,12 @@ class LocksController < ApplicationController
   
   before_filter :authenticate_user
 
+=begin
+  name: index
+  Explication: index of user locks
+  Params: current_user, @user
+  Return: @locks
+=end 
   def index
     @user = current_user
     assert_message(@user.kind_of?(User))
@@ -17,17 +23,35 @@ class LocksController < ApplicationController
     @locks = LockDecorator.decorate_collection(@user.locks)
   end
 
+=begin
+  name: show
+  Explication: show the new lock
+  Params: find_lock params (lock.id)
+  Return: @lock
+=end 
   def show
     lock = find_lock 
     @lock = LockDecorator.new(lock)
   end
 
+=begin
+  name: edit
+  Explication: edit find lock
+  Params: lock id
+  Return: @lock
+=end 
   def edit
     @lock = find_lock
     assert_message(@lock.kind_of?(Lock))
     assert_object_is_not_null(@lock)
   end
-  
+
+=begin
+  name: update
+  Explication: update lock for current user
+  Params: current_user and lock attributes
+  Return: @lock update and redirect user to locks url
+=end 
   def update
     @lock = find_lock
     assert_message(@lock.kind_of?(Lock))
@@ -40,10 +64,22 @@ class LocksController < ApplicationController
     end
   end
 
+=begin
+  name: new
+  Explication:
+  Params:
+  Return: @lock
+=end 
   def new
     @lock = Lock.new
   end
 
+=begin
+  name: create
+  Explication: create a new lock
+  Params: current_user
+  Return: @lock saved
+=end 
   def create
     @lock = Lock.new(params[:lock])
     @lock.user = current_user
@@ -55,6 +91,12 @@ class LocksController < ApplicationController
     end
   end
 
+=begin
+  name: destroy
+  Explication: destroy a lock object
+  Params: find_lock params (lock id)
+  Return: @lock destroyed
+=end 
   def destroy
     @lock = find_lock
     @lock.destroy
@@ -62,6 +104,13 @@ class LocksController < ApplicationController
   end
 
   protected
+ 
+=begin
+  name: find_lock
+  Explication: find lock by id
+  Params: lock, current user
+  Return: lock
+=end  
   def find_lock
     lock = Lock.find(params[:id])
     return lock if lock.user == current_user
