b5b1929d08079123f9e2f9641a58f547b324677a
sethherr <seth.william.herr@gmail.com>
2016-07-12 15:33:50 -0500

    switch to reg_embed layout

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -13,10 +13,14 @@ describe RegistrationsController do
     context 'no organization' do
       context 'no user' do
         it 'renders' do
-          get :new, stolen: true
+          get :new, stolen: true, skip_assets: true
           expect(response.status).to eq(200)
           expect(response).to render_template(:new)
           expect(flash).to_not be_present
+          expect(assigns(:stolen)).to be_truthy
+          expect(assigns(:skip_assets)).to be_truthy
+          expect(assigns(:creator)).to be_nil
+          expect(assigns(:owner_email)).to be_nil
         end
       end
       context 'with user' do
@@ -26,6 +30,10 @@ describe RegistrationsController do
           expect(response.status).to eq(200)
           expect(response).to render_template(:new)
           expect(flash).to_not be_present
+          expect(assigns(:stolen)).to be_falsey
+          expect(assigns(:skip_assets)).to be_falsey
+          expect(assigns(:creator)).to eq user
+          expect(assigns(:owner_email)).to eq user.email
           # Creator is user
           creator_input = response.body[/value=.*id..b_param_creator_id..*.\d*/i]
           creator_value = creator_input.gsub(/value=./, '').match(/\A[^\"]*/)[0]
@@ -42,12 +50,16 @@ describe RegistrationsController do
           expect(response).to render_template(:new)
           expect(flash).to_not be_present
           expect(response.headers['X-Frame-Options']).not_to be_present
+          expect(assigns(:stolen)).to be_falsey
+          expect(assigns(:skip_assets)).to be_falsey
+          expect(assigns(:organization)).to eq organization
+          expect(assigns(:creator)).to eq auto_user
         end
       end
       context 'with user' do
-        it 'renders' do
+        it 'renders, testing variables' do
           set_current_user(user)
-          get :new, organization_id: organization.id
+          get :new, organization_id: organization.id, stolen: true, skip_assets: true
           expect(response.status).to eq(200)
           expect(response).to render_template(:new)
           expect(flash).to_not be_present
@@ -66,6 +78,12 @@ describe RegistrationsController do
           creator_organization_input = body[/value=.*id..b_param_creation_organization_id/i]
           creator_organization_value = creator_organization_input.gsub(/value=./, '').match(/\A[^\"]*/)[0]
           expect(creator_organization_value).to eq organization.id.to_s
+
+          expect(assigns(:stolen)).to be_truthy
+          expect(assigns(:skip_assets)).to be_truthy
+          expect(assigns(:organization)).to eq organization
+          expect(assigns(:creator)).to eq auto_user
+          expect(assigns(:owner_email)).to eq user.email
         end
       end
     end
