7bb7c5757e91495de7def69c46ea4451263586f9
sethherr <seth.william.herr@gmail.com>
2016-07-13 11:39:13 -0500

    Stop failing on unpermitted parameters
    The fact that it raises in production and doesn't raise in dev/testing is bullshit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_controller.action_on_unpermitted_parameters :log
 
   # Code is not reloaded between requests.
   config.cache_classes = true
