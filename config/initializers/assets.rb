b9798effb7d7a01e248f5d659278cc8e1bd2a41d
sethherr <seth.william.herr@gmail.com>
2016-09-15 10:30:22 -0500

    bump assets version to recompile all assets. Hopefully fix #268

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.1'
+Rails.application.config.assets.version = '1.2'
 
 Rails.application.config.assets.precompile += %w(
   graphs.js embed.js embed_user.js documentation_v2.js application_revised.js
