30c6c749e8ab5570e434d3438c1fb3641b6292c1
sethherr <seth.william.herr@gmail.com>
2016-09-26 14:54:56 -0500

    improve creationstateworker

--- app/workers/create_creation_states_worker.rb
+++ app/workers/create_creation_states_worker.rb
@@ -6,13 +6,14 @@ class CreateCreationStatesWorker
     current = offset - 1
     while current < offset + 101
       current += 1
-      b = Bike.unscoped.find(current)
-      next unless b.present?
-      creation_state = CreationState.where(bike_id: b.id).first
-      creation_state ||= CreationState.new(bike_id: b.id)
-      creation_state.is_new ||= b.registered_new || false
-      creation_state.is_pos ||= b.registered_new || false
-      creation_state.update_attributes(creator_id: b.creator_id, organization_id: b.creation_organization_id)
+      bike = Bike.unscoped.where(id: current).first
+      next unless bike.present?
+      creation_state = CreationState.where(bike_id: bike.id).first
+      creation_state ||= CreationState.new(bike_id: bike.id)
+      creation_state.is_new ||= bike.registered_new || false
+      creation_state.is_pos ||= bike.registered_new || false
+      next if creation_state.id.present? && creation_state.creator_id.present?
+      creation_state.update_attributes(creator_id: bike.creator_id, organization_id: bike.creation_organization_id)
     end
   end
 end
