88e2e2ee340849446813e02ea2da8561e7d33473
sethherr <seth.william.herr@gmail.com>
2016-09-29 20:01:44 -0500

    test for landing page title

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -2,6 +2,6 @@ class LandingPagesController < ApplicationController
   layout 'application_revised'
   def show
     raise ActionController::RoutingError, 'Not found' unless current_organization.present?
-    @page_title = "#{current_organization.name} Bike Registration"
+    @page_title = "#{current_organization.short_name} Bike Registration"
   end
 end
--- spec/controllers/landing_pages_controller_spec.rb
+++ spec/controllers/landing_pages_controller_spec.rb
@@ -8,6 +8,8 @@ describe LandingPagesController do
       expect(response.status).to eq(200)
       expect(response).to render_template('show')
       expect(response).to render_with_layout('application_revised')
+      title = response.body[/<title[^>]*>([^<]*)/, 1] # First matching group ;)
+      expect(title).to eq 'University Bike Registration'
     end
   end
 end
