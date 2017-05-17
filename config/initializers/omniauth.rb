e9f8159bb31410cf901e848090a55008e0f58da3
Chris Olin <chris.olin@cloudcheckr.com>
2017-05-17 15:18:30 -0400

    configure google omniauth provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email'
   provider :strava, ENV['STRAVA_KEY'], ENV['STRAVA_SECRET'], scope: 'public'
+  provider :google, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: 'email'
 end
