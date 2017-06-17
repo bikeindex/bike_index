f8aef0c8045ef04f69e412696de0f07d3f014166
sethherr <seth.william.herr@gmail.com>
2017-06-17 15:21:47 -0700

    minor fixups

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -4,9 +4,7 @@ class LandingPagesController < ApplicationController
     raise ActionController::RoutingError, 'Not found' unless current_organization.present?
   end
 
-  def for_law_enforcement
-  end
+  def for_law_enforcement; end
 
-  def for_schools
-  end
+  def for_schools; end
 end
