43ca4f64e78288b76d760e755601a2e28e08ad90
sethherr <seth.william.herr@gmail.com>
2017-06-18 12:50:35 -0700

    render feedback stuff

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,5 +1,6 @@
 class LandingPagesController < ApplicationController
   layout 'application_revised'
+  before_filter :instantiate_feedback, except: [:show]
   def show
     raise ActionController::RoutingError, 'Not found' unless current_organization.present?
   end
@@ -7,4 +8,10 @@ class LandingPagesController < ApplicationController
   def for_law_enforcement; end
 
   def for_schools; end
+
+  protected
+
+  def instantiate_feedback
+    @feedback ||= Feedback.new
+  end
 end
