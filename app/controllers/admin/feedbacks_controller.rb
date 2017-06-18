ab7beb56da607a4e18e3f7d9830584ac774be4e8
sethherr <seth.william.herr@gmail.com>
2017-06-18 11:52:47 -0700

    add more controls

--- app/controllers/admin/feedbacks_controller.rb
+++ app/controllers/admin/feedbacks_controller.rb
@@ -1,15 +1,12 @@
 class Admin::FeedbacksController < Admin::BaseController
+  before_filter :find_feedback_for_params, except: [:show]
+
   def index
-    if params[:type].present?
-      type = params[:type] == 'msg' ? nil : params[:type]
-      feedbacks = Feedback.where(feedback_type: type).order(created_at: :desc)
-      @matching_count = feedbacks.count
-    else
-      feedbacks = Feedback.order(created_at: :desc)
-    end
+    @matching_count = @feedbacks.count if params[:type].present?
     page = params[:page] || 1
     per_page = params[:per_page] || 50
-    @feedbacks = feedbacks.page(page).per(per_page)
+    @feedbacks = @feedbacks.order(created_at: :desc)
+                           .page(page).per(per_page)
   end
 
   def show
@@ -20,16 +17,26 @@ class Admin::FeedbacksController < Admin::BaseController
     render json:
       case params[:start_at]
       when 'past_year'
-        Feedback.where('created_at >= ?', Time.now - 1.year)
+        @feedbacks.where('created_at >= ?', Time.now - 1.year)
           .group_by_week(:created_at).count
       when 'all_time'
-        Feedback.group_by_month(:created_at).count
+        @feedbacks.group_by_month(:created_at).count
       when 'past_week'
-        Feedback.where('created_at >= ?', Time.now - 1.week)
+        @feedbacks.where('created_at >= ?', Time.now - 1.week)
           .group_by_hour(:created_at).count
       else
-        Feedback.where('created_at >= ?', Time.now - 1.month)
+        @feedbacks.where('created_at >= ?', Time.now - 1.month)
           .group_by_day(:created_at).count
       end
+  end
+
+  private
+
+  def find_feedback_for_params
+    if params[:type].present?
+      @feedbacks = Feedback.where(feedback_type: (params[:type] == 'msg' ? nil : params[:type]))
+    else
+      @feedbacks = Feedback
+    end
   end
 end
