c2353e4d196f7e8fe4c6612fc713d7a873e77c46
sethherr <seth.william.herr@gmail.com>
2017-07-02 14:54:41 -0700

    no cache develop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
   config.cache_store = :dalli_store,
     { namespace: Bikeindex, expires_in: 0, compress: true }
 
