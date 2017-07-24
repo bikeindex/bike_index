843baedbe39b9e58b685cc0c249efcb9ffc30fbb
sethherr <seth.william.herr@gmail.com>
2017-07-23 18:28:56 -0700

    fix broken spec

--- app/models/recovery_display.rb
+++ app/models/recovery_display.rb
@@ -11,14 +11,14 @@ class RecoveryDisplay < ActiveRecord::Base
   before_validation :set_time
   def set_time
     if date_input.present?
-      self.date_recovered = DateTime.strptime("#{date_input} 06", "%m-%d-%Y %H")
+      self.date_recovered = DateTime.strptime("#{date_input} 06", '%m-%d-%Y %H')
     end
     self.date_recovered = Time.now unless date_recovered.present?
   end
 
   validate :quote_not_too_long
   def quote_not_too_long
-    return true if quote.length < 301
+    return true if quote.blank? || quote.length < 301
     errors.add :base, 'That quote is too long. Please shorten it to be less than 300 characters'
   end
 
@@ -35,5 +35,4 @@ class RecoveryDisplay < ActiveRecord::Base
     return nil unless stolen_record_id.present?
     StolenRecord.unscoped.find(stolen_record_id).bike
   end
-
 end
