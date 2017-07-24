57ed43855a1858f4843ee42f20c5534fadc02729
sethherr <seth.william.herr@gmail.com>
2017-07-23 18:19:32 -0700

    strings

--- spec/controllers/admin/recovery_displays_controller_spec.rb
+++ spec/controllers/admin/recovery_displays_controller_spec.rb
@@ -28,7 +28,7 @@ describe Admin::RecoveryDisplaysController do
     context 'valid create' do
       let(:invalid_attrs) do
         {
-          quote: "La croix scenester pug glossier, yuccie salvia humblebrag chia. Meditation health goth readymade flannel hot chicken austin tofu salvia cornhole tumeric hashtag plaid. Umami vegan hell of before they sold out copper mug chillwave authentic poke mumblecore godard la croix 8-bit. Venmo raw denim synth wolf. Food truck hot chicken waistcoat activated charcoal"
+          quote: 'La croix scenester pug glossier, yuccie salvia humblebrag chia. Meditation health goth readymade flannel hot chicken austin tofu salvia cornhole tumeric hashtag plaid. Umami vegan hell of before they sold out copper mug chillwave authentic poke mumblecore godard la croix 8-bit. Venmo raw denim synth wolf. Food truck hot chicken waistcoat activated charcoal'
         }
       end
       it 'does not create a recovery display that is too long' do
