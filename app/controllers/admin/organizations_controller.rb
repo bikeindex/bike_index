1942036128b650a6f51d579bdb9f8a7f3d8e9387
sethherr <seth.william.herr@gmail.com>
2017-09-10 16:39:34 -0400

    reorder instead of unscoping

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -5,10 +5,10 @@ class Admin::OrganizationsController < Admin::BaseController
   def index
     page = params[:page] || 1
     per_page = params[:per_page] || 50
-    orgs = Organization.unscoped.where(deleted_at: nil)
+    orgs = Organization.all
     orgs = orgs.paid if params[:is_paid].present?
     orgs = orgs.admin_text_search(params[:query]) if params[:query].present?
-    @organizations = orgs.order("#{@sort} #{@sort_direction}").page(page).per(per_page)
+    @organizations = orgs.reorder("#{@sort} #{@sort_direction}").page(page).per(per_page)
     @organizations_count = orgs.count
   end
 
