275ea824ae25cc4739d21f4992546b32761a60b2
sethherr <seth.william.herr@gmail.com>
2018-02-13 13:31:57 -0800

    Add footer email snippet

--- app/models/mail_snippet.rb
+++ app/models/mail_snippet.rb
@@ -18,6 +18,7 @@ class MailSnippet < ActiveRecord::Base
       {
         header: 'Top of email block',
         welcome: 'Below header',
+        footer: 'Above <3 <3 <3 <3 Bike Index Team - displays in text email too',
         security: 'How to keep your bike safe, in email "finished registration"'
       }.as_json
     end
