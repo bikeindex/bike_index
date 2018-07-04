a96921c61c2cd72d2ee8369b00b16843e88deace
sethherr <seth.william.herr@gmail.com>
2018-07-04 13:31:08 -0700

    maybe make travis work

--- spec/factories/bulk_imports.rb
+++ spec/factories/bulk_imports.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :bulk_import do
+    sequence(:id) { |n| n } # WTF Travis? Travis is blowing up, something to do with different postgres version I'm sure
     sequence(:file_url) { |n| "https://bikeindex.org/bulk_file#{n}.csv" }
     user { FactoryGirl.create(:user) }
     factory :bulk_import_success do
