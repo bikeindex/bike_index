5e26cc0499d74cd798a656bbfe208dcefeb622b8
sethherr <seth.william.herr@gmail.com>
2018-07-04 10:57:57 -0700

    vcr permit no cassette connections

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'vcr'
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.allow_http_connections_when_no_cassette = true
   config.hook_into :webmock
 end
 
