78efa12aa8e3a5d315e3aa61ab23fcf9e91f6301
sethherr <seth.william.herr@gmail.com>
2018-07-06 09:40:15 -0700

    wip

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ActiveRecord::Base
   acts_as_paranoid
-  belongs_to :organization, touch: true  # Locations are organization locations
+  belongs_to :organization # Locations are organization locations
   belongs_to :country
   belongs_to :state
   validates_presence_of :name, :organization_id, :city, :country_id
@@ -11,6 +11,14 @@ class Location < ActiveRecord::Base
   # scope :international, where("country_id IS NOT #{Country.united_states.id}")
 
   before_save :shown_from_organization
+  before_save :set_phone
+  after_commit :update_organization
+
+  unless Rails.env.test?
+    geocoded_by :address
+    after_validation :geocode
+  end
+
   def shown_from_organization
     self.shown = organization && organization.allowed_show
     true
@@ -25,18 +33,18 @@ class Location < ActiveRecord::Base
     (a+[zipcode, country.name]).compact.join(', ')
   end
 
-  unless Rails.env.test?
-    geocoded_by :address
-    after_validation :geocode
-  end
-
-  before_save :set_phone
   def set_phone
     self.phone = Phonifyer.phonify(self.phone) if self.phone
   end
 
   def org_location_id
     "#{self.organization_id}_#{self.id}"
+  end
+
+  def update_organization
+    # Because we need to update the organization and make sure it is shown on the map correctly
+    # Manually update to ensure that it runs the before save stuff
+    organization && organization.update_attributes(updated_at: Time.now)
   end
 
   def display_name
@@ -46,5 +54,4 @@ class Location < ActiveRecord::Base
       "#{organization.name} - #{name}"
     end
   end
-
 end
