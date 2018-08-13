6a24e4038cdcd15f75e451ecc7060a475d30a839
sethherr <seth.william.herr@gmail.com>
2018-08-13 11:51:08 -0700

    better delivery status message

--- app/workers/email_organization_message_worker.rb
+++ app/workers/email_organization_message_worker.rb
@@ -5,7 +5,7 @@ class EmailOrganizationMessageWorker
   def perform(organization_message_id)
     organization_message = OrganizationMessage.find(organization_message_id)
     return true if organization_message.delivery_status.present?
-    result = OrganizedMailer.custom_message(organization_message).deliver_now
-    organization_message.update_attribute :delivery_status, result.to_s # I'm not sure what this looks like, so just try it
+    OrganizedMailer.custom_message(organization_message).deliver_now
+    organization_message.update_attribute :delivery_status, "success" # I'm not sure how to make this actually representative
   end
 end
