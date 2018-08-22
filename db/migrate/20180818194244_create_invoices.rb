a41b95cc618b7259beb6b18d2edadd028c44d5ba
sethherr <seth.william.herr@gmail.com>
2018-08-22 09:43:28 -0700

    better attributes

--- db/migrate/20180818194244_create_invoices.rb
+++ db/migrate/20180818194244_create_invoices.rb
@@ -2,10 +2,11 @@ class CreateInvoices < ActiveRecord::Migration
   def change
     create_table :invoices do |t|
       t.references :organization, index: true
-      t.references :renews_subscription
+      t.references :subscription_first_invoice_id, index: true
       t.datetime :subscription_start_at
       t.datetime :subscription_end_at
-      t.integer :feature_cost_at_start
+      t.integer :features_at_start_cents
+      t.integer :amount_due_cents
 
       t.timestamps null: false
     end
