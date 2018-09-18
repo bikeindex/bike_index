010c7309323ad23616ab70fa6c2798100d275153
Seth Herr <seth.william.herr@gmail.com>
2018-09-18 14:59:47 -0700

    Fix flaky test on travis (#444)

--- spec/api/v3/search_spec.rb
+++ spec/api/v3/search_spec.rb
@@ -40,31 +40,34 @@ describe 'Search API V3' do
         expect(response.status).to eq(400)
       end
     end
-    context 'correct params' do
+    context "correct params" do
       let(:request_query_params) do
         {
-          serial: 's',
+          serial: "s",
           manufacturer: manufacturer.id,
           color_ids: [color.id],
-          location: 'Chicago, IL',
-          distance: 10,
-          stolenness: 'stolen'
+          location: "Chicago, IL",
+          distance: 20,
+          stolenness: "stolen"
         }
       end
-      let(:proximity_query_params) { request_query_params.merge(stolenness: 'proximity') }
-      let(:proximity_interpreted_params) { Bike.searchable_interpreted_params(proximity_query_params, ip: '') }
-      # Use the interpreted params, because they come with proximity data - it's what we do in the API
-      let(:stolen_interpreted_params) { proximity_interpreted_params.merge(stolenness: 'stolen') }
-      let(:non_stolen_interpreted_params) { proximity_interpreted_params.merge(stolenness: 'non') }
-      it 'calls Bike Search with the expected interpreted_params' do
-        expect(Bike).to receive(:search).with(hash_including(proximity_interpreted_params)) { %w(1) }
-        expect(Bike).to receive(:search).with(stolen_interpreted_params) { %w(1 2) }
-        expect(Bike).to receive(:search).with(non_stolen_interpreted_params) { %w(1 2 3) }
-        get '/api/v3/search/count', request_query_params, format: :json
-        result = JSON.parse(response.body)
-        # The result is counts of the arrays we stubbed :/
-        expect(result).to match({ non: 3, stolen: 2, proximity: 1 }.as_json)
-        expect(response.status).to eq(200)
+      let(:proximity_query_params) { request_query_params.merge(stolenness: "proximity") }
+      let(:proximity_interpreted_params) { Bike.searchable_interpreted_params(proximity_query_params, ip: "") }
+      # Use the interpreted params, because they come with proximity data - it"s what we do in the API
+      let(:stolen_interpreted_params) { proximity_interpreted_params.merge(stolenness: "stolen") }
+      let(:non_stolen_interpreted_params) { proximity_interpreted_params.merge(stolenness: "non") }
+      let!(:non_stolen) { FactoryGirl.create(:bike, manufacturer: manufacturer, primary_frame_color: color, serial_number: "s") }
+      let!(:stolen) { FactoryGirl.create(:stolen_bike, manufacturer: manufacturer, primary_frame_color: color, serial_number: "5") }
+      let!(:stolen_proximity) { FactoryGirl.create(:stolen_bike, manufacturer: manufacturer, secondary_frame_color: color, serial_number: "S", latitude: 41.8624488, longitude: -87.6591502) }
+      before { Geocoder.configure(lookup: :google, use_https: true) } # Configuration matches geocode initializer
+      after { Geocoder.configure(lookup: :test) } # reset to test
+      it "calls Bike Search with the expected interpreted_params" do
+        VCR.use_cassette("v3_bike_search-count") do
+          get "/api/v3/search/count", request_query_params, format: :json
+          result = JSON.parse(response.body)
+          expect(result).to match({ non: 1, stolen: 2, proximity: 1 }.as_json)
+          expect(response.status).to eq(200)
+        end
       end
     end
     context 'nil params' do
