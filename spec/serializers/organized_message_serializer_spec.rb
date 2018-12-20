5ef028acc7380e212f9a9c7c9e2279a7b3a0723a
sethherr <seth.william.herr@gmail.com>
2018-12-20 18:48:41 -0500

    nil fix for organized messages

---
+++ spec/serializers/organized_message_serializer_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe UserSerializer do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:organization) { FactoryGirl.create(:organization) }
+  let(:membership) { FactoryGirl.create(:membership, user: user, organization: organization) }
+  subject { UserSerializer.new(user) }
+
+  it { expect(subject.user_present).to be_truthy }
+  it { expect(subject.is_superuser).to be_falsey }
+  xit { expect(subject.memberships.first).to eq(membership) }
+end
