ff51876c7cec9f3d1cc4fade509eeac632d20859
sethherr <seth.william.herr@gmail.com>
2019-01-09 13:51:35 -0800

    better handling of potential zombie database entries

--- spec/controllers/api/v1/manufacturers_controller_spec.rb
+++ spec/controllers/api/v1/manufacturers_controller_spec.rb
@@ -1,17 +1,17 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Api::V1::ManufacturersController do
-  describe 'index' do
-    it 'loads the request' do
-      m = FactoryGirl.create(:manufacturer)
+  describe "index" do
+    it "loads the request" do
+      m = FactoryGirl.create(:manufacturer, name: "AAAA manufacturer")
       get :index, format: :json
-      expect(response.code).to eq('200')
-      expect(JSON.parse(response.body)['manufacturers'].last['name']).to eq(m.name)
-      expect(response.headers['Access-Control-Allow-Origin']).to eq('*')
-      expect(response.headers['Access-Control-Allow-Methods']).to eq('POST, PUT, GET, OPTIONS')
-      expect(response.headers['Access-Control-Request-Method']).to eq('*')
-      expect(response.headers['Access-Control-Allow-Headers']).to eq('Origin, X-Requested-With, Content-Type, Accept, Authorization')
-      expect(response.headers['Access-Control-Max-Age']).to eq('1728000')
+      expect(response.code).to eq("200")
+      expect(JSON.parse(response.body)["manufacturers"].first["name"]).to eq(m.name)
+      expect(response.headers["Access-Control-Allow-Origin"]).to eq("*")
+      expect(response.headers["Access-Control-Allow-Methods"]).to eq("POST, PUT, GET, OPTIONS")
+      expect(response.headers["Access-Control-Request-Method"]).to eq("*")
+      expect(response.headers["Access-Control-Allow-Headers"]).to eq("Origin, X-Requested-With, Content-Type, Accept, Authorization")
+      expect(response.headers["Access-Control-Max-Age"]).to eq("1728000")
     end
   end
 end
