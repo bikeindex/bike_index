ca32943c73f60fb87b235a437c0c8303fb31140e
sethherr <seth.william.herr@gmail.com>
2019-01-09 14:12:35 -0800

    actually, yeah 4 parallelization is p greatgst

--- Gemfile
+++ Gemfile
@@ -141,7 +141,7 @@ group :development, :test do
 end
 
 group :test do
-  gem "simplecov"
+  gem "simplecov", require: false
   gem "factory_girl_rails"
   gem "rspec-sidekiq"
   gem "pry"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ VCR.configure do |config|
   config.hook_into :webmock
 end
 
+# For codeclimate test coverage. Only enable if the environmental variable is set - i.e. on CI
 if ENV["COVERAGE"]
   require 'simplecov'
   SimpleCov.start "rails"
