40b371e570ed60b88b5d83f17f898f809c8ec263
Robertgervais1 <Robertgervais22@gmail.com>
2019-02-06 22:10:57 -0600

    commit

--- app/javascript/source/javascript/binx_admin.js
+++ app/javascript/source/javascript/binx_admin.js
@@ -3,8 +3,26 @@ if (process.env.NODE_ENV != "production") log.setLevel("debug");
 import moment from "moment-timezone";
 
 window.BinxAdmin = class BinxAdmin {
+  // Should be inside init() but having a hard time with errors
   // initGraph() {
-  //   console.debug("Hello")
+  //   $("select#graph_date_option_choice").on("change", e => {
+  //     e.preventDefault()
+  //     if ($("select#graph_date_option_choice")[0].value === "custom") {
+  //       $(".clearfix").slideDown()
+  //     }
+  //     else {
+  //       $(".clearfix").slideUp()
+  //       let $this = $('#start_at')
+  //       let graphSelected = $("select#graph_date_option_choice")[0].value.split(",")
+  //       let amount = Number(graphSelected[0])
+  //       let unit = graphSelected[1]
+  //       $this.val(
+  //         moment()
+  //           .subtract(amount, unit)
+  //           .format("YYYY-MM-DDTHH:mm")
+  //         )
+  //     }
+  //   });
   // }
 
   init() {
