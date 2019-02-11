a830ac87c30a2e9d2ccee1d9946c7088a89d5447
Robertgervais1 <Robertgervais22@gmail.com>
2019-02-11 15:09:58 -0600

    add atom prettier - hopefully formatted js

--- app/javascript/source/javascript/binx_admin.js
+++ app/javascript/source/javascript/binx_admin.js
@@ -3,7 +3,6 @@ if (process.env.NODE_ENV != "production") log.setLevel("debug");
 import moment from "moment-timezone";
 
 window.BinxAdmin = class BinxAdmin {
-
   init() {
     // If there is an element on the page with id pageContainerFluid, make the page container full width
     if ($("#pageContainerFluid").length) {
@@ -14,29 +13,30 @@ window.BinxAdmin = class BinxAdmin {
         .parents(".receptacle")
         .css("max-width", "100%");
     }
-    if (window.location.href.match('\\admin/graphs')){
-      this.initGraph()
-    };
+    if (window.location.href.match("\\admin/graphs")) {
+      this.initGraph();
+    }
   }
 
   initGraph() {
     $("select#graph_date_option_choice").on("change", e => {
-      e.preventDefault()
+      e.preventDefault();
       if ($("select#graph_date_option_choice")[0].value === "custom") {
-        $("#calendar-box").slideDown()
-      }
-      else {
-        $("#calendar-box").slideUp()
-        let $this = $('#start_at')
-        let graphSelected = $("select#graph_date_option_choice")[0].value.split(",")
-        let amount = Number(graphSelected[0])
-        let unit = graphSelected[1]
+        $("#calendar-box").slideDown();
+      } else {
+        $("#calendar-box").slideUp();
+        let $this = $("#start_at");
+        let graphSelected = $("select#graph_date_option_choice")[0].value.split(
+          ","
+        );
+        let amount = Number(graphSelected[0]);
+        let unit = graphSelected[1];
         $this.val(
           moment()
             .subtract(amount, unit)
             .format("YYYY-MM-DDTHH:mm")
-          )
-        }
+        );
+      }
     });
   }
 };
