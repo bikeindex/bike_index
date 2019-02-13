108c173b233d2bc96bb214eaf21d87f38aeac861
Steven Buccini <steven@stevenbuccini.com>
2019-02-13 20:33:25 +0900

    No longer any test regressions

--- app/controllers/api/v3/bikes.rb
+++ app/controllers/api/v3/bikes.rb
@@ -139,16 +139,18 @@ module API
         end
         post '/', serializer: BikeV2ShowSerializer, root: 'bike' do
           declared_p = { "declared_params" => declared(params, include_missing: false).merge(creation_state_params) }
-          b_param = BParam.create(creator_id: creation_user_id, params: {'bike': declared_p['declared_params']}, origin: 'api_v3')
+          # TODO: Changing this origin breaks things, but also the origin is incorrect. need to fix.
+          b_param = BParam.create(creator_id: creation_user_id, params: declared_p['declared_params'], origin: 'api_v2')
           ensure_required_stolen_attrs(b_param.params)
+
+
           # Prevents unnecessary DB hits
           if b_param.errors.present? or b_param.bike_errors.present?
             e = b_param.errors
             return error!(e.full_messages.to_sentence, 401)
           end
-
           existing_bike = Bike.find_by(serial_normalized: SerialNormalizer.new({serial: b_param[:serial]}).normalized)
-
+          
           # Assume only one bike for now, but may need to check len of existing bike later depending on what Seth says
           if existing_bike.present?
             # Does this check secondary emails?
@@ -161,7 +163,6 @@ module API
             existing_bike.reload
           end
 
-
           bike = BikeCreator.new(b_param).create_bike
           if b_param.errors.blank? && b_param.bike_errors.blank? && bike.present? && bike.errors.blank?
             bike
@@ -170,7 +171,103 @@ module API
             error!(e.full_messages.to_sentence, 401)
           end
         end
+
+
+        desc "Update a bike owned by the access token<span class='accstr'>*</span>", {
+          authorizations: { oauth2: [{ scope: :write_bikes }] },
+          notes: <<-NOTE
+            **Requires** `read_user` **in the access token** you use to send the notification.
+            
+            Update a bike owned by the access token you're using.
+
+          NOTE
+        }
+        params  do 
+          requires :id, type: Integer, desc: "Bike ID"
+          use :bike_attrs
+          optional :owner_email, type: String, desc: "Send the bike to a new owner!"
+          optional :components, type: Array do
+            optional :id, type: Integer, desc: "Component ID - if you don't supply this you will create a new component instead of update an existing one"
+            use :components_attrs
+            optional :destroy, type: Boolean, desc: "Delete this component (requires an ID)"
+          end
+        end
+        put ':id', serializer: BikeV2ShowSerializer, root: 'bike' do
+          declared_p = { "declared_params" => declared(params, include_missing: false) }
+          find_bike
+          authorize_bike_for_user
+          hash = BParam.v2_params(declared_p['declared_params'].as_json)
+          ensure_required_stolen_attrs(hash) if hash['stolen_record'].present? && @bike.stolen != true
+          begin
+            BikeUpdator.new(user: current_user, bike: @bike, b_params: hash).update_available_attributes
+          rescue => e
+            error!("Unable to update bike: #{e}", 401)
+          end
+          @bike.reload
+        end
+
+        desc "Add an image to a bike", {
+          authorizations: { oauth2: [{ scope: :write_bikes }] },
+          notes: <<-NOTE
+
+            To post a file to the API with curl:
+
+            `curl -X POST -i -F file=@{test_file.jpg} "#{ENV['BASE_URL']}/api/v2/bikes/{bike_id}/image?access_token={access_token}"`
+
+            Replace `{text_file.jpg}` with the relative path of the file you're posting.
+
+            **RIGHT NOW THIS DEMO DOESN'T WORK.** The `curl` command above does. We're working on the documentation issue, check back soon.
+
+          NOTE
+        }
+        params  do
+          requires :id, type: Integer, desc: "Bike ID"
+          requires :file, :type => Rack::Multipart::UploadedFile, :desc => "Attachment."
+        end
+        post ':id/image', serializer: PublicImageSerializer, root: 'image' do 
+          declared_p = { "declared_params" => declared(params, include_missing: false) }
+          find_bike
+          authorize_bike_for_user
+          public_image = PublicImage.new(imageable: @bike, image: params[:file])
+          if public_image.save
+            public_image
+          else
+            error!(public_image.errors.full_messages.to_sentence, 401)
+          end
+        end
+
+
+        desc "Send a stolen notification<span class='accstr'>*</span>", {
+          authorizations: { oauth2: [{ scope: :read_user }] },
+          notes: <<-NOTE 
+            **Requires** `read_user` **in the access token** you use to send the notification.
+
+            <hr>
+
+            Send a stolen bike notification.
+
+            Your application has to be approved to be able to do this. Email support@bikeindex.org to get access.
+
+            Before your application is approved you can send notifications to yourself (to a bike that you own that's stolen).
+          NOTE
+        }
+        params do 
+          requires :id, type: Integer, desc: "Bike ID. **MUST BE A STOLEN BIKE**"
+          requires :message, type: String, desc: "The message you are sending to the owner"
+        end
+        post ':id/send_stolen_notification', serializer: StolenNotificationSerializer  do 
+          find_bike
+          error!("Bike is not stolen", 400) unless @bike.present? && @bike.stolen
+          # Unless application is authorized....
+          authorize_bike_for_user(" (this application is not approved to send notifications)") 
+          StolenNotification.create(bike_id: params[:id],
+            message: params[:message],
+            sender: current_user
+          )
+        end
+
       end
+
     end
   end
 end
