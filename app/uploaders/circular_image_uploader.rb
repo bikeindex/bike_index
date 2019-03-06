b14d80ffd5350bc9f020ee4fbcb56bbe441c28b3
Seth Herr <seth.william.herr@gmail.com>
2019-03-06 12:18:22 -0800

    Add cache dir for uploaders (#575)

--- app/uploaders/circular_image_uploader.rb
+++ app/uploaders/circular_image_uploader.rb
@@ -26,6 +26,10 @@ class CircularImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "#{base_store_dir}/#{model.id}"
   end
+
+  def cache_dir
+    Rails.root.join("tmp", "uploads", "cache")
+  end
   
   def base_store_dir
     "uploads/#{model.class.to_s[0, 2]}"
@@ -34,7 +38,6 @@ class CircularImageUploader < CarrierWave::Uploader::Base
   def filename
     "recovery_#{model.id}.png"
   end
-
 
   process :fix_exif_rotation
   process :strip # Remove EXIF data, because we don't need it
