1308972f6d1f44aae6f82e8f2022f3cea8fee395
Lara Ferroni <laraferroni@Laras-MacBook-Pro.local>
2019-03-16 09:18:41 -0700

    tests

--- spec/serializers/bike_v2_show_serializer_spec.rb
+++ spec/serializers/bike_v2_show_serializer_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe BikeSerializer do
+describe BikeV2ShowSerializer do
   describe 'standard validations' do
     let(:bike) { FactoryBot.create(:bike, frame_size: '42') }
     let(:component) { FactoryBot.create(:component, bike: bike) }
     let(:public_image) { FactoryBot.create(:public_image, imageable_type: 'Bike', imageable_id: bike.id) }
-    subject { BikeSerializer.new(bike) }
+    subject { BikeV2ShowSerializer.new(bike) }
 
     it { expect(subject.manufacturer_name).to eq(bike.mnfg_name) }
     it { expect(subject.manufacturer_id).to eq(bike.manufacturer_id) }
@@ -17,15 +17,15 @@ describe BikeSerializer do
     it { expect(subject.description).to eq(bike.description) }
     it { expect(subject.rear_tire_narrow).to eq(bike.rear_tire_narrow) }
     it { expect(subject.front_tire_narrow).to eq(bike.front_tire_narrow) }
-    it { expect(subject.rear_wheel_size).to eq(bike.rear_wheel_size) }
+    it { expect(subject.rear_tire_narrow).to eq(bike.rear_tire_narrow) }
     it { expect(subject.serial).to eq(bike.serial_number) }
-    it { expect(subject.front_wheel_size).to eq(bike.front_wheel_size) }
     it { expect(subject.handlebar_type).to eq(bike.handlebar_type) }
     it { expect(subject.frame_material).to eq(bike.frame_material_name) }
-    it { expect(subject.front_gear_type).to eq(bike.front_gear_type) }
-    it { expect(subject.rear_gear_type).to eq(bike.rear_gear_type) }
+    it { expect(subject.front_gear_type_slug).to eq(bike.front_gear_type_id) }
+    it { expect(subject.rear_gear_type_slug).to eq(bike.rear_gear_type_id) }
     it { expect(subject.stolen_record).to eq(bike.current_stolen_record) }
     it { expect(subject.frame_size).to eq('42cm') }
+    it { expect(subject.additional_registration).to eq(bike.additional_registration) }
     # it { subject.photo.should == bike.reload.public_images.first.image_url(:large) }
     # it { subject.thumb.should == bike.reload.public_images.first.image_url(:small) }
   end
