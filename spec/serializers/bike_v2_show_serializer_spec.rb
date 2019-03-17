5358fdd6b9ed1eabcfeaa0ae46d4922e996627c1
Seth Herr <seth.william.herr@gmail.com>
2019-03-17 13:36:38 -0700

    Clean up some left-behinds (#606)

--- spec/serializers/bike_v2_show_serializer_spec.rb
+++ spec/serializers/bike_v2_show_serializer_spec.rb
@@ -2,31 +2,55 @@ require 'spec_helper'
 
 describe BikeV2ShowSerializer do
   describe 'standard validations' do
-    let(:bike) { FactoryBot.create(:bike, frame_size: '42') }
+    let(:bike) { FactoryBot.create(:bike, frame_size: "42", additional_registration: "XXYY") }
     let(:component) { FactoryBot.create(:component, bike: bike) }
-    let(:public_image) { FactoryBot.create(:public_image, imageable_type: 'Bike', imageable_id: bike.id) }
+    let(:public_image) { FactoryBot.create(:public_image, imageable_type: "Bike", imageable_id: bike.id) }
     subject { BikeV2ShowSerializer.new(bike) }
 
-    it { expect(subject.manufacturer_name).to eq(bike.mnfg_name) }
-    it { expect(subject.manufacturer_id).to eq(bike.manufacturer_id) }
-    it { expect(subject.stolen).to eq(bike.stolen) }
-    it { expect(subject.type_of_cycle).to eq(bike.cycle_type_name) }
-    it { expect(subject.name).to eq(bike.name) }
-    it { expect(subject.year).to eq(bike.year) }
-    it { expect(subject.frame_model).to eq(bike.frame_model) }
-    it { expect(subject.description).to eq(bike.description) }
-    it { expect(subject.rear_tire_narrow).to eq(bike.rear_tire_narrow) }
-    it { expect(subject.front_tire_narrow).to eq(bike.front_tire_narrow) }
-    it { expect(subject.rear_tire_narrow).to eq(bike.rear_tire_narrow) }
-    it { expect(subject.serial).to eq(bike.serial_number) }
-    it { expect(subject.handlebar_type).to eq(bike.handlebar_type) }
-    it { expect(subject.frame_material).to eq(bike.frame_material_name) }
-    it { expect(subject.front_gear_type_slug).to eq(bike.front_gear_type_id) }
-    it { expect(subject.rear_gear_type_slug).to eq(bike.rear_gear_type_id) }
-    it { expect(subject.stolen_record).to eq(bike.current_stolen_record) }
-    it { expect(subject.frame_size).to eq('42cm') }
-    it { expect(subject.additional_registration).to eq(bike.additional_registration) }
-    # it { subject.photo.should == bike.reload.public_images.first.image_url(:large) }
-    # it { subject.thumb.should == bike.reload.public_images.first.image_url(:small) }
+    let(:target) do
+      {
+        id: bike.id,
+        title: bike.title_string,
+        serial: bike.serial_number,
+        manufacturer_name: bike.mnfg_name,
+        frame_model: nil,
+        year: nil,
+        frame_colors: ["Black"],
+        thumb: nil,
+        large_img: nil,
+        is_stock_img: false,
+        stolen: false,
+        stolen_location: nil,
+        date_stolen: nil,
+        frame_material: nil,
+        handlebar_type: nil,
+        registration_created_at: bike.created_at.to_i,
+        registration_updated_at: bike.updated_at.to_i,
+        url: "http://test.host/bikes/#{bike.id}",
+        api_url: "http://test.host/api/v1/bikes/#{bike.id}",
+        manufacturer_id: bike.manufacturer_id,
+        paint_description: nil,
+        name: nil,
+        frame_size: "42cm",
+        description: nil,
+        rear_tire_narrow: true,
+        front_tire_narrow: nil,
+        type_of_cycle: "Bike",
+        test_bike: false,
+        rear_wheel_size_iso_bsd: nil,
+        front_wheel_size_iso_bsd: nil,
+        handlebar_type_slug: nil,
+        front_gear_type_slug: nil,
+        rear_gear_type_slug: nil,
+        additional_registration: "XXYY",
+        stolen_record: nil,
+        public_images: [],
+        components: []
+      }
+    end
+
+    it "returns the expected thing" do
+      expect(subject.as_json(root: false)).to eq target
+    end
   end
 end
