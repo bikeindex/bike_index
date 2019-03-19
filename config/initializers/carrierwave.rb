c6a168cbb731f198594ea82fa8edf90bd027de15
sethherr <seth.william.herr@gmail.com>
2019-03-19 16:48:34 -0700

    better comments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,10 @@ CarrierWave.configure do |config|
   config.storage = :fog # For some reason, uploading sitemap doesn't work unless this is included.
   config.asset_host = "https://files.bikeindex.org" if Rails.env.production?
   config.fog_credentials = {
-    provider: "AWS", # required
+    provider: "AWS",
     aws_access_key_id: ENV["S3_ACCESS_KEY"],
     aws_secret_access_key: ENV["S3_SECRET_KEY"],
-    region: "us-east-1"  # optional, defaults to "us-east-1"
+    region: "us-east-1"
 
   }
   config.fog_directory = ENV["S3_BUCKET"]
