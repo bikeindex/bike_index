77fd54b27ef5558dabcf5cbf683fdcdfe9a3719e
Geoff Ereth <geoffereth@gmail.com>
2019-04-03 20:34:08 -0700

    Adds lint and jest to circleci

--- app/javascript/packs/multi_serial_search/components/MultiSerialSearch.test.js
+++ app/javascript/packs/multi_serial_search/components/MultiSerialSearch.test.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import MultiSerialSearch from './MultiSerialSearch';
 
