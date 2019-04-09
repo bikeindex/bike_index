e945dccc4d06e0d5e5b0f355a07c637ab1e61d48
Geoff Ereth <geoffereth@gmail.com>
2019-04-09 10:25:47 -0700

    Disables honeybadger reporting in dev

--- app/javascript/packs/utils/honeybadger.js
+++ app/javascript/packs/utils/honeybadger.js
@@ -1,17 +1,23 @@
 import Honeybadger from 'honeybadger-js';
 import log from './log';
 
-const config = {
-  api_key: process.env.HONEYBADGER_API_KEY,
-  environment: process.env.RAILS_ENV,
-};
+const environment = process.env.RAILS_ENV;
 
+/* webpacker can't access this exported env */
+const apiKey = environment === 'production'
+  ? process.env.HONEYBADGER_API_KEY
+  : 'foo';
+
+const config = {
+  apiKey,
+  environment: process.env.RAILS_ENV,
+  disabled: environment !== 'production',
+};
 const honeybadger = Honeybadger.configure(config);
 
 Honeybadger.beforeNotify(notice => {
-  if (process.env.RAILS_ENV === 'development') {
+  if (environment === 'development') {
     return log.debug('Error', notice);
   }
 });
-
 export default honeybadger;
