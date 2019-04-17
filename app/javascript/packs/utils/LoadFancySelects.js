f9e9aa44c36eb59d5be11563e5d216b7ff9680fb
Seth Herr <seth.william.herr@gmail.com>
2019-04-17 12:56:29 -0700

    Enable fancy selects in new admin (#690)

---
+++ app/javascript/packs/utils/LoadFancySelects.js
@@ -0,0 +1,18 @@
+import log from "./log";
+import selectize from "selectize";
+
+// Right now we're using selectize.js (https://selectize.github.io/selectize.js/) to make our select boxes fancy.
+// This could be changed to some other library at some point - this makes it possible for us to abstract that away.
+// Just add the `unfancy` and `fancy-select` classes to a select box and it will be a fancy select box!
+const LoadFancySelects = () => {
+  $(".unfancy.fancy-select select").selectize({
+    create: false,
+    plugins: ["restore_on_backspace"]
+  });
+  // Remove them so we don't initialize twice
+  $(".unfancy.fancy-select, .unfancy.fancy-select-placeholder").removeClass(
+    "unfancy"
+  );
+};
+
+export default LoadFancySelects;
