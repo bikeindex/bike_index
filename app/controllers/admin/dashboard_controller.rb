eb9221ae0e720ba685306263490ea398fab74eb5
Bobby <Robertgervais22@gmail.com>
2019-04-22 18:33:47 -0500

    remove flavor table, add orgs table, add duplicates to nav bar, remove duplicates table

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < Admin::BaseController
   def index
+    @organizations = Organization.order("created_at desc").limit(10)
     @bikes = Bike.unscoped.includes(:creation_organization, :creation_state, :paint).order('created_at desc').limit(10)
     @users = User.includes(:memberships => [:organization]).limit(5).order("created_at desc")
     @flavors = FlavorText.all
