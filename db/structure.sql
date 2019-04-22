2e2412b5c27deb6d7e4ba678109e9f1bdd499be5
Bobby <Robertgervais22@gmail.com>
2019-04-22 17:24:25 -0500

    I hate structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -15,21 +15,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
----
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
 --
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
 --
 
