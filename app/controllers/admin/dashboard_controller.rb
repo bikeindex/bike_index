1c3e9ee7445bdcee964708ac23df3e5fb0a22749
sethherr <seth.william.herr@gmail.com>
2019-04-24 11:52:55 -0700

    small fix

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < Admin::BaseController
   def index
     @organizations = Organization.unscoped.order("created_at DESC").limit(10)
-    @bikes = Bike.unscoped.includes(:creation_organization, :creation_state, :paint).order('created_at desc').limit(10)
+    @bikes = Bike.unscoped.includes(:creation_organization, :creation_states, :paint).order('created_at desc').limit(10)
     @users = User.includes(:memberships => [:organization]).limit(5).order("created_at desc")
     @flavors = FlavorText.all
     @flavor = FlavorText.new
