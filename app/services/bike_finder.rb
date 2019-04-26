d9abef129fb9f886dfe733661dda53f47493f1c9
Jake Romer <mail@jakeromer.com>
2019-04-26 15:37:49 -0400

    Use normalized_serial Bike attribute

--- app/services/bike_finder.rb
+++ app/services/bike_finder.rb
@@ -1,8 +1,7 @@
 module BikeFinder
   # Check for the existence of a bike with the given serial number `serial`.
   #
-  # Matches based on the serial number and normalized serial number.
-  # (TODO: on any serial number normalization?)
+  # Matches based on the normalized serial number.
   # Matching is scoped to the user record associated with `owner_email`.
   #
   # Return an Bike object, or nil
@@ -10,21 +9,18 @@ module BikeFinder
     candidate_user_ids =
       User
         .joins("LEFT JOIN user_emails ON user_emails.user_id = users.id")
-        .where("users.email = ? OR user_emails.email = ?", owner_email, owner_email)
+        .where("users.email = ? OR user_emails.email = ?", *[owner_email] * 2)
         .select(:id)
         .uniq
 
     return if candidate_user_ids.blank?
 
-    normalized_serial =
-      SerialNormalizer
-        .new(serial: serial)
-        .normalized
+    normalized_serial = SerialNormalizer.new(serial: serial).normalized
 
     Bike
       .joins("RIGHT JOIN ownerships ON bikes.id = ownerships.bike_id")
-      .where(serial_number: [serial, normalized_serial])
-      .where("ownerships.user_id IN (?) OR ownerships.creator_id IN (?)", candidate_user_ids, candidate_user_ids)
+      .where(serial_normalized: normalized_serial)
+      .where("ownerships.user_id IN (?) OR ownerships.creator_id IN (?)", *[candidate_user_ids] * 2)
       .first
   end
 end
