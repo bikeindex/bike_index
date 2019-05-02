8e6d7bd0044940dc0c97eeefce8935355adf0ca9
sethherr <seth.william.herr@gmail.com>
2019-05-02 15:04:21 -0700

    wip

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -99,9 +99,8 @@ class Admin::OrganizationsController < Admin::BaseController
 
   def find_organization
     @organization = Organization.friendly_find(params[:id])
-    unless @organization
-      flash[:error] = "Sorry! That organization doesn't exist"
-      redirect_to admin_organizations_url and return
-    end
+    return true if @organization.present?
+    flash[:error] = "Sorry! That organization doesn't exist"
+    redirect_to admin_organizations_url and return
   end
 end
