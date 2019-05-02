7f65885d8f5027d7c65fa0c34a8a51b4cf34fa5b
Jake Romer <mail@jakeromer.com>
2019-05-02 14:11:45 -0400

    Fix exception, paginate results, eliminate n+1 query

--- app/controllers/admin/failed_bikes_controller.rb
+++ app/controllers/admin/failed_bikes_controller.rb
@@ -2,7 +2,15 @@ class Admin::FailedBikesController < Admin::BaseController
   layout "new_admin"
 
   def index
-    @b_params = BParam.where("created_bike_id IS NOT NULL").order("created_at desc")
+    page = params.fetch(:page, 1)
+    per_page = params.fetch(:per_page, 25)
+
+    @b_params =
+      BParam
+        .where("created_bike_id IS NOT NULL")
+        .order("created_at desc")
+        .page(page)
+        .per(per_page)
   end
 
   def show
