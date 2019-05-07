ebd952e0bb20833b564f932aa8ead3e88bbc491d
Bobby <Robertgervais22@gmail.com>
2019-05-07 16:43:01 -0500

    move JS, break table into columns

--- app/javascript/packs/pages/binx_admin.js
+++ app/javascript/packs/pages/binx_admin.js
@@ -1,6 +1,7 @@
 import log from "../utils/log";
 import moment from "moment-timezone";
 import LoadFancySelects from "../utils/LoadFancySelects";
+import BinxAdminInvoices from "./binx_admin_invoices.js";
 
 function BinxAdmin() {
   return {
@@ -25,8 +26,8 @@ function BinxAdmin() {
         this.adminLocations();
       }
       if ($(".inputTriggerRecalculation")) {
-        this.initializeInvoiceForm();
-        this.updateInvoiceCalculations();
+        const binxAdminInvoices = BinxAdminInvoices()
+        binxAdminInvoices.init();
       }
       // Enable bootstrap custom file upload boxes
       binxApp.enableFilenameForUploads();
@@ -113,48 +114,6 @@ function BinxAdmin() {
         }
         return (location.href = `${current_url}${joiner}period=${period}`);
       });
-    },
-
-    initializeInvoiceForm() {
-      this.updateInvoiceCalculations();
-      $(".inputTriggerRecalculation").on("change paste keyup", e =>
-        this.updateInvoiceCalculations()
-      );
-      $("#invoiceForm .paidFeatureCheck input").on("change", e =>
-        this.updateInvoiceCalculations()
-      );
-    },
-
-    updateInvoiceCalculations() {
-      let oneTimeCost, recurringCost;
-      const recurring = $(".paidFeatureCheck input.recurring:checked")
-        .get()
-        .map(i => parseInt($(i).attr("data-amount"), 10));
-      if (recurring.length > 0) {
-        recurringCost = recurring.reduce((x, y) => x + y);
-      } else {
-        recurringCost = 0;
-      }
-      const oneTime = $(".paidFeatureCheck input.oneTime:checked")
-        .get()
-        .map(i => parseInt($(i).attr("data-amount"), 10));
-      if (oneTime.length > 0) {
-        oneTimeCost = oneTime.reduce((x, y) => x + y);
-      } else {
-        oneTimeCost = 0;
-      }
-      $("#recurringCount").text(recurring.length);
-      $("#oneTimeCount").text(oneTime.length);
-      $("#recurringCost").text(`${recurringCost}.00`);
-      $("#oneTimeCost").text(`${oneTimeCost}.00`);
-      $("#totalCost").text(`${recurringCost + oneTimeCost}.00`);
-      const due = parseInt($("#invoice_amount_due").val(), 10);
-      $("#discountCost").text(`${-1 * (recurringCost + oneTimeCost - due)}.00`);
-
-      const checked_ids = $(".paidFeatureCheck input:checked")
-        .get()
-        .map(i => $(i).attr("data-id"));
-      $("#invoice_paid_feature_ids").val(checked_ids);
     }
   };
 }
