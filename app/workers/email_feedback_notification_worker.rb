6e6ae9bb85327c770a7c41e57fe48877a68101a0
sethherr <seth.william.herr@gmail.com>
2019-05-13 12:52:07 -0700

    temporarily stop sending feedback messages

--- app/workers/email_feedback_notification_worker.rb
+++ app/workers/email_feedback_notification_worker.rb
@@ -3,7 +3,7 @@ class EmailFeedbackNotificationWorker
   sidekiq_options queue: "notify", backtrace: true, retry: 1
 
   def perform(feedback_id)
-    @feedback = Feedback.find(feedback_id)
-    AdminMailer.feedback_notification_email(@feedback).deliver_now
+    # @feedback = Feedback.find(feedback_id)
+    # AdminMailer.feedback_notification_email(@feedback).deliver_now
   end
 end
