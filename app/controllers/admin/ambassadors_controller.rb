df9185c7d653b64a01738d32e8f9309e1ff8d072
Jake Romer <mail@jakeromer.com>
2019-05-17 21:31:33 -0400

    Add AmbassadorDecorator

--- app/controllers/admin/ambassadors_controller.rb
+++ app/controllers/admin/ambassadors_controller.rb
@@ -1,28 +1,6 @@
 class Admin::AmbassadorsController < Admin::BaseController
   layout "new_admin"
 
-  USER_ATTRIBUTES = %w[
-    id
-    email
-    name
-    phone
-    username
-    stripe_id
-    title
-    twitter
-    website
-    street
-    city
-    state_id
-    country_id
-    zipcode
-    latitude
-    longitude
-    last_login
-    updated_at
-    created_at
-  ].freeze
-
   def index
     @page = params.fetch(:page, 1)
     @per_page = params.fetch(:per_page, 25)
@@ -30,7 +8,6 @@ class Admin::AmbassadorsController < Admin::BaseController
   end
 
   def show
-    @ambassador = Ambassador.find(params[:id])
-    @ambassador_attributes = @ambassador.attributes.slice(*USER_ATTRIBUTES)
+    @ambassador = Ambassador.find(params[:id]).decorate
   end
 end
