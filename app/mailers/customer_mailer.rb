9f7c2290fd2df45f9c7775796d50ef7087a117af
sethherr <seth.william.herr@gmail.com>
2019-05-20 09:54:06 -0700

    fix reply_to

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -49,7 +49,9 @@ class CustomerMailer < ActionMailer::Base
 
   def stolen_notification_email(stolen_notification)
     @stolen_notification = stolen_notification
-    mail(to: [@stolen_notification.receiver_email, "lily@bikeindex.org", "bryan@bikeindex.org"],
+    mail(to: @stolen_notification.receiver_email,
+         cc: ["lily@bikeindex.org", "bryan@bikeindex.org"],
+         reply_to: @stolen_notification.sender.email,
          from: "bryan@bikeindex.org", subject: @stolen_notification.display_subject)
     dates = stolen_notification.send_dates_parsed + [Time.now.to_i]
     stolen_notification.update_attribute :send_dates, dates
