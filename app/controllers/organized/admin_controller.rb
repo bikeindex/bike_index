7e4e8ba21ad4e633ab0cb0c2982353d8e15f91b7
Jake Romer <mail@jakeromer.com>
2019-05-24 14:20:25 -0400

    Kill redundant before_filter

--- app/controllers/organized/admin_controller.rb
+++ app/controllers/organized/admin_controller.rb
@@ -2,6 +2,5 @@ module Organized
   class AdminController < Organized::BaseController
     before_filter :ensure_admin!
     skip_before_filter :ensure_member!
-    before_filter :ensure_not_ambassador_organization!
   end
 end
