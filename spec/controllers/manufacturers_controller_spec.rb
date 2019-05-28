39b85f76e106239f4c19056dbac201303e2d76b7
Jake Romer <mail@jakeromer.com>
2019-05-28 14:35:11 -0400

    Make before blocks into examples

--- spec/controllers/manufacturers_controller_spec.rb
+++ spec/controllers/manufacturers_controller_spec.rb
@@ -2,15 +2,17 @@ require "spec_helper"
 
 describe ManufacturersController do
   describe "index" do
-    it "renders with revised_layout" do
+    it "renders the index template with revised_layout" do
       get :index
       expect(response.status).to eq(200)
       expect(response).to render_template(:index)
     end
   end
+
   describe "tsv" do
-    before do
+    it "redirects to " do
       get :tsv
+      expect(response).to redirect_to("https://files.bikeindex.org/uploads/tsvs/manufacturers.tsv")
     end
   end
 end
