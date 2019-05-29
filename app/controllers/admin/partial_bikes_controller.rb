7db4cdd01b9da7ae7dc4255310afbe7d500b5c76
sethherr <seth.william.herr@gmail.com>
2019-05-29 12:32:51 -0700

    wip

---
+++ app/controllers/admin/partial_bikes_controller.rb
@@ -0,0 +1,26 @@
+class Admin::PartialBikesController < Admin::BaseController
+  include SortableTable
+
+  def index
+    page = params[:page] || 1
+    per_page = params[:per_page] || 25
+    @b_params = matching_b_params.reorder("b_params.#{sort_column} #{sort_direction}").page(page).per(per_page)
+    render layout: "new_admin"
+  end
+
+  protected
+
+  def sortable_columns
+    %w[created_at updated_at creator_id origin created_bike_id email]
+  end
+
+  def matching_b_params
+    @search_with_bike = ActiveRecord::Type::Boolean.new.type_cast_from_database(params[:search_with_bike])
+    @search_without_bike = ActiveRecord::Type::Boolean.new.type_cast_from_database(params[:search_without_bike])
+    b_params = BParam
+    b_params = b_params.with_bike if @search_with_bike
+    b_params = b_params.without_bike if @search_without_bike
+    b_params = b_params.email_search(params[:query]) if params[:query].present?
+    b_params
+  end
+end
