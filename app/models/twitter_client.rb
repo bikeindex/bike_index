04a44486a154e7d256028ea75dde0fd9449d9b33
Jake Romer <jake@bikeindex.org>
2019-06-03 14:24:13 -0400

    Add admin/tweets management facilities (#834)
    * Navigate to admin_tweet_path using id
    
    The index endpoint links to a given tweet's edit page using the record's
    tweet_id rather than its id.
    
    This makes some tweets unreachable via the UI (if more than one record with the
    same tweet_id is persisted).
    
    * Return most recent version of duplicate tweets
    
    * Set Twitter API ENV keys to match C66 config
    
    * Restore twitter id to for_cities partial
    
    * Kill temporary workaround
    
    * Validate tweet uniqueness by twitter_id
    
    * Add tweet deletion endpoint
    
    * Add tweet delete button

--- app/models/twitter_client.rb
+++ app/models/twitter_client.rb
@@ -12,8 +12,8 @@ class TwitterClient
       Twitter::REST::Client.new do |config|
         config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
         config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
-        config.access_token = ENV["TWITTER_OAUTH_TOKEN"]
-        config.access_token_secret = ENV["TWITTER_OAUTH_TOKEN_SECRET"]
+        config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
       end
   end
 end
