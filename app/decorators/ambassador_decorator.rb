6b69edd21ed8186ccbe83ea4f9d910fd977efe95
Jake Romer <mail@jakeromer.com>
2019-06-04 14:46:43 -0400

    Move query methods into model

--- app/decorators/ambassador_decorator.rb
+++ app/decorators/ambassador_decorator.rb
@@ -2,21 +2,6 @@ class AmbassadorDecorator < ApplicationDecorator
   delegate_all
   alias ambassador object
 
-  def ambassador_task_assignments
-    ambassador
-      .ambassador_task_assignments
-      .includes(:ambassador_task)
-      .task_ordered
-  end
-
-  def completed_activities
-    ambassador_task_assignments.locked_completed
-  end
-
-  def suggested_activities
-    ambassador_task_assignments.pending_completion
-  end
-
   def avatar
     avatar_url = ambassador.avatar&.url(:medium)
     return if avatar_url.blank? || avatar_url == "https://files.bikeindex.org/blank.png"
--- app/models/ambassador.rb
+++ app/models/ambassador.rb
@@ -1,6 +1,24 @@
 class Ambassador < User
   default_scope -> { ambassadors }
 
+  # Return all of the receiver's `AmbassadorTaskAssignment`s that are completed
+  # and locked.
+  def activities_completed
+    ambassador_task_assignments
+      .includes(:ambassador_task)
+      .locked_completed
+      .task_ordered
+  end
+
+  # Return all of the receiver's `AmbassadorTaskAssignment`s that have not been
+  # completed or have been completed but aren't locked.
+  def activities_pending
+    ambassador_task_assignments
+      .includes(:ambassador_task)
+      .pending_completion
+      .task_ordered
+  end
+
   def percent_complete
     return 0.0 if ambassador_task_assignments.empty?
     (completed_tasks_count / tasks_count.to_f).round(2)
