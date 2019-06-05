24c41e8cb0f26d6dae92179a60fc871594202c3f
Jake Romer <mail@jakeromer.com>
2019-06-05 13:09:15 -0400

    Consolidate partially migrates suites

--- spec/controllers/landing_pages_controller_spec.rb
+++ spec/controllers/landing_pages_controller_spec.rb
@@ -1,5 +1,42 @@
 require "spec_helper"
 
+RSpec.describe LandingPagesController, type: :request do
+  describe "#for_shops" do
+    it "redirects to for_bike_shops_path" do
+      expect(get("/for_shops")).to redirect_to(for_bike_shops_path)
+    end
+  end
+
+  describe "#for_advocacy" do
+    it "redirects to for_community_groups_path" do
+      expect(get("/for_advocacy")).to redirect_to(for_community_groups_path)
+    end
+  end
+
+  {
+    for_bike_shops: "Bike Index for Bike Shops",
+    for_cities: "Bike Index for Cities",
+    for_community_groups: "Bike Index for Community Groups",
+    for_law_enforcement: "Bike Index for Law Enforcement",
+    for_schools: "Bike Index for Schools",
+    ascend: "Ascend POS on Bike Index",
+    ambassadors_current: "Bike Index Ambassadors",
+    ambassadors_how_to: "Become a Bike Index Ambassador",
+    bike_shop_packages: "Bike Index for Bike Shops - Features and Pricing",
+    campus_packages: "Bike Index for Schools - Features and Pricing",
+    cities_packages: "Bike Index for Cities - Features and Pricing",
+  }.each_pair do |controller_action, page_title|
+    describe "##{controller_action}" do
+      it "renders the correct template with the correct title" do
+        get "/#{controller_action}"
+
+        expect(response.status).to eq(200)
+        expect(response).to render_template(controller_action)
+        expect(response.body).to match("<title>#{page_title}</title>")
+      end
+    end
+  end
+end
 describe LandingPagesController, type: :controller do
   include_context :page_content_values
 
--- spec/controllers/organized/messages_controller_spec.rb
+++ spec/controllers/organized/messages_controller_spec.rb
@@ -1,5 +1,59 @@
 require "spec_helper"
 
+describe Organized::MessagesController, type: :request do
+  include_context :geocoder_default_location
+  include_context :organization_with_geolocated_messages
+  let(:base_url) { "/o/#{organization.to_param}/messages" }
+  # Request specs don't have cookies so we need to stub stuff if we're in request specs
+  # This is suboptimal, but hey, it gets us to request specs for now
+  before { allow(User).to receive(:from_auth) { user } }
+
+  describe "messages root" do
+    let(:user) { FactoryBot.create(:organization_member, organization: organization) }
+    it "renders" do
+      get base_url, json_headers
+      expect(response.status).to eq(200)
+      expect(response).to render_template(:index)
+    end
+    context "json" do
+      it "returns empty" do
+        get base_url, format: :json
+        expect(response.status).to eq(200)
+        expect(json_result).to eq("messages" => [])
+        expect(response.headers["Access-Control-Allow-Origin"]).not_to be_present
+        expect(response.headers["Access-Control-Request-Method"]).not_to be_present
+      end
+      context "with a message" do
+        let!(:organization_message_1) { FactoryBot.create(:organization_message, organization: organization, kind: "geolocated", created_at: Time.now - 1.hour) }
+        let(:bike) { organization_message_1.bike }
+        let(:target) do
+          {
+            id: organization_message_1.id,
+            kind: "geolocated",
+            created_at: organization_message_1.created_at.to_i,
+            lat: organization_message_1.latitude,
+            lng: organization_message_1.longitude,
+            sender_id: organization_message_1.sender_id,
+            bike: {
+              id: bike.id,
+              title: bike.title_string,
+            },
+          }
+        end
+        it "renders json, no cors present" do
+          get base_url, format: :json
+          expect(response.status).to eq(200)
+          messages = json_result["messages"]
+          expect(messages.count).to eq 1
+          expect(messages.first).to eq target.as_json
+          expect(response.headers["Access-Control-Allow-Origin"]).not_to be_present
+          expect(response.headers["Access-Control-Request-Method"]).not_to be_present
+        end
+      end
+    end
+  end
+end
+
 describe Organized::MessagesController, type: :controller do
   include_context :geocoder_default_location
   include_context :organization_with_geolocated_messages
