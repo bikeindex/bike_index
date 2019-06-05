6df1ecf985203a39a435270be476ef4653189476
Jake Romer <mail@jakeromer.com>
2019-06-05 18:44:09 -0400

    Move query method tests to ambassador_task_assignment_spec

--- spec/models/ambassador_task_assignment_spec.rb
+++ spec/models/ambassador_task_assignment_spec.rb
@@ -56,4 +56,184 @@ describe AmbassadorTaskAssignment, type: :model do
       expect(assignment2.errors[:ambassador_task]).to eq(["has already been taken"])
     end
   end
+
+  describe ".completed_assignments" do
+    context "filtering" do
+      it "filters by the given organization_id" do
+        a1 = FactoryBot.create(:ambassador_task_assignment, :completed)
+        FactoryBot.create(:ambassador_task_assignment, :completed)
+
+        result = described_class.completed_assignments(
+          filters: { organization_id: a1.organization.id },
+        )
+
+        expect(result).to match_array([a1])
+      end
+
+      it "filters by the given task" do
+        a1 = FactoryBot.create(:ambassador_task_assignment, :completed)
+        FactoryBot.create(:ambassador_task_assignment, :completed)
+
+        result = described_class.completed_assignments(
+          filters: { ambassador_task_id: a1.ambassador_task.id },
+        )
+
+        expect(result).to match_array([a1])
+      end
+
+      it "filters by the given ambassador" do
+        a1 = FactoryBot.create(:ambassador_task_assignment, :completed)
+        FactoryBot.create(:ambassador_task_assignment, :completed)
+
+        result = described_class.completed_assignments(
+          filters: { ambassador_id: a1.ambassador.id },
+        )
+
+        expect(result).to match_array([a1])
+      end
+
+      it "filters by combinations of filter columns" do
+        a1 = FactoryBot.create(:ambassador_task_assignment, :completed)
+        a2 = FactoryBot.create(:ambassador_task_assignment, :completed,
+                               organization: a1.organization,
+                               ambassador_task: a1.ambassador_task)
+        a3 = FactoryBot.create(:ambassador_task_assignment, :completed,
+                               organization: a1.organization,
+                               ambassador_task: a1.ambassador_task)
+        FactoryBot.create(:ambassador_task_assignment, :completed)
+
+        result = described_class.completed_assignments(
+          filters: {
+            organization_id: a1.organization.id,
+            ambassador_task_id: a3.ambassador_task.id,
+          },
+        )
+
+        expect(result).to match_array([a1, a2, a3])
+      end
+    end
+
+    context "sorting by completion time" do
+      it "sorts in descending order of completion by default" do
+        ordered_assignments =
+          FactoryBot
+            .create_list(:ambassador_task_assignment, 3, :completed)
+            .map(&:id)
+
+        result = described_class.completed_assignments
+
+        expect(result.map(&:id)).to eq(ordered_assignments.reverse)
+      end
+
+      it "sorts in descending order of completion if passed asc" do
+        ordered_assignments =
+          FactoryBot
+            .create_list(:ambassador_task_assignment, 3, :completed)
+            .map(&:id)
+
+        result = described_class.completed_assignments(sort: { completed_at: :asc })
+
+        expect(result.map(&:id)).to eq(ordered_assignments)
+      end
+
+      it "sorts in descending order of completion if passed desc" do
+        ordered_assignments =
+          FactoryBot
+            .create_list(:ambassador_task_assignment, 3, :completed)
+            .map(&:id)
+
+        result = described_class.completed_assignments(sort: { completed_at: :desc })
+
+        expect(result.map(&:id)).to eq(ordered_assignments.reverse)
+      end
+    end
+
+    context "sorting by organization name" do
+      it "sorts in ascending order if passed asc" do
+        org1 = FactoryBot.create(:organization_ambassador, name: "A")
+        FactoryBot.create(:ambassador_task_assignment, :completed, organization: org1)
+        org2 = FactoryBot.create(:organization_ambassador, name: "B")
+        FactoryBot.create(:ambassador_task_assignment, :completed, organization: org2)
+        org3 = FactoryBot.create(:organization_ambassador, name: "C")
+        FactoryBot.create(:ambassador_task_assignment, :completed, organization: org3)
+
+        # ambassador for group "B" joins group "X"
+        org4 = FactoryBot.create(:organization_ambassador, name: "X")
+        FactoryBot.create(:membership_ambassador, user: Ambassador.second, organization: org4)
+
+        result = described_class.completed_assignments(sort: { organization_name: :asc })
+
+        # Tasks should be sorted by name of the ambassador's *current* ambassador org
+        names = result.map { |a| a.ambassador.current_ambassador_organization.name }
+        expect(names).to eq(["A", "C", "X"])
+      end
+
+      it "sorts in descending order if passed desc" do
+        org1 = FactoryBot.create(:organization_ambassador, name: "X")
+        FactoryBot.create(:ambassador_task_assignment, :completed, organization: org1)
+        org2 = FactoryBot.create(:organization_ambassador, name: "Y")
+        FactoryBot.create(:ambassador_task_assignment, :completed, organization: org2)
+        org3 = FactoryBot.create(:organization_ambassador, name: "Z")
+        FactoryBot.create(:ambassador_task_assignment, :completed, organization: org3)
+
+        # ambassador for group "Y" joins group "B"
+        org4 = FactoryBot.create(:organization_ambassador, name: "B")
+        FactoryBot.create(:membership_ambassador, user: Ambassador.second, organization: org4)
+
+        result = described_class.completed_assignments(sort: { organization_name: :desc })
+
+        # Tasks should be sorted by name of the ambassador's *current* ambassador org
+        names = result.map { |a| a.ambassador.current_ambassador_organization.name }
+        expect(names).to eq(["Z", "X", "B"])
+      end
+    end
+
+    context "sorting by task ambassador name" do
+      it "sorts in ascending order if passed asc" do
+        ambassador_names =
+          FactoryBot
+            .create_list(:ambassador_task_assignment, 3, :completed)
+            .map(&:ambassador_name)
+
+        result = described_class.completed_assignments(sort: { ambassador_name: :asc })
+
+        expect(result.map(&:ambassador_name)).to eq(ambassador_names)
+      end
+
+      it "sorts in descending order if passed desc" do
+        ambassador_names =
+          FactoryBot
+            .create_list(:ambassador_task_assignment, 3, :completed)
+            .map(&:ambassador_name)
+
+        result = described_class.completed_assignments(sort: { ambassador_name: :desc })
+
+        expect(result.map(&:ambassador_name)).to eq(ambassador_names.reverse)
+      end
+    end
+
+    context "sorting by task title" do
+      it "sorts in ascending order if passed asc" do
+        task_titles =
+          FactoryBot
+            .create_list(:ambassador_task_assignment, 3, :completed)
+            .map(&:title)
+
+        result = described_class.completed_assignments(sort: { task_title: :asc })
+
+        expect(result.map(&:title)).to eq(task_titles)
+      end
+
+      it "sorts in descending order if passed desc" do
+        task_titles =
+          FactoryBot
+            .create_list(:ambassador_task_assignment, 3, :completed)
+            .map(&:title)
+
+        result = described_class.completed_assignments(sort: { task_title: :desc })
+
+        expect(result.map(&:title)).to eq(task_titles.reverse)
+      end
+    end
+  end
 end
