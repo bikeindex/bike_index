9fdc25430242dc7f965f0c0a2a4310a5bd5f7743
Jake Romer <mail@jakeromer.com>
2019-06-05 03:15:40 -0400

    Inline sorted_assignments

--- app/controllers/admin/ambassador_task_assignments_controller.rb
+++ app/controllers/admin/ambassador_task_assignments_controller.rb
@@ -3,13 +3,9 @@ class Admin::AmbassadorTaskAssignmentsController < Admin::BaseController
   layout "new_admin"
 
   def index
-    sorted_assignments =
+    @ambassador_task_assignments =
       AmbassadorTaskAssignment
         .sort_by_association(criterion: sort_column, direction: sort_direction)
-
-    @ambassador_task_assignments =
-      Kaminari
-        .paginate_array(sorted_assignments)
         .page(params.fetch(:page, 1))
         .per(params.fetch(:per_page, 25))
   end
