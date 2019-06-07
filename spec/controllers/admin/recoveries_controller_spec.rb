b2a4047fa04d0bd346d2ca60e1984a44c69516a1
Bobby <Robertgervais22@gmail.com>
2019-06-07 15:49:42 -0500

    what

--- spec/controllers/admin/recoveries_controller_spec.rb
+++ spec/controllers/admin/recoveries_controller_spec.rb
@@ -1,12 +1,10 @@
 require "spec_helper"
 
 describe Admin::RecoveriesController do
-  before do
-    let(:user) { FactoryBot.create(:admin) }
-    set_current_user(user)
-  end
   describe "index" do
     it "renders" do
+      user = FactoryBot.create(:admin)
+      set_current_user(user)
       get :index
       expect(response).to be_success
       expect(response).to render_template(:index)
