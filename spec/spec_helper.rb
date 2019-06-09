eacd395c1c34550576a3a48f77d9a1673471dcb1
Jake Romer <mail@jakeromer.com>
2019-06-09 10:23:15 -0400

    Add GC deferral

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -66,3 +66,37 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 end
+
+# Defer garbage collection
+# ========================
+#
+# Set the ENV var `DEFER_GC` to tweak the interval at which to run the garbage
+# collector.
+class DeferredGarbageCollection
+  DEFERRED_GC_THRESHOLD = (ENV["DEFER_GC"] || 10.0).to_f
+
+  @@last_gc_run = Time.now
+
+  def self.start
+    GC.disable if DEFERRED_GC_THRESHOLD > 0
+  end
+
+  def self.reconsider
+    if DEFERRED_GC_THRESHOLD > 0 && Time.now - @@last_gc_run >= DEFERRED_GC_THRESHOLD
+      GC.enable
+      GC.start
+      GC.disable
+      @@last_gc_run = Time.now
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:all) do
+    DeferredGarbageCollection.start
+  end
+
+  config.after(:all) do
+    DeferredGarbageCollection.reconsider
+  end
+end
