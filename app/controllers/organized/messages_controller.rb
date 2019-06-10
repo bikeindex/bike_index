fbbab051e1b5784462521da961f21b6c4118f4ad
sethherr <seth.william.herr@gmail.com>
2019-06-10 16:35:40 -0700

    make variable plural

--- app/controllers/organized/messages_controller.rb
+++ app/controllers/organized/messages_controller.rb
@@ -38,9 +38,10 @@ module Organized
     end
 
     def ensure_permitted_message_kind!
-      @kind = params[:organization_message].present? ? params[:organization_message][:kind_slug] : params[:kind]
-      @kind ||= current_organization.message_kinds
-      return true if current_organization.paid_for?(@kind)
+      kind = params[:organization_message].present? ? params[:organization_message][:kind_slug] : params[:kind]
+      @kinds =  Array(kind).flatten if kind.present?
+      @kinds ||= current_organization.message_kinds
+      return true if current_organization.paid_for?(@kinds)
       flash[:error] = "Your organization doesn't have access to that, please contact Bike Index support"
       if current_organization.message_kinds.any?
         redirect_to organization_messages_path(organization_id: current_organization.to_param, kind: current_organization.message_kinds.first)
@@ -56,7 +57,7 @@ module Organized
     end
 
     def redirect_back
-      redirect_kind = @kind || current_organization.message_kinds.first
+      redirect_kind = @kinds || current_organization.message_kinds.first
       redirect_to organization_messages_path(organization_id: current_organization.to_param, kind: redirect_kind)
       return
     end
