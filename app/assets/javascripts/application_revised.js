41d03b7a3b76be52ec4fddecce15f309cb7aa302
Seth Herr <seth.william.herr@gmail.com>
2019-06-11 10:45:19 -0700

    Add some homepage counts (#874)

--- app/assets/javascripts/application_revised.js
+++ app/assets/javascripts/application_revised.js
@@ -21,9 +21,11 @@
 //= require external_scripts/zoom.js
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require chartkick
+//= require Chart.bundle
 
-// Legacy carousel. Used on homepage. 
-// Can use if we need a carousel, though would be nice to use rails-assets version instead 
+// Legacy carousel. Used on homepage.
+// Can use if we need a carousel, though would be nice to use rails-assets version instead
 //= require external_scripts/slick.js
 
 // Things that are required for File Upload:
--- spec/models/counts_spec.rb
+++ spec/models/counts_spec.rb
@@ -12,6 +12,16 @@ RSpec.describe Counts, type: :model do
     end
   end
 
+  context "week_creation_chart" do
+    let!(:bike) { FactoryBot.create(:bike) }
+    let(:target) { { "#{Date.today}" => 1 } }
+    it "saves the thing" do
+      expect(Counts.week_creation_chart).to be_nil # Ensure we aren't throwing errors
+      Counts.assign_week_creation_chart
+      expect(Counts.week_creation_chart).to eq target
+    end
+  end
+
   context "recoveries" do
     let!(:recovered_bike) { FactoryBot.create(:stolen_record_recovered, date_recovered: Time.now - 1.day) }
     it "returns constant" do
