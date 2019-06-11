df01cb4d7e07129a182247d1508c7c1e5b0447d1
sethherr <seth.william.herr@gmail.com>
2019-06-10 20:26:29 -0700

    and throw that in there too

--- app/models/counts.rb
+++ app/models/counts.rb
@@ -12,24 +12,40 @@ class Counts
     end
 
     def count_keys
-      %w[total_bikes stolen_bikes recoveries recoveries_value]
+      %w[total_bikes stolen_bikes recoveries recoveries_value week_creation_chart organizations]
     end
 
     def recovery_average_value
       1428 # 2019/5/20 - calculated by averaging the value of the recoveries that had listed values
     end
 
-    def assign_total_bikes; assign_for("total_bikes", Bike.count) end
-
     def total_bikes; retrieve_for("total_bikes") end
 
+    def stolen_bikes; retrieve_for("stolen_bikes") end
+
+    def week_creation_chart; retrieve_for("week_creation_chart") end
+
+    def organizations; retrieve_for("organizations") end
+
+    def week_creation_chart;
+      chart_data = redis { |r| r.hget STOREAGE_KEY, "week_creation_chart" }
+      chart_data.present? ? JSON.parse(chart_data) : chart_data
+    end
+
+    # And here are the assignments:
     def assign_stolen_bikes; assign_for("stolen_bikes", Bike.stolen.count) end
 
-    def stolen_bikes; retrieve_for("stolen_bikes") end
+    def assign_total_bikes; assign_for("total_bikes", Bike.count) end
+
+    def assign_organizations; assign_for("organizations", Organization.count) end
 
     def assign_recoveries
       # StolenBikeRegistry.com had just over 2k recoveries prior to merging. The recoveries weren't imported, so manually calculate
       assign_for("recoveries", StolenRecord.recovered.where("date_recovered < ?", Time.zone.now.beginning_of_day).count + 2_041)
+    end
+
+    def assign_week_creation_chart
+      assign_for("week_creation_chart", Bike.unscoped.where(created_at: (Time.now - 7.days)..Time.now).group_by_day(:created_at).count.to_json)
     end
 
     def recoveries; retrieve_for("recoveries") end
