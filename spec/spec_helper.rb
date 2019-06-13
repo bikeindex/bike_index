0abbce532031427024018b14e8054e7250d80f20
sethherr <seth.william.herr@gmail.com>
2019-06-13 15:29:38 -0700

    remove unnecessary spec_helper configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,13 +25,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  # These two settings work together to allow you to limit a spec run
-  # to individual examples or groups you care about by tagging them with
-  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
-  # get run.
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
-
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
