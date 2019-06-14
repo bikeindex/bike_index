40218c6afbf1ebe896c47b7e0d610b918f81d0bb
Robert Gervais <Robertgervais22@gmail.com>
2019-06-14 17:17:07 -0500

    Fix Recoveries edit form (#894)
    * Add buttons and routing
    
    * fix recoveries controller tests
    
    * Hey guess who didn't click test any of this it was me professional commit message
    
    * refactor tests, remove approval action, remove some ancient commented out code

--- app/controllers/admin/recoveries_controller.rb
+++ app/controllers/admin/recoveries_controller.rb
@@ -20,42 +20,18 @@ class Admin::RecoveriesController < Admin::BaseController
 
   def update
     @stolen_record = StolenRecord.unscoped.find(params[:id])
-    if params[:is_not_displayable].present?
-      @stolen_record.recovery_display_status = "not_displayed"
-    elsif params[:mark_as_eligible].present?
+    if params[:stolen_record][:mark_as_eligible].present?
       @stolen_record.recovery_display_status = "waiting_on_decision"
+      redirect = new_admin_recovery_display_path(@stolen_record)
+    elsif params[:stolen_record][:is_not_displayable].present?
+      @stolen_record.recovery_display_status = "not_displayed"
     end
     if @stolen_record.update_attributes(permitted_parameters)
       flash[:success] = "Recovery Saved!"
-      redirect_to admin_recoveries_url
+      redirect_to redirect ||= admin_recoveries_url
     else
       raise StandardError
       render action: :edit
-    end
-  end
-
-  def approve
-    if params[:multipost]
-      enqueued = false
-      if params[:recovery_selected].present?
-        params[:recovery_selected].keys.each do |rid|
-          recovery = StolenRecord.unscoped.find(rid)
-          unless recovery.recovery_posted && recovery.can_share_recovery == false
-            RecoveryNotifyWorker.perform_async(rid.to_i)
-            enqueued = true
-          end
-        end
-      end
-      if enqueued
-        flash[:success] = "Recovery notifications enqueued. Recoveries marked 'can share' haven't been posted, because they need your loving caress."
-      else
-        flash[:error] = "No recoveries were selected (or only recoveries you need to caress were)"
-      end
-      redirect_to admin_recoveries_url
-    else
-      RecoveryNotifyWorker.perform_async(params[:id].to_i)
-      flash[:success] = "Recovery notification enqueued."
-      redirect_to admin_recoveries_url
     end
   end
 
