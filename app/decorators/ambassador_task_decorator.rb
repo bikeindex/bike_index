f92ace773741b05e6b1312353fa8e37734a3d6eb
sethherr <seth.william.herr@gmail.com>
2019-06-17 11:16:04 -0700

    slim down the task toggle

--- app/decorators/ambassador_task_decorator.rb
+++
@@ -1,8 +0,0 @@
-# NB: Decorators are deprecated in this project.
-#     Use Helper methods for view logic, consider incrementally refactoring
-#     existing view logic from decorators to view helpers.
-class AmbassadorTaskDecorator < ApplicationDecorator
-  delegate_all
-
-  def button_to_toggle_completion_status(*); end
-end
