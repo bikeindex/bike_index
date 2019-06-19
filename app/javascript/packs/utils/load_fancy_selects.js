11433d7543674cbd54ed19e419ddbccaa4e6d13b
Seth Herr <seth.william.herr@gmail.com>
2019-06-19 10:09:46 -0700

    Revert serializer change (#917)

--- app/javascript/packs/utils/load_fancy_selects.js
+++ app/javascript/packs/utils/load_fancy_selects.js
@@ -4,20 +4,19 @@ import selectize from "selectize";
 // This could be changed to some other library at some point - this makes it possible for us to abstract that away.
 // Just add the `unfancy` and `fancy-select` classes to a select box and it will be a fancy select box!
 const LoadFancySelects = () => {
-  // Gross, check if
   $(".unfancy.fancy-select.no-restore-on-backspace select").selectize({
     create: false,
     plugins: []
   });
+  // Remove, because we've already added them
+  $(".unfancy.fancy-select.no-restore-on-backspace").removeClass("unfancy");
 
   $(".unfancy.fancy-select select").selectize({
     create: false,
     plugins: ["restore_on_backspace"]
   });
   // Remove them so we don't initialize twice
-  $(".unfancy.fancy-select, .unfancy.fancy-select-placeholder").removeClass(
-    "unfancy"
-  );
+  $(".unfancy.fancy-select").removeClass("unfancy");
 };
 
 export default LoadFancySelects;
