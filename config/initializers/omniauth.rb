d908fc659a3df112ebc4c1bc839f4d5c08fe7fba
sethherr <seth.william.herr@gmail.com>
2019-06-19 15:05:59 -0700

    add the env variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"], scope: "email"
   provider :strava, ENV["STRAVA_KEY"], ENV["STRAVA_SECRET"], scope: "public"
+  provider :localid, ENV["LOCALID_KEY"], ENV["LOCALID_KEY"]
 end
