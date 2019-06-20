818744092ef67940cbf02f42733c2addb27111c9
Bobby <Robertgervais22@gmail.com>
2019-06-20 17:46:49 -0500

    ahh

--- app/javascript/packs/pages/admin/binx_admin.js
+++ app/javascript/packs/pages/admin/binx_admin.js
@@ -1,3 +1,4 @@
+import * as FilePond from 'filepond';
 import log from "../../utils/log";
 import moment from "moment-timezone";
 import LoadFancySelects from "../../utils/load_fancy_selects.js";
--- app/javascript/packs/pages/admin/blogs.js
+++ app/javascript/packs/pages/admin/blogs.js
@@ -2,6 +2,7 @@ function BinxAdminBlogs() {
   return {
     init() {
       this.editDate()
+      this.publicImageFileUpload()
     },
 
     editDate() {
@@ -11,38 +12,7 @@ function BinxAdminBlogs() {
       })
     },
    publicImageFileUpload() {
-      return $('#new_public_image').fileupload({
-        dataType: "script",
-        add(e, data) {
-          const types = /(\.|\/)(gif|jpe?g|png)$/i;
-          const file = data.files[0];
-          $('#public_images').sortable('disable');
-          if (types.test(file.type) || types.test(file.name)) {
-            data.context = $(`<div class="upload"><p><em>${file.name}</em></p><div class="progress progress-striped active"><div class="bar" style="width: 0%"></div></div></div>`);
-            $('#new_public_image').append(data.context);
-            return data.submit();
-          } else {
-            return alert(`${file.name} is not a gif, jpeg, or png image file`);
-          }
-        },
-        progress(e, data) {
-          if (data.context) {
-            const progress = parseInt((data.loaded / data.total) * 95, 10); // Multiply by 95, so that it doesn't look done, since progress doesn't work.
-            return data.context.find('.bar').css('width', progress + '%');
-          }
-        },
-        done(e, data) {
-          const file = data.files[0];
-          return $.each(data.files, (index, file) =>
-            data.context.addClass('finished_upload').html(`\
-<p><em>${file.name}</em></p>
-<div class='alert-success'>
-    Finished uploading
-</div>\
-`).fadeOut('slow')
-            );
-        }
-      });
+
     }
   };
  }
