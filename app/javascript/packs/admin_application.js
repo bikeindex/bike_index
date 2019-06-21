5abc2ffa2eb38d3aecc99959934631864daa7eeb
Bobby <Robertgervais22@gmail.com>
2019-06-21 17:33:09 -0500

    change

--- app/javascript/packs/admin_application.js
+++ app/javascript/packs/admin_application.js
@@ -21,15 +21,15 @@ window.$ = window.jQuery = jQuery;
 
 
 // Add Blueimp File Upload
-require('script-loader!blueimp-file-upload/js/vendor/jquery.ui.widget.js');
-require('script-loader!blueimp-tmpl/js/tmpl.js');
-require('script-loader!blueimp-load-image/js/load-image.all.min.js');
-require('script-loader!blueimp-canvas-to-blob/js/canvas-to-blob.js');
-require('script-loader!blueimp-file-upload/js/jquery.iframe-transport.js');
-require('script-loader!blueimp-file-upload/js/jquery.fileupload.js');
-require('script-loader!blueimp-file-upload/js/jquery.fileupload-process.js');
-require('script-loader!blueimp-file-upload/js/jquery.fileupload-image.js');
-require('script-loader!blueimp-file-upload/js/jquery.fileupload-audio.js');
-require('script-loader!blueimp-file-upload/js/jquery.fileupload-video.js');
-require('script-loader!blueimp-file-upload/js/jquery.fileupload-validate.js');
-require('script-loader!blueimp-file-upload/js/jquery.fileupload-ui.js');
+//='script-loader!blueimp-file-upload/js/vendor/jquery.ui.widget.js'
+//='script-loader!blueimp-tmpl/js/tmpl.js'
+//='script-loader!blueimp-load-image/js/load-image.all.min.js'
+//='script-loader!blueimp-canvas-to-blob/js/canvas-to-blob.js'
+//='script-loader!blueimp-file-upload/js/jquery.iframe-transport.js'
+//='script-loader!blueimp-file-upload/js/jquery.fileupload.js'
+//='script-loader!blueimp-file-upload/js/jquery.fileupload-process.js'
+//='script-loader!blueimp-file-upload/js/jquery.fileupload-image.js'
+//='script-loader!blueimp-file-upload/js/jquery.fileupload-audio.js'
+//='script-loader!blueimp-file-upload/js/jquery.fileupload-video.js'
+//='script-loader!blueimp-file-upload/js/jquery.fileupload-validate.js'
+//='script-loader!blueimp-file-upload/js/jquery.fileupload-ui.js'
--- app/javascript/packs/pages/admin/blogs.js
+++ app/javascript/packs/pages/admin/blogs.js
@@ -11,8 +11,40 @@ function BinxAdminBlogs() {
         $("#blog-date").slideDown()
       })
     },
-   publicImageFileUpload() {
 
+    publicImageFileUpload() {
+        $('#new_public_image').fileupload({
+        dataType: "script",
+        add(e, data) {
+          const types = /(\.|\/)(gif|jpe?g|png)$/i;
+          const file = data.files[0];
+          if (types.test(file.type) || types.test(file.name)) {
+            data.context = $(`<div class="upload"><p><em>${file.name}</em></p><div class="progress progress-striped active"><div class="bar" style="width: 0%"></div></div></div>`);
+            $('#new_public_image').append(data.context);
+            return data.submit();
+          } else {
+            return alert(`${file.name} is not a gif, jpeg, or png image file`);
+          }
+        },
+        progress(e, data) {
+          if (data.context) {
+            const progress = parseInt((data.loaded / data.total) * 95, 10); // Multiply by 95, so that it doesn't look done, since progress doesn't work.
+            return data.context.find('.bar').css('width', progress + '%');
+          }
+        },
+        done(e, data) {
+          // runSortableImages
+          const file = data.files[0];
+          return $.each(data.files, (index, file) =>
+            data.context.addClass('finished_upload').html(`\
+<p><em>${file.name}</em></p>
+<div class='alert-success'>
+    Finished uploading
+</div>\
+`).fadeOut('slow')
+            );
+        }
+      });
     }
   };
  }
