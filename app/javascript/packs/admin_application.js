689ef917b4abc976c490b31066760063a37ea3c4
Bobby <Robertgervais22@gmail.com>
2019-06-21 15:50:53 -0500

    add blueimp and scriptloader

--- app/javascript/packs/admin_application.js
+++ app/javascript/packs/admin_application.js
@@ -18,3 +18,18 @@ Chartkick.addAdapter(Chart);
 
 // Add jQuery to the window so it's accessible in the console, and just in general
 window.$ = window.jQuery = jQuery;
+
+
+// Add Blueimp File Upload
+require('script-loader!blueimp-file-upload/js/vendor/jquery.ui.widget.js');
+require('script-loader!blueimp-tmpl/js/tmpl.js');
+require('script-loader!blueimp-load-image/js/load-image.all.min.js');
+require('script-loader!blueimp-canvas-to-blob/js/canvas-to-blob.js');
+require('script-loader!blueimp-file-upload/js/jquery.iframe-transport.js');
+require('script-loader!blueimp-file-upload/js/jquery.fileupload.js');
+require('script-loader!blueimp-file-upload/js/jquery.fileupload-process.js');
+require('script-loader!blueimp-file-upload/js/jquery.fileupload-image.js');
+require('script-loader!blueimp-file-upload/js/jquery.fileupload-audio.js');
+require('script-loader!blueimp-file-upload/js/jquery.fileupload-video.js');
+require('script-loader!blueimp-file-upload/js/jquery.fileupload-validate.js');
+require('script-loader!blueimp-file-upload/js/jquery.fileupload-ui.js');
