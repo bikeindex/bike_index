01d207342f21a121474a036768feae31eb6aaade
Jake Romer <mail@jakeromer.com>
2019-06-27 12:33:16 -0400

    Add MoneyHelper

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,10 @@ module Bikeindex
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.enforce_available_locales = false
     config.i18n.default_locale = :en
     config.i18n.available_locales = %i[en es nl]
-    config.i18n.enforce_available_locales = false
+    config.i18n.fallbacks = { "en-US": :en, "en-GB": :en }
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
---
+++ spec/helpers/money_helper_spec.rb
@@ -0,0 +1,83 @@
+# coding: utf-8
+require "rails_helper"
+
+RSpec.describe MoneyHelper, type: :helper do
+  describe "#default_currency" do
+    context "given the current locale is set to the default_locale (en)" do
+      it "returns the alphabetic_code USD" do
+        expect(default_currency).to eq("USD")
+      end
+    end
+
+    context "given the current locale is set to an available locale" do
+      it "returns the alphabetic_code for the locale's currency" do
+        I18n.with_locale(:nl) do
+          expect(default_currency).to eq("EUR")
+        end
+      end
+    end
+
+    context "given the current locale is set to a locale with a fallback" do
+      it "returns the alphabetic_code for the fallback locale's currency" do
+        I18n.with_locale(:"en-GB") do
+          expect(default_currency).to eq("USD")
+        end
+      end
+    end
+
+    context "given the current locale is set to an unavailable locale" do
+      it "returns the alphabetic_code for the default locale's currency" do
+        I18n.with_locale(:"unavailable") do
+          expect(default_currency).to eq("USD")
+        end
+      end
+    end
+  end
+
+  describe "#money_usd" do
+    context "given a valid target currency" do
+      it "returns a Money object converting to the target currency" do
+        conversion_rate = Money.default_bank.get_rate(:USD, :EUR)
+        currency = money_usd(1.00, exchange_to: :EUR)
+        expect(currency).to be_an_instance_of(Money)
+        expect(currency.fractional).to eq(conversion_rate * 100)
+        expect(currency.currency).to eq(:EUR)
+      end
+    end
+
+    context "given an invalid target currency" do
+      it "raises UnknownCurrency" do
+        expect { money_usd(1.00, exchange_to: :ERR) }.to raise_error(Money::Currency::UnknownCurrency)
+      end
+    end
+
+    context "given no target currency" do
+      it "returns a Money object converting to the default currency" do
+        currency = money_usd(1.00)
+        expect(currency).to be_an_instance_of(Money)
+        expect(currency.fractional).to eq(100)
+        expect(currency.currency).to eq(:USD)
+      end
+    end
+  end
+
+  describe "#as_currency" do
+    context "given a valid target currency" do
+      it "returns a Money object converting to the default currency" do
+        expect(as_currency(100, exchange_to: :EUR)).to eq("€88")
+      end
+    end
+
+    context "given an invalid target currency" do
+      it "raises UnknownCurrency" do
+        expect { as_currency(100, exchange_to: :ERR) }.to raise_error(Money::Currency::UnknownCurrency)
+      end
+    end
+
+    context "given no target currency" do
+      it "returns a Money object converting to the default currency" do
+        expect(as_currency(100)).to eq("$100")
+      end
+    end
+  end
+end
