ffad08d29cf97a47f6ea62d7d8ae6398115509dd
Bobby <Robertgervais22@gmail.com>
2019-07-03 23:51:52 -0500

    more filepond

--- app/javascript/packs/pages/admin/blogs.js
+++ app/javascript/packs/pages/admin/blogs.js
@@ -2,12 +2,14 @@ import * as FilePond from 'filepond';
 import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
 
 
 function BinxAdminBlogs() {
   return {
     init() {
       this.editDate()
+      this.filePondUpload
     },
 
     editDate() {
@@ -15,6 +17,29 @@ function BinxAdminBlogs() {
         e.preventDefault();
         $("#blog-date").slideDown()
       })
+    },
+
+    filePondUpload() {
+      FilePond.create(
+        document.querySelector('#pond')
+      );
+
+
+      FilePond.setOptions({
+        server: {
+          url: "/public_images/create",
+          process: {
+            url: './process',
+            method: 'POST',
+            withCredentials: false,
+            headers: {},
+            timeout: 7000,
+            onload: null,
+            onerror: null,
+            ondata: null
+          }
+        }
+      });
     }
   };
 }
