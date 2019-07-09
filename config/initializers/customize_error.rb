27e3fdcbdbd11263c99342408c180fd9b498f0b1
sethherr <seth.william.herr@gmail.com>
2019-07-08 17:33:01 -0700

    whatever

--- config/initializers/customize_error.rb
+++ config/initializers/customize_error.rb
@@ -1,6 +1,6 @@
 # We have a lot of custom forms
 # We don't actually want rails to wrap the fields in a "field_with_errors" div, it breaks forms like the organized form
 # Solution from https://rubyplus.com/articles/3401-Customize-Field-Error-in-Rails-5
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+ActionView::Base.field_error_proc = Proc.new do |html_tag, _instance_tag|
   html_tag
 end
