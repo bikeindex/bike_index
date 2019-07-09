631516d9f04ed2b3ad71cf6f7d69eae7460b8be8
Bobby <Robertgervais22@gmail.com>
2019-07-08 22:44:33 -0500

    remove filepond

--- app/javascript/packs/pages/admin/blogs.js
+++ app/javascript/packs/pages/admin/blogs.js
@@ -1,8 +1,4 @@
-import * as FilePond from 'filepond';
-import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
-import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+import log from "../../utils/log";
 
 
 function BinxAdminBlogs() {
@@ -18,36 +14,6 @@ function BinxAdminBlogs() {
         $("#blog-date").slideDown()
       })
     },
-
-    filePondUpload() {
-      FilePond.create(
-        document.querySelector('#pond')
-      );
-      FilePond.setOptions({
-        server: {
-          url: './',
-          timeout: 7000,
-          process: {
-            url: './public_images',
-            method: 'POST',
-            headers: {
-              'x-customheader': 'Hello World'
-            },
-            withCredentials: false,
-            onload: this.onLoadFile,
-            onerror: (response) => response.data,
-            ondata: (formData) => {
-              formData.append('Hello', 'World');
-              return formData;
-            }
-          },
-          revert: './revert',
-          restore: './restore/',
-          load: './load/',
-          fetch: './fetch/'
-        }
-      });
-    }
   };
 }
 
