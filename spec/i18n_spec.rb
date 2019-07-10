778088bd8c3ebb34f64fa5b7e5540b1a7e92e2e2
Jake Romer <jake@bikeindex.org>
2019-07-10 07:55:08 -0400

    Externalize organized templates (#999)
    Adds i18n-tasks
    
    Externalizes strings from the following organized templates:
    
    ambassador_dashboards/getting_started.html.haml
    ambassador_dashboards/resources.html.haml
    ambassador_dashboards/show.html.haml
    bikes/_list.html.haml
    bikes/_search.html.haml
    bikes/incompletes.html.haml
    bikes/index.html.haml
    bikes/multi_serial_search.html.haml
    bikes/new.html.haml
    bikes/recoveries.html.haml
    bulk_imports/index.html.haml
    bulk_imports/new.html.haml
    bulk_imports/show.html.haml
    exports/index.html.haml
    exports/new.html.haml
    exports/show.html.haml
    manage/_location_fields.html.haml
    manage/index.html.haml
    manage/locations.html.haml

---
+++ spec/i18n_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "i18n/tasks"
+
+RSpec.describe "I18n", skip: "pending internationalization" do
+  let(:i18n) { I18n::Tasks::BaseTask.new }
+  let(:missing_keys) { i18n.missing_keys }
+  let(:unused_keys) { i18n.unused_keys }
+
+  it "does not have missing keys" do
+    error_message = <<~STR
+      Missing #{missing_keys.leaves.count} i18n keys.\n
+      Run `i18n-tasks missing' to show them
+    STR
+
+    expect(missing_keys).to be_empty, error_message
+  end
+
+  it "does not have unused keys" do
+    error_message = <<~STR
+      #{unused_keys.leaves.count} unused i18n keys.\n
+      Run `i18n-tasks unused' to show them.
+    STR
+
+    expect(unused_keys).to be_empty, error_message
+  end
+
+  it "files are normalized" do
+    non_normalized = i18n.non_normalized_paths
+
+    error_message = <<~STR
+      The following files need to be normalized:
+      #{non_normalized.map { |path| "  #{path}" }.join("\n")}\n
+      Please run `i18n-tasks` normalize to fix.
+    STR
+
+    expect(non_normalized).to be_empty, error_message
+  end
+end
