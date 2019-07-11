1f280cecb130bb87f76ef56917bbcbcae2e90eb3
Seth Herr <seth.william.herr@gmail.com>
2019-07-11 13:27:21 -0700

    Fix js TimeParser  (#1019)

--- app/javascript/packs/pages/initializer.js
+++ app/javascript/packs/pages/initializer.js
@@ -1,5 +1,5 @@
 import log from "../utils/log";
-import moment from "moment-timezone";
+import TimeParser from "../utils/time_parser";
 import BinxMapping from "./binx_mapping.js";
 import BinxAdmin from "./admin/binx_admin.js";
 import BinxAppOrgExport from "./binx_org_export.js";
@@ -8,80 +8,6 @@ import BinxAppOrgBikes from "./binx_org_bikes.js";
 import BinxAppOrgUserForm from "./binx_org_user_form";
 
 window.binxApp || (window.binxApp = {});
-
-binxApp.displayLocalDate = function(time, preciseTime) {
-  // Ensure we return if it's a big future day
-  if (preciseTime == null) {
-    preciseTime = false;
-  }
-  if (time < window.tomorrow) {
-    if (time > window.today) {
-      return time.format("h:mma");
-    } else if (time > window.yesterday) {
-      return `Yesterday ${time.format("h:mma")}`;
-    }
-  }
-  if (time.year() === moment().year()) {
-    if (preciseTime) {
-      return time.format("MMM Do[,] h:mma");
-    } else {
-      return time.format("MMM Do[,] ha");
-    }
-  } else {
-    if (preciseTime) {
-      return time.format("YYYY-MM-DD h:mma");
-    } else {
-      return time.format("YYYY-MM-DD");
-    }
-  }
-};
-
-binxApp.preciseTimeSeconds = function(time) {
-  return time.format("YYYY-MM-DD h:mm:ssa");
-};
-
-binxApp.localizeTimes = function() {
-  if (!window.userTimezone) {
-    window.userTimezone = moment.tz.guess();
-  }
-  moment.tz.setDefault(window.userTimezone);
-  window.yesterday = moment()
-    .subtract(1, "day")
-    .startOf("day");
-  window.today = moment().startOf("day");
-  window.tomorrow = moment().endOf("day");
-
-  // Write local time
-  $(".convertTime").each(function() {
-    const $this = $(this);
-    $this.removeClass("convertTime");
-    const text = $this.text().trim();
-    if (!(text.length > 0)) {
-      return;
-    }
-    let time = "";
-    if (isNaN(text)) {
-      time = moment(text, moment.ISO_8601);
-    } else {
-      // it's a timestamp!
-      time = moment.unix(text);
-    }
-
-    if (!time.isValid) {
-      return;
-    }
-    $this
-      .text(binxApp.displayLocalDate(time, $this.hasClass("preciseTime")))
-      .attr("title", binxApp.preciseTimeSeconds(time));
-  });
-
-  // Write timezone
-  return $(".convertTimezone").each(function() {
-    const $this = $(this);
-    $this.text(moment().format("z"));
-    return $this.removeClass("convertTimezone");
-  });
-};
 
 binxApp.enableFilenameForUploads = function() {
   $("input.custom-file-input[type=file]").on("change", function(e) {
@@ -103,7 +29,8 @@ binxApp.enableFilenameForUploads = function() {
 // and make the instance of class (which I'm storing on window) the same name without the first letter capitalized
 // I'm absolutely sure there is a best practice that I'm ignoring, but just doing it for now.
 $(document).ready(function() {
-  binxApp.localizeTimes();
+  window.timeParser = TimeParser();
+  window.timeParser.localize();
   // Load admin, whatever
   if ($("#admin-content").length > 0) {
     const binxAdmin = BinxAdmin();
@@ -111,18 +38,18 @@ $(document).ready(function() {
   }
   // Load the page specific things
   const bodyId = document.getElementsByTagName("body")[0].id;
-  switch (bodyId) {
-    case "organized_messages_index":
-      window.binxMapping = new BinxMapping("geolocated_messages");
-      window.binxAppOrgMessages = new BinxAppOrgMessages();
-      binxAppOrgMessages.init();
-    case "organized_exports_show":
-    case "organized_exports_new":
-      window.binxAppOrgExport = new BinxAppOrgExport();
-      binxAppOrgExport.init();
-    case "organized_bikes_index":
-      const binxAppOrgBikes = BinxAppOrgBikes();
-      binxAppOrgBikes.init();
+  if (bodyId === "organized_messages_index") {
+    window.binxMapping = new BinxMapping("geolocated_messages");
+    window.binxAppOrgMessages = new BinxAppOrgMessages();
+    binxAppOrgMessages.init();
+  } else if (
+    ["organized_exports_show", "organized_exports_new"].includes(bodyId)
+  ) {
+    window.binxAppOrgExport = new BinxAppOrgExport();
+    binxAppOrgExport.init();
+  } else if (bodyId === "organized_bikes_index") {
+    const binxAppOrgBikes = BinxAppOrgBikes();
+    binxAppOrgBikes.init();
   }
   // This can be new, edit, create or update, so just checking for the element
   if ($("#multipleUserSelect").length) {
