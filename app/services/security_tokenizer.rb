fc587e3a67ae96b8d1f7236d88b861238c46fb6e
sethherr <seth.william.herr@gmail.com>
2019-07-15 08:17:43 -0700

    remove magic number

--- app/services/security_tokenizer.rb
+++ app/services/security_tokenizer.rb
@@ -1,4 +1,6 @@
 class SecurityTokenizer
+  EARLIEST_TOKEN_TIME = 1427848192 # 2015-03-31T17:29:52
+
   def self.new_token(time = nil)
     t = (time || Time.current).to_i
     "#{t}-" + SecureRandom.hex + Digest::MD5.hexdigest("#{SecureRandom.hex}-#{t}")
@@ -6,7 +8,7 @@ class SecurityTokenizer
 
   def self.token_time(str)
     t = str.to_s.split("-")[0]
-    t = (t.present? && t.to_i > 1427848192) ? t.to_i : 1364777722
+    t = (t.present? && t.to_i > EARLIEST_TOKEN_TIME ? t.to_i : EARLIEST_TOKEN_TIME
     Time.at(t)
   end
 end
