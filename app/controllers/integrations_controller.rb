b169a03042440d481f8183752456aeaf4636dfa4
sethherr <seth.william.herr@gmail.com>
2019-07-19 16:00:34 -0700

    add a bang to surface error

--- app/controllers/integrations_controller.rb
+++ app/controllers/integrations_controller.rb
@@ -9,6 +9,7 @@ class IntegrationsController < ApplicationController
     @integration.save
     if @integration.valid? && @integration.user.present?
       @user = @integration.user
+      @user.save!
       sign_in_and_redirect(@user)
     else
       integrations_controller_creation_error
