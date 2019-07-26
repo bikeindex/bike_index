fba1ba943aa2444c553c45a423d20728093b5a34
sethherr <seth.william.herr@gmail.com>
2019-07-26 15:20:38 -0500

    better explanations

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -31,11 +31,11 @@ class Integration < ActiveRecord::Base
   end
 
   def create_user(email:, name:)
-    pword = SecurityTokenizer.new_password_token
+    password = SecurityTokenizer.new_password_token
     i_user = User.new(email: email,
                       name: name,
-                      password: pword,
-                      password_confirmation: pword)
+                      password: password,
+                      password_confirmation: password)
     if i_user.save
       i_user.confirm(i_user.confirmation_token)
     else
--- app/services/security_tokenizer.rb
+++ app/services/security_tokenizer.rb
@@ -6,6 +6,7 @@ class SecurityTokenizer
     "#{t}-" + SecureRandom.hex + Digest::MD5.hexdigest("#{SecureRandom.hex}-#{t}")
   end
 
+  # Passwords have to be less than 72 characters. Lazy hack
   def self.new_password_token(time = nil)
     new_token(time).slice(12, 50)
   end
