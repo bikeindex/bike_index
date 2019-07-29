f6d648fe71a36dbda3ecc6bafa4cc67d579fdbcb
Jake Romer <mail@jakeromer.com>
2019-07-29 09:14:49 -0400

    Add alert_image to StolenRecord factory, create bike with image by default

--- spec/factories/stolen_records.rb
+++ spec/factories/stolen_records.rb
@@ -1,11 +1,17 @@
 FactoryBot.define do
   factory :stolen_record do
-    bike { FactoryBot.create(:bike) }
+    bike { FactoryBot.create(:bike, :with_image) }
     date_stolen { Time.current }
+    alert_image { File.open(Rails.root.join("spec", "fixtures", "bike.jpg")) }
+
     factory :stolen_record_recovered do
       date_recovered { Time.current }
       recovered_description { "Awesome help by Bike Index" }
       current { false }
+    end
+
+    trait :no_photo do
+      bike { FactoryBot.create(:bike) }
     end
   end
 end
