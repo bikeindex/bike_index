402af042c55d620e1b08d14dc0fa97fbddfe37fd
Jake Romer <jake@bikeindex.org>
2019-07-31 10:41:55 -0400

    Revert asset host change, add local carrier wave asset host url

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -33,13 +33,14 @@ end
 
 # Additional carrierwave configurations
 CarrierWave.configure do |config|
-  config.asset_host = ActionController::Base.asset_host
   config.cache_dir = Rails.root.join("tmp", "uploads")
   config.storage :file
+  config.asset_host = ENV["BASE_URL"]
 
   if Rails.env.production?
     # config.fog_provider "fog/aws" # Once carrierwave is updated
     config.storage = :fog
+    config.asset_host = ENV["S3_ASSET_HOST"]
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["S3_ACCESS_KEY"],
