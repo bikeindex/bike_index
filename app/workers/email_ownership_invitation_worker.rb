ca566d4dea8f911be2960c50d7f50623eae68118
Jake Romer <jake@bikeindex.org>
2019-08-05 12:52:46 -0400

    Don't attempt to send ownership email unless owner_email is present
    Resolves https://app.honeybadger.io/projects/35931/faults/53091091

--- app/workers/email_ownership_invitation_worker.rb
+++ app/workers/email_ownership_invitation_worker.rb
@@ -1,10 +1,10 @@
 class EmailOwnershipInvitationWorker < ApplicationWorker
-
   sidekiq_options queue: "notify"
 
   def perform(ownership_id)
     ownership = Ownership.where(id: ownership_id).first
     return true unless ownership.present?
+    return true unless ownership.owner_email.present?
     ownership.bike.save
     ownership.reload
     OrganizedMailer.finished_registration(ownership).deliver_now
