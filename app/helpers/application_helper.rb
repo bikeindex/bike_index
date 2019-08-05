0096931a2a249f0d375c55743b710e8497a5197e
sethherr <seth.william.herr@gmail.com>
2019-08-05 14:29:07 -0500

    sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -220,6 +220,6 @@ module ApplicationHelper
   def language_choices
     @language_choices ||= I18n.available_locales.map do |locale|
       [t(locale, scope: [:locales]), locale.to_s]
-    end
+    end.sort { |a, b| a[0].downcase <=> b[0].downcase }
   end
 end
