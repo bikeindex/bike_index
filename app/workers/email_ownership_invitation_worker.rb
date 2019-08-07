a7e522f81d01e120df0a190971fed37dc4cb6925
sethherr <seth.william.herr@gmail.com>
2019-08-07 10:57:39 -0700

    catch that nil

--- app/workers/email_ownership_invitation_worker.rb
+++ app/workers/email_ownership_invitation_worker.rb
@@ -5,7 +5,7 @@ class EmailOwnershipInvitationWorker < ApplicationWorker
   def perform(ownership_id)
     ownership = Ownership.where(id: ownership_id).first
     return true unless ownership.present?
-    ownership.bike.update_attributes(updated_at: Time.current)
+    ownership.bike&.update_attributes(updated_at: Time.current)
     ownership.reload
     if ownership.calculated_send_email
       OrganizedMailer.finished_registration(ownership).deliver_now
