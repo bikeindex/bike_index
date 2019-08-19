1172adf34ee05ce6e5f0a1afdee6cb7a403b7771
Bobby <Robertgervais22@gmail.com>
2019-08-19 17:19:00 -0500

    ope

--- spec/workers/email_promoted_alert_worker_spec.rb
+++ spec/workers/email_promoted_alert_worker_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
+
 RSpec.describe EmailPromotedAlertWorker, type: :worker do
-    include_context :logged_in_as_super_admin
-    let(:stolen_record) { FactoryBot.create(:stolen_record_recovered) }
-    let(:ownership) { FactoryBot.create(:ownership) }
-    let(:user) { ownership.creator }
-    let(:bike) { ownership.bike }
-    let!(:theft_alert) { FactoryBot.create(:theft_alert, stolen_record: stolen_record) }
-    it "sends a promoted alert emails" do
-      ActionMailer::Base.deliveries = []
-      EmailPromotedAlertWorker.new.perform(theft_alert.id)
-      expect(ActionMailer::Base.deliveries.empty?).to be_falsey
-    end
+  include_context :logged_in_as_super_admin
+  let(:stolen_record) { FactoryBot.create(:stolen_record_recovered) }
+  let(:ownership) { FactoryBot.create(:ownership) }
+  let(:user) { ownership.creator }
+  let(:bike) { ownership.bike }
+  let!(:theft_alert) { FactoryBot.create(:theft_alert, stolen_record: stolen_record) }
+  it "sends a promoted alert emails" do
+    ActionMailer::Base.deliveries = []
+    EmailPromotedAlertWorker.new.perform(theft_alert.id)
+    expect(ActionMailer::Base.deliveries.empty?).to be_falsey
   end
 end
