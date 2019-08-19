71f6d713b85f4cf6845617700dac2d77b2d27ee5
Bobby <Robertgervais22@gmail.com>
2019-08-19 17:43:50 -0500

    ahhhhhhhhhhh

--- app/workers/email_promoted_alert_worker.rb
+++ app/workers/email_promoted_alert_worker.rb
@@ -3,6 +3,6 @@ class EmailPromotedAlertWorker
 
   def perform(theft_alert_id)
     theft_alert = TheftAlert.find(theft_alert_id)
-    CustomerMailer.promoted_alert(theft_alert).deliver_now
+    CustomerMailer.promoted_alert_email(theft_alert).deliver_now
   end
 end
