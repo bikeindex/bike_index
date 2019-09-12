9b7932c1fe5a822afba8abd165d7cc348a267fdc
sethherr <seth.william.herr@gmail.com>
2019-09-12 11:01:37 -0700

    fix blog photo upload bug

--- app/javascript/packs/pages/admin/blogs.js
+++ app/javascript/packs/pages/admin/blogs.js
@@ -3,7 +3,7 @@ import log from "../../utils/log";
 function BinxAdminBlogs() {
   return {
     init() {
-      this.editDate()
+      this.editDate();
       this.setIndexImage();
       this.setIndex();
       this.noPrimaryPhotoToggle();
@@ -13,47 +13,55 @@ function BinxAdminBlogs() {
     editDate() {
       $("#change_published_date").on("click", e => {
         e.preventDefault();
-        $("#blog-date").slideDown()
-      })
+        $("#blog-date").slideDown();
+      });
     },
 
     publicImageDelete() {
-      $('ul#public_images').on('click', ".image-delete-button", function(e) {
+      $("ul#public_images").on("click", ".image-delete-button", function(e) {
         e.preventDefault();
-        const id = $(".image-delete-button").closest(".row").find("input").val()
+        const id = $(".image-delete-button")
+          .closest(".row")
+          .find("input")
+          .val();
         $.ajax({
           url: `/public_images/${id}`,
-          type: 'delete'
+          type: "delete"
         });
-        this.closest('li').remove()
-      })
+        this.closest("li").remove();
+      });
     },
 
     noPrimaryPhotoToggle() {
-      const noPrimaryPhotoBox = $(".index_image_0")
+      const noPrimaryPhotoBox = $(".index_image_0");
       noPrimaryPhotoBox.on("change", e => {
         if (noPrimaryPhotoBox.prop("checked")) {
-          $(".index-image-select input").prop("checked", false)
+          $(".index-image-select input").prop("checked", false);
         }
-      })
+      });
     },
 
     setIndex() {
-      const index_image = $('#blog_index_image_id').val()
-      return $("li#" + index_image).find($("input")).prop("checked", true)
+      const index_image = $("#blog_index_image_id").val();
+      if (index_image.length) {
+        return $("li#" + index_image)
+          .find($("input"))
+          .prop("checked", true);
+      }
     },
 
     setIndexImage(e) {
-      $("ul#public_images").on("change", '.index-image-select input', function(e) {
+      $("ul#public_images").on("change", ".index-image-select input", function(
+        e
+      ) {
         e.preventDefault();
-        if ($('#blog_index_image_id').val != 0) {
-          $('#blog_index_image_id').val($(e.target).val());
-          $(".index_image_0").prop("checked", false)
+        if ($("#blog_index_image_id").val != 0) {
+          $("#blog_index_image_id").val($(e.target).val());
+          $(".index_image_0").prop("checked", false);
         }
-      })
-    },
+      });
+    }
   };
 }
 
-
-export default BinxAdminBlogs
+export default BinxAdminBlogs;
