278064965d9ff98b6ded6f942c906fb42c0a6311
Jake Romer <jake@bikeindex.org>
2019-09-16 14:54:25 -0400

    Add validations tests, set creator

--- app/models/customer_contact.rb
+++ app/models/customer_contact.rb
@@ -1,22 +1,28 @@
 class CustomerContact < ActiveRecord::Base
-  validates_presence_of :title
-  validates_presence_of :body
-  validates_presence_of :contact_type
-  # validates_presence_of :creator_id
-  validates_presence_of :bike_id
-  validates_presence_of :creator_email
-  validates_presence_of :user_email
-  serialize :info_hash
-
   belongs_to :bike
   belongs_to :user
   belongs_to :creator, class_name: "User"
 
-  before_save :normalize_email_and_find_user
+  validates \
+    :bike,
+    :body,
+    :contact_type,
+    :creator_email,
+    :title,
+    :user_email,
+    presence: true
 
-  def normalize_email_and_find_user
+  serialize :info_hash
+
+  before_save :normalize_emails_and_find_users
+
+  def normalize_emails_and_find_users
     self.user_email = EmailNormalizer.normalize(user_email)
     self.user ||= User.fuzzy_confirmed_or_unconfirmed_email_find(user_email)
+
+    self.creator_email = EmailNormalizer.normalize(creator_email)
+    self.creator ||= User.fuzzy_confirmed_or_unconfirmed_email_find(creator_email)
+
     true
   end
 end
