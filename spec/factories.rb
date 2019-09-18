febf0d91c15ac0d14b9d87007f3545b729861627
Jake Romer <jake@bikeindex.org>
2019-09-18 09:17:51 -0400

    Fix factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -113,7 +113,7 @@ FactoryBot.define do
     body { "some message" }
     creator_email { "something@example.com" }
     user_email { "something_else@example.com" }
-    contact_type { "stolen_message" }
-    kind { :stolen_message }
+    contact_type { "stolen_contact" }
+    kind { :stolen_contact }
   end
 end
