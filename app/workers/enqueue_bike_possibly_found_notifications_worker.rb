43cbb26d074337bef1a3099362853914712dcdcf
Jake Romer <mail@jakeromer.com>
2019-10-03 12:20:35 -0400

    Split up bike possibly found notification jobs

---
+++ app/workers/enqueue_bike_possibly_found_notifications_worker.rb
@@ -0,0 +1,20 @@
+class EnqueueBikePossiblyFoundNotificationsWorker < ScheduledWorker
+  prepend ScheduledWorkerRecorder
+
+  def self.frequency
+    24.hours
+  end
+
+  def perform
+    bikes_with_matches.each do |bike, match|
+      EmailBikePossiblyFoundNotificationWorker
+        .perform_async(bike.id, match.class, match.id)
+    end
+  end
+
+  def bikes_with_matches
+    internal_matches = Bike.possibly_found_with_match
+    external_matches = Bike.possibly_found_externally_with_match
+    internal_matches.concat(external_matches)
+  end
+end
