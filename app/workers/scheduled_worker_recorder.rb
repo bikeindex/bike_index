1df53972986f87a397f127ca146524b580e62f3b
Jake Romer <mail@jakeromer.com>
2019-10-02 20:22:54 -0400

    Branch on the presence of args

--- app/workers/scheduled_worker_recorder.rb
+++ app/workers/scheduled_worker_recorder.rb
@@ -1,7 +1,11 @@
 module ScheduledWorkerRecorder
-  def perform
-    record_scheduler_started
-    super
-    record_scheduler_finished
+  def perform(*args, **kwargs)
+    if [*args, *kwargs.values].all?(&:blank?)
+      record_scheduler_started
+      super()
+      record_scheduler_finished
+    else
+      super(*args, **kwargs)
+    end
   end
 end
