e931d6807303ff429a82fe5dce6ee97086fee530
Jake Romer <mail@jakeromer.com>
2019-10-11 09:56:32 -0400

    Add admin management of external registry bikes

---
+++ app/controllers/admin/external_registry_bikes_controller.rb
@@ -0,0 +1,38 @@
+class Admin::ExternalRegistryBikesController < Admin::BaseController
+  include SortableTable
+  before_filter :find_bike, only: %i[show]
+
+  def index
+    @page = params[:page] || 1
+    @bikes =
+      matching_bikes
+        .reorder("external_registry_bikes.#{sort_column} #{sort_direction}")
+        .page(@page)
+        .per(params[:per_page] || 100)
+  end
+
+  def show
+  end
+
+  private
+
+  def find_bike
+    @bike = ExternalRegistryBike.find(params[:id])
+  end
+
+  def matching_bikes
+    return @matching_bikes if defined?(@matching_bikes)
+
+    @matching_bikes = ExternalRegistryBike.all
+
+    if params[:search_serial_normalized].present?
+      @matching_bikes = @matching_bikes.where(serial_normalized: params[:search_serial_normalized])
+    end
+
+    @matching_bikes
+  end
+
+  def sortable_columns
+    %i[type country date_stolen created_at]
+  end
+end
