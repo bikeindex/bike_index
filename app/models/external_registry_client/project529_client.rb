cb69046386a07d9bdccfbe6db300976508e918ab
Jake Romer <mail@jakeromer.com>
2019-10-14 10:56:08 -0400

    Re-namespace project529 classes

---
+++ app/models/external_registry_client/project529_client.rb
@@ -0,0 +1,82 @@
+class ExternalRegistryClient::Project529Client
+  BASE_URL = ENV.fetch("PROJECT_529_BASE_URL", "https://project529.com/garage")
+  APP_ID = ENV["PROJECT_529_APP_ID"]
+
+  TTL_HOURS = ENV.fetch("EXTERNAL_REGISTRY_REQUEST_CACHE_TTL_HOURS", 24).to_i.hours
+  TIMEOUT_SECS = ENV.fetch("EXTERNAL_REGISTRY_REQUEST_TIMEOUT", 5).to_i
+
+  attr_accessor :conn, :base_url
+
+  def initialize(base_url: nil)
+    self.base_url = base_url || BASE_URL
+    self.conn = Faraday.new(url: self.base_url) do |conn|
+      conn.response :json, content_type: /\bjson$/
+      conn.use Faraday::RequestResponseLogger::Middleware,
+               logger_level: :info,
+               logger: Rails.logger if Rails.env.development?
+      conn.adapter Faraday.default_adapter
+      conn.options.timeout = TIMEOUT_SECS
+    end
+  end
+
+  def get_oauth_token
+    response = conn.post("oauth/token") do |req|
+      req.headers["Content-Type"] = "application/json"
+      req.params = {
+        grant_type: "refresh_token",
+        client_id: credentials.app_id,
+        refresh_token: credentials.refresh_token,
+      }
+    end
+
+    unless response.status == 200 && response.body.is_a?(Hash)
+      raise Project529ClientError, response.body
+    end
+
+    response.body.with_indifferent_access
+  end
+
+  def bikes(updated_at: nil, page: 1, per_page: 10)
+    credentials.set_access_token if credentials.access_token_expired?
+
+    req_params = {
+      updated_at: (updated_at.presence || Time.current - 20.days).strftime("%Y-%m-%d"),
+      page: page,
+      per_page: per_page,
+    }
+
+    cache_key = [self.class.to_s, __method__.to_s, req_params]
+
+    response =
+      Rails.cache.fetch(cache_key, expires_in: TTL_HOURS) do
+        response = conn.get("services/v1/bikes") do |req|
+          req.headers["Content-Type"] = "application/json"
+          req.params = req_params.merge(access_token: credentials.access_token)
+        end
+
+        { status: response.status, body: response.body.with_indifferent_access }
+      end
+
+    unless response[:status] == 200 && response[:body].is_a?(Hash)
+      raise Project529ClientError, response
+    end
+
+    results =
+      response
+        .dig(:body, :bikes)
+        .map { |attrs| ExternalRegistryBike::Project529Bike.build_from_api_response(attrs) }
+        .compact
+        .each(&:save)
+        .select(&:persisted?)
+    ExternalRegistryBike.where(id: results.map(&:id))
+  rescue Faraday::TimeoutError
+    ExternalRegistryBike.none
+  end
+
+  def credentials
+    @credentials ||= ExternalRegistryCredential::Project529Credential.last
+  end
+
+  class Project529ClientError < StandardError; end
+  class Project529ClientInvalidCredentialsError < Project529ClientError; end
+end
--- app/models/external_registry_credential/project529_credential.rb
+++ app/models/external_registry_credential/project529_credential.rb
@@ -1,30 +1,28 @@
-class ExternalRegistryCredential
-  class Project529Credential < ExternalRegistryCredential
-    BASE_URL = ENV.fetch("PROJECT_529_BASE_URL", "https://project529.com/garage/services/v1")
-    TIMEOUT_SECS = ENV.fetch("EXTERNAL_REGISTRY_REQUEST_TIMEOUT", 5).to_i
+class ExternalRegistryCredential::Project529Credential < ExternalRegistryCredential
+  BASE_URL = ENV.fetch("PROJECT_529_BASE_URL", "https://project529.com/garage/services/v1")
+  TIMEOUT_SECS = ENV.fetch("EXTERNAL_REGISTRY_REQUEST_TIMEOUT", 5).to_i
 
-    validates :type, uniqueness: true
-    validates :app_id, :refresh_token, presence: true
-    validates :app_id, uniqueness: { scope: :type }
+  validates :type, uniqueness: true
+  validates :app_id, :refresh_token, presence: true
+  validates :app_id, uniqueness: { scope: :type }
 
-    def set_access_token
-      return unless access_token_expired?
+  def set_access_token
+    return unless access_token_expired?
 
-      credentials = api&.get_oauth_token
-      expires_at_unix =
-        %i[created_at expires_in]
-          .map { |k| credentials[k] }
-          .sum
+    credentials = api&.get_oauth_token
+    expires_at_unix =
+      %i[created_at expires_in]
+        .map { |k| credentials[k] }
+        .sum
 
-      update(
-        refresh_token: credentials[:refresh_token],
-        access_token: credentials[:access_token],
-        access_token_expires_at: Time.at(expires_at_unix).utc,
-      )
-    end
+    update(
+      refresh_token: credentials[:refresh_token],
+      access_token: credentials[:access_token],
+      access_token_expires_at: Time.at(expires_at_unix).utc,
+    )
+  end
 
-    def api
-      @api ||= ExternalRegistry::Project529Client.new
-    end
+  def api
+    @api ||= ExternalRegistryClient::Project529Client.new
   end
 end
