bc45307ddf9df4d82d8dd83d9d1d5160cf8afa4b
Jake Romer <mail@jakeromer.com>
2019-10-14 10:56:07 -0400

    Add admin management of external_registry_credentials

--- app/models/external_registry_credential/project529_credential.rb
+++ app/models/external_registry_credential/project529_credential.rb
@@ -3,23 +3,14 @@ class ExternalRegistryCredential
     BASE_URL = ENV.fetch("PROJECT_529_BASE_URL", "https://project529.com/garage/services/v1")
     TIMEOUT_SECS = ENV.fetch("EXTERNAL_REGISTRY_REQUEST_TIMEOUT", 5).to_i
 
-    validates :access_token,
-              :access_token_expires_at,
-              :refresh_token,
-              :app_id,
-              presence: true
-
-    attr_accessor :api_client
-
-    def access_token_expired?
-      return true if access_token_expires_at.blank?
-
-      access_token_expires_at < Time.current
-    end
+    validates :type, uniqueness: true
+    validates :app_id, :refresh_token, presence: true
+    validates :app_id, uniqueness: { scope: :type }
 
     def set_access_token
-      credentials = api_client.access_token
+      return unless access_token_expired?
 
+      credentials = api&.get_oauth_token
       expires_at_unix =
         %i[created_at expires_in]
           .map { |k| credentials[k] }
@@ -30,6 +21,10 @@ class ExternalRegistryCredential
         access_token: credentials[:access_token],
         access_token_expires_at: Time.at(expires_at_unix).utc,
       )
+    end
+
+    def api
+      @api ||= ExternalRegistry::Project529Client.new
     end
   end
 end
