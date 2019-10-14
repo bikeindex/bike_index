6cb971ff4ac0778a3a26d0daeace0a4c64afcb6b
Jake Romer <mail@jakeromer.com>
2019-10-14 10:56:09 -0400

    Fix docstring typo, refactor for self-documentation

--- app/models/external_registry_credential.rb
+++ app/models/external_registry_credential.rb
@@ -7,17 +7,22 @@ class ExternalRegistryCredential < ActiveRecord::Base
   # The list of acceptable STI types.
   #
   # Return an array containing each ExternalRegistryCredential subclass name,
-  # stringified and without the namespace prepended.
+  # stringified.
   def self.types
-    %w[
+    subclasses = %w[
       Project529Credential
       StopHelingCredential
-    ].map { |type| [to_s, type].join("::") }
+    ]
+
+    subclasses
+      .map { |subclass_name| [to_s, subclass_name] }
+      .map { |namespace, subclass| "#{namespace}::#{subclass}" }
   end
 
   # The registry name for receiver record. Inferred from the class name.
   def registry_name
-    self.class.to_s.split("::").last.chomp("Credential")
+    credential_classname = self.class.to_s.split("::").last
+    credential_classname.chomp("Credential")
   end
 
   # Return true if the access token expiration is set and the access_token has
