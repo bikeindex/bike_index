fef70a86228606d5d4223e53914e596e9f7d0a78
Jake Romer <mail@jakeromer.com>
2019-10-14 09:47:48 -0400

    Externalize organized/base_controller

--- app/controllers/organized/base_controller.rb
+++ app/controllers/organized/base_controller.rb
@@ -7,39 +7,45 @@ module Organized
     def ensure_member!
       if current_user && current_user.member_of?(current_organization)
         return true if current_user.accepted_vendor_terms_of_service?
-        flash[:success] = "Please accept the terms of service for organizations"
+        flash[:success] = translation(:accept_tos_for_orgs, scope: t_scope(__method__))
         redirect_to accept_vendor_terms_path and return
       end
       set_passive_organization(nil) # remove the active organization, because it failed so don't show it anymore
-      flash[:error] = "You're not a member of that organization!"
+      flash[:error] = translation(:not_a_member_of_that_org, scope: t_scope(__method__))
       redirect_to user_root_url and return
     end
 
     def ensure_admin!
       return true if current_user && current_user.admin_of?(current_organization)
-      flash[:error] = "You have to be an organization administrator to do that!"
+      flash[:error] = translation(:must_be_org_admin, scope: t_scope(__method__))
       redirect_to organization_root_path and return
     end
 
     def ensure_ambassador_authorized!
       if current_organization&.ambassador?
         return true if current_user && current_user.superuser? || current_user.ambassador?
-        flash[:error] = "You have to be an ambassador to do that!"
+        flash[:error] = translation(:must_be_ambassador, scope: t_scope(__method__))
       else
-        flash[:error] = "You have to be in an ambassador organization to see that!"
+        flash[:error] = translation(:must_be_in_ambassador_org, scope: t_scope(__method__))
       end
       redirect_to user_root_url
     end
 
     def ensure_not_ambassador_organization!
       return true unless current_organization&.ambassador?
-      flash[:error] = "You have to be an admin to do that!"
+      flash[:error] = translation(:must_be_an_admin, scope: t_scope(__method__))
       redirect_to organization_root_path
     end
 
     def ensure_current_organization!
       return true if current_organization.present?
       fail ActiveRecord::RecordNotFound
+    end
+
+    private
+
+    def t_scope(method_name)
+      [:controllers, :organized, :base, method_name.to_sym]
     end
   end
 end
