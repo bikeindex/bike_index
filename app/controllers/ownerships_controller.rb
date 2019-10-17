99fe7737f1852af135db841cfc9744da25f6b088
Jake Romer <mail@jakeromer.com>
2019-10-16 20:09:29 -0400

    Inline no_user_flash_msg_translation_args

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,6 +1,6 @@
 class OwnershipsController < ApplicationController
   before_filter :find_ownership
-  before_filter :authenticate_user_and_determine_flash_message
+  before_filter :authenticate_user_and_set_flash
 
   def show
     bike = Bike.unscoped.find(@ownership.bike_id)
@@ -19,23 +19,18 @@ class OwnershipsController < ApplicationController
     end
   end
 
-  # Return the translation key and, optionally, any keyword args to provide to
-  # `authenticate_user`.
-  def no_user_flash_msg_translation_args
-    return [:cannot_find_bike, {}] if @ownership&.bike.blank?
-
-    if @ownership&.user.present?
-      [:owner_already_has_account, { bike_type: @ownership.bike.type }]
-    else
-      [:create_account_to_claim, { bike_type: @ownership.bike.type }]
-    end
-  end
-
   private
 
-  def authenticate_user_and_determine_flash_message
-    key, args = no_user_flash_msg_translation_args
-    authenticate_user(translation_key: key, translation_args: args)
+  def authenticate_user_and_set_flash
+    if @ownership&.bike.blank?
+      authenticate_user(translation_key: :cannot_find_bike)
+    elsif @ownership&.user.present?
+      authenticate_user(translation_key: :owner_already_has_account,
+                        translation_args: { bike_type: @ownership.bike.type })
+    else
+      authenticate_user(translation_key: :create_account_to_claim,
+                        translation_args: { bike_type: @ownership.bike.type })
+    end
   end
 
   def find_ownership
