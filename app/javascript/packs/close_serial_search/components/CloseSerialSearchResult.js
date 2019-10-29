a0a43801f79a08f0c41623e201d81e88f25cc41f
Jake Romer <mail@jakeromer.com>
2019-10-29 16:16:01 -0400

    Rename PartialSerialSearch -> CloseSerialSearch

--- app/javascript/packs/close_serial_search/components/CloseSerialSearchResult.js
+++ app/javascript/packs/close_serial_search/components/CloseSerialSearchResult.js
@@ -4,7 +4,7 @@ import React, { Fragment } from "react";
 
 const t = BikeIndex.translator("bikes_search");
 
-const PartialSerialSearchResult = ({bike}) => (
+const CloseSerialSearchResult = ({bike}) => (
   <li className="bike-box-item">
     <ResultImage bike={bike}/>
 
@@ -90,4 +90,4 @@ const ResultImage = ({ bike }) => {
           </a>);
 }
 
-export default PartialSerialSearchResult;
+export default CloseSerialSearchResult;
