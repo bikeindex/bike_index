eaebef005f746608841aaecdb82c6945e9c96155
Jake Romer <mail@jakeromer.com>
2019-11-07 12:40:41 -0500

    Style "try one now" link as a button

--- app/workers/email_recovered_from_link_worker.rb
+++ app/workers/email_recovered_from_link_worker.rb
@@ -4,10 +4,5 @@ class EmailRecoveredFromLinkWorker < ApplicationWorker
   def perform(stolen_record_id)
     stolen_record = StolenRecord.unscoped.find(stolen_record_id)
     CustomerMailer.recovered_from_link(stolen_record).deliver_now
-
-    promoted_alert = stolen_record.theft_alerts.active.last
-    if promoted_alert.present?
-      AdminMailer.theft_alert_purchased(promoted_alert, notify_of_recovery: true)
-    end
   end
 end
