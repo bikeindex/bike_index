aea168d71c5909e6d5596a4e6ad2f7d9b5c8592b
sethherr <seth.william.herr@gmail.com>
2019-11-19 10:33:45 -0800

    minor adjust for demonstration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"],
            authorize_params: { force_login: true }
   provider :globalid, ENV["GLOBALID_CLIENT_ID"], ENV["GLOBALID_CLIENT_SECRET"],
-    scope: "openid", decrypt_pii_on_login: true, acrc_id: ENV["ACRC_ID"],
+    scope: "openid", decrypt_pii_on_login: true, acrc_id: ENV["ACRC_ID"], provider_ignores_state: true,
     private_key: ENV["GLOBALID_PRIVATE_KEY"], private_key_pass: ENV["GLOBALID_PRIVATE_KEY_PASS"]
 end
