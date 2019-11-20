6750b360d934c428c7b04030133ecf38e602c46e
sethherr <seth.william.herr@gmail.com>
2019-11-20 11:08:07 -0800

    that too

--- app/controllers/organized/dashboard_controller.rb
+++ app/controllers/organized/dashboard_controller.rb
@@ -15,23 +15,10 @@ module Organized
       @child_organizations = current_organization.child_organizations
       if current_organization.regional?
         @nearby_organizations = current_organization.nearby_organizations
+        @affiliated_organization_ids = current_organization.nearby_organizations.pluck(:id) + @child_organizations.pluck(:id)
         @bikes_in_child_organizations_count = Bike.organization(@child_organizations.pluck(:id)).where(created_at: @time_range).count(:all)
         @bikes_in_nearby_organizations_count = Bike.organization(current_organization.nearby_organizations.pluck(:id)).where(created_at: @time_range).count(:all)
         @bikes_in_region_unaffiliated_count = current_organization.bikes_nearby_unorganized.where(created_at: @time_range).count(:all)
-        # @unaffiliated_regional_organizations = current_organization
-        # @regional_bike_counts = {}.tap do |cts|
-        #   cts[:bikes_in_organizations_count] =
-        #     current_organization.bikes_in_nearby_organizations.count(:all)
-        #   cts[:bikes_in_region_unaffiliated_count] =
-        #     current_organization.bikes_nearby_unaffiliated.count(:all)
-        #   cts[:bikes_in_region_count] =
-        #     current_organization.bikes_nearby.count(:all)
-        #   cts[:organizations_nearby] =
-        #     current_organization
-        #       .organizations_nearby
-        #       .includes(:bikes)
-        #       .group("organizations.id, organizations.name")
-        #       .count(:bikes)
       end
     end
 
