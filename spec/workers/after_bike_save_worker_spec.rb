8add3900739195ef292774aaaaa2eb6637da8fb2
sethherr <seth.william.herr@gmail.com>
2019-12-02 13:05:46 -0600

    get test working

--- spec/workers/after_bike_save_worker_spec.rb
+++ spec/workers/after_bike_save_worker_spec.rb
@@ -78,24 +78,17 @@ RSpec.describe AfterBikeSaveWorker, type: :job do
 
   describe "serialized" do
     let!(:bike) { FactoryBot.create(:stolen_bike) }
-    before do
-      require 'webmock/rspec'
-      stub = stub_request(:post, "example.com")
-    end
     it "doesn't call the webhook" do
+      expect_any_instance_of(Faraday::Connection).to_not receive(:post) { true }
       instance.post_bike_to_webhook(instance.serialized(bike))
-      expect(stub).to_not have_been_requested
     end
     context "with webhook url set" do
-    it "calls the things we expect it to call" do
-      ENV["BIKE_WEBHOOK_URL"] = "example.com"
-      ENV["BIKE_WEBHOOK_AUTH_TOKEN"] = "xxxx"
-      instance.post_bike_to_webhook(instance.serialized(bike))
-      expect(stub).to_not have_been_requested
-      # expect(serialized[:auth_token]).to eq "xxxx" # fails on travis :/
-      expect(serialized[:bike][:id]).to be_present
-      expect(serialized[:bike][:stolen_record]).to be_present
-      expect(serialized[:update]).to be_truthy
+      it "calls the things we expect it to call" do
+        ENV["BIKE_WEBHOOK_AUTH_TOKEN"] = "xxxx"
+        stub_const("AfterBikeSaveWorker::POST_URL", "https://example.com")
+        expect_any_instance_of(Faraday::Connection).to receive(:post) { true }
+        instance.post_bike_to_webhook(instance.serialized(bike))
+      end
     end
   end
 
