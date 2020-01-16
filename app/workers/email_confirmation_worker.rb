17d82d5138eeb9d016783018699e7c0d0020ec8a
Seth Herr <seth.william.herr@gmail.com>
2020-01-15 19:53:23 -0800

    Fix confirmation bug

--- app/workers/email_confirmation_worker.rb
+++ app/workers/email_confirmation_worker.rb
@@ -4,7 +4,7 @@ class EmailConfirmationWorker < ApplicationWorker
   def perform(user_id)
     user = User.find(user_id)
     # We need to clean up situations where there are two users created
-    return user.destroy if User.where("id < ?", user_id).present?
+    return user.destroy if User.where(email: user.email).where("id < ?", user_id).present?
     CustomerMailer.confirmation_email(user).deliver_now
   end
 end
