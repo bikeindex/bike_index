66974e3aea7d3c05c77092b4cde9d1fdc8dfcd56
Jake Romer <mail@jakeromer.com>
2020-01-27 09:37:48 -0500

    Fix deprecation: "Method map is deprecated"

--- app/controllers/admin/ambassador_task_assignments_controller.rb
+++ app/controllers/admin/ambassador_task_assignments_controller.rb
@@ -23,6 +23,7 @@ class Admin::AmbassadorTaskAssignmentsController < Admin::BaseController
   def filter_params
     params
       .slice(:search_organization_id, :search_ambassador_task_id, :search_ambassador_id)
+      .to_h
       .map { |k, v| [k[/(?<=search_).+/].to_sym, v] }
       .to_h
   end
