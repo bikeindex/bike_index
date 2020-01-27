8b86e97462049a49e7e880029eeccfe4ac5d68ea
Jake Romer <mail@jakeromer.com>
2020-01-27 09:37:49 -0500

    Update ActionControllerParameters#to_hash shim
    TODO:
    
    - Remove this override
    - Why is `to_hash` still being called in every `redirect_to` call?

--- config/initializers/action_controller_parameters_shim.rb
+++ config/initializers/action_controller_parameters_shim.rb
@@ -1,5 +1,11 @@
 class ActionController::Parameters
+  # DEPRECATION WARNING: #to_hash unexpectedly ignores parameter filtering, and
+  # will change to enforce it in Rails 5.1.
+  # Enable # `raise_on_unfiltered_parameters` to respect parameter filtering, which is the
+  # default in new applications.
+  # For the existing deprecated behaviour, call #to_unsafe_h instead.
+
   def to_hash
-    to_h
+    to_unsafe_h
   end
 end
