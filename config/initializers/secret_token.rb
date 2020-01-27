7f15683ab8f4d07408a5e7d5a544f3b4fc80e4f9
Jake Romer <mail@jakeromer.com>
2020-01-27 09:37:54 -0500

    Disable deprecated secrets.secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,4 @@
-Bikeindex::Application.config.secret_token = ENV["SESSION_SECRET"]
+# DEPRECATION WARNING: `secrets.secret_token` is deprecated in favor of
+# `secret_key_base` and will be removed in Rails 6.0.
+# Bikeindex::Application.config.secret_token = ENV["SESSION_SECRET"]
 Bikeindex::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
