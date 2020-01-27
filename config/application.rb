9076b3b39afd1f7f88859b99647afd934eb31c55
Jake Romer <mail@jakeromer.com>
2020-01-27 09:37:55 -0500

    Update defaults to 5.2

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 module Bikeindex
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Use our custom error pages
     config.exceptions_app = self.routes
