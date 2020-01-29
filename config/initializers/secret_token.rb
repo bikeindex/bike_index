ad32c1953f8d6ab25ad7cec9f13aa0ad2fc6a4cc
Jake Romer <mail@jakeromer.com>
2020-01-29 12:18:07 -0500

    Update secret_token.rb to use app secrets

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,1 @@
-# DEPRECATION WARNING: `secrets.secret_token` is deprecated in favor of
-# `secret_key_base` and will be removed in Rails 6.0.
-# Bikeindex::Application.config.secret_token = ENV["SESSION_SECRET"]
-Bikeindex::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
+Bikeindex::Application.config.secret_key_base = Rails.application.secrets[:secret_key_base]
