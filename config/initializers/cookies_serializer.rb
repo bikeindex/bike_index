f7dccc20d09efeeeeac8d4a7db3cf194d0c20244
Jake Romer <mail@jakeromer.com>
2020-01-30 10:05:09 -0500

    Set action_dispatch.cookies_serializer to hybrid

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -2,4 +2,7 @@
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
-Rails.application.config.action_dispatch.cookies_serializer = :json
+
+# Set to hybrid because it was set to hybrid prior to 5.1
+# See: https://github.com/rails/rails/issues/33580
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
