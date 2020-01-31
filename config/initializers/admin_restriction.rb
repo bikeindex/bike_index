33aed274af9fb14bb526d1963b78034292aed552
Jake Romer <mail@jakeromer.com>
2020-01-30 20:17:10 -0500

    Use hybrid cookie serializer

--- config/initializers/admin_restriction.rb
+++ config/initializers/admin_restriction.rb
@@ -1,8 +1,12 @@
 class AdminRestriction
   def self.matches?(req)
-    return false unless req.cookies["auth"].present?
+    cookie = req.cookies["auth"]
+    return false unless cookie.present?
 
-    auth = Rack::Session::Cookie::Base64::Marshal.new.decode(req.cookies["auth"])
+    auth =
+      Rack::Session::Cookie::Base64::JSON.new.decode(cookie) ||
+        Rack::Session::Cookie::Base64::Marshal.new.decode(cookie)
+
     user = User.from_auth(auth)
 
     user&.superuser?
