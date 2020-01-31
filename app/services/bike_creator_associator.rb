52b53e2d2107c68bec8be499c331781a8d344eba
sethherr <seth.william.herr@gmail.com>
2020-01-31 15:53:06 -0800

    fix silly broken spec

--- app/services/bike_creator_associator.rb
+++ app/services/bike_creator_associator.rb
@@ -73,7 +73,7 @@ class BikeCreatorAssociator
       create_normalized_serial_segments(bike)
       assign_user_attributes(bike, ownership&.user)
       create_stolen_record(bike) if bike.stolen
-      create_abandoned_record(@b_param, bike) if @b_param.state_abandoned?
+      create_abandoned_record(@b_param, bike) if @b_param&.state_abandoned?
       attach_photo(bike)
       attach_photos(bike)
       add_other_listings(bike)
