b3c9a0c04171f2f3361a8c8e672f1e03cae37d19
sethherr <seth.william.herr@gmail.com>
2020-03-30 10:37:28 -0700

    add additional tests

--- spec/serializers/ctypes_serializer_spec.rb
+++ spec/serializers/ctypes_serializer_spec.rb
@@ -1,10 +1,14 @@
 require "rails_helper"
 
-RSpec.describe CtypeSerializer do
-  let(:ctype) { FactoryBot.create(:ctype, has_multiple: true) }
-  subject { CtypeSerializer.new(ctype) }
+RSpec.describe CtypeSerializer, type: :lib do
+  let(:subject) { described_class }
+  let(:obj) { FactoryGirl.create(:ctype, has_multiple: true) }
+  let(:serializer) { subject.new(obj, root: false) }
 
-  it { expect(subject.name).to eq(ctype.name) }
-  it { expect(subject.slug).to eq(ctype.slug) }
-  it { expect(subject.has_multiple).to be_truthy }
+  it "is as expected" do
+    expect(serializer.name).to eq(ctype.name)
+    expect(serializer.slug).to eq(ctype.slug)
+    expect(serializer.has_multiple).to be_truthy
+    expect(serializer.as_json.is_a?(Hash)).to be_truthy
+  end
 end
--- spec/serializers/organized_message_serializer_spec.rb
+++ spec/serializers/organized_message_serializer_spec.rb
@@ -2,10 +2,14 @@ require "rails_helper"
 
 RSpec.describe OrganizedMessageSerializer, type: :lib do
   let(:subject) { described_class }
-  let(:obj) { FactoryBot.create(:organization_message) }
+  let(:obj) { FactoryGirl.create(:organization_message) }
   let(:serializer) { subject.new(obj, root: false) }
 
-  it "works" do
-    expect(serializer.as_json.is_a?(Hash)).to be_truthy
+  describe "caching" do
+    include_context :caching_basic
+    it "is cached" do
+      # expect(serializer.perform_caching).to be_truthy
+      expect(serializer.as_json.is_a?(Hash)).to be_truthy
+    end
   end
 end
