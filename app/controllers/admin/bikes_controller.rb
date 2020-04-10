7b65171b7de3fb3304675de1d887d55e30505fe3
sethherr <seth.william.herr@gmail.com>
2020-04-10 10:30:58 -0700

    whatever again

--- app/controllers/admin/bikes_controller.rb
+++ app/controllers/admin/bikes_controller.rb
@@ -6,7 +6,6 @@ class Admin::BikesController < Admin::BaseController
   def index
     @page = params[:page] || 1
     per_page = params[:per_page] || 100
-    @render_chart = ParamsNormalizer.boolean(params[:render_chart])
     @bikes = available_bikes.includes(:creation_organization, :creation_states, :paint)
                             .reorder("bikes.#{sort_column} #{sort_direction}")
                             .page(@page).per(per_page)
