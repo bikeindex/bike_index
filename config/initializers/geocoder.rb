2b354eb91e495dbbc8a7924c193061a955c9bf2d
sethherr <seth.william.herr@gmail.com>
2020-04-24 08:57:20 -0700

    switch to mapbox for geocoding

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,9 +1,9 @@
 unless Rails.env.test?
   Geocoder.configure(
     cache: Redis.new,
-    lookup: :google,
+    lookup: :mapbox,
     use_https: true,
-    api_key: ENV["GOOGLE_GEOCODER"],
+    api_key: ENV["MAPBOX_GEOCODER"],
     ip_lookup: :maxmind,
     maxmind: { service: :city, api_key: ENV["MAXMIND_KEY"] },
   )
