6b38167caf8a11871d2a232bc487f3245630b516
Seth Herr <seth.william.herr@gmail.com>
2020-04-28 11:27:25 -0700

    Skip tests that require secure variables on CircleCI (#1575)

--- spec/models/external_registry_client/verloren_of_gevonden_client_spec.rb
+++ spec/models/external_registry_client/verloren_of_gevonden_client_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe ExternalRegistryClient::VerlorenOfGevondenClient, type: :model do
+  RUN_EXTERNAL_VERLOREN_TESTS = ENV["VERLOREN_OF_GEVONDEN_BASE_URL"] != "test"
+
   before do
     null_cache = ActiveSupport::Cache.lookup_store(:null_store)
     allow(Rails).to receive(:cache).and_return(null_cache)
@@ -12,7 +14,7 @@ RSpec.describe ExternalRegistryClient::VerlorenOfGevondenClient, type: :model do
 
   describe "#search" do
     context "given a valid query" do
-      it "returns search results" do
+      it "returns search results", if: RUN_EXTERNAL_VERLOREN_TESTS do
         VCR.use_cassette("external_registry/verlorenofgevonden_2722_with_results") do
           results = described_class.new.search(2722)
 
@@ -28,7 +30,7 @@ RSpec.describe ExternalRegistryClient::VerlorenOfGevondenClient, type: :model do
     end
 
     context "given no results" do
-      it "returns an empty collection" do
+      it "returns an empty collection", if: RUN_EXTERNAL_VERLOREN_TESTS do
         VCR.use_cassette("external_registry/verlorenofgevonden_13949483_no_results") do
           results = described_class.new.search(13949483)
           expect(results).to be_an(Array)
@@ -38,7 +40,7 @@ RSpec.describe ExternalRegistryClient::VerlorenOfGevondenClient, type: :model do
     end
 
     context "given an invalid query" do
-      it "returns an empty collection" do
+      it "returns an empty collection", if: RUN_EXTERNAL_VERLOREN_TESTS do
         results = described_class.new.search("")
         expect(results).to be_an(Array)
         expect(results).to be_empty
