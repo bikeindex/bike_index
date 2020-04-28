3a7cae6e98ff1377b638c53c0e8e98bb3ad70f1a
sethherr <seth.william.herr@gmail.com>
2020-04-28 11:02:24 -0700

    add conditions for running, make them all fail to verify

--- spec/models/external_registry_client/stop_heling_client_spec.rb
+++ spec/models/external_registry_client/stop_heling_client_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe ExternalRegistryClient::StopHelingClient, type: :model do
+  RUN_EXTERNAL_STOP_HELING_TESTS = ENV["STOP_HELING_BASE_URL"] != "test"
+
   before do
     null_cache = ActiveSupport::Cache.lookup_store(:null_store)
     allow(Rails).to receive(:cache).and_return(null_cache)
@@ -13,28 +15,31 @@ RSpec.describe ExternalRegistryClient::StopHelingClient, type: :model do
 
   describe "#search" do
     context "given no matching results" do
-      it "returns an empty array" do
+      it "returns an empty array", if: RUN_EXTERNAL_STOP_HELING_TESTS do
         client = build_client
         results = client.search("nothing")
         expect(results).to be_empty
+        fail
       end
     end
 
     context "given matching results but no bikes" do
-      it "returns an empty array" do
+      it "returns an empty array", if: RUN_EXTERNAL_STOP_HELING_TESTS do
         client = build_client(results: [non_bike_result])
         results = client.search("28484")
         expect(results).to be_empty
+        fail
       end
     end
 
     context "given matching results but no bikes" do
-      it "returns an array of unpersisted ExternalRegistryBikes" do
+      it "returns an array of unpersisted ExternalRegistryBikes", if: RUN_EXTERNAL_STOP_HELING_TESTS do
         client = build_client(results: [bike_result])
         results = client.search("28484")
         expect(results).to_not be_empty
         expect(results).to all(be_an_instance_of(ExternalRegistryBike::StopHelingBike))
         expect(results.none?(&:persisted?)).to eq(true)
+        fail
       end
     end
   end
--- spec/models/external_registry_client/verloren_of_gevonden_client_spec.rb
+++ spec/models/external_registry_client/verloren_of_gevonden_client_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe ExternalRegistryClient::VerlorenOfGevondenClient, type: :model do
+  RUN_EXTERNAL_VERLOREN_TESTS = ENV["VERLOREN_OF_GEVONDEN_BASE_URL"] != "test"
+
   before do
     null_cache = ActiveSupport::Cache.lookup_store(:null_store)
     allow(Rails).to receive(:cache).and_return(null_cache)
@@ -12,7 +14,7 @@ RSpec.describe ExternalRegistryClient::VerlorenOfGevondenClient, type: :model do
 
   describe "#search" do
     context "given a valid query" do
-      it "returns search results" do
+      it "returns search results", if: RUN_EXTERNAL_VERLOREN_TESTS do
         VCR.use_cassette("external_registry/verlorenofgevonden_2722_with_results") do
           results = described_class.new.search(2722)
 
@@ -23,25 +25,28 @@ RSpec.describe ExternalRegistryClient::VerlorenOfGevondenClient, type: :model do
           expect(results.map(&:description)).to all(match(/fiets/))
           expect(results.first.external_id).to match(/F\d+\w*-.+/)
           expect(results.first.serial_number).to be_present
+          fail
         end
       end
     end
 
     context "given no results" do
-      it "returns an empty collection" do
+      it "returns an empty collection", if: RUN_EXTERNAL_VERLOREN_TESTS do
         VCR.use_cassette("external_registry/verlorenofgevonden_13949483_no_results") do
           results = described_class.new.search(13949483)
           expect(results).to be_an(Array)
           expect(results).to be_empty
+          fail
         end
       end
     end
 
     context "given an invalid query" do
-      it "returns an empty collection" do
+      it "returns an empty collection", if: RUN_EXTERNAL_VERLOREN_TESTS do
         results = described_class.new.search("")
         expect(results).to be_an(Array)
         expect(results).to be_empty
+        fail
       end
     end
   end
