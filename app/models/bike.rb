9ac27c40395af6fedcfd3b8a3cd4e4014dd118ad
sethherr <seth.william.herr@gmail.com>
2020-04-28 17:23:57 -0700

    watever

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -772,9 +772,9 @@ class Bike < ApplicationRecord
   def calculated_status
     # NOTE: If it's impounded, status is still unregistered_parking_notification
     return "unregistered_parking_notification" if status == "unregistered_parking_notification"
+    return "status_impounded" if current_impound_record.present?
     return "status_abandoned" if abandoned? || parking_notifications.active.appears_abandoned_notification.any?
     return "status_stolen" if stolen
-    return "status_impounded" if current_impound_record.present?
 
     "status_with_owner"
   end
