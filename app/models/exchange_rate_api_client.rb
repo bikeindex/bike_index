d1f7e6399bd56ae8099b2af2bec646e83bd34855
Jake Romer <mail@jakeromer.com>
2020-05-17 06:47:17 -0400

    Cache api response

--- app/models/exchange_rate_api_client.rb
+++ app/models/exchange_rate_api_client.rb
@@ -1,25 +1,28 @@
 # frozen_string_literal: true
 
 class ExchangeRateApiClient
-  attr_accessor :base_iso, :base_url
+  attr_accessor :base_iso, :base_url, :cache_key
 
   BASE_URL = ENV.fetch("EXCHANGE_RATE_API_BASE_URL", "https://api.exchangeratesapi.io")
 
   def initialize(base_iso="USD")
     self.base_iso = base_iso
     self.base_url = BASE_URL
+    self.cache_key = ["exchange_rate", Date.current].join("::")
   end
 
   def latest
-    resp = conn.get("latest") do |req|
-      req.params = {"base" => base_iso}
-    end
+    Rails.cache.fetch(cache_key, expires_in: 24.hours) do
+      resp = conn.get("latest") do |req|
+        req.params = {"base" => base_iso}
+      end
 
-    unless resp.status == 200 && resp.body.is_a?(Hash)
-      raise ExchangeRateApiError, resp.body
-    end
+      unless resp.status == 200 && resp.body.is_a?(Hash)
+        raise ExchangeRateApiError, resp.body
+      end
 
-    resp.body.with_indifferent_access
+      resp.body.with_indifferent_access
+    end
   end
 
   private
