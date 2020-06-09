a486ee057fc2de59a972f483ed262d76d4d9bacb
sethherr <seth.william.herr@gmail.com>
2020-06-09 14:58:19 -0700

    wip

--- app/models/hot_sheet_configuration.rb
+++ app/models/hot_sheet_configuration.rb
@@ -2,6 +2,7 @@ class HotSheetConfiguration < ApplicationRecord
   belongs_to :organization
 
   validates_presence_of :organization_id, :send_seconds_past_midnight, :search_radius_miles
+  validate :ensure_location_if_enabled
 
   before_validation :set_default_attributes
 
@@ -12,8 +13,16 @@ class HotSheetConfiguration < ApplicationRecord
   def bounding_box; Geocoder::Calculations.bounding_box(search_coordinates, search_radius_miles) end
 
   def set_default_attributes
-    self.search_radius_miles ||= organization&.search_radius
+    unless search_radius_miles.present? && search_radius_miles > 0
+      self.search_radius_miles = (organization&.search_radius || 50)
+    end
     self.send_seconds_past_midnight ||= 360
     # Need to enable setting these more comfortably: :send_seconds_past_midnight :timezone_str
+  end
+
+  def ensure_location_if_enabled
+    return true unless enabled
+    return true if search_coordinates.reject(&:blank?).count == 2
+    errors.add(:base, "Organization must have a location set to enable Hot Sheets")
   end
 end
