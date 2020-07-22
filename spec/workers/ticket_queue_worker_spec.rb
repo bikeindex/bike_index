67db3252082b88208f2d084f5094f42bec2f702e
sethherr <seth.william.herr@gmail.com>
2020-07-22 16:16:31 -0700

    wip

--- spec/workers/ticket_queue_worker_spec.rb
+++ spec/workers/ticket_queue_worker_spec.rb
@@ -52,45 +52,30 @@ RSpec.describe TicketQueueWorker, type: :job do
           expect(location.tickets.paging_or_on_deck.pluck(:number)).to eq([102, 103])
         end
       end
+      context "ticket number passed is being_helped" do
+        let(:ticket2) { tickets.line_ordered[1] }
+        let!(:appointment) { ticket2.claim(email: "stuff@example.com") }
+        it "behaves as if the next number was passed" do
+          ticket2.reload
+          expect(ticket2.number).to eq 101
+          expect(ticket2.status).to eq "waiting"
+          expect(appointment.status).to eq "waiting"
+          expect do
+            appointment.record_status_update(new_status: "being_helped")
+          end.to change(described_class.jobs, :count).by 1
+          ticket2.reload
+          expect(ticket2.status).to eq "being_helped"
+          expect do
+            instance.perform(location.id, ticket2.number)
+          end.to change(Ticket, :count).by 4
+          ticket1.reload
+          expect(ticket1.status).to eq "removed"
+          ticket2.reload
+          expect(ticket2.status).to eq "being_helped"
+          expect(location.tickets.in_line.pluck(:number)).to eq([103, 104, 105, 106, 107])
+          expect(location.tickets.paging_or_on_deck.pluck(:number)).to eq([103, 104])
+        end
+      end
     end
-    # it "marks the next tickets as in line" do
-    #   Ticket.create_tickets(10, initial_number: 101, organization: organization) # Tests that it pulls the default location
-    #   organization.reload
-    #   expect(Appointment.count).to eq 0
-    #   expect(organization.tickets.count).to eq 10
-    #   expect(organization.tickets.unused.count).to eq 10
-    #   ticket1 = organization.tickets.number_ordered.first
-    #   ticket2 = organization.tickets.friendly_find(102)
-    #   expect(ticket1.number).to eq 101
-    #   instance.perform(ticket2.id)
-    #   ticket1.reload
-    #   expect(ticket1.status).to eq "unused"
-    #   ticket2.reload
-    #   expect(ticket2.status).to eq "waiting"
-    #   expect(organization.tickets.unused.count).to eq 5
-    #   expect(organization.tickets.in_line.count).to eq 5
-    #   expect(Appointment.count).to eq 2
-    #   expect(Appointment.where(creator_kind: "queue_worker").count).to eq 2
-    #   ticket3 = organization.tickets.friendly_find(103)
-    #   instance.perform(ticket3.id)
-    #   ticket2.reload
-    #   expect(ticket2.in_line?).to be_truthy
-    #   expect(organization.tickets.unused.count).to eq 4
-    #   expect(organization.tickets.in_line.count).to eq 6
-    #   expect(Appointment.in_line.count).to eq 6
-    #   # If passed resolve_earlier_tickets, it marks them resolved
-    #   instance.perform(ticket3.id, true)
-    #   ticket1.reload
-    #   expect(ticket1.status).to eq "unused"
-    #   ticket2.reload
-    #   expect(ticket2.status).to eq "resolved"
-    #   ticket3.reload
-    #   expect(ticket3.status).to eq "waiting"
-    #   expect(organization.tickets.unused.count).to eq 4
-    #   expect(organization.tickets.in_line.count).to eq 5
-    #   expect(organization.tickets.resolved.count).to eq 1
-    #   expect(Appointment.in_line.count).to eq 2
-    #   expect(Appointment.removed.count).to eq 1
-    # end
   end
 end
