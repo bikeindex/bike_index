14910cc7b7c60b721afa928c6bb3941b5590c7a4
sethherr <seth.william.herr@gmail.com>
2020-08-13 15:49:22 -0700

    actual state spec fix

--- spec/models/bike_spec.rb
+++ spec/models/bike_spec.rb
@@ -1097,7 +1097,8 @@ RSpec.describe Bike, type: :model do
         context "with address_set_manually" do
           let(:target) { {street: "1313 N Milwaukee Ave", city: "Chicago", state: "IL", zipcode: "66666", country: "US", latitude: 43.9, longitude: -88.7} }
           it "returns address set" do
-            bike.update(target.merge(address_set_manually: true, state: state))
+            FactoryBot.create(:state, name: "Illinois", abbreviation: "IL", country: Country.united_states)
+            bike.update(target.merge(address_set_manually: true))
             bike.reload
             expect(bike.b_params.pluck(:id)).to match_array([b_param2.id, b_param.id])
             expect(bike.registration_address).to eq target.as_json
