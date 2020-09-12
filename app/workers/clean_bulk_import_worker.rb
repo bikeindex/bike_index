6c3a3fcc6f221aa791d9e2ef8ad136c5449e1ceb
Seth Herr <seth.william.herr@gmail.com>
2020-09-11 18:08:50 -0700

    Bulk Import file cleaned updates (#1759)

--- app/workers/clean_bulk_import_worker.rb
+++ app/workers/clean_bulk_import_worker.rb
@@ -12,13 +12,14 @@ class CleanBulkImportWorker < ScheduledWorker
   def perform(import_id = nil)
     return enqueue_workers unless import_id.present?
     bulk_import = BulkImport.find(import_id)
+    return true if bulk_import.file_cleaned
     bulk_import.file_cleaned = true
     bulk_import.file&.remove!
     bulk_import.save
   end
 
   def enqueue_workers
-    BulkImport.ascend.where("created_at < ?", self.class.clean_before)
+    BulkImport.ascend.where(file_cleaned: false).where("created_at < ?", self.class.clean_before)
       .limit(1_000)
       .pluck(:id).each { |id| self.class.perform_async(id) }
   end
