277d26865d49f45e96138de15be000a8e252133a
Seth Herr <seth.william.herr@gmail.com>
2020-10-13 15:57:50 -0700

    Add token to ownership (#1788)

--- app/services/security_tokenizer.rb
+++ app/services/security_tokenizer.rb
@@ -2,18 +2,23 @@ class SecurityTokenizer
   EARLIEST_TOKEN_TIME = 1427848192 # 2015-03-31T17:29:52
 
   def self.new_token(time = nil)
-    t = (time || Time.current).to_i
+    t = (time.blank? ? Time.current : time).to_i
     "#{t}-" + SecureRandom.hex + Digest::MD5.hexdigest("#{SecureRandom.hex}-#{t}")
   end
 
   # Passwords have to be less than 72 characters. Lazy hack
   def self.new_password_token(time = nil)
-    new_token(time).slice(12, 50)
+    new_token(time).slice(0, 60)
+  end
+
+  # Because of texting length concerns, use an abbreviated token
+  def self.new_short_token
+    new_token.split("-")[1].slice(2, 21)
   end
 
   def self.token_time(str)
-    t = str.to_s.split("-")[0]
-    t = t.present? && t.to_i > EARLIEST_TOKEN_TIME ? t.to_i : EARLIEST_TOKEN_TIME
+    t, toke = str.to_s.split("-")
+    t = t.present? && toke.present? && t.to_i > EARLIEST_TOKEN_TIME ? t.to_i : EARLIEST_TOKEN_TIME
     Time.at(t)
   end
 end
---
+++ spec/services/security_tokenizer_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe SecurityTokenizer do
+  describe "new_token" do
+    let(:token) { SecurityTokenizer.new_token }
+    it "is long" do
+      expect(token.length).to be > 50
+      expect(token).to match("-")
+      expect(SecurityTokenizer.token_time(token)).to be_within(1).of Time.current
+    end
+  end
+
+  describe "new_password_token" do
+    let(:token) { SecurityTokenizer.new_password_token }
+    it "is slightly shorter" do
+      expect(token.length).to be < 72 # Max password length
+      expect(token).to match("-")
+      expect(SecurityTokenizer.token_time(token)).to be > (Time.current - 2)
+    end
+  end
+
+  describe "new_short_token" do
+    let(:token) { SecurityTokenizer.new_short_token }
+    it "is short" do
+      expect(token.length).to_not match("-")
+      expect(SecurityTokenizer.token_time(token)).to eq Time.at(SecurityTokenizer::EARLIEST_TOKEN_TIME)
+      expect(token.length).to be < 30
+    end
+  end
+end
