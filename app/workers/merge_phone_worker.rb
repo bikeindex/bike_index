f6efc74d716d598af4af0642f1fda4901956b019
sethherr <seth.william.herr@gmail.com>
2020-10-13 15:27:44 -0700

    merge user phone

---
+++ app/workers/merge_phone_worker.rb
@@ -0,0 +1,17 @@
+class MergePhoneWorker < ApplicationWorker
+  sidekiq_options queue: "high_priority"
+
+  def perform(user_phone_id)
+    user_phone = UserPhone.find(user_phone_id)
+    return true unless user_phone.confirmed?
+
+    # Add the bikes to the user
+    Bike.where(is_phone: true, owner_email: user_phone.phone).each do |bike|
+      bike.current_ownership.create_user_registration_for_phone_registration!(user_phone.user)
+    end
+
+    # Manually run after user change to add a general alert to the user
+    # (rather than spinning up a new worker)
+    AfterUserChangeWorker.new.perform(user_phone.user_id, user_phone.user)
+  end
+end
