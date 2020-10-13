e7effa5a2d16d09f0fb2ad12ad7884f401312962
sethherr <seth.william.herr@gmail.com>
2020-10-13 10:43:43 -0700

    migration

--- db/migrate/20201009210429_add_is_phone_to_bikes.rb
+++ db/migrate/20201009210429_add_is_phone_to_bikes.rb
@@ -1,4 +1,4 @@
-class AddPhoneToBikes < ActiveRecord::Migration[5.2]
+class AddIsPhoneToBikes < ActiveRecord::Migration[5.2]
   def change
     add_column :bikes, :is_phone, :boolean, default: false
     add_column :ownerships, :is_phone, :boolean, default: false
