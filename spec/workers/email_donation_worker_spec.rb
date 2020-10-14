a145b1000c48e500af59c4ded1bcc3f65d52e8ec
sethherr <seth.william.herr@gmail.com>
2020-10-14 13:53:13 -0700

    wip

--- spec/workers/email_donation_worker_spec.rb
+++ spec/workers/email_donation_worker_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe EmailDonationWorker, type: :job do
     payment.reload
     expect(payment.notifications.count).to eq 1
   end
+
+  context "not a donation" do
+    it "does not send"
+  end
+
+  context "donation_second" do
+    it "sends a donation_second message"
+  end
+
+  context "donation_stolen" do
+    it "sends a donation_stolen message"
+  end
+
+  context "donation_recovered" do
+    it "sends a donation_recovered message"
+  end
+
+  context "donation_theft_alert" do
+    it "sends a donation_theft_alert message"
+  end
 end
