6689116881fc4aeb9d451f2eee095fce7ea4ec9c
sethherr <seth.william.herr@gmail.com>
2020-10-19 14:08:25 -0700

    f

--- app/workers/previous_ownership_migration_worker.rb
+++ app/workers/previous_ownership_migration_worker.rb
@@ -1,6 +1,6 @@
 class PreviousOwnershipMigrationWorker < ApplicationWorker
   def perform(bike_id)
-    bike = Bike.where(id: bike_id).first
+    bike = Bike.unscoped.find_by_id(bike_id)
     return true unless bike.present? && bike.ownerships.count > 1
     ownerships.each { |o| o.update(updated_at: Time.current) }
   end
