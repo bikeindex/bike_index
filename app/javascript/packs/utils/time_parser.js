c7fd66dfb329308006443c291b956e041c026014
sethherr <seth.william.herr@gmail.com>
2020-11-02 09:37:26 -0800

    more functional time parser

--- app/javascript/packs/utils/time_parser.js
+++ app/javascript/packs/utils/time_parser.js
@@ -1,10 +1,25 @@
 import log from "../utils/log";
 import moment from "moment-timezone";
 
-// Should be imported and initialized like this:
+// TimeParser updates all HTML elements with class '.convertTime', making them:
+// - Human readable
+// - Display time in current timezone
+// - With context relevant data (e.g. today shows hour, last month just date)
+// - Gives the element a title of the precise time with seconds (so hovering shows it)
+// - If elements have classes '.preciseTime' or '.preciseTimeSeconds', includes extra specificity in output
+// - If elements have class '.withPreposition' it includes preposition (to make time fit better in a sentence)
+// - Requires elements have HTML content of a time string (e.g. a unix timestamp)
+//
+// Imported and initialized like this:
 // if (!window.timeParser) { window.timeParser = new TimeParser() }
-
-// And then you can add this to the react component:
+//
+// To update an individual element with a time in it (doesn't require element to have class '.convertTime'):
+// timeParser.writeTime(el)
+//
+// To get just the text that is output into the element:
+// timeFromText("1604337131")
+//
+// You can add this to a react component:
 // componentDidUpdate() { window.timeParser.localize() }
 
 export default class TimeParser {
@@ -33,7 +48,7 @@ export default class TimeParser {
     }
   }
 
-  localizedDateText(time, classList) {
+  localizedDateText(time, preciseTime, includeSeconds, includePreposition) {
     let prefix = "";
     // If we're dealing with yesterday or today (not the future)
     if (time < this.tomorrowEnd && time > this.yesterdayStart) {
@@ -45,28 +60,20 @@ export default class TimeParser {
       }
       return (
         prefix +
-        this.hourFormat(
-          time,
-          "h:mm",
-          classList.contains("preciseTimeSeconds"),
-          classList.contains("withPreposition")
-        )
+        this.hourFormat(time, "h:mm", includeSeconds, includePreposition)
       );
     }
-    if (classList.contains("withPreposition")) {
+    if (includePreposition) {
       prefix = "on ";
     }
 
     // If it's preciseTime (or preciseTimeSeconds), always show the hours and mins
-    if (
-      classList.contains("preciseTime") ||
-      classList.contains("preciseTimeSeconds")
-    ) {
+    if (preciseTime || includeSeconds) {
       let hourStr = this.hourFormat(
         time,
         "h:mm",
-        classList.contains("preciseTimeSeconds"),
-        classList.contains("withPreposition")
+        includeSeconds,
+        includePreposition
       );
       // Include the year if it isn't the current year
       if (time.year() === moment().year()) {
@@ -77,7 +84,7 @@ export default class TimeParser {
     }
     // Otherwise, format in basic format
     if (time.year() === moment().year()) {
-      if (classList.contains("withPreposition")) {
+      if (includePreposition) {
         return prefix + time.format("MMM Do");
       } else {
         return prefix + time.format("MMM Do");
@@ -95,29 +102,53 @@ export default class TimeParser {
     el.value = this.localTimezone;
   }
 
-  writeUpdatedTimeText(el) {
-    let text = el.textContent.trim();
-    let time = null;
+  parse(text) {
+    // If time is only a number, parse as a timestamp
+    // Otherwise, parse as ISO_8601 which is the default time string
+    if (/^\d+$/.test(text)) {
+      return moment.unix(text);
+    } else if (text !== null) {
+      return moment(text, moment.ISO_8601);
+    }
+    if (text === null || !time.isValid()) {
+      return null;
+    }
+  }
+
+  // timeFromText() is not used, included to make getting the text output separately easier
+  timeFromText(
+    text,
+    preciseTime = false,
+    includeSeconds = false,
+    includePreposition = false
+  ) {
+    return this.localizedDateText(
+      this.parse(text),
+      preciseTime,
+      includeSeconds,
+      includePreposition
+    );
+  }
+
+  writeTime(el) {
+    const text = el.textContent.trim();
+    const time = this.parse(text);
     // So running this again doesn't reapply to this element
     el.classList.remove("convertTime");
     // So we know which were updated (for styling, future updates, etc)
     el.classList.add("convertedTime");
 
-    if (!(text.length > 0)) {
-      return;
-    }
-    // If time is only a number, parse as a timestamp
-    // Otherwise, parse as ISO_8601 which is the default time string
-    if (/^\d+$/.test(text)) {
-      time = moment.unix(text);
-    } else {
-      time = moment(text, moment.ISO_8601);
-    }
-    if (time === null || !time.isValid()) {
+    // If we couldn't parse the time, exit
+    if (!(text.length > 0) || time === null) {
       return;
     }
 
-    el.innerHTML = this.localizedDateText(time, el.classList);
+    el.innerHTML = this.localizedDateText(
+      time,
+      el.classList.contains("preciseTime"),
+      el.classList.contains("preciseTimeSeconds"),
+      el.classList.contains("withPreposition")
+    );
     el.setAttribute("title", this.preciseTimeSeconds(time));
   }
 
@@ -129,7 +160,7 @@ export default class TimeParser {
   localize() {
     // Write times
     Array.from(document.getElementsByClassName("convertTime")).forEach((el) =>
-      this.writeUpdatedTimeText(el)
+      this.writeTime(el)
     );
 
     // Write timezones
