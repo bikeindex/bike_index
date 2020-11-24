7dff62541866be4917c57773fe53f7dfe9397e41
Seth Herr <seth.william.herr@gmail.com>
2020-11-24 11:17:44 -0800

    Graduated notification mark_remaining! undeletes associated bike_organizations (#1826)

--- app/models/graduated_notification.rb
+++ app/models/graduated_notification.rb
@@ -23,8 +23,8 @@ class GraduatedNotification < ApplicationRecord
   scope :processed, -> { where(status: processed_statuses) }
   scope :unprocessed, -> { where(status: unprocessed_statuses) }
   scope :not_marked_remaining, -> { where.not(status: "marked_remaining") }
-  scope :primary_notifications, -> { where("primary_notification_id = id") }
-  scope :secondary_notifications, -> { where.not("primary_notification_id  = id") }
+  scope :primary_notification, -> { where("primary_notification_id = id") }
+  scope :secondary_notification, -> { where.not("primary_notification_id  = id") }
   scope :email_success, -> { where(delivery_status: "email_success") }
 
   def self.statuses
@@ -174,7 +174,7 @@ class GraduatedNotification < ApplicationRecord
     # We don't want to re-mark remaining
     update(updated_at: Time.current)
     bike_organization.update(deleted_at: nil)
-    associated_notifications.each { |n| n.update(updated_at: Time.current, skip_update: true, marked_remaining_at: marked_remaining_at) }
+    associated_notifications.each { |n| n.mark_remaining!(resolved_at: marked_remaining_at) } if primary_notification?
     # Long shot - but update any graduated notifications that might have been missed, just in case
     organization.graduated_notifications.where(bike_id: bike_id).active.each do |pre_notification|
       if bike_organization.created_at.present? && pre_notification.bike_organization.created_at.present?
