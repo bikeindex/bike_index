25045c0b821f0ea1021db7b3f6ba035d7acb17b9
sethherr <seth.william.herr@gmail.com>
2020-12-17 13:31:20 -0800

    🧹

--- app/models/impound_record.rb
+++ app/models/impound_record.rb
@@ -6,7 +6,6 @@ class ImpoundRecord < ApplicationRecord
 
   has_one :parking_notification
   has_many :impound_record_updates
-  has_many :impound_claims
 
   validates_presence_of :bike_id, :user_id
   validates_uniqueness_of :bike_id, if: :current?, conditions: -> { current }
