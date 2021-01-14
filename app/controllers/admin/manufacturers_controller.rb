160db7e6948b79695e890cb80a79ea4597d70b86
Seth Herr <seth.william.herr@gmail.com>
2021-01-13 19:44:12 -0800

    Add twitter to manufacturers (#1864)

--- app/controllers/admin/manufacturers_controller.rb
+++ app/controllers/admin/manufacturers_controller.rb
@@ -66,7 +66,8 @@ class Admin::ManufacturersController < Admin::BaseController
   end
 
   def permitted_parameters
-    params.require(:manufacturer).permit(:name, :slug, :website, :frame_maker, :total_years_active, :notes, :open_year, :close_year, :logo, :description, :logo_source)
+    params.require(:manufacturer).permit(:name, :slug, :website, :frame_maker, :total_years_active, :notes,
+      :open_year, :close_year, :logo, :description, :logo_source, :twitter_name)
   end
 
   def find_manufacturer
