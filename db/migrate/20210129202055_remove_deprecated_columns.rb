d126709c695c28e951a2db09d9b22ce9889b0cbe
sethherr <seth.william.herr@gmail.com>
2021-02-04 10:39:14 -0800

    more columns to remove

--- db/migrate/20210129202055_remove_deprecated_columns.rb
+++ db/migrate/20210129202055_remove_deprecated_columns.rb
@@ -6,5 +6,11 @@ class RemoveDeprecatedColumns < ActiveRecord::Migration[5.2]
     remove_column :bike, :is_abandoned, :boolean
     # Remove this column, it's unused and unnecessary
     remove_column :stolen_records, :time, :text
+
+    # Parking Notifications are now tracked through creation_states
+    remove_column :bikes, :created_by_parking_notification, :boolean
+
+    # impound_configuration tracks public_impound_bikes
+    remove_column :organizations, :public_impound_bikes, :boolean
   end
 end
