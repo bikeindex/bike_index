6b83b8b382bfae716f61997abb9392e2ed0f8fb6
sethherr <seth.william.herr@gmail.com>
2021-02-08 10:57:49 -0800

    bulk imports request specs

--- spec/requests/admin/bulk_imports_request_spec.rb
+++ spec/requests/admin/bulk_imports_request_spec.rb
@@ -1,15 +1,13 @@
 require "rails_helper"
 
-RSpec.describe Admin::BulkImportsController, type: :controller do
-  let(:user) { FactoryBot.create(:admin) }
-  before do
-    set_current_user(user)
-  end
+RSpec.describe Admin::BulkImportsController, type: :request do
+  let(:base_url) { "/admin/bulk_imports/" }
+  include_context :request_spec_logged_in_as_superuser
 
   describe "index" do
     let!(:bulk_import) { FactoryBot.create(:bulk_import) }
     it "renders" do
-      get :index
+      get base_url
       expect(response).to be_ok
       expect(response).to render_template(:index)
       expect(flash).to_not be_present
@@ -19,7 +17,7 @@ RSpec.describe Admin::BulkImportsController, type: :controller do
   describe "show" do
     let!(:bulk_import) { FactoryBot.create(:bulk_import) }
     it "renders" do
-      get :show, params: {id: bulk_import.id}
+      get "#{base_url}/#{bulk_import.id}"
       expect(response).to be_ok
       expect(response).to render_template(:show)
       expect(flash).to_not be_present
@@ -28,7 +26,7 @@ RSpec.describe Admin::BulkImportsController, type: :controller do
 
   describe "new" do
     it "renders" do
-      get :new
+      get "#{base_url}/new"
       expect(response).to be_ok
       expect(response).to render_template(:new)
       expect(flash).to_not be_present
@@ -39,7 +37,7 @@ RSpec.describe Admin::BulkImportsController, type: :controller do
     context "passed params" do
       let(:organization) { FactoryBot.create(:organization) }
       it "includes them" do
-        get :new, params: {organization_id: organization.slug, no_notify: 1}
+        get "#{base_url}/new?organization_id=#{organization.slug}&no_notify=1"
         expect(response).to be_ok
         expect(response).to render_template(:new)
         expect(flash).to_not be_present
@@ -57,11 +55,11 @@ RSpec.describe Admin::BulkImportsController, type: :controller do
       let(:valid_attrs) { {file: file, organization_id: organization.id, no_notify: "1"} }
       it "creates" do
         expect {
-          post :create, params: {bulk_import: valid_attrs}
+          post base_url, params: {bulk_import: valid_attrs}
         }.to change(BulkImport, :count).by 1
 
         bulk_import = BulkImport.last
-        expect(bulk_import.user).to eq user
+        expect(bulk_import.user).to eq current_user
         expect(bulk_import.file_url).to be_present
         expect(bulk_import.progress).to eq "pending"
         expect(bulk_import.organization).to eq organization
@@ -76,7 +74,7 @@ RSpec.describe Admin::BulkImportsController, type: :controller do
     it "reenqueues" do
       Sidekiq::Worker.clear_all
       expect {
-        put :update, params: {id: bulk_import.id, reprocess: true}
+        patch "#{base_url}/#{bulk_import.id}", params: {reprocess: true}
       }.to change(BulkImportWorker.jobs, :count).by 1
       expect(flash[:success]).to be_present
       expect(BulkImportWorker.jobs.map { |j| j["args"] }.flatten).to eq([bulk_import.id])
