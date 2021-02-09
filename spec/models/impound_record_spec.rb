e20d9e0bf1311543dfe88d0f846e6ada0e7ceaec
sethherr <seth.william.herr@gmail.com>
2021-02-09 15:45:35 -0800

    spec fix

--- spec/models/impound_record_spec.rb
+++ spec/models/impound_record_spec.rb
@@ -391,7 +391,7 @@ RSpec.describe ImpoundRecord, type: :model do
       it "is truthy if bike creation state is status_impounded" do
         expect(CreationState.where(bike_id: bike.id).count).to eq 1
         expect(bike.creation_state.status).to eq "status_impounded"
-        expect(bike.created_by_notification_or_impounding?).to be_falsey
+        expect(bike.created_by_notification_or_impounding?).to be_truthy
         expect(impound_record.reload.send("calculated_unregistered_bike?")).to be_truthy
       end
       context "impound record earlier?" do
