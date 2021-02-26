b4f4437405d79151c739164d2fd2b9ff0819d960
sethherr <seth.william.herr@gmail.com>
2021-02-26 13:43:00 -0800

    add controller

--- app/controllers/theft_ring_listings_controller.rb
+++ app/controllers/theft_ring_listings_controller.rb
@@ -1,13 +1,28 @@
 class TheftRingListingsController < ApplicationController
+  include SortableTable
+
   def index
-    @manufacturers = Manufacturer.all
-    respond_to do |format|
-      format.html
-      format.csv { render plain: @manufacturers.to_csv }
-    end
+    page = params[:page] || 1
+    per_page = params[:per_page] || 25
+    @theft_ring_listings = matching_theft_ring_listings
+      .reorder("theft_ring_listings.#{sort_column} #{sort_direction}")
+      .page(page).per(per_page)
+    @selected_query_items_options = TheftRingListing.selected_query_items_options(@interpreted_params)
   end
 
-  def tsv
-    redirect_to "https://files.bikeindex.org/uploads/tsvs/manufacturers.tsv"
+  private
+
+  def sortable_columns
+    %w[listed_at amount_cents mnfg_name]
+  end
+
+  def permitted_search_params
+    params.permit(*TheftRingListing.permitted_search_params)
+  end
+
+  def matching_theft_ring_listings
+    # This might become more sophisticated someday...
+    theft_ring_listings = TheftRingListing
+    theft_ring_listings.search(@interpreted_params)
   end
 end
