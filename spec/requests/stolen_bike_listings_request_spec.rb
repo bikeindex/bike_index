af4e38986e786acccf992abbff6598ac1be8e7d5
sethherr <seth.william.herr@gmail.com>
2021-03-02 13:41:02 -0800

    theft-ring to stolen bike listings

--- spec/requests/stolen_bike_listings_request_spec.rb
+++ spec/requests/stolen_bike_listings_request_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe StolenBikeListingsController, type: :request do
+  describe "theft-ring" do
+    it "redirects to stolen_bike_listings" do
+      get "/theft-ring"
+      expect(response.status).to eq(200)
+      expect(response).to render_template(:index)
+    end
+  end
   describe "index" do
     it "renders the index template with revised_layout" do
       get "/stolen_bike_listings"
