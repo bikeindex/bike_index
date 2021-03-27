9353b97b134f5ec42bf84d04936d0c289d87222a
sethherr <seth.william.herr@gmail.com>
2021-03-26 17:50:50 -0700

    fix mimemagic dependency?

--- spec/requests/stolen_bike_listings_request_spec.rb
+++ spec/requests/stolen_bike_listings_request_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe StolenBikeListingsController, type: :request do
       expect(response.status).to eq(200)
       expect(response).to render_template(:index)
       expect(assigns(:blog)&.id).to be_blank
+      expect(assigns(:render_info)).to be_truthy
       blog = FactoryBot.create(:blog, title: "Theft rings")
       blog.update_column :id, Blog.theft_rings_id
       blog.reload
@@ -15,6 +16,7 @@ RSpec.describe StolenBikeListingsController, type: :request do
       expect(response.status).to eq(200)
       expect(response).to render_template(:index)
       expect(assigns(:blog)&.id).to eq blog.id
+      expect(assigns(:render_info)).to be_truthy
       get "/theft-ring"
       expect(response).to redirect_to("/theft-rings")
     end
@@ -28,6 +30,7 @@ RSpec.describe StolenBikeListingsController, type: :request do
       get "/stolen_bike_listings"
       expect(response.status).to eq(200)
       expect(response).to render_template(:index)
+      expect(assigns(:render_info)).to be_truthy
       expect(assigns(:stolen_bike_listings).pluck(:id)).to match_array([stolen_bike_listing1.id, stolen_bike_listing2.id])
       # And test that search works
       get "/stolen_bike_listings?query_items%5B%5D=#{Color.black.search_id}"
@@ -36,6 +39,7 @@ RSpec.describe StolenBikeListingsController, type: :request do
       expect(assigns(:stolen_bike_listings).pluck(:id)).to eq([stolen_bike_listing2.id])
       get "/stolen_bike_listings?query_items%5B%5D=#{color.search_id}&query_items%5B%5D=#{Color.black.search_id}"
       expect(assigns(:stolen_bike_listings).pluck(:id)).to eq([stolen_bike_listing2.id])
+      expect(assigns(:render_info)).to be_falsey
     end
   end
 end
