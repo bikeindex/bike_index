49d5aa92f57f4da316b425dccd77b6080d8ceeb2
sethherr <seth.william.herr@gmail.com>
2021-04-21 15:21:00 -0700

    wip

--- app/controllers/review_impound_claims_controller.rb
+++ app/controllers/review_impound_claims_controller.rb
@@ -5,66 +5,10 @@ class ReviewImpoundClaimsController < ApplicationController
   def show
   end
 
-  def create
-    @impound_claim = ImpoundClaim.new(permitted_create_params)
-    impound_record = @impound_claim.impound_record
-    stolen_record = @impound_claim.stolen_record
-    bike_type = stolen_record&.bike&.type || impound_record&.bike&.type || "bike"
-    impound_type = "#{impound_record&.kind || ImpoundRecord.impounded_kind} #{bike_type}"
-    errors = []
-    if @impound_claim.impound_record.blank?
-      errors << "Unable to find that #{impound_type}"
-    elsif !impound_record.active?
-      errors << "That #{impound_type} record has been marked '#{impound_record.status_humanized}' and cannot be claimed"
-    end
-    if stolen_record.blank?
-      errors << "Unable to find that bike"
-    elsif stolen_record.user != current_user
-      errors << "It doesn't look like you own the #{bike_type} you're claiming matches the #{impound_type}"
-    end
-    if errors.blank?
-      @impound_claim.save
-      if @impound_claim.valid?
-        flash[:success] = "Claim opened, please add information"
-      else
-        errors = @impound_claim.errors.full_messages
-      end
-    end
-    flash[:error] = errors.to_sentence if errors.any?
-    # Avoid issues with bike_claimed assignment by accessing through impound_record
-    redirect_to bike_path(@impound_claim.impound_record.bike, contact_owner: 1)
-  end
-
   def update
-    if @impound_claim.update(permitted_update_params)
-      flash[:success] = if @impound_claim.submitting?
-        "Claim submitted!"
-      else
-        "Claim saved"
-      end
-    else
-      flash[:error] = "Unable to save: #{@impound_claim.errors.full_messages.to_sentence}"
-    end
-    redirect_back(fallback_location: bike_path(@impound_claim.bike_claimed, contact_owner: 1))
   end
 
   protected
-
-  def permitted_create_params
-    params.require(:impound_claim).permit(:impound_record_id, :stolen_record_id)
-      .merge(user_id: current_user.id, status: "pending")
-  end
-
-  def permitted_update_params
-    permitted_statuses = %w[pending submitting canceled]
-    update_params = params.require(:impound_claim).permit(:message, :status)
-    update_params[:status] = "pending" unless permitted_statuses.include?(update_params[:status])
-    update_params
-  end
-
-  def user_impound_records
-    current_user.impound_records
-  end
 
   def find_impound_claim
     impound_claim = ImpoundClaim.find(params[:id])
@@ -73,8 +17,11 @@ class ReviewImpoundClaimsController < ApplicationController
       redirect_to organization_impound_claim_path(params[:id], organization_id: organization_id)
       return
     end
-
-    @impound_claim = ImpoundClaim.where(impound_record_id: user_impound_records.pluck(:id))
-        .find(params[:id])
+    @impound_record = impound_claim.impound_record
+    if @impound_record.authorized?(current_user)
+      @impound_claim = impound_claim
+    else
+      raise ActiveRecord::RecordNotFound
+    end
   end
 end
--- spec/requests/review_impound_claims_request_spec.rb
+++ spec/requests/review_impound_claims_request_spec.rb
@@ -5,13 +5,15 @@ RSpec.describe ReviewImpoundClaimsController, type: :request do
   include_context :request_spec_logged_in_as_user_if_present
   let(:impound_record) { FactoryBot.create(:impound_record, user: current_user, bike: bike_claimed) }
   let(:bike_claimed) { FactoryBot.create(:bike, :with_ownership_claimed, user: current_user) }
-  let(:impound_claim) { FactoryBot.create(:impound_claim, :with_stolen_record, impound_record: impound_record) }
+  let(:impound_claim) { FactoryBot.create(:impound_claim, :with_stolen_record, status: status, impound_record: impound_record) }
   let(:bike_submitting) { impound_claim.bike_submitting }
+  let(:status) { "submitting" }
 
   describe "show" do
     it "renders" do
       impound_claim.reload
       expect(impound_record.reload.organized?).to be_falsey
+      expect(impound_record.authorized?(current_user)).to be_truthy
       expect(bike_claimed.reload.status).to eq "status_impounded"
       expect(bike_submitting)
       expect(current_user.authorized?(bike_claimed)).to be_truthy
@@ -20,6 +22,21 @@ RSpec.describe ReviewImpoundClaimsController, type: :request do
       expect(response.status).to eq(200)
       expect(response).to render_template(:show)
       expect(assigns(:impound_claim)).to eq impound_claim
+    end
+    context "not users impound_record" do
+      let(:impound_record) { FactoryBot.create(:impound_record) }
+      it "raises" do
+        expect(impound_record.reload.authorized?(current_user)).to be_falsey
+        expect {
+          get "#{base_url}/#{impound_claim.to_param}"
+        }.to raise_error(ActiveRecord::RecordNotFound)
+        # unless user is a superuser
+        current_user.update(superuser: true)
+        get "#{base_url}/#{impound_claim.to_param}"
+        expect(response.status).to eq(200)
+        expect(response).to render_template(:show)
+        expect(assigns(:impound_claim)).to eq impound_claim
+      end
     end
     context "organized impound_record" do
       let(:impound_record) { FactoryBot.create(:impound_record_with_organization, user: current_user) }
