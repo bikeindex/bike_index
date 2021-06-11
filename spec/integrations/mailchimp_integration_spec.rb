41c5704d2d3b5c5ec75a61f229c89edeed755a32
sethherr <seth.william.herr@gmail.com>
2021-06-11 14:16:24 -0700

    wip

--- spec/integrations/mailchimp_integration_spec.rb
+++ spec/integrations/mailchimp_integration_spec.rb
@@ -5,10 +5,7 @@ RSpec.describe MailchimpIntegration do
 
   describe "get_lists" do
     let(:target) do
-      [{name: "Police Departments", id: "0eb9428151"},
-       {name: "Donors", id: "180a1141a4"},
-       {name: "Universities", id: "564456d501"},
-       {name: "Recovered bike owners", id: "692e788302"},
+       [{name: "Individuals", id: "180a1141a4"},
        {name: "Organizations", id: "b675299293"}]
     end
     it "gets the lists" do
