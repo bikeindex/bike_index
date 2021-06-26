f1d3b699f878e8b3814e1308db4a298c0bc32c1a
sethherr <seth.william.herr@gmail.com>
2021-06-26 14:54:10 -0700

    improve comment in bike_stickers factory

--- spec/factories/bike_stickers.rb
+++ spec/factories/bike_stickers.rb
@@ -9,7 +9,8 @@ FactoryBot.define do
       end
       after(:create) do |bike_sticker, evaluator|
         # Have to set these things or previous_bike_id doesn't work correctly,
-        # ... because transient doesn't actually work
+        # ... because transient doesn't skip assigning the attributes if they exist.
+        # NOTE: passing bike_id or user_id (rather than bike/user) doesn't work correctly
         bike_claiming = evaluator.bike || bike_sticker.bike
         user_claiming = evaluator.user || bike_sticker.user
         bike_sticker.bike_id = nil
