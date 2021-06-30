ee92797fe6c2f5af3423c7123f81bc4c2f67d513
sethherr <seth.william.herr@gmail.com>
2021-06-30 15:49:06 -0700

    user alerts, display that

--- app/controllers/user_alerts_controller.rb
+++ app/controllers/user_alerts_controller.rb
@@ -2,14 +2,16 @@ class UserAlertsController < ApplicationController
   before_action :authenticate_user_for_user_alerts_controller
 
   def update
-    user_alert = current_user.user_alerts.find_by_id(params[:id])
-    if user_alert.present?
+    @user_alert = current_user.user_alerts.find_by_id(params[:id])
+    if @user_alert.present?
       if params[:alert_action] == "dismiss"
-        if user_alert.dismissable?
-          user_alert.dismiss! if user_alert.active?
+        if @user_alert.dismissable?
+          @user_alert.dismiss! if @user_alert.active?
         else
           flash[:error] = "We're sorry, you can't hide that alert"
         end
+      elsif ParamsNormalizer.boolean(params[:add_bike_organization])
+        add_bike_organization
       else
         flash[:error] = "Unknown alert action!"
       end
@@ -23,5 +25,17 @@ class UserAlertsController < ApplicationController
 
   def authenticate_user_for_user_alerts_controller
     authenticate_user(translation_key: :you_have_to_log_in, flash_type: :info)
+  end
+
+  # Probably only should be called for "unassigned_bike_org"
+  def add_bike_organization
+    if @user_alert.user.authorized?(@user_alert.bike)
+      bike_organization = BikeOrganization.find_or_create_by(bike_id: @user_alert.bike_id,
+        organization_id: @user_alert.organization_id)
+      @user_alert.resolve! if bike_organization.valid?
+      AfterUserChangeWorker.perform_async(@user_alert.user_id)
+    else
+      flash[:error] = "You don't have permission to edit that bike"
+    end
   end
 end
--- spec/requests/user_alerts_request_spec.rb
+++ spec/requests/user_alerts_request_spec.rb
@@ -2,7 +2,11 @@ require "rails_helper"
 
 base_url = "/user_alerts"
 RSpec.describe UserAlertsController, type: :request do
-  let(:user_alert) { FactoryBot.create(:user_alert, kind: "unassigned_bike_org", bike: FactoryBot.create(:bike)) }
+  let(:organization_id) { nil } # Ignore, because we don't generally need it
+  let(:bike) { FactoryBot.create(:bike) }
+  let(:alert_user) { FactoryBot.create(:user_confirmed) }
+  let(:kind) { "unassigned_bike_org" }
+  let(:user_alert) { FactoryBot.create(:user_alert, user: alert_user, kind: kind, bike: bike, organization_id: organization_id) }
 
   describe "update" do
     before { host! "bikeindex.org" } # I do not know why the host is getting fucked with in here :(
@@ -28,7 +32,7 @@ RSpec.describe UserAlertsController, type: :request do
           expect(user_alert.reload.dismissed?).to be_falsey
         end
         context "user's alert" do
-          let(:current_user) { user_alert.user }
+          let(:alert_user) { current_user }
           it "dismisses" do
             expect(user_alert.dismissable?).to be_truthy
             expect(user_alert.user_id).to eq current_user.id
@@ -40,8 +44,9 @@ RSpec.describe UserAlertsController, type: :request do
             dismissed_time = Time.current - 1.day
             user_alert.update(dismissed_at: dismissed_time)
             # And dismiss without failing again
-            patch "#{base_url}/#{user_alert.id}", params: { alert_action: "dismiss" }
-            expect(response).to redirect_to "/my_account"
+            patch "#{base_url}/#{user_alert.id}", params: { alert_action: "dismiss" },
+              headers: { "HTTP_REFERER" => "http://bikeindex.org/bikes" }
+            expect(response).to redirect_to "/bikes"
             expect(flash).to be_blank
             expect(user_alert.reload.dismissed_at).to be_within(1).of dismissed_time
             # And unknown action errors
@@ -63,7 +68,7 @@ RSpec.describe UserAlertsController, type: :request do
             end
           end
           context "not dismissable" do
-            let(:user_alert) { FactoryBot.create(:user_alert) }
+            let(:kind) { "phone_waiting_confirmation" }
             it "flash errors" do
               expect(user_alert.reload.dismissable?).to be_falsey
               expect(user_alert.dismissed?).to be_falsey
@@ -75,6 +80,40 @@ RSpec.describe UserAlertsController, type: :request do
             end
           end
         end
+      end
+    end
+    describe "add_bike_organization" do
+      include_context :request_spec_logged_in_as_user
+      let(:alert_user) { current_user }
+      let(:organization) { FactoryBot.create(:organization) }
+      let(:bike) { FactoryBot.create(:bike, :with_ownership, user: current_user) }
+      let(:organization_id) { organization.id }
+      it "adds an organization" do
+        user_alert.reload
+        expect(user_alert.organization_id).to eq organization.id
+        bike.reload
+        expect(bike.claimed?).to be_falsey
+        expect(current_user.authorized?(bike)).to be_truthy
+        expect(bike.organizations.pluck(:id)).to eq([])
+        expect(bike.editable_organizations.pluck(:id)).to eq([])
+        expect(current_user.alert_slugs).to eq([])
+        AfterUserChangeWorker.new.perform(current_user.id)
+        user_alert.reload
+        current_user.reload
+        expect(current_user.alert_slugs).to eq(["unassigned_bike_org"])
+        Sidekiq::Worker.clear_all
+        patch "#{base_url}/#{user_alert.to_param}", params: {add_bike_organization: "true"},
+          headers: { "HTTP_REFERER" => "http://bikeindex.org/my_account" }
+        expect(AfterUserChangeWorker.jobs.count).to eq 1
+        expect(response).to redirect_to "/my_account"
+        expect(flash).to be_blank
+        bike.reload
+        expect(bike.claimed?).to be_falsey
+        expect(bike.organizations.pluck(:id)).to match_array([organization.id])
+        expect(bike.editable_organizations.pluck(:id)).to match_array([organization.id])
+
+        user_alert.reload
+        expect(user_alert.resolved?).to be_truthy
       end
     end
   end
