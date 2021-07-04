93b19f15884db82f1c462c53a119806ab650ffee
sethherr <seth.william.herr@gmail.com>
2021-07-04 13:06:20 -0700

    wip

--- spec/integrations/facebook_ads_integration_spec.rb
+++ spec/integrations/facebook_ads_integration_spec.rb
@@ -5,13 +5,17 @@ RSpec.describe FacebookAdsIntegration do
 
   it "gets account" do
     VCR.use_cassette("facebook_ads_integration-get_account", match_requests_on: [:path]) do
-      # pp instance.account.name
+      expect(instance.account.name).to eq "Bike Index"
     end
   end
 
-  it "gets campaigns" do
-    VCR.use_cassette("facebook_ads_integration-get_campaigns", match_requests_on: [:path]) do
-      # pp instance.account
+  describe "get_campaign" do
+    let(:campaign_id) { "6250389631214" }
+    it "get_campaign" do
+      VCR.use_cassette("facebook_ads_integration-get_campaign", match_requests_on: [:path]) do
+        campaign = instance.get_campaign(campaign_id)
+        pp campaign, campaign.objective
+      end
     end
   end
 end
