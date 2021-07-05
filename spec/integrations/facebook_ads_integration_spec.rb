f2a66a9b3fc10505d3e975c49384bdb4f7e5b990
sethherr <seth.william.herr@gmail.com>
2021-07-04 18:42:16 -0700

    add stuff

--- spec/integrations/facebook_ads_integration_spec.rb
+++ spec/integrations/facebook_ads_integration_spec.rb
@@ -3,39 +3,41 @@ require "rails_helper"
 RSpec.describe Facebook::AdsIntegration do
   let(:instance) { described_class.new }
 
-  it "gets account" do
-    VCR.use_cassette("facebook_ads_integration-get_account", match_requests_on: [:path]) do
-      expect(instance.account.name).to eq "Bike Index"
-    end
-  end
-
-  describe "get_campaign" do
-    let(:campaign_id) { "6250389631214" }
-    it "get_campaign" do
-      VCR.use_cassette("facebook_ads_integration-get_campaign", match_requests_on: [:path]) do
-        campaign = instance.get_campaign(campaign_id)
-        pp campaign, campaign.objective
-        pp campaign.special_ad_categories
-        pp campaign.ads.first.creative
+  if Facebook::AdsIntegration::TOKEN.present?
+    it "gets account" do
+      VCR.use_cassette("facebook/ads_integration-get_account", match_requests_on: [:path]) do
+        expect(instance.account.name).to eq "Bike Index"
       end
     end
-  end
 
-  describe "create_campaign" do
-    let(:theft_alert) { TheftAlert.new(id: 12) }
-    it "creates a campaign" do
-      VCR.use_cassette("facebook_ads_integration-create_campaign", match_requests_on: [:path]) do
-        campaign = instance.create_campaign(theft_alert)
-        expect(campaign).to be_present
+    describe "get_campaign" do
+      let(:campaign_id) { "6250389631214" }
+      it "get_campaign" do
+        VCR.use_cassette("facebook/ads_integration-get_campaign", match_requests_on: [:path]) do
+          campaign = instance.get_campaign(campaign_id)
+          pp campaign, campaign.objective
+          pp campaign.special_ad_categories
+          pp campaign.ads.first.creative
+        end
       end
     end
-  end
 
-  describe "create_adset" do
-    let(:theft_alert) { TheftAlert.new(id: 12, facebook_data: {campaign_id: "6250389631214"}) }
-    xit "creates an ad" do
-      VCR.use_cassette("facebook_ads_integration-create_adset", match_requests_on: [:path]) do
-        ad = instance.create_ad(theft_alert)
+    describe "create_campaign" do
+      let(:theft_alert) { TheftAlert.new(id: 12) }
+      it "creates a campaign" do
+        VCR.use_cassette("facebook/ads_integration-create_campaign", match_requests_on: [:path]) do
+          campaign = instance.create_campaign(theft_alert)
+          expect(campaign).to be_present
+        end
+      end
+    end
+
+    describe "create_adset" do
+      let(:theft_alert) { TheftAlert.new(id: 12, facebook_data: {campaign_id: "6250389631214"}) }
+      xit "creates an ad" do
+        VCR.use_cassette("facebook/ads_integration-create_adset", match_requests_on: [:path]) do
+          ad = instance.create_ad(theft_alert)
+        end
       end
     end
   end
