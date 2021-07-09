8f817a25fb4f69bb311a7b0161bcd1ff4970855f
sethherr <seth.william.herr@gmail.com>
2021-07-08 18:31:10 -0700

    fixxxxx

--- app/workers/update_theft_alert_facebook_worker.rb
+++ app/workers/update_theft_alert_facebook_worker.rb
@@ -6,7 +6,7 @@ class UpdateTheftAlertFacebookWorker < ApplicationWorker
     return false unless theft_alert.facebook_data&.dig("ad_id").present?
     Facebook::AdsIntegration.new.update_facebook_data(theft_alert)
     return true unless theft_alert.notify? &&
-      theft_alert.notifications.theft_alert_posted.none
+      theft_alert.notifications.theft_alert_posted.none?
 
     # Perform inline rather than re-querying for objects
     EmailTheftAlertNotificationWorker.new.perform(theft_alert_id, "theft_alert_posted", theft_alert)
