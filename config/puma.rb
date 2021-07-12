045078fad1d821a2041e9c6002519bc65f60f765
sethherr <seth.william.herr@gmail.com>
2021-07-12 10:15:51 -0700

    another update

--- config/puma.rb
+++ config/puma.rb
@@ -1,33 +1,30 @@
 # Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers a minimum and maximum.
+# The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum, this matches the default thread size of Active Record.
-
-# Running puma with 4,16 threads and 3 workers
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
-threads threads_count, threads_count
-
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
+# and maximum; this matches the default thread size of Active Record.
 #
-port ENV.fetch("PORT", 3000)
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV", "development")
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
 
 # Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
+# Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
-# incompatible with zero downtime restarts, see: https://help.cloud66.com/rails/how-to-guides/rack-servers/puma-rack-server.html
-# Discourse uses it though
-
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
 preload_app!
 
-# `on_worker_boot` is no longer needed after Rails 5.2
+# Set the directory to Cloud 66 specific environment variable so that puma can follow symlinks to new code on redeployment
+#
+directory ENV.fetch("STACK_PATH") { "." }
+# Make sure to bind to Cloud 66 specific socket so that NGINX can direct traffic here
+#
