99a798bbe1baa9f2befbd4b464b05fabd5df294a
sethherr <seth.william.herr@gmail.com>
2021-07-12 09:58:29 -0700

    update puma configuration

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,8 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
-#
+
+# Running puma with 4,16 threads and 3 workers
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
 threads threads_count, threads_count
 
@@ -21,27 +22,12 @@ environment ENV.fetch("RAILS_ENV", "development")
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory. If you use this option
-# you need to make sure to reconnect any threads in the `on_worker_boot`
-# block.
-#
-# preload_app!
+# incompatible with zero downtime restarts, see: https://help.cloud66.com/rails/how-to-guides/rack-servers/puma-rack-server.html
+# Discourse uses it though
 
-# The code in the `on_worker_boot` will be called if you are using
-# clustered mode by specifying a number of `workers`. After each worker
-# process is booted this block will be run, if you are using `preload_app!`
-# option you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, Ruby
-# cannot share connections between processes.
-#
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-# end
+preload_app!
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# `on_worker_boot` is no longer needed after Rails 5.2
