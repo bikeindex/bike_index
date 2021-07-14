fea12c27a62ef86e6032cfdc28a914ab3bd04acb
sethherr <seth.william.herr@gmail.com>
2021-07-14 15:43:48 -0500

    translated

--- app/workers/customer_contact_notification_create_worker.rb
+++ app/workers/customer_contact_notification_create_worker.rb
@@ -4,10 +4,10 @@ class CustomerContactNotificationCreateWorker < ApplicationWorker
   def perform(customer_contact_id)
     customer_contact = CustomerContact.find_by_id(customer_contact_id)
     return true if customer_contact.blank? || customer_contact.notification.present?
-    notification = Notification.create(notifiable: customer_contact,
-      delivery_status: "email_success",
-      user_id: customer_contact.user_id,
-      bike_id: customer_contact.bike_id,
-      kind: customer_contact.kind)
+    Notification.create(notifiable: customer_contact,
+                        delivery_status: "email_success",
+                        user_id: customer_contact.user_id,
+                        bike_id: customer_contact.bike_id,
+                        kind: customer_contact.kind)
   end
 end
