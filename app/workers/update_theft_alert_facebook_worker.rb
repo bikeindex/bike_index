4e2bec86e47489fd3499eb4470d8de2b739ee793
sethherr <seth.william.herr@gmail.com>
2021-08-04 15:05:54 -0700

    wip

--- app/workers/update_theft_alert_facebook_worker.rb
+++ app/workers/update_theft_alert_facebook_worker.rb
@@ -15,15 +15,11 @@ class UpdateTheftAlertFacebookWorker < ScheduledWorker
       return ActivateTheftAlertWorker.perform_async(theft_alert_id)
     end
     Facebook::AdsIntegration.new.update_facebook_data(theft_alert)
+
     return true unless theft_alert.notify? &&
       theft_alert.notifications.theft_alert_posted.none?
-
-    # Temporary pause on notification - set no notify so we don't notify when we turn notifications back on
-    unless theft_alert.facebook_data&.dig("no_notify")
-      theft_alert.update(facebook_data: theft_alert.facebook_data.merge(no_notify: true))
-    end
     # Perform inline rather than re-querying for objects
-    # EmailTheftAlertNotificationWorker.new.perform(theft_alert_id, "theft_alert_posted", theft_alert)
+    EmailTheftAlertNotificationWorker.new.perform(theft_alert_id, "theft_alert_posted", theft_alert)
   end
 
   def enqueue_workers
