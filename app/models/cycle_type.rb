8c207c1d1c4669288515a975c133d6ecac0c2378
sethherr <seth.william.herr@gmail.com>
2021-09-02 16:34:24 -0700

    add e-scooters as a cycle type

--- app/models/cycle_type.rb
+++ app/models/cycle_type.rb
@@ -18,7 +18,9 @@ class CycleType
     'cargo-trike-rear': 13,
     'trail-behind': 14,
     'pedi-cab': 15,
-    personal_mobility: 16
+    e_scooter: 16,
+    e_skateboard: 17,
+    personal_mobility: 18
   }.freeze
 
   NAMES = {
@@ -38,7 +40,9 @@ class CycleType
     "cargo-trike-rear": "Cargo Tricycle (rear storage)",
     "trail-behind": "Trail behind (half bike)",
     "pedi-cab": "Pedi Cab (rickshaw)",
-    personal_mobility: "Personal mobility device (electric scooter, skateboard, unicycle, etc)"
+    "e-scooter": "E-Scooter",
+    "e-skateboard": "E-skateboard",
+    personal_mobility: "Personal mobility device (electric unicycle, etc)"
   }.freeze
 
   def initialize(slug)
