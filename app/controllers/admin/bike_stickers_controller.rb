062784eb487dfe2d0286a0e3ea023059093fbedf
Seth Herr <seth.william.herr@gmail.com>
2021-10-20 16:56:09 -1000

    Admin Sticker creation (#2082)

--- app/controllers/admin/bike_stickers_controller.rb
+++ app/controllers/admin/bike_stickers_controller.rb
@@ -1,6 +1,5 @@
 class Admin::BikeStickersController < Admin::BaseController
   include SortableTable
-
   before_action :set_period, only: [:index]
 
   def index
@@ -12,6 +11,36 @@ class Admin::BikeStickersController < Admin::BaseController
         .includes(:organization)
         .page(page)
         .per(per_page)
+    @bike_sticker_batches = if @bike_sticker_batch.present?
+      [@bike_sticker_batch]
+    elsif @matching_batches
+      BikeStickerBatch.where(id: @bike_stickers.reorder(:bike_sticker_batch_id).distinct.pluck(:bike_sticker_batch_id))
+        .reorder(id: :desc)
+    else
+      @all_batches = ParamsNormalizer.boolean(params[:search_all_batches])
+      batches = BikeStickerBatch.reorder(id: :desc)
+      @all_batches ? batches : batches.limit(5)
+    end
+  end
+
+  def new
+    @bike_sticker_batch ||= BikeStickerBatch.new
+    @bike_sticker_batch.organization ||= current_organization
+    @bike_sticker_batch.initial_code_integer ||= 1
+    @organizations = Organization.all
+  end
+
+  def create
+    create_batch_if_valid
+    if @bike_sticker_batch.id.present?
+      flash[:success] = "Batch ##{@bike_sticker_batch.id} created. Please wait a few minutes for the stickers to finish creating"
+      CreateBikeStickerCodesWorker.perform_async(@bike_sticker_batch.id,
+        @bike_sticker_batch.stickers_to_create_count, @bike_sticker_batch.initial_code_integer)
+      redirect_to admin_bike_stickers_path(search_bike_sticker_batch_id: @bike_sticker_batch.id)
+    else
+      @organizations = Organization.all
+      render :new
+    end
   end
 
   helper_method :matching_bike_stickers
@@ -26,6 +55,7 @@ class Admin::BikeStickersController < Admin::BaseController
     return @matching_bike_stickers if defined?(@matching_bike_stickers)
     bike_stickers = BikeSticker.all
     if current_organization.present?
+      @matching_batches = true
       bike_stickers = bike_stickers.where(organization_id: current_organization.id)
     end
     if params[:search_bike_sticker_batch_id].present?
@@ -34,13 +64,45 @@ class Admin::BikeStickersController < Admin::BaseController
     end
     if params[:search_claimed].present? || sort_column == "claimed_at"
       @search_claimed = true
+      @matching_batches = true
       bike_stickers = bike_stickers.claimed
     end
     if params[:search_query].present?
+      @matching_batches = true
       bike_stickers = bike_stickers.admin_text_search(params[:search_query])
     end
     @time_range_column = sort_column if %w[created_at updated_at claimed_at].include?(sort_column)
     @time_range_column ||= "created_at"
     @matching_bike_stickers = bike_stickers.where(@time_range_column => @time_range)
+  end
+
+  def permitted_parameters
+    params.require(:bike_sticker_batch)
+      .permit(:notes, :prefix, :initial_code_integer, :code_number_length,
+        :stickers_to_create_count, :organization_id)
+      .merge(user_id: current_user.id)
+  end
+
+  def create_batch_if_valid
+    @bike_sticker_batch = BikeStickerBatch.new(permitted_parameters)
+    @bike_sticker_batch.validate
+    unless @bike_sticker_batch.stickers_to_create_count.to_i > 0
+      @bike_sticker_batch.errors.add(:base, "Number of stickers to create is required")
+    end
+    if @bike_sticker_batch.organization_id.blank?
+      @bike_sticker_batch.errors.add(:organization_id, "Organization required")
+    end
+
+    if @bike_sticker_batch.prefix.blank?
+      @bike_sticker_batch.errors.add(:prefix, "Prefix is required")
+    else
+      overlapping_batches = BikeStickerBatch.where(prefix: @bike_sticker_batch.prefix).select do |batch|
+        @bike_sticker_batch.initial_code_integer.between?(batch.min_code_integer, batch.max_code_integer)
+      end
+      overlapping_batches.each { |b| @bike_sticker_batch.errors.add(:base, "Existing sticker batch ##{b.id} has overlapping codes") }
+    end
+
+    @bike_sticker_batch.save unless @bike_sticker_batch.errors.any?
+    @bike_sticker_batch
   end
 end
