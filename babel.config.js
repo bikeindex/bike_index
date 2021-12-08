bf16e1524c8b73b3c85e320399bccc36db1f02a1
sethherr <seth.william.herr@gmail.com>
2021-12-08 12:15:54 -0800

    update other associated things

--- babel.config.js
+++ babel.config.js
@@ -47,24 +47,9 @@ module.exports = function (api) {
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true,
-          spec: true
-        }
-      ],
-      [
         '@babel/plugin-proposal-object-rest-spread',
         {
           useBuiltIns: true
-        }
-      ],
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          helpers: false,
-          regenerator: true,
-          corejs: false
         }
       ],
       [
