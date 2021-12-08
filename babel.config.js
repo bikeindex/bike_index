c5a1e02ef478316350b2fdc3e9886471a2803e64
sethherr <seth.william.herr@gmail.com>
2021-12-08 13:45:16 -0800

    plugin syntax?

--- babel.config.js
+++ babel.config.js
@@ -42,6 +42,7 @@ module.exports = function (api) {
       ]
     ].filter(Boolean),
     plugins: [
+      "@babel/plugin-syntax-jsx",
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
