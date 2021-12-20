caf9863391b9c45bb5a99b0efd7ad95900213d60
sethherr <seth.william.herr@gmail.com>
2021-12-20 12:52:29 -0800

    minimal first approach for migration check

--- app/workers/migrate_creation_state_to_ownership_worker.rb
+++ app/workers/migrate_creation_state_to_ownership_worker.rb
@@ -1,7 +1,15 @@
-class AutocompleteLoaderWorker < ApplicationWorker
-  sidekiq_options queue: "high_priority"
+class MigrateCreationStateToOwnershipWorker < ApplicationWorker
+  sidekiq_options queue: "low_priority"
+  # This timestamp is when the migration started - so any creation_state with an updated_at *after* this timestamp
+  # is assumed to be correct
+  END_TIMESTAMP = 1640033379
 
-  def perform(loader_method)
-    AutocompleteLoader.new.send(loader_method)
+  def self.migrate?(creation_state, ownership)
+    creation_state.updated_at.to_i < END_TIMESTAMP &&
+      ownership.updated_at.to_i < END_TIMESTAMP
+  end
+
+  def perform(creation_state_id)
+
   end
 end
--- spec/workers/migrate_creation_state_to_ownership_worker_spec.rb
+++ spec/workers/migrate_creation_state_to_ownership_worker_spec.rb
@@ -2,11 +2,28 @@ require "rails_helper"
 
 RSpec.describe MigrateCreationStateToOwnershipWorker, type: :job do
   it "is the correct queue" do
-    expect(described_class.sidekiq_options["queue"]).to eq "high_priority"
+    expect(described_class.sidekiq_options["queue"]).to eq "low_priority"
   end
 
-  it "calls passed arguments on autocomplete loader" do
-    expect_any_instance_of(AutocompleteLoader).to receive(:clear)
-    AutocompleteLoaderWorker.new.perform("clear")
+  describe "migrate?" do
+    let(:bike) { FactoryBot.create(:bike) }
+    let(:ownership) { FactoryBot.create(:ownership, bike: bike) }
+    let!(:creation_state) { FactoryBot.create(:creation_state, bike: bike, creator: ownership.creator) }
+    it "is falsey" do
+      creation_state.reload
+      ownership.reload
+      expect(ownership.updated_at.to_i).to be > MigrateCreationStateToOwnershipWorker::END_TIMESTAMP
+      expect(MigrateCreationStateToOwnershipWorker.migrate?(ownership, creation_state)).to be_falsey
+    end
+    context "before time" do
+      let(:updated_at) { Time.at(1640030537) }
+      it "is truthy" do
+        creation_state.update_column :updated_at, updated_at
+        ownership.update_column :updated_at, updated_at
+        creation_state.reload
+        ownership.reload
+        expect(MigrateCreationStateToOwnershipWorker.migrate?(ownership, creation_state)).to be_truthy
+      end
+    end
   end
 end
