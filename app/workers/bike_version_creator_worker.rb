6e29886355df6125a46a5624f9c8bf429f74bc78
sethherr <seth.william.herr@gmail.com>
2022-01-01 17:49:57 -0500

    wip

--- app/workers/bike_version_creator_worker.rb
+++ app/workers/bike_version_creator_worker.rb
@@ -3,6 +3,16 @@ class BikeVersionCreatorWorker < ApplicationWorker
 
   def perform(bike_id)
     bike = Bike.unscoped.find_by_id(bike_id)
-
+    bike_version = bike.bike_versions.build(manufacturer: bike.manufacturer,
+      mnfg_name: bike.mnfg_name,
+      primary_frame_color: bike.primary_frame_color,
+      secondary_frame_color: bike.secondary_frame_color,
+      tertiary_frame_color: bike.tertiary_frame_color,
+      rear_wheel_size: bike.rear_wheel_size,
+      front_wheel_size: bike.front_wheel_size,
+      rear_gear_type: bike.rear_gear_type,
+      front_gear_type: bike.front_gear_type,
+      owner_id: bike.owner.id)
+    bike_version.save
   end
 end
--- spec/workers/bike_version_creator_worker_spec.rb
+++ spec/workers/bike_version_creator_worker_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe BikeVersionCreatorWorker, type: :job do
   let(:bike) { FactoryBot.create(:bike, :with_ownership_claimed) }
   let(:user) { bike.user }
   it "creates the bike_version" do
-    expect(bike.reload.bike_versions.count).to eq 1
+    expect(bike.reload.bike_versions.count).to eq 0
     expect {
       instance.perform(bike.id)
     }.to change(BikeVersion, :count).by 1
     expect(bike.reload.bike_versions.count).to eq 1
     bike_version = bike.bike_versions.first
-    expect(bike_version.user_id).to eq user.id
+    expect(bike_version.owner_id).to eq user.id
     expect(bike_version.authorized?(user)).to be_truthy
     expect(bike_version.mnfg_name).to eq bike.mnfg_name
     expect(bike_version.frame_colors).to eq bike.frame_colors
