b9f54b6cbeae35c4fd4116c2d1849c9521efa31d
sethherr <seth.william.herr@gmail.com>
2022-01-18 13:58:46 -0800

    fix version creator issue

--- app/workers/bike_version_creator_worker.rb
+++ app/workers/bike_version_creator_worker.rb
@@ -36,14 +36,8 @@ class BikeVersionCreatorWorker < ApplicationWorker
 
     bike.public_images.each do |public_image|
       next if bike_version.public_images.where(external_image_url: public_image.image_url).present?
-      # Complicated mainly because of testing and local development
-      new_public_image = bike_version.public_images.new(name: public_image.name)
-      if public_image.local_file?
-        new_public_image.external_image_url = public_image.image_url
-      else
-        new_public_image.image = public_image.image.image
-      end
-      new_public_image.save
+      bike_version.public_images.create(name: public_image.name,
+        external_image_url: public_image.image_url)
     end
 
     bike_version # Needs to return bike version because it is run inline
