bbbf4de4f52fba9d28e51591a4ba9aaecdd9e6cd
sethherr <seth.william.herr@gmail.com>
2022-04-21 16:25:59 -0700

    wip

--- app/workers/scheduled_email_survey_worker.rb
+++ app/workers/scheduled_email_survey_worker.rb
@@ -1,8 +1,42 @@
-class EmailWelcomeWorker < ApplicationWorker
-  sidekiq_options queue: "notify", retry: 3
+class ScheduledEmailSurveyWorker < ScheduledWorker
+  prepend ScheduledWorkerRecorder
+  sidekiq_options retry: 1
+  SURVEY_COUNT = (ENV["THEFT_SURVEY"] || 200).to_i
 
-  def perform(user_id)
-    user = User.find(user_id)
-    CustomerMailer.welcome_email(user).deliver_now
+  def self.frequency
+    24.hours
+  end
+
+  def perform(stolen_record_id = nil, force_send = false)
+    return enqueue_workers if stolen_record_id.blank?
+    stolen_record = StolenRecord.unscoped.find(stolen_record_id)
+    return if !force_send && no_survey?(stolen_record)
+    # In case something got enqueued in between
+    pp "sending!!!"
+  end
+
+  def send_survey?(stolen_record = nil)
+    return false unless stolen_record.present? && !stolen_record.no_notify? &&
+      survey_period.cover?(stolen_record.date_stolen)
+    user = stolen_record.user
+    user.present? && user.notifications.theft_surveys.none?
+  end
+
+  def no_survey?(stolen_record)
+    !send_survey?(stolen_record)
+  end
+
+  def enqueue_workers
+    StolenRecord.unscoped.where(no_notify: false, date_stolen: survey_period)
+      .left_joins(:theft_surveys).where(notifications: {notifiable_id: nil})
+      .order(:updated_at).limit(SURVEY_COUNT)
+      .find_each do |stolen_record|
+        next if no_survey?(stolen_record)
+        ScheduledEmailSurveyWorker.perform_async(stolen_record.id)
+      end
+  end
+
+  def survey_period
+    (Time.current - 8.weeks)..(Time.current - 4.weeks)
   end
 end
--- spec/workers/scheduled_email_survey_worker_spec.rb
+++ spec/workers/scheduled_email_survey_worker_spec.rb
@@ -1,76 +1,53 @@
 require "rails_helper"
 
-RSpec.describe EmailSurveyWorker, type: :job do
-  it "sends a welcome email" do
-    user = FactoryBot.create(:user)
-    ActionMailer::Base.deliveries = []
-    EmailConfirmationWorker.new.perform(user.id)
-    expect(ActionMailer::Base.deliveries.empty?).to be_falsey
-  end
-  context "user with email already exists" do
-    let(:email) { "test@test.com" }
-    let!(:user1) { FactoryBot.create(:user, email: email) }
-    let(:user2) do
-      u = FactoryBot.create(:user)
-      u.update_column :email, email
-      u
-    end
-    it "deletes user" do
-      expect(user1.email).to eq user2.email
-      expect(user1.id).to be < user2.id
-      ActionMailer::Base.deliveries = []
-      expect {
-        EmailConfirmationWorker.new.perform(user2.id)
-      }.to change(User, :count).by(-1)
-      expect(ActionMailer::Base.deliveries.empty?).to be_truthy
-    end
-    context "calling other user" do
-      it "does not delete the other user" do
-        expect(user1.id).to be < user2.id
-        ActionMailer::Base.deliveries = []
-        expect {
-          EmailConfirmationWorker.new.perform(user1.id)
-        }.to change(User, :count).by(0)
-        expect(ActionMailer::Base.deliveries.empty?).to be_falsey
-      end
-    end
-    context "recent notification" do
-      let(:user) { FactoryBot.create(:user) }
-      let!(:notification) { FactoryBot.create(:notification, kind: "confirmation_email", user: user, created_at: created_at, delivery_status: delivery_status) }
-      let(:delivery_status) { "email_success" }
-      let(:created_at) { Time.current - 30.seconds }
-      it "doesn't resend" do
-        ActionMailer::Base.deliveries = []
-        expect {
-          EmailConfirmationWorker.new.perform(user.id)
-        }.to change(Notification, :count).by(0)
-        expect(ActionMailer::Base.deliveries.empty?).to be_truthy
-      end
-      context "sent 1:10 seconds ago" do
-        let(:created_at) { Time.current - 70.seconds }
-        it "resends" do
-          ActionMailer::Base.deliveries = []
-          expect {
-            EmailConfirmationWorker.new.perform(user.id)
-          }.to change(Notification, :count).by(1)
-          expect(ActionMailer::Base.deliveries.empty?).to be_falsey
-          notification2 = Notification.last
-          expect(notification2.user).to eq user
-          expect(notification2.delivery_status).to eq "email_success"
-        end
-      end
-      context "delivery_status nil" do
-        let(:delivery_status) { nil }
-        it "resends, updates existing notification" do
-          ActionMailer::Base.deliveries = []
-          expect {
-            EmailConfirmationWorker.new.perform(user.id)
-          }.to change(Notification, :count).by(0)
-          expect(ActionMailer::Base.deliveries.empty?).to be_falsey
-          notification.reload
-          expect(notification.email_success?).to be_truthy
-        end
-      end
+RSpec.describe ScheduledEmailSurveyWorker, type: :job do
+  let(:instance) { described_class.new }
+  include_context :scheduled_worker
+  include_examples :scheduled_worker_tests
+
+  describe "enqueue workers" do
+    let(:at) { Time.current - 5.weeks }
+    let!(:bike) { FactoryBot.create(:bike, :with_ownership_claimed, date_stolen: at) }
+    let!(:stolen_record1) { FactoryBot.create(:stolen_bike, :with_ownership_claimed, date_stolen: at).current_stolen_record }
+    let(:user1) { stolen_record1.user }
+    let!(:stolen_record2) { FactoryBot.create(:stolen_bike, :with_ownership_claimed, date_stolen: at).current_stolen_record }
+    let(:user2) { stolen_record2.user }
+    let!(:stolen_record3) { FactoryBot.create(:stolen_bike, :with_ownership_claimed, date_stolen: at, user: user2).current_stolen_record }
+    let!(:stolen_record_no_notify1) { FactoryBot.create(:stolen_bike, :with_ownership_claimed, date_stolen: at, stolen_no_notify: true).current_stolen_record }
+    let!(:stolen_record_no_notify2) { FactoryBot.create(:stolen_bike, :with_ownership_claimed, date_stolen: at, user: user1, stolen_no_notify: true).current_stolen_record }
+    let!(:recovered_bike) { FactoryBot.create(:bike, :with_ownership_claimed, date_stolen: at) }
+    let!(:recovered_record) { FactoryBot.create(:stolen_record_recovered, date_stolen: at, bike: recovered_bike) }
+    it "enqueues expected emails" do
+      expect(stolen_record_no_notify2.reload.user&.id).to eq stolen_record1.user.id
+      # Test that no_notify is the same for everyone
+      expect(stolen_record1.reload.no_notify).to be_falsey
+      expect(stolen_record_no_notify1.reload.no_notify).to be_truthy
+      expect(stolen_record_no_notify2.reload.no_notify).to be_truthy
+      expect(stolen_record2.reload.no_notify).to be_falsey
+      expect(stolen_record3.reload.no_notify).to be_falsey
+      expect(StolenRecord.recovered.where(bike_id: recovered_bike.id).first.no_notify).to be_falsey
+      # Notifications
+      notification = Notification.create(kind: "theft_survey_4_2022", user: user2, notifiable: stolen_record3)
+      expect(notification).to be_valid
+      #
+      notification2 = Notification.create(kind: "stolen_notification_sent", user: user1, notifiable: stolen_record1)
+      expect(notification2).to be_valid
+      Sidekiq::Worker.clear_all
+      instance.perform
+      expect(instance.send_survey?(stolen_record1)).to be_truthy
+      expect(instance.send_survey?(stolen_record2)).to be_falsey # user has theft_survey notification
+      expect(instance.send_survey?(stolen_record3)).to be_falsey # stolen record has theft_survey notification
+      expect(instance.send_survey?(stolen_record_no_notify1)).to be_falsey # no notify
+      expect(instance.send_survey?(stolen_record_no_notify2)).to be_falsey # no notify
+      expect(instance.send_survey?(recovered_record)).to be_truthy
+      # It enqueues the bikes that we want - even though some won't be surveyed
+      enqueued_ids = ScheduledEmailSurveyWorker.jobs.map { |j| j["args"] }.last&.flatten || []
+      # expect(enqueued_ids).to match_array([stolen_record1.id, recovered_record.id])
     end
   end
+  # it "sends a theft survey email" do
+  #   ActionMailer::Base.deliveries = []
+  #   EmailConfirmationWorker.new.perform(user.id)
+  #   expect(ActionMailer::Base.deliveries.empty?).to be_falsey
+  # end
 end
