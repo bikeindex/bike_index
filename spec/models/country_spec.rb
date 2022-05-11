beea1b9e7278380b105f9c1c6bc108623eb96aec
sethherr <seth.william.herr@gmail.com>
2022-05-11 10:01:47 -0700

    switch fuzzy_find to friendly_find for consistency

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,22 +1,22 @@
 require "rails_helper"
 
 RSpec.describe Country, type: :model do
-  describe "fuzzy_find" do
+  describe "friendly_find" do
     it "finds the country by name or iso" do
       country = Country.create(name: "Svenborgia", iso: "SVE")
-      expect(Country.fuzzy_find(" SVENBORGia")).to eq(country)
-      expect(Country.fuzzy_find(" sve ")).to eq(country)
+      expect(Country.friendly_find(" SVENBORGia")).to eq(country)
+      expect(Country.friendly_find(" sve ")).to eq(country)
     end
 
     it "finds the country by ISO address when the case doesn't match" do
       country = Country.create(name: "EEEEEEEh", iso: "LULZ")
-      expect(Country.fuzzy_find("lulz ")).to eq(country)
+      expect(Country.friendly_find("lulz ")).to eq(country)
     end
 
     it "finds the United States by its proper ISO or 'USA'" do
       country = Country.create(name: "United States", iso: "US")
-      expect(Country.fuzzy_find("USA")).to eq(country)
-      expect(Country.fuzzy_find("us")).to eq(country)
+      expect(Country.friendly_find("USA")).to eq(country)
+      expect(Country.friendly_find("us")).to eq(country)
     end
   end
 end
