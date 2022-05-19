067c4385a07b9639780b6af251be8016c0896622
sethherr <seth.william.herr@gmail.com>
2022-05-18 20:12:56 -0700

    add country_id

--- app/workers/migrate_postal_code_worker.rb
+++ app/workers/migrate_postal_code_worker.rb
@@ -2,9 +2,12 @@ class MigratePostalCodeWorker < ApplicationWorker
   sidekiq_options queue: "low_priority"
   sidekiq_options retry: false
 
-  def perform(code, country_id)
+  def perform(code, country_id = nil)
+    country_id ||= Bike.unscoped.where("zipcode ILIKE ?", code&.strip).where.not(country_id: nil).first&.country_id
     new_zipcode = Geocodeable.format_postal_code(code, country_id)
-    Bike.where(zipcode: code, country_id: country_id)
-      .update_all(zipcode: new_zipcode)
+
+    Bike.unscoped.where(country_id: [country_id, nil])
+      .where(zipcode: code)
+      .update_all(zipcode: new_zipcode, country_id: country_id)
   end
 end
