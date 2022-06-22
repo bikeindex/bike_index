5de82ba01b8efaf8d8383b3ce415d3d3746c8ef5
sethherr <seth.william.herr@gmail.com>
2022-06-22 09:09:07 -0700

    fix where.not bug

--- app/workers/create_stolen_geojson_worker.rb
+++ app/workers/create_stolen_geojson_worker.rb
@@ -31,7 +31,7 @@ class CreateStolenGeojsonWorker < ScheduledWorker
   end
 
   def geojson_bike_features
-    Bike.unscoped.status_stolen.current.where.not(latitude: nil, occurred_at: nil)
+    Bike.unscoped.status_stolen.current.where.not(latitude: nil).where.not(occurred_at: nil)
       .order(occurred_at: :desc).pluck(:id, :occurred_at, :latitude, :longitude)
       .map { |id, oc, lat, lng| BikeGeojsoner.feature_from_plucked(id, oc, lat, lng) }
   end
