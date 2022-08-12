d564de3f5bd6b420e821371cf4f0ceaa0e4e026a
sethherr <seth.william.herr@gmail.com>
2022-08-12 10:01:59 -0700

    improve time_range for admin/invoices

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -38,10 +38,9 @@ class Admin::InvoicesController < Admin::BaseController
       Invoice
     end
 
-    if %w[subscription_start_at subscription_end_at].include?(params[:time_range_column])
-      @time_range_column = params[:time_range_column]
-      @search_endless = "not_endless"
-      invoices = invoices.not_endless if @time_range_column == "subscription_end_at"
+    if %w[subscription_start_at subscription_end_at].include?(params[:sort])
+      @time_range_column = params[:sort]
+      @search_endless = "not_endless" if @time_range_column == "subscription_end_at"
     else
       @time_range_column = "created_at"
     end
--- spec/requests/admin/invoices_request_spec.rb
+++ spec/requests/admin/invoices_request_spec.rb
@@ -5,10 +5,29 @@ RSpec.describe Admin::InvoicesController, type: :request do
     include_context :request_spec_logged_in_as_superuser
 
     describe "GET /admin/invoices" do
+      let(:end_at) { Time.current + 10.years }
+      let!(:invoice) { FactoryBot.create(:invoice, subscription_start_at: Time.current - 1.day, subscription_end_at: end_at) }
       it "responds with 200 OK and renders the index template" do
         get "/admin/invoices"
         expect(response).to be_ok
         expect(response).to render_template(:index)
+        # Do some testing of latest_period_date and time_ranges
+        expect(assigns(:time_range).last).to be_within(1.day).of end_at
+        expect(assigns(:invoices).pluck(:id)).to eq([invoice.id])
+        get "/admin/invoices?direction=desc&period=year&render_chart=true&sort=subscription_start_at"
+        expect(response).to render_template(:index)
+        expect(assigns(:time_range).last).to be_within(1.day).of(Time.current)
+        expect(assigns(:time_range).first).to be_within(1.day).of(Time.current - 1.year)
+        expect(assigns(:invoices).pluck(:id)).to eq([invoice.id])
+        get "/admin/invoices?direction=desc&period=custom&start_time=#{(Time.current - 1.year).to_s}&sort=subscription_start_at"
+        expect(response).to render_template(:index)
+        expect(assigns(:time_range).last).to be_within(1.day).of(end_at)
+        expect(assigns(:time_range).first).to be_within(1.day).of(Time.current - 1.year)
+        expect(assigns(:invoices).pluck(:id)).to eq([invoice.id])
+        get "/admin/invoices?direction=desc&period=custom&start_time=#{(Time.current - 1.year).to_s}&end_time=#{Time.current.to_s}&sort=subscription_start_at"
+        expect(response).to render_template(:index)
+        expect(assigns(:time_range).last).to be_within(1.day).of(Time.current)
+        expect(assigns(:invoices).pluck(:id)).to eq([invoice.id])
       end
     end
   end
