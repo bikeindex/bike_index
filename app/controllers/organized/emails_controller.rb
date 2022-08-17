19738971f8f5742654f442098fb39737bad12c19
sethherr <seth.william.herr@gmail.com>
2022-08-17 14:24:23 -0700

    better handling

--- app/controllers/organized/emails_controller.rb
+++ app/controllers/organized/emails_controller.rb
@@ -9,6 +9,8 @@ module Organized
 
     def show
       @email_preview = true
+      # NOTE: This is a bad hack, but there isn't a good place to define this method
+      @email_preview_tokenized_url = "#"
       if ParkingNotification.kinds.include?(@kind)
         find_or_build_parking_notification
         render template: "/organized_mailer/parking_notification", layout: "email"
