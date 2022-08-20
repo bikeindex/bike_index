5a6424ea4207cec25c76a3177be1ab51e887c9d0
sethherr <seth.william.herr@gmail.com>
2022-08-19 18:31:50 -0700

    wip

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,18 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Notification, type: :model do
-  describe "notifiable_display_name" do
-    let(:notification) { FactoryBot.create(:notification) }
-    it "is blank" do
-      expect(notification.notifiable_display_name).to be_blank
+  describe "sender_display_name" do
+    let(:payment) { FactoryBot.create(:payment) }
+    let(:notification) { FactoryBot.create(:notification, kind: "donation_stolen", notifiable: payment, user: payment.user) }
+    it "is payment" do
+      expect(notification.sender_display_name).to eq "auto"
     end
-    context "donation" do
-      let(:payment) { FactoryBot.create(:payment) }
-      let(:notification) { FactoryBot.create(:notification, kind: "donation_stolen", notifiable: payment, user: payment.user) }
-      it "is payment" do
-        expect(notification.notifiable_display_name).to eq "Payment ##{payment.id}"
-        expect(notification.sender_display_name).to eq "auto"
-      end
     end
   end
 
