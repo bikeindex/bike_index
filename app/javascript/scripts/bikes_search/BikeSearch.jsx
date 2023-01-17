4a5370c750288af2c66cf20d86253dae82816c1a
sethherr <seth.william.herr@gmail.com>
2023-01-17 13:18:15 -0800

    wip

--- app/javascript/scripts/bikes_search/BikeSearch.jsx
+++ app/javascript/scripts/bikes_search/BikeSearch.jsx
@@ -1,10 +1,10 @@
 /* eslint import/no-unresolved: 0 */
 
-import React, { Fragment, Component } from "react";
+import React, { Fragment, Component } from 'react'
 
-import Loading from "../Loading";
-import TimeParser from "../utils/time_parser.js";
-import honeybadger from "../utils/honeybadger";
+import Loading from '../Loading'
+import TimeParser from '../utils/time_parser.js'
+import honeybadger from '../utils/honeybadger'
 
 class BikeSearch extends Component {
   // loading states :
@@ -14,93 +14,96 @@ class BikeSearch extends Component {
 
   state = {
     loading: null,
-    results: [],
-  };
+    results: []
+  }
 
-  componentDidMount() {
-    this.resultsBeingFetched();
+  componentDidMount () {
+    this.resultsBeingFetched()
     this.props
       .fetchBikes(this.props.interpretedParams)
       .then(this.resultsFetched)
-      .catch(this.handleError);
+      .catch(this.handleError)
   }
 
-  componentDidUpdate() {
+  componentDidUpdate () {
     if (!window.timeParser) {
-      window.timeParser = new TimeParser();
+      window.timeParser = new TimeParser()
     }
-    window.timeParser.localize();
+    window.timeParser.localize()
   }
 
   resultsBeingFetched = () => {
-    this.setState({ loading: true });
-  };
+    this.setState({ loading: true })
+  }
 
   resultsFetched = ({ bikes, error }) => {
-    const results = bikes || [];
-    const loading = !results.length ? null : false;
-    this.setState({ results, loading });
+    const results = bikes || []
+    const loading = !results.length ? null : false
+    this.setState({ results, loading })
     if (error) {
-      this.handleError(error);
+      this.setState({ results, loading })
+      this.handleError(error)
     }
-  };
+  }
 
-  handleError = (error) => {
-    honeybadger.notify(error, { component: this.props.searchName });
-  };
+  handleError = error => {
+    // honeybadger.notify(error, { component: this.props.searchName })
+  }
 
-  render() {
-    const { serial } = this.props.interpretedParams;
+  render () {
+    const { serial } = this.props.interpretedParams
 
     if (this.state.loading === null) {
       return (
-        <div className="row">
-          <div className="col-md-12">
-            <h3 className="no-exact-results">
-              {this.props.t("no_matches_found_html", { serial })}
+        <div className='row'>
+          <div className='col-md-12'>
+            <h3 className='no-exact-results'>
+              {this.props.t('no_matches_found_html', { serial })}
             </h3>
           </div>
         </div>
-      );
+      )
     }
 
     if (this.state.loading) {
       return (
-        <div className="row">
-          <div className="col-md-12">
-            <h3 className="secondary-matches">
-              {this.props.t("searching_html", { serial })}
+        <div className='row'>
+          <div className='col-md-12'>
+            <h3 className='secondary-matches'>
+              {this.props.t('searching_html', { serial })}
             </h3>
             <Loading />
           </div>
         </div>
-      );
+      )
     }
 
-    const Result = this.props.resultComponent;
+    const Result = this.props.resultComponent
     // external registry search doesn't care about proximity, and for translation
     const stolenness = {
-      non: "abandoned",
-      all: "all",
-      stolen: "stolen",
-      proximity: "stolen",
-    }[this.props.interpretedParams.stolenness];
+      non: 'abandoned',
+      all: '', // Saying all isn't clear
+      stolen: 'stolen',
+      proximity: 'stolen'
+    }[this.props.interpretedParams.stolenness]
+
+    console.log(this.state.results)
 
     return (
-      <div className="row">
-        <div className="col-md-12">
-          <h3 className="secondary-matches">
-            {this.props.t("matches_found_html", { serial, stolenness })}
+      <div className='row'>
+        <div className='col-md-12'>
+          <h3 className='secondary-matches'>
+            {this.props.t('matches_found_html', { serial, stolenness })}
           </h3>
-          <ul className="bike-boxes">
-            {this.state.results.map((bike) => (
+          <ul className='bike-boxes'>
+            {this.state.results.map(bike => (
               <Result key={bike.id} bike={bike} />
             ))}
           </ul>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default BikeSearch;
+export default BikeSearch
