e065d48d57f78a7caf6bddb9d69268cbad9ce2dc
Seth Herr <seth.william.herr@gmail.com>
2023-05-31 11:04:56 -0700

    Resending partial registration emails

--- spec/workers/email_partial_registration_worker_spec.rb
+++ spec/workers/email_partial_registration_worker_spec.rb
@@ -3,8 +3,15 @@ require "rails_helper"
 RSpec.describe EmailPartialRegistrationWorker, type: :job do
   it "sends a partial registration email" do
     b_param = FactoryBot.create(:b_param)
+    expect(b_param.creator_id).to be_present
+    expect(Notification.count).to eq 0
     ActionMailer::Base.deliveries = []
     EmailPartialRegistrationWorker.new.perform(b_param.id)
     expect(ActionMailer::Base.deliveries.empty?).to be_falsey
+    exect(Notification.count).to eq 1
+    notification = Notification.last
+    expect(notification.notifiable).to eq b_param
+    # expect(notification.creator_id).to eq b_param.creator_id
+    expect(notification.kind).to eq "partial_registration"
   end
 end
