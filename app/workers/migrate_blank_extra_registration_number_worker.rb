d07fc2530f5b1b2d41bc77fd6d15a6d3039e5838
Seth Herr <seth.william.herr@gmail.com>
2023-06-26 19:43:18 -0700

    fix job for nils

--- app/workers/migrate_blank_extra_registration_number_worker.rb
+++ app/workers/migrate_blank_extra_registration_number_worker.rb
@@ -3,7 +3,7 @@ class MigrateBlankExtraRegistrationNumberWorker < ApplicationWorker
 
   def perform(bike_id)
     bike = Bike.unscoped.find_by_id(bike_id)
-
+    return if bike.extra_registration_number.blank?
     if bike.extra_registration_number.match?(/(serial.)?#{bike.serial_number}/i)
       bike.update_column(extra_registration_number: nil)
     end
