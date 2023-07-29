14e4fc59ceedd164ff9d1817c73664089b04cd06
Seth Herr <seth.william.herr@gmail.com>
2023-07-29 16:16:47 -0700

    Fix some image issues (#2367)

--- app/workers/carrier_wave_store_worker.rb
+++ app/workers/carrier_wave_store_worker.rb
@@ -1,3 +1,3 @@
 class CarrierWaveStoreWorker < ::CarrierWave::Workers::StoreAsset
-  sidekiq_options queue: "carrierwave", backtrace: true, retry: 3, dead: false
+  sidekiq_options queue: "carrierwave", backtrace: true, retry: 2, dead: false
 end
