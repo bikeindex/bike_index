1712b1de59ba9f9b41b468175809a27537251c79
Seth Herr <seth.william.herr@gmail.com>
2023-07-29 17:26:12 -0700

    Create notification on partial registration email (#2368)

--- app/workers/email_partial_registration_worker.rb
+++ app/workers/email_partial_registration_worker.rb
@@ -3,6 +3,13 @@ class EmailPartialRegistrationWorker < ApplicationWorker
 
   def perform(b_param_id)
     b_param = BParam.find(b_param_id)
-    OrganizedMailer.partial_registration(b_param).deliver_now
+    if b_param.present?
+      notification = Notification.create(kind: "partial_registration",
+        message_channel: "email",
+        notifiable: b_param)
+
+      OrganizedMailer.partial_registration(b_param).deliver_now
+      notification.update(delivery_status: "email_success")
+    end
   end
 end
--- spec/workers/email_partial_registration_worker_spec.rb
+++ spec/workers/email_partial_registration_worker_spec.rb
@@ -3,8 +3,18 @@ require "rails_helper"
 RSpec.describe EmailPartialRegistrationWorker, type: :job do
   it "sends a partial registration email" do
     b_param = FactoryBot.create(:b_param)
+    expect(b_param.creator_id).to be_present
+    expect(Notification.count).to eq 0
     ActionMailer::Base.deliveries = []
     EmailPartialRegistrationWorker.new.perform(b_param.id)
     expect(ActionMailer::Base.deliveries.empty?).to be_falsey
+    expect(Notification.count).to eq 1
+    notification = Notification.last
+    expect(notification.notifiable).to eq b_param
+    expect(notification.kind).to eq "partial_registration"
+    expect(notification.user_id).to be_blank
+    expect(notification.delivery_status).to eq "email_success"
+    expect(notification.b_param?).to be_truthy
+    expect(notification.calculated_email).to eq b_param.email
   end
 end
