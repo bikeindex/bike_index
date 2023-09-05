5d542f73c5b17606e44f92669d3e0d99c84e5382
Seth Herr <seth.william.herr@gmail.com>
2023-09-05 15:53:04 -0700

    Fix cycle type nil bug

--- app/workers/autocomplete_loader_worker.rb
+++ app/workers/autocomplete_loader_worker.rb
@@ -1,5 +1,5 @@
 class AutocompleteLoaderWorker < ApplicationWorker
-  sidekiq_options queue: "high_priority"
+  sidekiq_options queue: "high_priority", retry: 1
 
   def perform(loader_method)
     AutocompleteLoader.new.send(loader_method)
