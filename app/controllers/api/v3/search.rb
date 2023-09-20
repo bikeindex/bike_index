651972e1c707f609c608c0aea61ab25ae371b81d
Seth Herr <seth.william.herr@gmail.com>
2023-09-20 14:01:05 -0700

    add search

--- app/controllers/api/v3/search.rb
+++ app/controllers/api/v3/search.rb
@@ -50,7 +50,9 @@ module API
           optional :per_page, type: Integer, default: 25, desc: "Bikes per page (max 100)"
         end
         get "/" do
-          serialized_bikes_results(paginate(Bike.search(interpreted_params)))
+          ActiveRecord::Base.connected_to(role: :reading) do
+            serialized_bikes_results(paginate(Bike.search(interpreted_params)))
+          end
         end
 
         desc "Count of bikes matching search", {
@@ -76,20 +78,22 @@ module API
           use :search
         end
         get "/count" do
-          # Doing extra stuff to make this query more efficient, since this is called all the time
-          interpreted_params = Bike.searchable_interpreted_params(params.merge(stolenness: "proximity"), ip: forwarded_ip_address)
-          # Un-scope to remove the unnecessary eager loading
-          bikes = Bike.unscoped.current.search(interpreted_params.merge(stolenness: "all"))
-          # And then execute the specific BikeSearchable#search_matching_stolenness query for each
-          {
-            non: bikes.status_with_owner.count,
-            stolen: bikes.stolen_or_impounded.count,
-            proximity: if interpreted_params[:bounding_box].present?
-                         bikes.stolen_or_impounded.within_bounding_box(interpreted_params[:bounding_box]).count
-                       else # we're probably in testing, but regardless, just skip
-                         0
-                       end
-          }
+          ActiveRecord::Base.connected_to(role: :reading) do
+            # Doing extra stuff to make this query more efficient, since this is called all the time
+            interpreted_params = Bike.searchable_interpreted_params(params.merge(stolenness: "proximity"), ip: forwarded_ip_address)
+            # Un-scope to remove the unnecessary eager loading
+            bikes = Bike.unscoped.current.search(interpreted_params.merge(stolenness: "all"))
+            # And then execute the specific BikeSearchable#search_matching_stolenness query for each
+            {
+              non: bikes.status_with_owner.count,
+              stolen: bikes.stolen_or_impounded.count,
+              proximity: if interpreted_params[:bounding_box].present?
+                           bikes.stolen_or_impounded.within_bounding_box(interpreted_params[:bounding_box]).count
+                         else # we're probably in testing, but regardless, just skip
+                           0
+                         end
+            }
+          end
         end
 
         # TODO: When next bumping the API version, rename this endpoint to
