813767f402c8c490f5dd6c825cccb941b7b15471
Seth Herr <seth.william.herr@gmail.com>
2023-09-26 07:54:04 -0700

    Add pedal assist and throttle propulsion type (#2419)

--- app/models/propulsion_type.rb
+++ app/models/propulsion_type.rb
@@ -5,6 +5,7 @@ class PropulsionType
     "foot-pedal": 0,
     "pedal-assist": 1,
     throttle: 2,
+    "pedal-assist-and-throttle": 5,
     "hand-pedal": 3,
     "propulsion-other": 4
   }.freeze
@@ -13,6 +14,7 @@ class PropulsionType
     "foot-pedal": "Foot Pedal",
     "pedal-assist": "Pedal Assist",
     throttle: "Electric Throttle",
+    "pedal-assist-and-throttle": "Pedal Assist and Throttle",
     "hand-pedal": "Hand Cycle (hand pedal)",
     "propulsion-other": "Other Style"
   }.freeze
