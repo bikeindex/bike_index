0974ad2ccddac649015f1c5472057ead24a27654
Seth Herr <seth.william.herr@gmail.com>
2023-10-04 13:34:55 -0700

    Add priority to manufacturers (#2429)

--- app/uploaders/application_uploader.rb
+++ app/uploaders/application_uploader.rb
@@ -3,6 +3,14 @@ class ApplicationUploader < CarrierWave::Uploader::Base
 
   after :remove, :delete_empty_upstream_dirs
 
+  def store_dir
+    "#{base_store_dir}/#{model.id}"
+  end
+
+  def base_store_dir
+    "uploads/#{model.class.to_s[0, 2]}"
+  end
+
   def delete_empty_upstream_dirs
     path = ::File.expand_path(store_dir, root)
     Dir.delete(path) # fails if path not empty dir
