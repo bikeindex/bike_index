d1cde26d714703e23dca1afd2b9da91a1df3d2bf
Seth Herr <seth.william.herr@gmail.com>
2023-10-04 16:10:13 -0700

    remove soulheart from gemfile

--- app/workers/autocomplete_loader_worker.rb
+++ app/workers/autocomplete_loader_worker.rb
@@ -1,7 +1,8 @@
 class AutocompleteLoaderWorker < ApplicationWorker
   sidekiq_options queue: "high_priority", retry: 1
 
-  def perform(loader_method)
-    AutocompleteLoader.new.send(loader_method)
+  def perform(kinds_to_reload = nil)
+    Autocomplete::Loader.clear_redis
+    Autocomplete::Loader.load_all(kinds_to_reload)
   end
 end
--- spec/workers/autocomplete_loader_worker_spec.rb
+++ spec/workers/autocomplete_loader_worker_spec.rb
@@ -1,12 +1,9 @@
 require "rails_helper"
 
 RSpec.describe AutocompleteLoaderWorker, type: :job do
-  it "is the correct queue" do
-    expect(described_class.sidekiq_options["queue"]).to eq "high_priority"
-  end
-
-  it "calls passed arguments on autocomplete loader" do
-    expect_any_instance_of(AutocompleteLoader).to receive(:clear)
+  it "calls AutocompleteLoaderWorker" do
+    expect(Autocomplete::Loader).to receive(:clear_redis)
+    expect(Autocomplete::Loader).to receive(:load_all)
     AutocompleteLoaderWorker.new.perform("clear")
   end
 end
