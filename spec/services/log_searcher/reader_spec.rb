a9f7db230133a091628c22a3777ab4c4afa96e26
Seth Herr <seth.william.herr@gmail.com>
2023-10-26 14:15:44 -0700

    mmmmm, job

--- spec/services/log_searcher/reader_spec.rb
+++ spec/services/log_searcher/reader_spec.rb
@@ -37,7 +37,11 @@ RSpec.describe LogSearcher::Reader do
       redis.expire(LogSearcher::Reader::KEY, 0)
       expect(LogSearcher::Reader.log_lines_in_redis).to eq 0
       LogSearcher::Reader.write_log_lines(LogSearcher::Reader.rgrep_command(time, log_path: log_path))
-      sleep 1 if ENV["CI"] # Maybe fix CI?
+      if ENV["CI"] # Maybe fix CI?
+        log_line = described_class.get_log_line
+        pp log_line
+        expect(log_line).to be_present
+      end
       expect(LogSearcher::Reader.log_lines_in_redis).to eq 3
       redis.expire(LogSearcher::Reader::KEY, 0)
     end
--- spec/workers/scheduled_store_log_searches_worker_spec.rb
+++ spec/workers/scheduled_store_log_searches_worker_spec.rb
@@ -1,8 +1,23 @@
 require "rails_helper"
 
 RSpec.describe ScheduledStoreLogSearchesWorker, type: :job do
+  let(:instance) { described_class.new }
   include_context :scheduled_worker
   include_examples :scheduled_worker_tests
 
-
+  describe "perform" do
+    let!(:organization) { FactoryBot.create(:organization, name: "Hogwarts") }
+    let(:log_line) { 'I, [2023-10-23T13:18:57.628282 #444563]  INFO -- : [74dbf83c-07ca-4bcf-ac82-aa9c6d9ab1f4] {"method":"GET","path":"/o/hogwarts/impound_records","format":"html","controller":"Organized::ImpoundRecordsController","action":"index","status":200,"duration":49.0,"view":28.72,"db":10.93,"remote_ip":"11.11.22.11","u_id":111,"params":{"organization_id":"hogwarts"},"@timestamp":"2023-10-23T13:18:57.628Z","@version":"1","message":"[200] GET /o/hogwarts/impound_records (Organized::ImpoundRecordsController#index)"}' }
+    before { allow_any_instance_of(described_class).to receive(:get_log_line) { log_line } }
+    it "parses a log line" do
+      expect(LoggedSearch.count).to eq 0
+      logged_search = instance.perform(true)
+      expect(LoggedSearch.count).to eq 1
+      expect(logged_search.log_line).to eq log_line
+      expect(logged_search.endpoint).to eq "org_impounded"
+      expect(logged_search.stolenness).to eq "impounded"
+      expect(logged_search.organization_id).to eq organization.id
+      expect(logged_search.user_id).to eq 111
+    end
+  end
 end
