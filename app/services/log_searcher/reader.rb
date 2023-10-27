59fb873a7ab1c0b5124630029f0aae45f96e4110
Seth Herr <seth.william.herr@gmail.com>
2023-10-27 08:52:57 -0700

    Update LogSearcher::Reader to make it easier to call

--- app/services/log_searcher/reader.rb
+++ app/services/log_searcher/reader.rb
@@ -20,24 +20,25 @@ class LogSearcher::Reader
       SEARCHES_MATCHES.reject { |s| s.match?(/.BikesController#index/) }.join("|")
     end
 
-    # If a time is included, it only returns lines that occurred in the hour of the time
-    def rgrep_command(time = nil, log_path: nil)
+    # If a time is included, it returns lines that occurred in the hour of the time
+    def rgrep_command_str(time = nil, log_path: nil)
       log_path ||= DEFAULT_LOG_PATH
       "rg '#{searches_regex}' '#{log_path}'" + time_rgrep(time)
     end
 
-    def write_log_lines(rgrep_command)
+    # This is for diagnostics, to count how many are returned
+    def rgrep_log_lines_count(time = nil, log_path: nil, rgrep_command: nil)
+      rgrep_command ||= rgrep_command_str(time, log_path: log_path)
+      `#{rgrep_command} | wc -l`.strip.to_i
+    end
+
+    def write_log_lines(time = nil, log_path: nil, rgrep_command: nil)
+      rgrep_command ||= rgrep_command_str(time, log_path: log_path)
       RedisPool.conn do |r|
         r.pipelined do |pipeline|
           IO.popen(rgrep_command) { |io| io.each { |l| pipeline.lpush(KEY, l) } }
         end
       end
-    end
-
-    # This is for diagnostics, to count how many are returned
-    # Probably won't include forever
-    def rgrep_command_log_lines(command)
-      `#{command}" | wc -l`.strip.to_i
     end
 
     def get_log_line
