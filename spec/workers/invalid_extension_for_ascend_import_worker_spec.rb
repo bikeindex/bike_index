283243560b05cc9d1e7eead12efdbaed20952f49
Seth Herr <seth.william.herr@gmail.com>
2024-01-03 13:55:57 -0800

    wip

--- spec/workers/invalid_extension_for_ascend_import_worker_spec.rb
+++ spec/workers/invalid_extension_for_ascend_import_worker_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe InvalidExtensionForAscendImportWorker, type: :job do
 
   it "sends an email" do
     ActionMailer::Base.deliveries = []
+    expect(Notification.count).to eq 0
     InvalidExtensionForAscendImportWorker.new.perform(bulk_import.id)
-    expect(ActionMailer::Base.deliveries.empty?).to be_falsey
+    InvalidExtensionForAscendImportWorker.new.perform(bulk_import.id)
+    expect(ActionMailer::Base.deliveries.count).to eq 1
+    expect(Notification.count).to eq 1
+    notification = Notification.last
+    expect(notification.kind).to eq organization_pos_integration_broken
   end
 end
