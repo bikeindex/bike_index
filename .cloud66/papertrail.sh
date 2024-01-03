f3a5f791d62786a7832bc560dd3bad85531d4500
Seth Herr <seth.william.herr@gmail.com>
2024-01-03 15:10:41 -0800

    update papertrail.sh

--- .cloud66/papertrail.sh
+++ .cloud66/papertrail.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# If doing manually, make sure you run:
+# sudo cp .cloud66/log_files.yml /etc/log_files.yml
+# make sure /etc/log_files.yml # and replace <%= ENV['STACK_BASE'] %> with the values
+
 # Download and install the remote_syslog2 binary from papertrail
-wget https://github.com/papertrail/remote_syslog2/releases/download/v0.20/remote_syslog_linux_amd64.tar.gz
+wget https://github.com/papertrail/remote_syslog2/releases/download/v0.21/remote_syslog_linux_amd64.tar.gz
+
 tar xzf ./remote_syslog*.tar.gz
 cd remote_syslog
 sudo cp ./remote_syslog /usr/local/bin
 
 # Set it up as a service (assumes init script has already been cp'd)
-sudo chmod +x /etc/init.d/remote_syslog
-sudo service remote_syslog start
+sudo remote_syslog
 sudo update-rc.d remote_syslog defaults
