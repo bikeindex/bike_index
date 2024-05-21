01eeefa7c843b3c12a43e2f63345ee2c4b3dc586
Seyamack Afzali <seyamack@gmail.com>
2024-05-21 10:23:19 -0500

    updating requested language choices

--- spec/helpers/localization_helper_spec.rb
+++ spec/helpers/localization_helper_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe LocalizationHelper, type: :helper do
             ["English", "en"],
             ["Nederlands (Dutch)", "nl"],
             ["Norsk Bokmål (Norwegian)", "nb"],
-            #["Español (Spanish)", "es"]
+            #["Español (Spansk)", "es"],
+            #["Italiano (Italiensk)", "it"],
+            #["עִברִית (Hebraisk)", "he"],
           ]
           expect(language_choices).to eq(choices)
         end
@@ -17,13 +19,15 @@ RSpec.describe LocalizationHelper, type: :helper do
     end
 
     context "in Dutch" do
-      it "returns with dutch included" do
+      it "returns with Dutch included" do
         I18n.with_locale(:nl) do
           choices = [
             ["English (Engels)", "en"],
             ["Nederlands", "nl"],
             ["Norsk Bokmål (Noors)", "nb"],
-            #["Español (Spaans)", "es"]
+            #["Español (Spaans)", "es"],
+            #["Italiano (Italiaans)", "it"],
+            #["עִברִית (Hebreeuws)", "he"],
           ]
           expect(language_choices).to eq(choices)
         end
@@ -31,13 +35,15 @@ RSpec.describe LocalizationHelper, type: :helper do
     end
 
     context "in Norwegian" do
-      it "returns with norwegian included" do
+      it "returns with Norwegian included" do
         I18n.with_locale(:nb) do
             choices = [
               ["English (Engelsk)", "en"],
               ["Nederlands (Nederlandsk)", "nl"],
               ["Norsk Bokmål", "nb"],
-              #["Español (Spansk)", "es"]
+              #["Español (Spansk)", "es"],
+              #["Italiano (Italiensk)", "it"],
+              #["עִברִית (Hebraisk)", "he"],
             ]
             expect(language_choices).to eq(choices)
           end
@@ -45,13 +51,47 @@ RSpec.describe LocalizationHelper, type: :helper do
       end
   
       # context "in Spanish" do
-      #   it "returns with spanish included" do
+      #   it "returns with Spanish included" do
       #     I18n.with_locale(:es) do
       #       choices = [
       #         ["English (Inglés)", "en"],
       #         ["Nederlands (Holandés)", "nl"],
       #         ["Norsk Bokmål (Noruego)", "nb"],
-      #         ["Español", "es"]             
+      #         ["Español", "es"],
+      #         ["Italiano (Italiano)", "it"],
+      #         ["עִברִית (Hebreo)", "he"],             
+      #       ]
+      #       expect(language_choices).to eq(choices)
+      #     end
+      #   end
+      # end
+
+      # context "in Italian" do
+      #   it "returns with Italian included" do
+      #     I18n.with_locale(:it) do
+      #       choices = [
+      #         ["English (Inglese)", "en"],
+      #         ["Nederlands (Olandese)", "nl"],
+      #         ["Norsk Bokmål (Norvegese)", "nb"],
+      #         ["Español (Spagnolo)", "es"],
+      #         ["Italiano", "it"],
+      #         ["עִברִית (Ebraico)", "he"],             
+      #       ]
+      #       expect(language_choices).to eq(choices)
+      #     end
+      #   end
+      # end
+
+      # context "in Hebrew" do
+      #   it "returns with Hebrew included" do
+      #     I18n.with_locale(:he) do
+      #       choices = [
+      #         ["English (אנגלית)", "en"],
+      #         ["Nederlands (הוֹלַנדִי)", "nl"],
+      #         ["Norsk Bokmål (נורבגית)", "nb"],
+      #         ["Español (ספרדית)", "es"],
+      #         ["Italiano (אִיטַלְקִית)", "it"],
+      #         ["עִברִית", "he"],             
       #       ]
       #       expect(language_choices).to eq(choices)
       #     end
