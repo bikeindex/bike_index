7b78f0fdfdddb3d3e5c8eff7dfc16b9a4e39d647
Seth Herr <seth.william.herr@gmail.com>
2024-07-02 09:46:09 -0600

    Assign organization from user

--- app/workers/process_logged_search_worker.rb
+++ app/workers/process_logged_search_worker.rb
@@ -5,5 +5,18 @@ class ProcessLoggedSearchWorker < ApplicationWorker
   sidekiq_options queue: "droppable", retry: 1
 
   def perform(logged_search_id)
+    logged_search = LoggedSearch.find logged_search_id
+
+    assign_user_attributes(logged_search) if logged_search.user.present?
+    logged_search.processed = true
+    logged_search.update(updated_at: Time.current) if logged_search.changed?
+  end
+
+  private
+
+  def assign_user_attributes(logged_search)
+    return if logged_search.user.superuser? || logged_search.organization_id.present?
+
+    logged_search.organization = logged_search.user.organization_prioritized
   end
 end
--- spec/workers/process_logged_search_worker_spec.rb
+++ spec/workers/process_logged_search_worker_spec.rb
@@ -1,8 +1,48 @@
 require "rails_helper"
 
 RSpec.describe ProcessLoggedSearchWorker, type: :job do
+  let(:instance) { described_class.new }
   describe "perform" do
-    it "Process the logged_search", :flaky do
+    context "user member of organization" do
+      let!(:logged_search) { FactoryBot.create(:logged_search, user_id: user.id) }
+      let(:user) { FactoryBot.create(:user_confirmed, :with_organization) }
+      let(:organization) { user.reload.organizations.first }
+
+      it "Process the logged_search" do
+        expect(logged_search.user_id).to eq user.id
+        expect(logged_search.organization_id).to be_nil
+        expect(logged_search.processed?).to be_falsey
+
+        instance.perform(logged_search.id)
+
+        expect(logged_search.reload.processed?).to be_truthy
+        expect(logged_search.organization_id).to eq organization.id
+      end
+
+      context 'user is superuser' do
+        let(:user) { FactoryBot.create(:admin, :with_organization) }
+        it "doesn't associate" do
+          expect(logged_search.user_id).to eq user.id
+          expect(logged_search.organization_id).to be_nil
+
+          instance.perform(logged_search.id)
+
+          expect(logged_search.reload.processed?).to be_truthy
+          expect(logged_search.organization_id).to be_nil
+        end
+      end
+
+      context 'organization_id is assigned' do
+        before { logged_search.update(organization_id: 122) }
+        it "doesn't update organization_id" do
+          expect(logged_search.reload.processed?).to be_falsey
+
+          instance.perform(logged_search.id)
+
+          expect(logged_search.reload.processed?).to be_truthy
+          expect(logged_search.organization_id).to eq 122
+        end
+      end
     end
   end
 end
