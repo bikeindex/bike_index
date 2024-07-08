119731a8a969250a273a00644626e6e1c6a1860d
Seth Herr <seth.william.herr@gmail.com>
2024-07-08 11:50:11 -0700

    Maybe fix?

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # simplecov must be required before anything else
 if ENV["COVERAGE"]
   require "simplecov"
+  require "simplecov_json_formatter"
   SimpleCov.start("rails") do
     add_filter "/spec/"
     add_filter "/config/"
