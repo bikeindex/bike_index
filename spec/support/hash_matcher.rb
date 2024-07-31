f5edb5b76c3bdab9e797431b6891e4ad5eaf85b4
Seth Herr <seth.william.herr@gmail.com>
2024-07-31 10:01:21 -0700

    wip

--- spec/support/hash_matcher.rb
+++ spec/support/hash_matcher.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 class RspecHashMatcher
+  DEFAULT_OPTS = {
+    match_time_within: 1.01, # Times within 1 second match
+    ignore_array_order: false,
+    match_symbols_as_strings: true, # This is useful for enum comparisons
+    match_number_types: false, # i.e. 1 == 1.0
+    coerce_values_to_json: false # JSON doesn't have booleans
+  }
   class << self
-    def recursive_match_hashes(hash_1, hash_2, inside: [], match_array_order: nil,
-                               match_time_within: nil)
+    # rubocop:disable Metrics/MethodLength
+    def recursive_match_hashes_errors(hash_1, hash_2, inside: [], options: {})
+      options = DEFAULT_OPTS.merge(options)
+      validate_options!(options)
       match_errors = []
       matched_keys = []
       hash_1_indiff = indifferent_hash(hash_1, hash_2)
@@ -15,10 +24,10 @@ class RspecHashMatcher
       hash_2_indiff.each do |key, value|
         match_value = hash_1_indiff[key]
         if value.is_a?(Hash)
-          match_errors += recursive_match_hashes(
-            value, match_value, inside: inside << key, match_array_order:, match_time_within:
+          match_errors += recursive_match_hashes_errors(
+            value, match_value, inside: inside << key, options:
           )
-        elsif !values_match?(value, match_value)
+        elsif !values_match?(value, match_value, options:)
           match_errors << match_error_for(key:, inside:, value:, match_value:)
         end
 
@@ -32,7 +41,9 @@ class RspecHashMatcher
 
       match_errors.flatten.compact
     end
+    # rubocop:enable Metrics/MethodLength
 
+    # rubocop:disable Style/StringConcatenation
     def match_errors_message(match_errors)
       match_errors.map do |match_error|
         inside_str = if match_error[:inside].present?
@@ -42,9 +53,10 @@ class RspecHashMatcher
         msg = "Expected #{match_error[:key]}: #{render_error_value(match_error[:value])} to " \
               "#{match_error[:match_with]}: #{render_error_value(match_error[:match_value])}"
 
-        [inside_str, msg].compact.join(", ")
+        [inside_str, msg].compact.join(', ')
       end.join("\n") + "\n"
     end
+    # rubocop:enable Style/StringConcatenation
 
     def indifferent_hash(hash_or_obj, hash_to_match)
       if defined?(hash_or_obj.attributes) && hash_to_match.is_a?(Hash)
@@ -58,18 +70,48 @@ class RspecHashMatcher
 
     private
 
-    def not_both_hashes(hash_1, hash_2, inside:)
-      match_error_for(key: "hash", inside:, value: hash_1, match_value: hash_2,
-                      match_with: "be a hash")
+    def validate_options!(options)
+      return unless options[:coerce_values_to_json]
+
+      error_messages = []
+      unless options[:match_symbols_as_strings]
+        error_messages << '`coerce_values_to_json: true` incompatible ' \
+          'with `match_symbols_as_strings: true`'
+      end
+      unless options[:match_number_types]
+        error_messages << '`coerce_values_to_json: true` incompatible ' \
+          'with `match_nuimber_types: true`'
+      end
+
+      raise "Invalid options: #{error_messages.join(', ')}" if error_messages.any?
     end
 
-    def values_match?(value, match_value, match_array_order: nil, match_time_within: nil)
+    def not_both_hashes(hash_1, hash_2, inside:)
+      match_error_for(key: 'hash', inside:, value: hash_1, match_value: hash_2,
+                      match_with: 'be a hash')
+    end
+
+    def values_match?(value, match_value, options:)
       if value.is_a?(Time) && match_value.is_a?(Time)
-        times_match?(value, match_value, match_time_within:)
+        times_match?(value, match_value, match_time_within: options[:match_time_within])
       elsif value.is_a?(Array) && match_value.is_a?(Array)
-        arrays_match?(value, match_value, match_array_order:)
-      else
+        arrays_match?(value, match_value, ignore_array_order: options[:ignore_array_order])
+      elsif value.is_a?(Numeric) || match_value.is_a?(Numeric)
+        if options[:match_number_types]
+          value == match_value
+        else
+          BigDecimal(value.to_s) == BigDecimal(match_value.to_s)
+        end
+      elsif value.is_a?(Symbol) || match_value.is_a?(Symbol)
+        if options[:match_symbols_as_strings]
+          value.to_s == match_value.to_s
+        else
+          value == match_value
+        end
+      elsif options[:coerce_values_to_json]
         value.to_s == match_value.to_s
+      else
+        value == match_value
       end
     end
 
@@ -80,14 +122,14 @@ class RspecHashMatcher
     end
 
     # By default, match arrays without worrying about order
-    def arrays_match?(array_1, array_2, match_array_order: nil)
-      match_array_order ||= false
-      return array_1 == array_2 if match_array_order
+    def arrays_match?(array_1, array_2, ignore_array_order: nil)
+      ignore_array_order ||= false
+      return array_1 == array_2 if ignore_array_order
 
       array_1.map(&:to_s).sort == array_2.map(&:to_s).sort
     end
 
-    def match_error_for(key:, value:, match_value:, inside:, match_with: "equal")
+    def match_error_for(key:, value:, match_value:, inside:, match_with: 'equal')
       { key:, value:, match_value:, match_with: }.merge(inside.any? ? { inside: } : {})
     end
 
@@ -107,11 +149,11 @@ class RspecHashMatcher
   end
 end
 
-# TODO: figure out how to pass match_array_order and match_time_within
+# TODO: figure out how to pass options
 
 RSpec::Matchers.define :match_hash_flexibly do |expected|
   match do |actual|
-    match_errors = RspecHashMatcher.recursive_match_hashes(expected, actual)
+    match_errors = RspecHashMatcher.recursive_match_hashes_errors(expected, actual)
 
     # If there are any match errors, it didn't match!
     match_errors == []
@@ -121,7 +163,7 @@ RSpec::Matchers.define :match_hash_flexibly do |expected|
     # Redefine expected  'actual' to be what we're actually comparing against for the diff
     # Symbolize keys because in general, expected has symbolized keys and it improves the diff
     @actual = RspecHashMatcher.indifferent_hash(actual, expected).deep_symbolize_keys
-    match_errors = RspecHashMatcher.recursive_match_hashes(expected, actual)
+    match_errors = RspecHashMatcher.recursive_match_hashes_errors(expected, actual)
     RspecHashMatcher.match_errors_message(match_errors)
   end
 
--- spec/support/hash_matcher_spec.rb
+++ spec/support/hash_matcher_spec.rb
@@ -3,58 +3,63 @@ require 'rails_helper'
 # Tests to define what this custom matcher should do
 # file is spec/lib because if it was in support it would be required by rails_helper.rb
 RSpec.describe 'custom match_hash_flexibly and RspecHashMatcher' do # rubocop:disable RSpec/DescribeClass
-  context 'with two hashes' do
-    describe 'match_hash_flexibly' do
-      let(:time) { Time.current }
-      let(:hash_1) { { something: 12, 'else' => 'party', time: } }
-      let(:hash_2) { { 'something' => '12', else: :party, time: time + 0.1 } }
+  let(:options) { RspecHashMatcher::DEFAULT_OPTS }
+  # context 'with two hashes' do
+  #   describe 'match_hash_flexibly' do
+  #     let(:time) { Time.current }
+  #     let(:hash_1) { { something: 12, 'else' => 'party', time: } }
+  #     let(:hash_2) { { 'something' => 12.0, else: :party, time: time + 0.1 } }
 
-      it 'matches indifferently' do
-        expect(RspecHashMatcher.recursive_match_hashes(hash_1, hash_2)).to eq([])
-        expect(hash_1).to match_hash_flexibly hash_2
-      end
+  #     it 'matches indifferently' do
+  #       expect(RspecHashMatcher.recursive_match_hashes_errors(hash_1, hash_2)).to eq([])
+  #       expect(hash_1).to match_hash_flexibly hash_2
+  #     end
 
-      context 'with hash_2 missing key' do
-        let(:hash_2) { hash_1.except(:something) }
-        let(:target_error) do
-          [
-            { key: 'keys', value: %w[else time], match_value: %w[else something time],
-              match_with: 'equal' }
-          ]
-        end
+  #     context 'with hash_2 missing key' do
+  #       let(:hash_2) { hash_1.except(:something) }
+  #       let(:target_error) do
+  #         [
+  #           { key: 'keys', value: %w[else time], match_value: %w[else something time],
+  #             match_with: 'equal' }
+  #         ]
+  #       end
 
-        it "doesn't match" do
-          expect(RspecHashMatcher.recursive_match_hashes(hash_1, hash_2)).to eq(target_error)
-          expect(hash_1).not_to match_hash_flexibly hash_2
-        end
-      end
+  #       it "doesn't match" do
+  #         expect(RspecHashMatcher.recursive_match_hashes_errors(hash_1, hash_2)).to eq(target_error)
+  #         expect(hash_1).not_to match_hash_flexibly hash_2
+  #       end
+  #     end
 
-      context 'with hash_1 missing key' do
-        let(:hash_1) { hash_2.except(:else) }
+  #     context 'with hash_1 missing key' do
+  #       let(:hash_1) { hash_2.except(:else) }
 
-        it "doesn't match" do
-          expect(hash_1).not_to match_hash_flexibly hash_2
-        end
-      end
+  #       it "doesn't match" do
+  #         expect(hash_1).not_to match_hash_flexibly hash_2
+  #       end
+  #     end
 
-      context 'with nested hash' do
-        let(:hash_1) { { something: { foo: 'bar', bar: :foo } } }
-        let(:hash_2) { { 'something' => { bar: :foo, foo: :bar } }.as_json }
+  #     context 'with nested hash' do
+  #       let(:hash_1) { { something: { foo: 'bar', bar: :foo } } }
+  #       let(:hash_2) { { 'something' => { bar: :foo, foo: :bar } }.as_json }
 
-        it 'matches' do
-          expect(hash_1).to match_hash_flexibly hash_2
-        end
+  #       it 'matches' do
+  #         expect(hash_1).to match_hash_flexibly hash_2
+  #       end
 
-        context 'with a non match' do
-          let(:hash_2) { { 'something' => { bar: :foo, foo: :bar, barfoo: :foobar } } }
+  #       context 'with a non match' do
+  #         let(:hash_2) { { 'something' => { bar: :foo, foo: :bar, barfoo: :foobar } } }
 
-          it "doesn't match" do
-            expect(hash_1).not_to match_hash_flexibly hash_2
-          end
-        end
-      end
-    end
-  end
+  #         it "doesn't match" do
+  #           expect(hash_1).not_to match_hash_flexibly hash_2
+  #         end
+  #       end
+  #     end
+
+  #     context 'match_symbols_as_strings: false' do
+
+  #     end
+  #   end
+  # end
 
   context 'with ActiveRecord model' do
     let(:time) { 1.hour.ago }
@@ -78,6 +83,59 @@ RSpec.describe 'custom match_hash_flexibly and RspecHashMatcher' do # rubocop:di
     end
   end
 
+  describe 'validate_options!' do
+    it 'noops' do
+      expect(RspecHashMatcher.send(:validate_options!, options)).to be_nil
+    end
+    context 'with invalid match_symbols_as_strings' do
+      let(:options) { {coerce_values_to_json: true, match_symbols_as_strings: false} }
+      it 'raises' do
+        expect do
+          RspecHashMatcher.send(:validate_options!, options)
+        end.to raise_error(/invalid options/i)
+      end
+    end
+    context 'with invalid match_number_types' do
+      let(:options) { {coerce_values_to_json: true, match_number_types: true} }
+      it 'raises' do
+        expect do
+          RspecHashMatcher.send(:validate_options!, options)
+        end.to raise_error(/invalid options/i)
+      end
+    end
+  end
+
+  describe 'values_match?' do
+    context 'with number' do
+      it 'is truthy for matching Integer and Float' do
+        expect(options[:match_number_types]).to be_falsey # Sanity check default options
+        expect(RspecHashMatcher.send(:values_match?, 12, 12.0, options:)).to be_truthy
+        expect(RspecHashMatcher.send(:values_match?, 12.0, 12, options:)).to be_truthy
+      end
+      it 'is truthy for matching Float and BigDecimal' do
+        expect(RspecHashMatcher.send(:values_match?, 12.0, BigDecimal(12), options:)).to be_truthy
+        expect(RspecHashMatcher.send(:values_match?, BigDecimal(12), 12, options:)).to be_truthy
+      end
+      it 'is truthy for matching Number and String' do
+        expect(RspecHashMatcher.send(:values_match?, 12.0, '12', options:)).to be_truthy
+        expect(RspecHashMatcher.send(:values_match?, '12', 12.0, options:)).to be_truthy
+        expect(RspecHashMatcher.send(:values_match?, '12', BigDecimal('12.0'), options:)).to be_truthy
+      end
+      it 'is falsey if both are strings' do
+        expect(RspecHashMatcher.send(:values_match?, '12.0', '12', options:)).to be_falsey
+      end
+      context 'with match_number_types: true' do
+        let(:options) { RspecHashMatcher::DEFAULT_OPTS.merge(match_number_types: true) }
+        it 'is falsey' do
+          expect(RspecHashMatcher.send(:values_match?, 12.0, BigDecimal(12), options:)).to be_truthy
+          expect(RspecHashMatcher.send(:values_match?, 12.0, 12, options:)).to be_truthy
+          expect(RspecHashMatcher.send(:values_match?, 12, 12.00, options:)).to be_truthy
+          expect(RspecHashMatcher.send(:values_match?, 12.0, 12.00, options:)).to be_truthy
+        end
+      end
+    end
+  end
+
   describe 'times_match?' do
     let(:time_1) { Time.at(1_718_123_393) } # 2024-06-11 16:29:53
     let(:time_2) { time_1 - 0.2 }
@@ -85,6 +143,7 @@ RSpec.describe 'custom match_hash_flexibly and RspecHashMatcher' do # rubocop:di
     let(:round_time_2) { RspecHashMatcher.send(:round_time, time_2) }
 
     it 'matches the time' do
+      RspecHashMatcher.send(:values_match?, 12, 12.0, options:)
       expect(RspecHashMatcher.send(:times_match?, time_1, time_2)).to be_truthy
     end
 
