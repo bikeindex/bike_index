c9f5a767bf001d1b2feeac7ec630bed8ada4bb13
Seth Herr <seth.william.herr@gmail.com>
2025-01-20 09:52:48 -0800

    Add helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,10 @@ module ApplicationHelper
     super([key, locale: I18n.locale], options, &block)
   end
 
+  def translate_with_args(key, ...)
+    t(key, ...)
+  end
+
   def check_mark
     "&#x2713;".html_safe
   end
