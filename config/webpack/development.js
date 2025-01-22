1db73398ad403767f175cce020f3517d9ede74a7
Seth Herr <seth.william.herr@gmail.com>
2025-01-22 12:05:56 -0800

    clean install (probably revert most of these changes

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,13 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const environment = require('./environment')
+const webpackConfig = require('./base')
 
-// livereload on view file changes h/t https://github.com/rails/webpacker/issues/1879
-const chokidar = require('chokidar')
-environment.config.devServer.before = (app, server) => {
-  chokidar
-    .watch(['app/views/**/*.haml'])
-    .on('change', () => server.sockWrite(server.sockets, 'content-changed'))
-}
-
-module.exports = environment.toWebpackConfig()
+module.exports = webpackConfig
