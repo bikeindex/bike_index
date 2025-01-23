395eb8a61890664c25f00c76e7398b86fcd64da7
Seth Herr <seth.william.herr@gmail.com>
2025-01-22 16:34:43 -0800

    update configs

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
   # Don't check for precompiled assets
   config.assets.check_precompiled_asset = false
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+  # Render exception templates for rescuable exceptions and raise for other exceptions.
+  config.action_dispatch.show_exceptions = :rescuable
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
