9b29f6c75d33ace6b48f637a5b4767aaa4091d89
Seth Herr <seth.william.herr@gmail.com>
2025-01-23 16:07:04 -0800

    Revert unwanted upgrade changes

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+# Load the landing_pages organizations
+require_relative "landing_pages"
+
 # Load the Rails application.
 require_relative "application"
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,8 @@
-require "active_support/core_ext/integer/time"
-
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.enable_reloading = true
+  config.cache_classes = false
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -14,43 +10,37 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable server timing.
-  config.server_timing = true
-
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
+    config.cache_store = :redis_cache_store, {url: config.redis_cache_url}
 
-    config.cache_store = :memory_store
-    config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{2.days.to_i}" }
+    config.public_file_server.headers = {
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+    }
   else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.action_mailer.perform_caching = false
+  # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  # Disable caching for Action Mailer templates even if Action Controller
-  # caching is enabled.
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {host: "localhost", port: 3042}
+  if Rails.root.join("tmp", "skip-letter_opener.txt").exist?
+    config.action_mailer.perform_deliveries = false
+    config.action_mailer.delivery_method = :smtp
+  else
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :letter_opener
+  end
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
-  # Raise exceptions for disallowed deprecations.
-  config.active_support.disallowed_deprecation = :raise
-
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
@@ -58,21 +48,51 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
 
-  # Annotate rendered view with file names.
-  config.action_view.annotate_rendered_view_with_filenames = true
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
 
-  # Raise error when a before_action's only/except options reference missing actions.
-  config.action_controller.raise_on_missing_callback_actions = true
+  # Lograge is what we use in production, it makes requests create one log line, rather than the multitude created by default
+  # Useful to turn off primarily to monitor caching
+  # Run rails dev:lograge to toggle lograge. It's enabled by default
+  unless Rails.root.join("tmp", "non-lograge-dev.txt").exist?
+    config.lograge.enabled = true
+    config.log_level = :debug
+    config.lograge.formatter = Lograge::Formatters::Logstash.new # Use logstash format
+    config.lograge.custom_options = lambda do |event|
+      {
+        remote_ip: event.payload[:ip],
+        u_id: event.payload[:u_id],
+        params: event.payload[:params].except("controller", "action", "format", "id")
+      }
+    end
+  end
 
-  # Apply autocorrection by RuboCop to files generated by `bin/rails generate`.
-  # config.generators.apply_rubocop_autocorrect_after_generate!
+  # Bullet for n+1's
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
+
+  # Raises error for missing translations
+  config.i18n.raise_on_missing_translations = true
+
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
