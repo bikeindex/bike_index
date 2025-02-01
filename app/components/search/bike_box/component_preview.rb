353599cf79d85aa0162e3ad12a5eca7ac8f87b2c
Seth Herr <seth.william.herr@gmail.com>
2025-02-01 11:50:29 -0800

    wip, not correctly resolving paths

--- app/components/search/bike_box/component_preview.rb
+++ app/components/search/bike_box/component_preview.rb
@@ -3,6 +3,8 @@
 module Search::BikeBox
   class ComponentPreview < ApplicationComponentPreview
     def default
+      bike = Bike.find(2677107)
+      current_user = User.find(85)
       render(Search::BikeBox::Component.new(bike:, current_user:))
     end
   end
