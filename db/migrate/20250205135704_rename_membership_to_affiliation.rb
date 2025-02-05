cdd72809498ba5d79ae0a4f7b78df38f1d1519a4
Seth Herr <seth.william.herr@gmail.com>
2025-02-05 15:17:32 -0800

    wip

--- db/migrate/20250205135704_rename_membership_to_affiliation.rb
+++
@@ -1,4 +0,0 @@
-class RenameMembershipToAffiliation < ActiveRecord::Migration[8.0]
-  def change
-  end
-end
---
+++ db/migrate/20250205135704_rename_membership_to_organization_membership.rb
@@ -0,0 +1,4 @@
+class RenameMembershipToOrganizationMembership < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
