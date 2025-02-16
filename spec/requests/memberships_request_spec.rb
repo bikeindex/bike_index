58f3146457c0764a23d1f6d84e8dbb5a641b375c
Seth Herr <seth.william.herr@gmail.com>
2025-02-16 13:18:37 -0800

    wip

--- spec/requests/memberships_request_spec.rb
+++ spec/requests/memberships_request_spec.rb
@@ -6,9 +6,11 @@ RSpec.describe MembershipsController, type: :request do
 
   describe "new" do
     context "user not logged in" do
-      it "redirects" do
+      it "renders" do
         get "#{base_url}/new"
-        expect(response).to redirect_to(/users\/new/) # weird subdomain issue matching url directly otherwise
+        expect(response.code).to eq("200")
+        expect(response).to render_template("new")
+        expect(flash).to_not be_present
       end
     end
     context "with user" do
