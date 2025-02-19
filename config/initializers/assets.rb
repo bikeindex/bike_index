0225b93eac586750c6ac91e7c17cf57e3eba2759
Seth Herr <seth.william.herr@gmail.com>
2025-02-19 15:04:02 -0800

    update assets key

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.2"
+Rails.application.config.assets.version = "1.3"
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
