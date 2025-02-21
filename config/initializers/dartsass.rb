6a40d694f46de957fb76dd2be7f5939f1d452c18
Seth Herr <seth.william.herr@gmail.com>
2025-02-20 20:30:51 -0800

    Install dartsass (#2706)

---
+++ config/initializers/dartsass.rb
@@ -0,0 +1,10 @@
+Rails.application.config.dartsass.builds = {
+  "embed_styles.scss" => "embed_styles.css",
+  "embed_user_styles.scss" => "embed_user_styles.css",
+  # "registration_pdf.scss" => "registration_pdf.css", # unused, pdf generation is broken
+  "revised.scss" => "revised.css",
+  "spokecard.scss" => "spokecard.css",
+  "documentation_v2.scss" => "documentation_v2.css",
+  "email.scss" => "email.css",
+  "og_application.scss" => "og_application.css"
+}
