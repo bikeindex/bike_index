ab9c4d800ae4afd6057a820bfb3578ecfb1094ad
Seth Herr <seth.william.herr@gmail.com>
2025-02-20 19:49:39 -0800

    this seems to work too?

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,7 +1,8 @@
 Rails.application.config.dartsass.builds = {
   "embed_styles.scss" => "embed_styles.css",
   "embed_user_styles.scss" => "embed_user_styles.css",
-  "registration_pdf.scss" => "registration_pdf.css",
+  # "registration_pdf.scss" => "registration_pdf.css", # unused, pdf generation is broken
+  "legacy_app.scss" => "revised.css",
   "spokecard.scss" => "spokecard.css",
   "documentation_v2.scss" => "documentation_v2.css",
   "email.scss" => "email.css",
