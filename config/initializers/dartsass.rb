709b6010505859c27f0a5b48fe4ae8fc23a6c28d
Seth Herr <seth.william.herr@gmail.com>
2025-02-20 19:53:38 -0800

    maybe?

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -2,10 +2,9 @@ Rails.application.config.dartsass.builds = {
   "embed_styles.scss" => "embed_styles.css",
   "embed_user_styles.scss" => "embed_user_styles.css",
   # "registration_pdf.scss" => "registration_pdf.css", # unused, pdf generation is broken
-  "legacy_app.scss" => "revised.css",
+  "revised.scss" => "revised.css",
   "spokecard.scss" => "spokecard.css",
   "documentation_v2.scss" => "documentation_v2.css",
   "email.scss" => "email.css",
-  "og_application.scss" => "og_application.css",
-  # "registrations.scss" => "registrations.css",
+  "og_application.scss" => "og_application.css"
 }
