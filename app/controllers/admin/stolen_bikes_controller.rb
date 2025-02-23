011cedeeeef472c7f7f61d428df7985cd8243a96
Seth Herr <seth.william.herr@gmail.com>
2025-02-23 10:10:33 -0800

    Move ApproveStolenListingJob

--- app/controllers/admin/stolen_bikes_controller.rb
+++ app/controllers/admin/stolen_bikes_controller.rb
@@ -16,7 +16,7 @@ class Admin::StolenBikesController < Admin::BaseController
         stolen_record_ids.each do |id|
           stolen_record = StolenRecord.unscoped.find(id)
           stolen_record.update_attribute :approved, true
-          ApproveStolenListingJob.perform_async(stolen_record.bike_id)
+          StolenBike::ApproveStolenListingJob.perform_async(stolen_record.bike_id)
         end
         # Lazy pluralize hack
         flash[:success] = "#{stolen_record_ids.count} stolen #{(stolen_record_ids.count == 1) ? "bike" : "bikes"} approved!"
@@ -27,7 +27,7 @@ class Admin::StolenBikesController < Admin::BaseController
     else
       find_bike
       @bike.current_stolen_record.update_attribute :approved, true
-      ApproveStolenListingJob.perform_async(@bike.id)
+      StolenBike::ApproveStolenListingJob.perform_async(@bike.id)
       flash[:success] = "Stolen Bike was approved"
       redirect_to edit_admin_stolen_bike_url(@bike)
     end
