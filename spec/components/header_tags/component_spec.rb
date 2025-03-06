8758a6dcf9925c8911769f20e24075fc10c9fa80
Seth Herr <seth.william.herr@gmail.com>
2025-03-06 12:39:43 -0800

    update icons

--- spec/components/header_tags/component_spec.rb
+++ spec/components/header_tags/component_spec.rb
@@ -6,13 +6,15 @@ RSpec.describe HeaderTags::Component, type: :component do
   let(:options) { {page_title:, page_obj:, controller_name:, controller_namespace:, action_name:} }
   let(:page_title) { nil }
   let(:page_obj) { nil }
-  let(:controller_name) { "welcome" }
   let(:controller_namespace) { nil }
   let(:action_name) { "index" }
   let(:component) { render_inline(described_class.new(**options)) }
 
-  it "renders" do
-    expect(component).to be_present
-    pp component
+  context "welcome index" do
+    let(:controller_name) { "welcome" }
+    it "renders" do
+      expect(component).to be_present
+      pp component
+    end
   end
 end
