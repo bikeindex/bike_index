e60eecde747f29ec17abf1413c35e8d140de9ffb
Seth Herr <seth.william.herr@gmail.com>
2025-03-06 12:40:57 -0800

    remove unnecessary things from header tags component

--- app/components/header_tags/component_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module HeaderTags
-  class ComponentPreview < ApplicationComponentPreview
-    def default
-      render(HeaderTags::Component.new(page_title:, page_obj:, controller_name:, controller_namespace:, action_name:))
-    end
-  end
-end
