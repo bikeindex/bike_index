b9fc21812d847dfcf8be0446113bb2181bc5e221
Seth Herr <seth.william.herr@gmail.com>
2025-03-07 16:12:23 -0800

    Add it there

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
-# Use the GitHub Annotations formatter for CI
-if ENV["GITHUB_ACTIONS"] == "true"
-  require "rspec/github"
-  config.add_formatter RSpec::Github::Formatter
-end
 RSpec.configure do |config|
+  # Use the GitHub Annotations formatter for CI
+  if ENV["GITHUB_ACTIONS"] == "true"
+    require "rspec/github"
+    config.add_formatter RSpec::Github::Formatter
+  end
+
   # Use color in STDOUT
   config.color = true
 
