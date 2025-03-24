dc586509c63ff47671b8460c5086b7ceaaa27760
Seth Herr <seth.william.herr@gmail.com>
2025-03-24 15:03:32 -0700

    wip

--- app/components/bike_search_form/component.rb
+++ app/components/bike_search_form/component.rb
@@ -2,7 +2,7 @@
 
 module BikeSearchForm
   class Component < ApplicationComponent
-    def initialize() #distance:, include_hidden_search_fields:, include_location_search:, include_organized_search_fields:, location:, query:, raw_serial:, search_address:, search_email:, search_model_audit_id:, search_path:, search_secondary:, search_stickers:, serial:, skip_serial_field:, stolenness:)
+    def initialize(include_organized_search_fields: false, serial: nil) #distance:, include_hidden_search_fields:, include_location_search:, location:, query:, raw_serial:, search_address:, search_email:, search_model_audit_id:, search_path:, search_secondary:, search_stickers:, serial:, skip_serial_field:, stolenness:)
       # @distance = distance
       # @include_hidden_search_fields = include_hidden_search_fields
       # @include_location_search = include_location_search
@@ -16,7 +16,7 @@ module BikeSearchForm
       # @search_path = search_path
       # @search_secondary = search_secondary
       # @search_stickers = search_stickers
-      # @serial = serial
+      @serial = serial
       # @skip_serial_field = skip_serial_field
       # @stolenness = stolenness
     end
