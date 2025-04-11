952f1934d0b7e0596596c1e4c5b58e475ed0da03
Seth Herr <seth.william.herr@gmail.com>
2025-04-11 11:02:51 -0700

    f

--- app/models/marketplace_listing.rb
+++ app/models/marketplace_listing.rb
@@ -28,8 +28,13 @@
 #
 class MarketplaceListing < ApplicationRecord
   STATUS_ENUM = {draft: 0, for_sale: 1, sold: 2, removed: 3}.freeze
+  CONDITION_ENUM = {new: 0, like_new: 1, excellent: 2, good: 3, fair: 4, salvage: 5}.freeze
+
+  include Amountable
+  include Currencyable
 
   enum :status, STATUS_ENUM
+  enum :condition, CONDITION_ENUM
 
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
