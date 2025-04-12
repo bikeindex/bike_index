c42a6bc0586adcbc7b61905963c4196bada7a711
Seth Herr <seth.william.herr@gmail.com>
2025-04-11 17:21:09 -0700

    user text block

---
+++ spec/components/user_text_block_display/component_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UserTextBlockDisplay::Component, type: :component do
+  let(:text) do
+    "I'm baby xOXO migas hot chicken, succulents marfa art party kogi hammock glossier synth squid."
+    "\n\nKnausgaard wolf cornhole, intelligentsia solarpunk cray pour-over gluten-free edison bulb " \
+    "fixie squid paleo artisan gochujang gastropub."
+  end
+  let(:component) { render_inline(described_class.new(text:)) }
+
+  it "renders" do
+    expect(component).to be_present
+  end
+end
