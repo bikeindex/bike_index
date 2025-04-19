3d856c52dc8bc4416c0ae95a9d4a99692a54e2b0
Seth Herr <seth.william.herr@gmail.com>
2025-04-18 20:56:17 -0700

    drive by remove unnecessary service

--- spec/jobs/callbacks/ambassador_task_after_create_job_spec.rb
+++ spec/jobs/callbacks/ambassador_task_after_create_job_spec.rb
@@ -13,4 +13,30 @@ RSpec.describe Callbacks::AmbassadorTaskAfterCreateJob, type: :job do
         .to(have_received(:assign_task_to_all_ambassadors).once)
     end
   end
+
+  describe ".assign_task_to_all_ambassadors" do
+    context "given an AmbassadorTask or AmbassadorTask id" do
+      it "idempotently creates assignments to the given task for all ambassadors" do
+        user = FactoryBot.create(:user_confirmed)
+        a1, a2, a3 = FactoryBot.create_list(:ambassador, 3)
+        task = FactoryBot.create(:ambassador_task)
+
+        AmbassadorTaskAssignmentCreator.assign_task_to_all_ambassadors(task)
+
+        expect(AmbassadorTaskAssignment.count).to eq(3)
+        expect(user.ambassador_task_assignments.count).to eq(0)
+        expect(a1.ambassador_task_assignments.count).to eq(1)
+        expect(a2.ambassador_task_assignments.count).to eq(1)
+        expect(a3.ambassador_task_assignments.count).to eq(1)
+
+        AmbassadorTaskAssignmentCreator.assign_task_to_all_ambassadors(task)
+
+        expect(AmbassadorTaskAssignment.count).to eq(3)
+        expect(user.ambassador_task_assignments.count).to eq(0)
+        expect(a1.ambassador_task_assignments.count).to eq(1)
+        expect(a2.ambassador_task_assignments.count).to eq(1)
+        expect(a3.ambassador_task_assignments.count).to eq(1)
+      end
+    end
+  end
 end
