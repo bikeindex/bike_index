3d75fa68f98f5196ec2059607905c55d986820ac
Seth Herr <seth.william.herr@gmail.com>
2025-04-21 08:55:02 -0700

    wip

--- app/models/marketplace_listing.rb
+++ app/models/marketplace_listing.rb
@@ -27,19 +27,20 @@
 #  index_marketplace_listings_on_seller_id          (seller_id)
 #
 class MarketplaceListing < ApplicationRecord
-  STATUS_ENUM = {draft: 0, for_sale: 1, sold: 2, removed: 3}.freeze
-  CONDITION_ENUM = {new: 0, like_new: 1, excellent: 2, good: 3, fair: 4, salvage: 5}.freeze
+  # STATUS_ENUM = {draft: 0, for_sale: 1, sold: 2, removed: 3}.freeze
+  # CONDITION_ENUM = {new: 0, like_new: 1, excellent: 2, good: 3, fair: 4, salvage: 5}.freeze
 
-  include Amountable
-  include Currencyable
+  # include Amountable
+  # include Currencyable
 
-  enum :status, STATUS_ENUM
-  enum :condition, CONDITION_ENUM
+  # enum :status, STATUS_ENUM
+  # enum :condition, CONDITION_ENUM
 
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
   belongs_to :item, polymorphic: true
   belongs_to :address_record
+  belongs_to :primary_activity
 
 
 end
