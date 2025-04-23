76614a9ea8a39906353a1a53f9a71b311d06bb1f
Seth Herr <seth.william.herr@gmail.com>
2025-04-23 16:35:02 -0700

    what about that?

--- spec/services/spreadsheets/manufacturers_spec.rb
+++ spec/services/spreadsheets/manufacturers_spec.rb
@@ -71,15 +71,15 @@ RSpec.describe Spreadsheets::Manufacturers do
         expect_target_manufacturer(manufacturer.reload)
       end
       context "with existing mnfg with alternate_name" do
-        let(:manufacturer) { FactoryBot.create(:manufacturer, name: "B'twin something (Btwin)") }
+        let!(:manufacturer) { FactoryBot.create(:manufacturer, name: "B'twin something (Btwin)") }
         it "updates" do
           expect { described_class.send(:update_or_create_for!, row) }.to change(Manufacturer, :count).by 0
           expect_target_manufacturer(manufacturer.reload)
         end
       end
       context "with no matching manufacturer" do
-        let!(:manufacturer) { nil }
-        it "creates", :flaky do
+        let!(:manufacturer) { FactoryBot.create(:manufacturer) }
+        it "creates" do
           expect { described_class.send(:update_or_create_for!, row) }.to change(Manufacturer, :count).by 1
           expect_target_manufacturer(Manufacturer.order(:id).last)
         end
