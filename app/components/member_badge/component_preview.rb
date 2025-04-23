fd155f08ae8671398f12b083303ebd1721350fce
Seth Herr <seth.william.herr@gmail.com>
2025-04-23 14:16:03 -0700

    good enough for now?

--- app/components/member_badge/component_preview.rb
+++ app/components/member_badge/component_preview.rb
@@ -5,19 +5,23 @@ module MemberBadge
     # @group Level Variants
     # @param level "The membership level"
     def none(level: nil)
-      render(MemberBadge::Component.new(level:))
+      render(MemberBadge::Component.new(level:, classes: "tw:max-w-xs"))
     end
 
     def basic(level: :basic)
-      render(MemberBadge::Component.new(level:))
+      render(MemberBadge::Component.new(level:, classes: "tw:max-w-xs"))
     end
 
     def plus(level: :plus)
-      render(MemberBadge::Component.new(level:))
+      render(MemberBadge::Component.new(level:, classes: "tw:max-w-xs"))
     end
 
     def patron(level: :patron)
-      render(MemberBadge::Component.new(level:))
+      render(MemberBadge::Component.new(level:, classes: "tw:max-w-xs"))
+    end
+
+    def patron_shadow(level: :patron)
+      render(MemberBadge::Component.new(level:, classes: "tw:max-w-xs", shadow: true))
     end
   end
 end
