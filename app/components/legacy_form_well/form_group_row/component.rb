7bc7d3fffa378011d8fd891b5efeb768760a7f52
Seth Herr <seth.william.herr@gmail.com>
2025-04-29 18:59:38 -0700

    add amount

---
+++ app/components/legacy_form_well/form_group_row/component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module LegacyFormWell::FormGroupRow
+  class Component < ApplicationComponent
+    def initialize(form_builder:, label:, label_translation: nil, row_classes: "form-group row")
+      @form_builder = form_builder
+      @label = label
+      @label_translation = label_translation
+      @row_classes = row_classes&.to_s + " form-group row"
+    end
+  end
+end
