3cc3a145595d6a054b8ae23e46917cb88336f44f
Seth Herr <seth.william.herr@gmail.com>
2025-05-07 09:59:02 -0700

    multi_column

--- app/components/definition_list/container/component.rb
+++ app/components/definition_list/container/component.rb
@@ -5,13 +5,5 @@ module DefinitionList::Container
     def initialize(multi_columns: false)
       @multi_columns = multi_columns
     end
-
-    private
-
-    def dl_split_classes
-      return "" unless @multi_columns
-
-      "tw:grid tw:@sm:grid-cols-2 tw:gap-x-4"
-    end
   end
 end
