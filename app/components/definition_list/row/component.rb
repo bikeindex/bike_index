07d5eccfdc71d13235120a9cf0fdf2b6dabbd938
Seth Herr <seth.william.herr@gmail.com>
2025-05-08 21:36:19 -0700

    wip

--- app/components/definition_list/row/component.rb
+++ app/components/definition_list/row/component.rb
@@ -4,11 +4,16 @@ module DefinitionList::Row
   class Component < ApplicationComponent
     PERMITTED_KINDS = %i[full_width].freeze
 
-    def initialize(label:, value: nil, render_with_no_value: false, kind: nil)
+    def initialize(label:, value: nil, render_with_no_value: false, kind: nil, time_localizer_settings: nil)
       @label = label
       @value = value
       @render_with_no_value = render_with_no_value
       @kind = kind.to_sym if PERMITTED_KINDS.include?(kind&.to_sym)
+
+      # TODO: actually support originalTimeZone. We add the timezone, but it's currently the user's timezone
+      @include_time_zone = time_localizer_settings&.include?("originalTimeZone") || false
+
+      @time_localizer_classes = time_localizer_classes(time_localizer_settings)
     end
 
     def render?
@@ -41,6 +46,12 @@ module DefinitionList::Row
       else
         "tw:@sm:text-right tw:@sm:w-1/4 tw:min-w-[100px]"
       end + " tw:text-sm tw:leading-none tw:opacity-65 tw:font-bold!"
+    end
+
+    def time_localizer_classes(time_localizer_settings)
+      time_localizer_settings ||= ""
+      time_localizer_settings += "convertTime"
+      time_localizer_settings
     end
   end
 end
