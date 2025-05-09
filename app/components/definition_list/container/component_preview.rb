45691443f90d9f7c08d4804dc83cf0be1a5be916
Seth Herr <seth.william.herr@gmail.com>
2025-05-09 11:30:02 -0700

    oop, keep default

--- app/components/definition_list/container/component_preview.rb
+++ app/components/definition_list/container/component_preview.rb
@@ -3,12 +3,12 @@
 module DefinitionList::Container
   class ComponentPreview < ApplicationComponentPreview
     # @!group Multi Column
-    def multi_columns_false(multi_columns: false)
-      {template: "definition_list/container/component_preview/default", locals: {multi_columns:}}
+    def default
+      {template: "definition_list/container/component_preview/default", locals: {multi_columns: false}}
     end
 
-    def multi_columns_true(multi_columns: true)
-      {template: "definition_list/container/component_preview/default", locals: {multi_columns:}}
+    def multi_columns_true
+      {template: "definition_list/container/component_preview/default", locals: {multi_columns: true}}
     end
     # @!endgroup
   end
