6fcaf5fad5b0f706bbfe3b7bb542670f6fcc86db
Seth Herr <seth.william.herr@gmail.com>
2025-05-09 11:22:24 -0700

    lint

--- app/components/definition_list/container/component_preview.rb
+++ app/components/definition_list/container/component_preview.rb
@@ -4,11 +4,11 @@ module DefinitionList::Container
   class ComponentPreview < ApplicationComponentPreview
     # @!group Multi Column
     def multi_columns_false(multi_columns: false)
-      { template: 'definition_list/container/component_preview/default', locals: { multi_columns: } }
+      {template: "definition_list/container/component_preview/default", locals: {multi_columns:}}
     end
 
     def multi_columns_true(multi_columns: true)
-      { template: 'definition_list/container/component_preview/default', locals: { multi_columns: } }
+      {template: "definition_list/container/component_preview/default", locals: {multi_columns:}}
     end
     # @!endgroup
   end
