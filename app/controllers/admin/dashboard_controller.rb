12f267410bc2947a68774dd0438f1fbce880af66
Seth Herr <seth.william.herr@gmail.com>
2025-05-21 09:16:40 -0700

    revert drive by bug fixes, handle in separate PR

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -16,10 +16,10 @@ class Admin::DashboardController < Admin::BaseController
     # @bikes here because this is the only one we're using the standard admin bikes table
     @bikes = Bike.unscoped.order("created_at desc").where(example: true).limit(10)
     mnfg_other_id = Manufacturer.other.id
-    @component_mnfgs = Component.where(manufacturer_id: mnfg_other_id).reorder(id: :desc).limit(100)
+    @component_mnfgs = Component.where(manufacturer_id: mnfg_other_id)
     @bike_mnfgs = Bike.where(manufacturer_id: mnfg_other_id)
     @component_types = Component.where(ctype_id: Ctype.other.id)
-    @handlebar_types = Bike.where(handlebar_type: Bike.handlebar_types[:other]).reorder(id: :desc).limit(50)
+    @handlebar_types = Bike.where(handlebar_type: Bike.handlebar_types[:other])
     @paint = Paint.where("color_id IS NULL")
   end
 
