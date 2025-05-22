0504fa14cbba37dbc238b2937012c59fa455761c
Seth Herr <seth.william.herr@gmail.com>
2025-05-22 11:50:40 -0500

    Admin fixes (#2828)
    - Replace `shared/errors` with the `AlertForErrors::Component`
    - Updates to `admin/maintenance` to render components better
    - Manufacturers assign matching components (not just bikes)

--- spec/jobs/callbacks/after_manufacturer_change_job_spec.rb
+++ spec/jobs/callbacks/after_manufacturer_change_job_spec.rb
@@ -10,13 +10,20 @@ RSpec.describe Callbacks::AfterManufacturerChangeJob, type: :job do
   let!(:bike2) { FactoryBot.create(:bike, manufacturer: Manufacturer.other, manufacturer_other: "PEDEGO ELECTRIC bikes ") }
   let!(:bike3) { FactoryBot.create(:bike, manufacturer: Manufacturer.other, manufacturer_other: "PEDEGOgo") }
   let!(:bike4) { FactoryBot.create(:bike, manufacturer: Manufacturer.other, manufacturer_other: "PEDEGOes") }
+  let!(:component1) { FactoryBot.create(:component, bike:, manufacturer: Manufacturer.other, manufacturer_other: "\nPEDEGO ELECTRIC bikes ") }
+  let!(:component2) { FactoryBot.create(:component, bike:, manufacturer: Manufacturer.other, manufacturer_other: "   PEDEGOes") }
 
   it "assigns the user coordinates" do
     expect(bike2.reload.manufacturer_other).to eq "PEDEGO ELECTRIC bikes"
 
+    Sidekiq::Job.clear_all
     manufacturer.update(name: "Pedego Electric Bikes (Pedegogo)")
-    expect(manufacturer.priority).to eq 0
-    instance.perform(manufacturer.id)
+    expect(manufacturer.reload.priority).to eq 0
+    expect(described_class.jobs.count).to eq 1
+
+    expect do
+      instance.perform(manufacturer.id)
+    end.to_not change(described_class.jobs, :count)
 
     expect(bike.reload.manufacturer_id).to eq manufacturer.id
 
@@ -30,6 +37,14 @@ RSpec.describe Callbacks::AfterManufacturerChangeJob, type: :job do
     expect(bike3.manufacturer_other).to be_nil
 
     expect(bike4.reload.manufacturer_id).to eq Manufacturer.other.id
+    expect(bike4.manufacturer_other).to eq "PEDEGOes"
+
+    expect(component1.reload.manufacturer_id).to eq manufacturer.id
+    expect(component1.manufacturer_other).to be_nil
+
+    expect(component2.reload.manufacturer_id).to eq Manufacturer.other.id
+    expect(component2.manufacturer_other).to eq "PEDEGOes"
+
     expect(manufacturer.reload.priority).to eq 10
   end
 end
