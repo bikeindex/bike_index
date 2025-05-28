5050351e3a661deb18c14de27a9e9772c4aa103e
Seth Herr <seth.william.herr@gmail.com>
2025-05-28 09:21:30 -0700

    always enable importmaps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,28 +1,29 @@
-import '@hotwired/turbo-rails'
+import "@hotwired/turbo-rails";
 
 // Import stimulus controllers
-import { Application } from '@hotwired/stimulus'
+import { Application } from "@hotwired/stimulus";
 // Lazy load all controllers
-import { lazyLoadControllersFrom } from '@hotwired/stimulus-loading'
+import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading";
 
-import TimeLocalizer from 'utils/time_localizer'
+import TimeLocalizer from "utils/time_localizer";
 
 /* global Turbo */
 // Disable Turbo by default, only enable on case-by-case
 // You must include data-turbo="true" on the elements you want to enable turbo on
-Turbo.session.drive = false
-const application = Application.start()
+Turbo.session.drive = false;
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus = application
+application.debug = false;
+window.Stimulus = application;
 
-lazyLoadControllersFrom('components', application)
+lazyLoadControllersFrom("components", application);
 
-function localizeTime () {
-  if (!window.timeLocalizer) window.timeLocalizer = new TimeLocalizer()
-  window.timeLocalizer.localize()
+function localizeTime() {
+  console.log("localizeTime!");
+  if (!window.timeLocalizer) window.timeLocalizer = new TimeLocalizer();
+  window.timeLocalizer.localize();
 }
 
-document.addEventListener('DOMContentLoaded', localizeTime)
-document.addEventListener('turbo:render', localizeTime)
+document.addEventListener("DOMContentLoaded", localizeTime);
+document.addEventListener("turbo:render", localizeTime);
