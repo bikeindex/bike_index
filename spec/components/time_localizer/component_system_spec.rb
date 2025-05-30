8c298ab749a8dffcd58fafbd6c9a3d235c882ff6
Seth Herr <seth.william.herr@gmail.com>
2025-05-30 14:25:51 -0500

    Hide discourse by default (#2831)

--- spec/components/time_localizer/component_system_spec.rb
+++ spec/components/time_localizer/component_system_spec.rb
@@ -7,17 +7,21 @@ RSpec.describe "time_localizer.js", :js, type: :system do
   let(:preview_path) { "/rails/view_components/time_localizer/component/default?time_zone=#{CGI.escape(time_zone)}" }
   let(:current_in_zone) { TimeParser.parse(Time.current, time_zone, in_time_zone: true) }
 
+  def strip(str)
+    str.strip.gsub("  ", " ") # required because dumb spaces in strftime
+  end
+
   it "has the expected times" do
     visit(preview_path)
 
-    expect(page).to have_content("Current time: #{current_in_zone.strftime("%l:%M %p")}", wait: 5)
+    expect(page).to have_content("Current time: #{strip(current_in_zone.strftime("%l:%M %p"))}", wait: 5)
 
-    expect(page).to have_content("Yesterday: #{(current_in_zone - 1.day).strftime("%B %e")}")
-    expect(page).to have_content("Tomorrow: #{(current_in_zone + 1.day).strftime("%B %e")}")
-    expect(page).to have_content("One week ago: #{(current_in_zone - 7.days).strftime("%B %e")}")
-    expect(page).to have_content("One year ago: #{(current_in_zone - 1.year).strftime("%B %e, %Y")}")
+    expect(page).to have_content("Yesterday: #{strip((current_in_zone - 1.day).strftime("%B %e"))}")
+    expect(page).to have_content("Tomorrow: #{strip((current_in_zone + 1.day).strftime("%B %e"))}")
+    expect(page).to have_content("One week ago: #{strip((current_in_zone - 7.days).strftime("%B %e"))}")
+    expect(page).to have_content("One year ago: #{strip((current_in_zone - 1.year).strftime("%B %e, %Y"))}")
 
-    expect(page).to have_content("Yesterday (precise time): #{(current_in_zone - 1.day).strftime("%B %e, %l:%M %p")}")
-    expect(page).to have_content("One year ago (precise time seconds): #{(current_in_zone - 1.year).strftime("%B %-e, %Y, %-l:%M:%S %p")}")
+    expect(page).to have_content("Yesterday (precise time): #{strip((current_in_zone - 1.day).strftime("%B %e, %l:%M %p")).gsub("  ", " ")}")
+    expect(page).to have_content("One year ago (precise time seconds): #{strip((current_in_zone - 1.year).strftime("%B %-e, %Y, %-l:%M:%S %p"))}")
   end
 end
