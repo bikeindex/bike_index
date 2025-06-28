c917bf4f277720e12f2abcab21ffab1e569c0366
Seth Herr <seth.william.herr@gmail.com>
2025-06-28 11:25:00 -0700

    Fix spec issues

--- spec/requests/webhooks_request_spec.rb
+++ spec/requests/webhooks_request_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe WebhooksController, type: :request do
-  let(:re_record_interval) { 90.days }
+  let(:re_record_interval) { 6.months }
 
   describe "POST stripe" do
     let(:webhook_url) { "/webhooks/stripe" }
