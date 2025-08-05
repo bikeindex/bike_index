1866c058a598b2070e6222fdbe0cc23bb6b66c41
Seth Herr <seth.william.herr@gmail.com>
2025-08-05 14:23:39 -0700

    github actions setup-imagemagick

--- app/jobs/bike_deleter_job.rb
+++ app/jobs/bike_deleter_job.rb
@@ -1,7 +1,12 @@
 class BikeDeleterJob < ApplicationJob
   sidekiq_options retry: false, queue: "low_priority"
 
-  def perform(bike_id)
-    Bike.unscoped.find_by_id(bike_id).destroy
+  def perform(bike_id, really_delete=false)
+    if really_delete
+      Ownership.where(bike_id:).destroy_all
+      Bike.unscoped.find_by_id(bike_id).really_destroy!
+    else
+      Bike.unscoped.find_by_id(bike_id).destroy
+    end
   end
 end
