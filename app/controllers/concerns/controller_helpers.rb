465a62b3d3f153a42b124fd98189b8eabce9fe85
Seth Herr <seth.william.herr@gmail.com>
2025-08-15 10:24:40 -0700

    refactor

--- app/controllers/concerns/controller_helpers.rb
+++ app/controllers/concerns/controller_helpers.rb
@@ -223,15 +223,12 @@ module ControllerHelpers
   # maps to controllers.locks.find_lock.not_your_lock.
   def translation(key, scope: nil, controller_method: nil, **kwargs)
     if scope.blank? && controller_method.blank?
-      controller_method =
-        caller_locations
-          .slice(0, 2)
-          .map(&:label)
-          .reject { |label| label.include?("rescue in") }
-          .first
-      # Ruby 3.4+ includes class name in label (e.g., "MyAccountsController#update")
-      # Extract just the method name
-      controller_method = controller_method.split("#").last if controller_method&.include?("#")
+      controller_method = caller_locations
+        .first(2)
+        .map(&:label)
+        .find { |label| !label.include?("rescue in") }
+        &.split("#")
+        &.last
     end
 
     scope ||= [:controllers, controller_namespace, controller_name, controller_method.to_sym]
