a8e85cf4ca2f72225b66931e3d67b89f69b4c76c
Seth Herr <seth.william.herr@gmail.com>
2025-08-15 09:53:46 -0700

    remove unused controller

--- app/javascript/controllers/search/result_view_select_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@hotwired/stimulus'
-
-// Connects to data-controller='search--result-view-select'
-export default class extends Controller {
-  connect () {
-    console.log('app/javascript/controllers/search/result_view_select_controller.js - connected to:')
-    console.log(this.element)
-  }
-}
