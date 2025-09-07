bikeindex/bike_index

commit 2e0c787a9ea772f531e5802558306fce9b82f073

Organized bike panel update (#2898)
- Improve registration number display
- Show more ownership information

app/helpers/application_component_helper.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
+# General helper methods shared with View Components
 module ApplicationComponentHelper
   def number_display(number)
     content_tag(:span, number_with_delimiter(number), class: ((number == 0) ? "less-less-strong" : ""))
+  end
+
+  def check_mark
+    "&#x2713;".html_safe
+  end
+
+  def cross_mark
+    "&#x274C;".html_safe
+  end
+
+  def search_emoji
+    "🔎"
+  end
+
+  def link_emoji
+    image_tag("link.svg", class: "link-emoji")
   end
 end

app/helpers/application_helper.rb
@@ -5,22 +5,6 @@ module ApplicationHelper
     super([key, locale: I18n.locale], options, &block)
   end
 
-  def check_mark
-    "&#x2713;".html_safe
-  end
-
-  def cross_mark
-    "&#x274C;".html_safe
-  end
-
-  def search_emoji
-    "🔎"
-  end
-
-  def link_emoji
-    image_tag("link.svg", class: "link-emoji")
-  end
-
   def notification_delivery_display(status)
     text = if status == "delivery_success"
       check_mark

app/components/org/bike_access_panel/component.en.yml
@@ -1,0 +1,79 @@
+---
+en:
+  components:
+    org:
+      bike_access_panel:
+        access_panel: Access Panel
+        added_to_track_parking_notification: >-
+          This %{bike_type} is not registered to a user. It was added to track parking
+          notifications.
+        additional_registrations_information: Registrations by user
+        additional_registrations_information_visible_because: visible with %{org_name}
+          law enforcement access
+        additional_registrations_potential_duplicates: 'Potential duplicate bikes
+          matching serial segments:'
+        additional_registrations_registered_at: Registered at
+        additional_registrations_this_bike: This %{bike_type}
+        address: Registration Address
+        address_or_intersection: Address or intersection
+        as_a_bikeindex_ambassador_html: >-
+          <em><span class="hidden-md-down">As a Bike Index ambassador,</span> you
+          can mark it as recovered:</em>
+        assign_sticker: Assign sticker
+        avery_exportable: Avery Exportable
+        believe_it_is_anyway: Believe it is anyway?
+        bike_is_not_registered_html: This %{bike_type} is <em>not</em> registered
+          with %{org_name}
+        bike_is_registered: This %{bike_type} is registered with %{org_name}
+        bike_not_marked_stolen: "%{bike_type} is not marked stolen."
+        choose_country: Choose country
+        city: City
+        claimed: Claimed
+        contact_the_owner_it_is_stolen: to the owner of this %{bike_type} that you
+          believe is stolen.
+        creation_source: Source
+        creator: Creator
+        edit: Edit
+        email_to_deal_with_this_html: Email %{support_link} to deal with this.
+        initial_registration: 'initial registration:'
+        is_false: 'false'
+        is_not_registered_html: "<em>not</em> reged with %{org_name}"
+        is_registered: reged with %{org_name}
+        is_true: 'true'
+        link_sticker: Link Sticker
+        link_url_for_online_sighting: Link url for online sighting (optional)
+        mark_bike_recovered: Mark bike recovered
+        missing_address: missing address
+        missing_owner_name: missing user name
+        model_audit: E-Vehicle audit
+        not_audited: Not audited
+        or_call_owner_html: "<em>Or call</em> <strong>Owner's phone number:</strong>"
+        org_can_edit_bike: "%{org_name} can edit this %{bike_type}"
+        org_cannot_edit: "%{org_name} can not edit"
+        organization_can_edit_after_claimed: and can edit after %{bike_type} claimed
+        organization_child: Child organization
+        organization_registered: Organization
+        owner_email: Owner email
+        owner_name: Owner name
+        parking_notification_notes_html: "%{org_name} internal notes <small>not shown
+          to user</small>"
+        permission: Permission
+        registered: Registered
+        send_message: Send message
+        state: State
+        sticker: Sticker
+        sticker_code: Sticker Code
+        support_email: support@bikeindex.org
+        unable_to_display_additional_information: >-
+          Unable to display additional information because %{bike_type} is not registered
+          with %{org_name}
+        unregistered_parking_notification: created for parking notification
+        user_revoked_permission_html: >-
+          This user <strong>revoked</strong> permission to contact them prior to their
+          bike being marked stolen.
+        waiting_on_device_location: Waiting on device location
+        where_did_you_see_this_bike: >-
+          Where did you see this %{bike_type}? If you're contacting about an ad you
+          found online, include a link in the field below
+        write_a_message: Write a message
+        zipcode: Postal code

app/components/org/bike_access_panel/component.rb
@@ -1,0 +1,57 @@
+# frozen_string_literal: true
+
+module Org::BikeAccessPanel
+  class Component < ApplicationComponent
+    include OrganizedHelper
+
+    def initialize(bike: nil, organization: nil, current_user: nil)
+      @bike = bike
+      @organization = organization
+      @user = current_user
+    end
+
+    def render?
+      @bike.present? && @organization.present? && @user.present? &&
+        @user.authorized?(@organization) && @bike.visible_by?(@user)
+    end
+
+    private
+
+    def organization_registered?
+      return @organization_registered if defined?(@organization_registered)
+
+      @organization_registered = @bike.organized?(@organization)
+    end
+
+    def organization_authorized?
+      @bike.authorized_by_organization?(org: @organization)
+    end
+
+    def user_can_edit?
+      @bike.authorized?(@user)
+    end
+
+    # Only show the unstolen notification form if bike is with_owner (ie, not if bike is found)
+    def display_unstolen_notification_form?
+      @bike.status_with_owner? && @organization.enabled?("unstolen_notifications")
+    end
+
+    def show_sticker_modal?
+      # display stickers if org has paid for them
+      @organization.enabled?("bike_stickers")
+    end
+
+    def display_original_ownership?
+      original_ownership.id != @bike.current_ownership_id &&
+        original_ownership.organization_id == @organization.id
+    end
+
+    def current_ownership
+      @current_ownership ||= @bike.current_ownership
+    end
+
+    def original_ownership
+      @original_ownership ||= @bike.ownerships.initial.first
+    end
+  end
+end

app/components/org/bike_access_panel/component_preview.rb
@@ -1,0 +1,14 @@
+# frozen_string_literal: true
+
+module Org::BikeAccessPanel
+  class ComponentPreview < ApplicationComponentPreview
+    # @display legacy_stylesheet true
+    def default
+      # TODO: render something - without personal info
+      organization = Organization.friendly_find("hogwarts")
+      bike = nil
+      current_user = nil
+      render(Org::BikeAccessPanel::Component.new(bike:, organization:, current_user:))
+    end
+  end
+end

app/controllers/bikes_controller.rb
@@ -21,9 +21,7 @@ class BikesController < Bikes::BaseController
       @impound_claim ||= @bike.current_impound_record&.impound_claims&.build
       @submitted_impound_claims = impound_claims.where.not(id: @impound_claim.id).submitted
     end
-    # These ivars are here primarily to make testing possible
-    @passive_organization_registered = passive_organization.present? && @bike.organized?(passive_organization)
-    @passive_organization_authorized = passive_organization.present? && @bike.authorized_by_organization?(org: passive_organization)
+
     if params[:scanned_id].present?
       @bike_sticker = BikeSticker.lookup_with_fallback(params[:scanned_id], organization_id: params[:organization_id], user: current_user)
     end

app/jobs/revert_bike_sticker_update_job.rb
@@ -26,7 +26,7 @@ class RevertBikeStickerUpdateJob < ApplicationJob
       .where("update_number > ?", bike_sticker_update.update_number)
 
     if following_updates.present?
-      raise "Following claim failed"
+      raise "Following claim, failing"
 
     elsif previous_updates.present?
       last_update = previous_updates.first

commit 5c09a38bd12195b0601ba8e87f547d696d7d5c60

Fix phone link in org access panel (#2903)
bug introduced in #2898

app/components/org/bike_access_panel/component.html.haml
@@ -289,7 +289,7 @@
                     %span.localizeTime
                       = l duplicate_bike.created_at, format: :convert_time
                   %td
-                    = link_to organized_bike_textranslation(duplicate_bike), bike_path(duplicate_bike)
+                    = link_to organized_bike_text(duplicate_bike), bike_path(duplicate_bike)
                     %small.less-strong
                       = duplicate_bike.normalized_serial_segments.considered_for_duplicate.pluck(:segment).join(", ")
       - # TODO: add translations

app/helpers/application_component_helper.rb
@@ -21,4 +21,17 @@ module ApplicationComponentHelper
   def link_emoji
     image_tag("link.svg", class: "link-emoji")
   end
+
+  def phone_display(str)
+    return "" if str.blank?
+    phone_components = Phonifyer.components(str)
+    number_to_phone(phone_components[:number], phone_components.except(:number))
+  end
+
+  def phone_link(phone, html_options = {})
+    return "" if phone.blank?
+    phone_d = phone_display(phone)
+    # Switch extension to be pause in link
+    link_to(phone_d, "tel:#{phone_d.tr("x", ";")}", html_options)
+  end
 end

app/helpers/application_helper.rb
@@ -152,19 +152,6 @@ module ApplicationHelper
     )
   end
 
-  def phone_display(str)
-    return "" if str.blank?
-    phone_components = Phonifyer.components(str)
-    number_to_phone(phone_components[:number], phone_components.except(:number))
-  end
-
-  def phone_link(phone, html_options = {})
-    return "" if phone.blank?
-    phone_d = phone_display(phone)
-    # Switch extension to be pause in link
-    link_to(phone_d, "tel:#{phone_d.tr("x", ";")}", html_options)
-  end
-
   def twitterable(user)
     if user.show_twitter && user.twitter
       link_to "Twitter", "https://twitter.com/#{user.twitter}"

spec/helpers/application_component_helper_spec.rb
@@ -38,4 +38,30 @@ RSpec.describe ApplicationComponentHelper, type: :helper do
       end
     end
   end
+
+  describe "phone_link and phone_display" do
+    it "displays phone with an area code and country code" do
+      expect(phone_display("999 999 9999")).to eq("999-999-9999")
+      expect(phone_display("+91 8041505583")).to eq("+91-804-150-5583")
+    end
+    context "no phone" do
+      it "returns empty string if empty" do
+        expect(phone_link(nil, class: "phone-number-link")).to eq ""
+      end
+    end
+    context "with extension" do
+      let(:target) { '<a href="tel:+11-121-1111 ; 2929222">+11-121-1111 x 2929222</a>' }
+      it "returns link" do
+        expect(phone_display("+11 1211111 x2929222")).to eq "+11-121-1111 x 2929222"
+        expect(phone_link("+11 121 1111 x2929222")).to eq target
+      end
+    end
+    context "passed class" do
+      let(:target) { '<a class="phone-number-link" href="tel:777-777-7777 ; 2929222">777-777-7777 x 2929222</a>' }
+      it "has class" do
+        expect(phone_display("777 777 7777 ext. 2929222")).to eq "777-777-7777 x 2929222"
+        expect(phone_link("777 777 7777 ext. 2929222", class: "phone-number-link")).to eq target
+      end
+    end
+  end
 end

spec/helpers/application_helper_spec.rb
@@ -1,32 +1,6 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe "phone_link and phone_display" do
-    it "displays phone with an area code and country code" do
-      expect(phone_display("999 999 9999")).to eq("999-999-9999")
-      expect(phone_display("+91 8041505583")).to eq("+91-804-150-5583")
-    end
-    context "no phone" do
-      it "returns empty string if empty" do
-        expect(phone_link(nil, class: "phone-number-link")).to eq ""
-      end
-    end
-    context "with extension" do
-      let(:target) { '<a href="tel:+11-121-1111 ; 2929222">+11-121-1111 x 2929222</a>' }
-      it "returns link" do
-        expect(phone_display("+11 1211111 x2929222")).to eq "+11-121-1111 x 2929222"
-        expect(phone_link("+11 121 1111 x2929222")).to eq target
-      end
-    end
-    context "passed class" do
-      let(:target) { '<a class="phone-number-link" href="tel:777-777-7777 ; 2929222">777-777-7777 x 2929222</a>' }
-      it "has class" do
-        expect(phone_display("777 777 7777 ext. 2929222")).to eq "777-777-7777 x 2929222"
-        expect(phone_link("777 777 7777 ext. 2929222", class: "phone-number-link")).to eq target
-      end
-    end
-  end
-
   describe "#show_sharing_links" do
     it "combines twitter, instagram, and website" do
       user = User.new(