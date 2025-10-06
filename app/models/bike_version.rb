e5a045b46134194c63c7f605af601a54a44bbf7c
Seth Herr <seth.william.herr@gmail.com>
2025-10-06 09:59:26 -0700

    wip

--- app/models/bike_version.rb
+++ app/models/bike_version.rb
@@ -197,10 +197,11 @@ class BikeVersion < ApplicationRecord
   end
 
   def visible_by?(passed_user = nil)
-    return true unless user_hidden?
+    return true unless user_hidden? || deleted?
 
     if passed_user.present?
       return true if passed_user.superuser?
+      return false if deleted?
       return true if user_hidden? && authorized?(passed_user)
     end
     false
