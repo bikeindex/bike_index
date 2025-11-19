7d75c5becf340af1aad9a9c6a48520e6438e2264
Seth Herr <seth.william.herr@gmail.com>
2025-11-19 12:18:57 -0800

    Does that fail?

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -2,11 +2,11 @@ import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller='alert'
 export default class extends Controller {
-  close () {
-    this.element.classList.add('tw:opacity-0', 'tw:scale-95')
-    // Wait for transition to complete before hiding completely
-    setTimeout(() => {
-      this.element.classList.add('tw:hidden')
-    }, 300)
+close () {
+this.element.classList.add('tw:opacity-0', 'tw:scale-95')
+// Wait for transition to complete before hiding completely
+setTimeout(() => {
+this.element.classList.add('tw:hidden')
+}, 300)
   }
 }
