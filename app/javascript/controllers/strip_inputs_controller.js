e123f3295135491ff87bfc3a837b996df52e6f10
Seth Herr <seth.william.herr@gmail.com>
2025-11-21 08:30:23 -0800

    Improve implementation

--- app/javascript/controllers/strip_inputs_controller.js
+++ app/javascript/controllers/strip_inputs_controller.js
@@ -1,18 +1,34 @@
 import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller='strip-inputs'
-// Strips leading and trailing whitespace from all text inputs on form submit
+// Strips leading and trailing whitespace from all text inputs on form submit and blur
 export default class extends Controller {
-  submit (event) {
-    // Find all text-based inputs in the form
-    const textInputs = this.element.querySelectorAll(
-      'input[type="text"], input[type="email"], input[type="search"], input[type="url"], input[type="tel"], input:not([type]), textarea'
-    )
+  static inputSelector = 'input[type="text"], input[type="email"], input[type="search"], input[type="url"], input[type="tel"], input:not([type]), textarea'
 
-    // Strip whitespace from each input
+  connect () {
+    // Strip all inputs when any input loses focus
+    this.element.addEventListener('focusout', this.handleFocusout.bind(this))
+  }
+
+  disconnect () {
+    this.element.removeEventListener('focusout', this.handleFocusout.bind(this))
+  }
+
+  handleFocusout (event) {
+    if (event.target.matches(this.constructor.inputSelector)) {
+      this.stripAllInputs()
+    }
+  }
+
+  stripAllInputs () {
+    const textInputs = this.element.querySelectorAll(this.constructor.inputSelector)
     textInputs.forEach(input => {
       input.value = input.value.trim()
     })
+  }
+
+  submit (event) {
+    this.stripAllInputs()
 
     // Find first invalid field after stripping
     const firstInvalid = this.element.querySelector(':invalid')
