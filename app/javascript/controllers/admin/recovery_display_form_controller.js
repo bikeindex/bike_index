5766556f013794c0acb0546ab29419e3c23e7cfe
Seth Herr <seth.william.herr@gmail.com>
2025-11-28 09:57:56 -0600

    lint

--- app/javascript/controllers/admin/recovery_display_form_controller.js
+++ app/javascript/controllers/admin/recovery_display_form_controller.js
@@ -1,16 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 import { collapse } from 'utils/collapse_utils'
 // import { collapse } from "../../utils/collapse-utils"
 
 // Connects to data-controller='admin--update-cached-sortable-links
 export default class extends Controller {
   static targets = [
-    "characterCounter",
-    "characterTotal",
-    "photoUploadInput",
-    "remoteImageUrl",
-    "bikeImageText",
-    "useImageButton"
+    'characterCounter',
+    'characterTotal',
+    'photoUploadInput',
+    'remoteImageUrl',
+    'bikeImageText',
+    'useImageButton'
   ]
 
   static values = {
@@ -18,7 +18,7 @@ export default class extends Controller {
     maxCharacterCount: Number
   }
 
-  connect() {
+  connect () {
     this.setCharacterCount()
 
     if (this.toggleImageInitiallyValue) {
@@ -26,28 +26,28 @@ export default class extends Controller {
     }
   }
 
-  toggleImage(event) {
+  toggleImage (event) {
     event.preventDefault()
     this.toggleBikeImageForDisplay()
   }
 
-  toggleBikeImageForDisplay() {
-    if (this.bikeImageTextTarget.classList.contains("bike-image-added")) {
+  toggleBikeImageForDisplay () {
+    if (this.bikeImageTextTarget.classList.contains('bike-image-added')) {
       collapse('show', this.photoUploadInputTarget)
-      this.remoteImageUrlTarget.value = ""
-      this.bikeImageTextTarget.classList.remove("bike-image-added")
+      this.remoteImageUrlTarget.value = ''
+      this.bikeImageTextTarget.classList.remove('bike-image-added')
     } else {
       collapse('hide', this.photoUploadInputTarget)
       this.remoteImageUrlTarget.value = this.useImageButtonTarget.dataset.url
-      this.bikeImageTextTarget.classList.add("bike-image-added")
+      this.bikeImageTextTarget.classList.add('bike-image-added')
     }
   }
 
-  updateCharacterCount() {
+  updateCharacterCount () {
     this.setCharacterCount()
   }
 
-  setCharacterCount() {
+  setCharacterCount () {
     const length = this.characterCounterTarget.value.length
     this.characterTotalTarget.textContent = `${length}/${this.maxCharacterCountValue}`
   }
