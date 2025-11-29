42367f65b030a6ae1ba6a62334daab08e6467b09
Seth Herr <seth.william.herr@gmail.com>
2025-11-29 15:08:05 -0600

    Update redesign_2025_homepage.css

--- app/components/page_block/homepage_top/component.rb
+++ app/components/page_block/homepage_top/component.rb
@@ -4,8 +4,8 @@ module PageBlock::HomepageTop
   class Component < ApplicationComponent
     def initialize(recoveries_value:, organization_count:, recovery_displays:)
       @recoveries_value = recoveries_value
-    @organization_count = organization_count
-    @recovery_displays = recovery_displays
+      @organization_count = organization_count
+      @recovery_displays = recovery_displays
     end
   end
 end
