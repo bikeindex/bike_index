d55730a9fed824a7270448083e0acbad09fb4f35
Seth Herr <seth.william.herr@gmail.com>
2025-12-05 09:33:56 -0800

    buttons-grid

--- spec/components/page_block/homepage_top/component_spec.rb
+++ spec/components/page_block/homepage_top/component_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe PageBlock::HomepageTop::Component, type: :component do
   let(:recovery_displays) { [] }
 
   it "renders" do
-    expect(component).to be_present
     expect(component).to have_css("div")
+    expect(component).to have_text("Cities")
+    expect(component).to have_text("The bike registry that works")
   end
 end
