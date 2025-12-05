b3de0333a1cea5b05ea44d87f8b0feef82b49b56
Seth Herr <seth.william.herr@gmail.com>
2025-12-05 09:19:21 -0800

    update landing pages

--- app/components/page_block/homepage_top/component_preview.rb
+++ app/components/page_block/homepage_top/component_preview.rb
@@ -2,6 +2,7 @@
 
 module PageBlock::HomepageTop
   class ComponentPreview < ApplicationComponentPreview
+    # @display redesign_2025_stylesheet true
     def default
       render(PageBlock::HomepageTop::Component.new(recoveries_value: Counts.recoveries_value,
         organization_count: Organization.count, recovery_displays: RecoveryDisplay.limit(5)))
