3ba6e82537c9da823de890f43affd2312ea88620
Seth Herr <seth.william.herr@gmail.com>
2025-12-13 13:33:03 -0800

    Fix merge issues

--- gems/binxtils/spec/input_normalizer_spec.rb
+++ gems/binxtils/spec/input_normalizer_spec.rb
@@ -1,135 +1,134 @@
-# frozen_string_literal: true
-
-require "spec_helper"
+require "rails_helper"
 
 RSpec.describe Binxtils::InputNormalizer do
+  let(:subject) { described_class }
   describe "boolean" do
     context "1" do
       it "returns true" do
-        expect(Binxtils::InputNormalizer.boolean(1)).to eq true
-        expect(Binxtils::InputNormalizer.boolean(" 1")).to eq true
+        expect(subject.boolean(1)).to eq true
+        expect(subject.boolean(" 1")).to eq true
       end
     end
     context "string" do
       it "returns true" do
-        expect(Binxtils::InputNormalizer.boolean("something")).to eq true
-        expect(Binxtils::InputNormalizer.boolean(" Other Stuffff")).to eq true
+        expect(subject.boolean("something")).to eq true
+        expect(subject.boolean(" Other Stuffff")).to eq true
       end
     end
     context "true" do
       it "returns true" do
-        expect(Binxtils::InputNormalizer.boolean(true)).to eq true
-        expect(Binxtils::InputNormalizer.boolean("true ")).to eq true
+        expect(subject.boolean(true)).to eq true
+        expect(subject.boolean("true ")).to eq true
       end
     end
     context "nil" do
       it "returns false" do
-        expect(Binxtils::InputNormalizer.boolean).to eq false
-        expect(Binxtils::InputNormalizer.boolean(nil)).to eq false
+        expect(subject.boolean).to eq false
+        expect(subject.boolean(nil)).to eq false
       end
     end
     context "false" do
       it "returns false" do
-        expect(Binxtils::InputNormalizer.boolean(false)).to eq false
-        expect(Binxtils::InputNormalizer.boolean("false\n")).to eq false
+        expect(subject.boolean(false)).to eq false
+        expect(subject.boolean("false\n")).to eq false
       end
     end
   end
 
   describe "present_or_false?" do
     it "is true for false values" do
-      expect(false.present?).to be_falsey
-      expect(Binxtils::InputNormalizer.present_or_false?(false)).to eq true
-      expect(Binxtils::InputNormalizer.present_or_false?("false\n")).to eq true
-      expect(Binxtils::InputNormalizer.present_or_false?("0\n")).to eq true
-      expect(Binxtils::InputNormalizer.present_or_false?(0)).to eq true
+      expect(false.present?).to be_falsey # This is the problem!
+      expect(subject.present_or_false?(false)).to eq true
+      expect(subject.present_or_false?("false\n")).to eq true
+      expect(subject.present_or_false?("0\n")).to eq true
+      expect(subject.present_or_false?(0)).to eq true
     end
     it "is false for blank" do
-      expect(Binxtils::InputNormalizer.present_or_false?(nil)).to eq false
-      expect(Binxtils::InputNormalizer.present_or_false?("")).to eq false
-      expect(Binxtils::InputNormalizer.present_or_false?("   \n")).to eq false
+      expect(subject.present_or_false?(nil)).to eq false
+      expect(subject.present_or_false?("")).to eq false
+      expect(subject.present_or_false?("   \n")).to eq false
     end
     it "is true for strings" do
-      expect(Binxtils::InputNormalizer.present_or_false?("something")).to eq true
-      expect(Binxtils::InputNormalizer.present_or_false?(true)).to eq true
-      expect(Binxtils::InputNormalizer.present_or_false?(3)).to eq true
+      expect(subject.present_or_false?("something")).to eq true
+      expect(subject.present_or_false?(true)).to eq true
+      expect(subject.present_or_false?(3)).to eq true
     end
   end
 
   describe "string" do
     it "returns nil for blank" do
-      expect(Binxtils::InputNormalizer.string(nil)).to be_nil
-      expect(Binxtils::InputNormalizer.string("")).to be_nil
-      expect(Binxtils::InputNormalizer.string("   ")).to be_nil
+      expect(subject.string(nil)).to be_nil
+      expect(subject.string("")).to be_nil
+      expect(subject.string("   ")).to be_nil
     end
     it "strips and removes extra spaces" do
-      expect(Binxtils::InputNormalizer.string(" D  ")).to eq "D"
-      expect(Binxtils::InputNormalizer.string(" D HI \Z \nf ")).to eq "D HI \Z f"
+      expect(subject.string(" D  ")).to eq "D"
+      expect(subject.string(" D HI \Z \nf ")).to eq "D HI \Z f"
     end
   end
 
   describe "regex_escape" do
     it "is nil for blank" do
-      expect(Binxtils::InputNormalizer.string(" ")).to be_nil
+      expect(subject.string(" ")).to be_nil
     end
     it "replaces" do
-      expect(Binxtils::InputNormalizer.regex_escape("(((..{?}")).to eq "........"
+      expect(subject.regex_escape("(((..{?}")).to eq "........"
     end
   end
 
   describe "sanitize" do
     it "is empty string for nil" do
-      expect(Binxtils::InputNormalizer.sanitize).to eq ""
-      expect(Binxtils::InputNormalizer.sanitize(nil)).to eq ""
-      expect(Binxtils::InputNormalizer.sanitize("\n\n")).to eq ""
+      expect(subject.sanitize).to eq ""
+      expect(subject.sanitize(nil)).to eq ""
+      expect(subject.sanitize("\n\n")).to eq ""
     end
     it "is string for boolean" do
-      expect(Binxtils::InputNormalizer.sanitize(true)).to eq "true"
-      expect(Binxtils::InputNormalizer.sanitize(false)).to eq "false"
-      expect(Binxtils::InputNormalizer.sanitize(" true")).to eq "true"
+      expect(subject.sanitize(true)).to eq "true"
+      expect(subject.sanitize(false)).to eq "false"
+      expect(subject.sanitize(" true")).to eq "true"
     end
     it "is strings for numbers" do
-      expect(Binxtils::InputNormalizer.sanitize(5)).to eq "5"
-      expect(Binxtils::InputNormalizer.sanitize(5_000)).to eq "5000"
-      expect(Binxtils::InputNormalizer.sanitize(" 5000 ")).to eq "5000"
-      expect(Binxtils::InputNormalizer.sanitize(5.000)).to eq "5.0"
-      expect(Binxtils::InputNormalizer.sanitize(BigDecimal(0))).to eq "0.0"
+      expect(subject.sanitize(5)).to eq "5"
+      expect(subject.sanitize(5_000)).to eq "5000"
+      expect(subject.sanitize(" 5000 ")).to eq "5000"
+      expect(subject.sanitize(5.000)).to eq "5.0"
+      expect(subject.sanitize(BigDecimal(0))).to eq "0.0"
     end
     it "doesn't remove text but does remove whitespace" do
-      expect(Binxtils::InputNormalizer.sanitize("Some cool text ")).to eq "Some cool text"
-      expect(Binxtils::InputNormalizer.sanitize(" Some \tcool \ntext")).to eq "Some cool text"
+      expect(subject.sanitize("Some cool text ")).to eq "Some cool text"
+      expect(subject.sanitize(" Some \tcool \ntext")).to eq "Some cool text"
     end
     it "strips html tags" do
-      expect(Binxtils::InputNormalizer.sanitize("<b>Hello</b> Plus other things</a>")).to eq "Hello Plus other things"
-      expect(Binxtils::InputNormalizer.sanitize("<div><b>Hello</b> Plus other </div>things</a>")).to eq "Hello Plus other things"
+      expect(subject.sanitize("<b>Hello</b> Plus other things</a>")).to eq "Hello Plus other things"
+      expect(subject.sanitize("<div><b>Hello</b> Plus other </div>things</a>")).to eq "Hello Plus other things"
     end
     it "strips out script" do
-      expect(Binxtils::InputNormalizer.sanitize("<script>alert();</script>")).to eq ""
-      expect(Binxtils::InputNormalizer.sanitize("<b>Hello</b><script>alert()</script>")).to eq "Hello"
-      expect(Binxtils::InputNormalizer.sanitize('<b class="something">Hello</b><script>alert()</script>\\')).to eq "Hello\\"
+      expect(subject.sanitize("<script>alert();</script>")).to eq ""
+      expect(subject.sanitize("<b>Hello</b><script>alert()</script>")).to eq "Hello"
+      expect(subject.sanitize('<b class="something">Hello</b><script>alert()</script>\\')).to eq "Hello\\"
     end
     it "returns bare ampersands" do
-      expect(Binxtils::InputNormalizer.sanitize("Bike & Ski")).to eq "Bike & Ski"
-      expect(Binxtils::InputNormalizer.sanitize("Bike &amp; Ski")).to eq "Bike & Ski"
+      expect(subject.sanitize("Bike & Ski")).to eq "Bike & Ski"
+      expect(subject.sanitize("Bike &amp; Ski")).to eq "Bike & Ski"
     end
     it "leaves useful special characters" do
-      expect(Binxtils::InputNormalizer.sanitize("Bike ())( /// Ski ")).to eq "Bike ())( /// Ski"
-      expect(Binxtils::InputNormalizer.sanitize(' Bike [[] \\\ Ski ')).to eq 'Bike [[] \\\ Ski'
-      expect(Binxtils::InputNormalizer.sanitize("Surly's Cross-check bike")).to eq "Surly's Cross-check bike"
+      expect(subject.sanitize("Bike ())( /// Ski ")).to eq "Bike ())( /// Ski"
+      expect(subject.sanitize(' Bike [[] \\\ Ski ')).to eq 'Bike [[] \\\ Ski'
+      expect(subject.sanitize("Surly's Cross-check bike")).to eq "Surly's Cross-check bike"
     end
     it "leaves accents" do
-      expect(Binxtils::InputNormalizer.sanitize("paké")).to eq "paké"
+      expect(subject.sanitize("paké")).to eq "paké"
     end
     it "leaves emojis" do
-      expect(Binxtils::InputNormalizer.sanitize("🧹")).to eq "🧹"
+      expect(subject.sanitize("🧹")).to eq "🧹"
     end
     it "removes angle brackets" do
-      expect(Binxtils::InputNormalizer.sanitize("Bike < Ski")).to eq "Bike &lt; Ski"
-      expect(Binxtils::InputNormalizer.sanitize("Bike &lt; Ski")).to eq "Bike &lt; Ski"
-      expect(Binxtils::InputNormalizer.sanitize("Bike > Ski")).to eq "Bike &gt; Ski"
-      expect(Binxtils::InputNormalizer.sanitize("Bike &gt; Ski")).to eq "Bike &gt; Ski"
-      expect(Binxtils::InputNormalizer.sanitize("Bike <> Ski")).to eq "Bike &lt;&gt; Ski"
-      expect(Binxtils::InputNormalizer.sanitize("Bike &lt;&gt; Ski")).to eq "Bike &lt;&gt; Ski"
+      expect(subject.sanitize("Bike < Ski")).to eq "Bike &lt; Ski"
+      expect(subject.sanitize("Bike &lt; Ski")).to eq "Bike &lt; Ski"
+      expect(subject.sanitize("Bike > Ski")).to eq "Bike &gt; Ski"
+      expect(subject.sanitize("Bike &gt; Ski")).to eq "Bike &gt; Ski"
+      expect(subject.sanitize("Bike <> Ski")).to eq "Bike &lt;&gt; Ski"
+      expect(subject.sanitize("Bike &lt;&gt; Ski")).to eq "Bike &lt;&gt; Ski"
     end
   end
 end
