5ecea768498d3b5e4c10b25e2b709da79ae7239b
Seth Herr <seth.william.herr@gmail.com>
2025-12-28 11:11:47 -0800

    Add specs for BikeDeleterJob really_delete

--- app/jobs/bike_deleter_job.rb
+++ app/jobs/bike_deleter_job.rb
@@ -4,7 +4,7 @@ class BikeDeleterJob < ApplicationJob
   def perform(bike_id, really_delete = false)
     if really_delete
       Ownership.where(bike_id:).destroy_all
-      Bike.unscoped.find_by_id(bike_id).really_destroy!
+      Bike.unscoped.find_by_id(bike_id)&.really_destroy!
     else
       Bike.unscoped.find_by_id(bike_id).destroy
     end
