f1454a8545379036527fab52ddf49693c8a31d10
Seth Herr <seth.william.herr@gmail.com>
2026-01-14 08:32:45 -0800

    wip

--- app/components/search_results/container/component.rb
+++ app/components/search_results/container/component.rb
@@ -8,21 +8,32 @@ module SearchResults::Container
     }.freeze
     SEARCH_KINDS = %i[registration marketplace].freeze
 
-    def self.permitted_search_kind(search_kind = nil)
-      kind_sym = search_kind&.to_sym
-      SEARCH_KINDS.include?(kind_sym) ? kind_sym : SEARCH_KINDS.first
-    end
+    class << self
+      def permitted_search_kind(search_kind = nil)
+        kind_sym = search_kind&.to_sym
+        SEARCH_KINDS.include?(kind_sym) ? kind_sym : SEARCH_KINDS.first
+      end
 
-    def self.permitted_result_view(result_view, default: nil)
-      kind_sym = result_view&.to_sym
-      default ||= RESULT_VIEW_COMPONENT.keys.first
-      raise "Unknown default '#{default}'" unless RESULT_VIEW_COMPONENT.key?(default)
+      def permitted_result_view(result_view, default: nil)
+        kind_sym = result_view&.to_sym
+        default ||= RESULT_VIEW_COMPONENT.keys.first
+        raise "Unknown default '#{default}'" unless RESULT_VIEW_COMPONENT.key?(default)
 
-      RESULT_VIEW_COMPONENT.key?(kind_sym) ? kind_sym : default
-    end
+        RESULT_VIEW_COMPONENT.key?(kind_sym) ? kind_sym : default
+      end
 
-    def self.component_class_for_result_view(result_view)
-      RESULT_VIEW_COMPONENT[permitted_result_view(result_view)]
+      def component_class_for_result_view(result_view)
+        RESULT_VIEW_COMPONENT[permitted_result_view(result_view)]
+      end
+
+      def container_class(component_class)
+        if component_class == SearchResults::VehicleThumbnail::Component
+          "tw:grid tw:gap-x-3 tw:gap-y-6 tw:lg:gap-y-8 tw:justify-items-center tw:xs:grid-cols-2 " \
+          "tw:sm:grid-cols-[repeat(auto-fit,minmax(16rem,1fr))]"
+        else
+          "bike-boxes"
+        end
+      end
     end
 
     def initialize(result_view: nil, search_kind: nil, current_user: nil, vehicles: nil, skip_cache: false, no_results: nil)
@@ -35,17 +46,14 @@ module SearchResults::Container
       @no_results = no_results || translation(".no_results")
     end
 
+    private
+
     def render_no_results?
       @vehicles.blank? && content.blank?
     end
 
     def container_class
-      if @component_class == SearchResults::VehicleThumbnail::Component
-        "tw:grid tw:gap-x-3 tw:gap-y-6 tw:lg:gap-y-8 tw:justify-items-center tw:xs:grid-cols-2 " \
-        "tw:sm:grid-cols-[repeat(auto-fit,minmax(16rem,1fr))]"
-      else
-        "bike-boxes"
-      end
+      self.class.container_class(@component_class)
     end
   end
 end
--- spec/components/messages/for_sale_item/component_spec.rb
+++ spec/components/messages/for_sale_item/component_spec.rb
@@ -5,8 +5,8 @@ require "rails_helper"
 RSpec.describe Messages::ForSaleItem::Component, type: :component do
   let(:instance) { described_class.new(**options) }
   let(:component) { render_inline(instance) }
-  let(:options) { {result_view:, search_kind:, vehicles:, skip_cache:, no_results:} }
-  let(:vehicles) { [FactoryBot.build(:bike, id: 42)] }
+  let(:options) { {result_view:, vehicle:} }
+  let(:vehicle) { FactoryBot.build(:bike, id: 42) }
   let(:result_view) { :thumbnail }
   let(:search_kind) { :registration }
   let(:skip_cache) { nil }
@@ -28,25 +28,6 @@ RSpec.describe Messages::ForSaleItem::Component, type: :component do
       expect(component.css("li")).to be_present
       expect(component.css("a").first["href"]).to match("/bikes/42")
       expect(component).to_not have_text default_no_results_text
-    end
-  end
-
-  context "no vehicles" do
-    let(:vehicles) { [] }
-
-    it "renders" do
-      expect(component).to be_present
-      expect(component.css("ul")).to_not be_present
-      expect(component).to have_text default_no_results_text
-    end
-
-    context "passed no_results" do
-      let(:no_results) { "OH NO! There are no results" }
-      it "renders no_results text" do
-        expect(component).to be_present
-        expect(component.css("ul")).to_not be_present
-        expect(component).to have_text no_results
-      end
     end
   end
 end
