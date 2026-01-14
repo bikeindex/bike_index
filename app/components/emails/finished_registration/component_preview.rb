d657bf1e0d604f9bf668126b1aec56859cd1c93d
Seth Herr <seth.william.herr@gmail.com>
2026-01-14 13:59:41 -0800

    remove unnecessary things

--- app/components/emails/finished_registration/component_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Emails::FinishedRegistration
-  class ComponentPreview < ApplicationComponentPreview
-    def default
-      render(Emails::FinishedRegistration::Component.new(ownership:))
-    end
-  end
-end
