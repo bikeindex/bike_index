dee46d86afc97503a20f06a837129100be73eef2
Seth Herr <seth.william.herr@gmail.com>
2026-01-15 08:52:06 -0800

    Rename Messages::Thread > Messages::ThreadsIndex

--- spec/components/messages/threads_index/component_system_spec.rb
+++ spec/components/messages/threads_index/component_system_spec.rb
@@ -3,8 +3,8 @@
 require "rails_helper"
 
 # As of now, this just verifies that the preview renders
-RSpec.describe Messages::Thread::Component, :js, type: :system do
-  let(:preview_path) { "/rails/view_components/messages/thread/component/default" }
+RSpec.describe Messages::ThreadsIndex::Component, :js, type: :system do
+  let(:preview_path) { "/rails/view_components/messages/threads_index/component/default" }
   let!(:user) { FactoryBot.create(:user_confirmed) }
 
   before { ENV["LOOKBOOK_USER_ID"] = user.id.to_s }
