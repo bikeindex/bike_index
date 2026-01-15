5df6c436d192679cb8817285b2c2cf034d25f2e6
Seth Herr <seth.william.herr@gmail.com>
2026-01-15 08:44:11 -0800

    wip

--- app/components/messages/thread_show_message/component_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Messages::ThreadShowMessage
-  class ComponentPreview < ApplicationComponentPreview
-    def default
-      render(Messages::ThreadShowMessage::Component.new(marketplace_message:, initial_message:, current_user:))
-    end
-  end
-end
--- spec/components/messages/thread_show_message/component_spec.rb
+++ spec/components/messages/thread_show_message/component_spec.rb
@@ -11,7 +11,18 @@ RSpec.describe Messages::ThreadShowMessage::Component, type: :component do
   let(:current_user) { initial_message.sender }
 
   it "renders" do
+    expect(marketplace_message.initial_message?).to be_falsey
     expect(component).to have_css("div")
     expect(component).to have_content marketplace_message.body
+    expect(component).to_not have_content "Subject"
+  end
+
+  context "when initial message" do
+    it "includes subject" do
+      expect(marketplace_message.initial_message?).to be_falsey
+      expect(component).to have_css("div")
+      expect(component).to have_content marketplace_message.body
+      expect(component).to_not have_content "Subject"
+    end
   end
 end
