427340f354ca0cd71085fb705b876332fa20df3d
Seth Herr <seth.william.herr@gmail.com>
2026-01-15 08:22:15 -0800

    revert messages changes

--- app/components/messages/thread_show/component_preview.rb
+++ app/components/messages/thread_show/component_preview.rb
@@ -3,56 +3,36 @@
 module Messages::ThreadShow
   class ComponentPreview < ApplicationComponentPreview
     # @display legacy_stylesheet true
-    def buyer
-      @item_owner = :other_user
-      marketplace_message = built_marketplace_message(marketplace_message_attrs)
+    def default
+      marketplace_message = built_marketplace_message(default_marketplace_message_attrs)
 
       render(Messages::ThreadShow::Component.new(current_user: lookbook_user,
-        marketplace_messages: [marketplace_message]))
-    end
-
-    # @display legacy_stylesheet true
-    def seller
-      @item_owner = :lookbook_user
-      marketplace_message = built_marketplace_message(marketplace_message_attrs)
-
-      render(Messages::ThreadShow::Component.new(current_user: other_user,
         marketplace_messages: [marketplace_message]))
     end
 
     private
 
     def other_user
-      return @other_user if defined?(@other_user)
-
-      @other_user = User.find_by_email("contact@bikeindex.org") ||
-        User.new(name: "John Smith", username: "some-username", id: 42)
+      User.new(name: "John Smith", username: "some-username", id: 42)
     end
 
-    def marketplace_message_attrs(receiver: lookbook_user, sender: other_user)
-      sender_val = (@item_owner == :other_user) ? lookbook_user : other_user
-      # The initial record always needs to be
-      (@item_owner == :other_user) ? other_user : lookbook_user
+    def default_marketplace_message_attrs
       {
         id: 42,
         messages_prior_count: 0,
         subject: "I'm interested in buying this bike",
         body: "When are you available? Can I come by to pick it up sometime this week?",
-        initial_record_id: MarketplaceMessage.new(id: 42, sender: sender_val, marketplace_listing:),
-        marketplace_listing:,
-        receiver:,
-        sender:,
+        initial_record_id: 42,
+        marketplace_listing: marketplace_listing,
+        receiver: lookbook_user,
+        sender: other_user,
         created_at: Time.current - 2.hours - 6.months
       }
     end
 
     def marketplace_listing
-      return @marketplace_listing if defined? @marketplace_listing
-      seller_val = (@item_owner == :other_user) ? other_user : lookbook_user
-
-      @marketplace_listing = MarketplaceListing.new(id: 42, seller: seller_val, item:,
-        condition: :excellent, amount_cents: 10_000, status: :for_sale,
-        published_at: Time.current - 1.day)
+      MarketplaceListing.new(id: 42, seller: lookbook_user, item:, condition: :excellent,
+        amount_cents: 1_000, status: :for_sale)
     end
 
     def built_marketplace_message(attrs)
--- spec/components/messages/thread_show/component_system_spec.rb
+++ spec/components/messages/thread_show/component_system_spec.rb
@@ -3,23 +3,14 @@
 require "rails_helper"
 
 RSpec.describe Messages::ThreadShow::Component, :js, type: :system do
-  let(:preview_path) { "/rails/view_components/messages/thread_show/component/buyer" }
+  let(:preview_path) { "/rails/view_components/messages/thread_show/component/default" }
   let!(:user) { FactoryBot.create(:user_confirmed) }
 
   before { ENV["LOOKBOOK_USER_ID"] = user.id.to_s }
 
-  it "buyer preview" do
+  it "default preview" do
     visit(preview_path)
 
     expect(page).to have_content "When are you available"
-  end
-
-  context "seller preview" do
-    let(:preview_path) { "/rails/view_components/messages/thread_show/component/seller" }
-    it "renders preview" do
-      visit(preview_path)
-
-      expect(page).to have_content "When are you available"
-    end
   end
 end
