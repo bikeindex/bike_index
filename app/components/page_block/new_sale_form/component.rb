b78a72c7a21d190e8108804c365d949a5e8d5f47
Seth Herr <seth.william.herr@gmail.com>
2026-01-23 14:30:18 -0800

    form is working

--- app/components/page_block/new_sale_form/component.rb
+++ app/components/page_block/new_sale_form/component.rb
@@ -5,10 +5,11 @@ module PageBlock::NewSaleForm
     def initialize(currency:, sale:, item: nil, marketplace_message: nil)
       @currency = currency
       @sale = sale
-      # Assign an amount
-      @sale.amount_cents ||= @sale.marketplace_listing&.amount_cents
       @marketplace_message = marketplace_message || @sale.marketplace_message
       @item = item || @marketplace_message&.item
+
+      # Assign a default amount
+      @sale.amount_cents ||= @sale.marketplace_listing&.amount_cents
     end
 
     private
--- spec/components/page_block/new_sale_form/component_spec.rb
+++ spec/components/page_block/new_sale_form/component_spec.rb
@@ -5,7 +5,8 @@ require "rails_helper"
 RSpec.describe PageBlock::NewSaleForm::Component, type: :component do
   let(:instance) { described_class.new(**options) }
   let(:component) { render_inline(instance) }
-  let(:options) { {sale:, item:, marketplace_message:} }
+  let(:options) { {currency:, sale:, item:, marketplace_message:} }
+  let(:currency) { Currency.default }
   let(:item) { nil }
   let(:marketplace_message) { nil }
   let(:marketplace_listing) { FactoryBot.create(:marketplace_listing, :for_sale, amount_cents: 16900) }
@@ -17,12 +18,26 @@ RSpec.describe PageBlock::NewSaleForm::Component, type: :component do
 
     it "renders" do
       sale.validate
-      expect(sale.amount).to be_nil
       expect(sale.validate).to be_truthy
+      expect(sale.amount_cents).to be_nil
+
       expect(component).to have_css "div"
-      expect(component).to have_text "sold to"
-      # Rendering assigns amount
-      expect(sale.amount_cents).to eq 16900
+      expect(component).to have_button "Record sale"
+      # It assigns the marketplace_listing amount_cents as a default
+      expect(component).to have_field(name: "sale[amount]", type: "number", with: 169)
+    end
+
+    context "with an amount assigned" do
+      it "renders" do
+        sale.validate
+        expect(sale.validate).to be_truthy
+        sale.amount_cents = 424200
+
+        expect(component).to have_css "div"
+        expect(component).to have_button "Record sale"
+        # It assigns the marketplace_listing amount_cents as a default
+        expect(component).to have_field(name: "sale[amount]", type: "number", with: 4242)
+      end
     end
   end
 end
