65f2556edac1c5386c26187c83f3ce2458d1c6ce
Seth Herr <seth.william.herr@gmail.com>
2026-01-23 15:44:37 -0800

    switch back to max_per_page settings

--- app/controllers/api/v3/search.rb
+++ app/controllers/api/v3/search.rb
@@ -45,7 +45,7 @@ module API
             If `location` is "**IP**" (the default), the location is determined via geolocation of your IP address.
           NOTE
         }
-        paginate max_per_page: Pagy.options[:limit_max]
+        paginate
         params do
           use :search
           optional :per_page, type: Integer, default: 25, desc: "Bikes per page (max 100)"
@@ -109,7 +109,7 @@ module API
             It returns matches that are off of the submitted `serial` by less than 3 characters (postgres levenshtein, if you're curious).
           NOTE
         }
-        paginate max_per_page: Pagy.options[:limit_max]
+        paginate
         params do
           requires :serial, type: String, desc: "Serial, homoglyph matched"
           use :non_serial_search_params
@@ -129,7 +129,7 @@ module API
                 It returns bikes with partially-matching serial numbers (for which the requested serial is a substring).
           NOTE
         }
-        paginate max_per_page: Pagy.options[:limit_max]
+        paginate
         params do
           requires :serial, type: String, desc: "Serial, homoglyph matched"
           use :non_serial_search_params
@@ -151,7 +151,7 @@ module API
                  If no exact matches are found, partial matches are returned.
           NOTE
         }
-        paginate max_per_page: Pagy.options[:limit_max]
+        paginate
         params do
           requires :serial, type: String, desc: "Serial, homoglyph matched"
           use :non_serial_search_params
