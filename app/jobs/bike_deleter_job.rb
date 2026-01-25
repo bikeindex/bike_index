f75a8fe50e7452a1c2dc31671c884cb573f52a88
Seth Herr <seth.william.herr@gmail.com>
2026-01-25 14:23:00 -0800

    and that

--- app/jobs/bike_deleter_job.rb
+++ app/jobs/bike_deleter_job.rb
@@ -2,16 +2,19 @@ class BikeDeleterJob < ApplicationJob
   sidekiq_options retry: false, queue: "low_priority"
 
   def perform(bike_id, really_delete = false, user_id = nil)
+    bike = Bike.unscoped.find(bike_id)
+
+    bike.current_marketplace_listing&.update(status: "removed")
+    if bike.current_impound_record.present?
+      bike.current_impound_record.impound_record_updates.create(user_id:, kind: "removed_from_bike_index")
+    end
+
     if really_delete
       Ownership.where(bike_id:).destroy_all
       PublicImage.where(imageable_type: "Bike", imageable_id: bike_id).destroy_all
-      Bike.unscoped.find_by_id(bike_id)&.really_destroy!
+      bike.really_destroy!
     else
-      bike = Bike.unscoped.find_by_id(bike_id)
-      bike.current_marketplace_listing&.update(status: "removed")
-      if bike.current_impound_record.present?
-        bike.current_impound_record.impound_record_updates.create(user_id:, kind: "removed_from_bike_index")
-      end
+      CallbackJob::AfterBikeSaveJob.perform_async(bike_id)
       bike.destroy
     end
   end
