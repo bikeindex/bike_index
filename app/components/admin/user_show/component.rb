2f2c4cf76c335a9188c76617c3588df1ff531fc1
Seth Herr <seth.william.herr@gmail.com>
2026-01-25 19:54:34 -0800

    cleanup

--- app/components/admin/user_show/component.rb
+++ app/components/admin/user_show/component.rb
@@ -7,5 +7,31 @@ module Admin::UserShow
       @bikes = bikes
       @bikes_count = bikes_count
     end
+
+    private
+
+    def object_limit
+      10
+    end
+
+    def marketplace_messages
+      MarketplaceMessage.where(sender_id: @user.id).order(id: :desc)
+    end
+
+    def memberships
+      Membership.where(user_id: @user.id).order(id: :desc)
+    end
+
+    def user_alerts
+      @user.user_alerts.order(created_at: :desc)
+    end
+
+    def payments
+      @user.payments.reorder(created_at: :desc).paid
+    end
+
+    def mailchimp_datum
+      @user.mailchimp_datum
+    end
   end
 end
