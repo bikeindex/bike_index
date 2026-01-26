3788e3ac2271ea7edde0fe2814d99b36186ac4bf
Seth Herr <seth.william.herr@gmail.com>
2026-01-25 18:49:09 -0800

    Update component

--- app/components/admin/user_show/component.rb
+++ app/components/admin/user_show/component.rb
@@ -4,8 +4,8 @@ module Admin::UserShow
   class Component < ApplicationComponent
     def initialize(user:, bikes:, bikes_count:)
       @user = user
-    @bikes = bikes
-    @bikes_count = bikes_count
+      @bikes = bikes
+      @bikes_count = bikes_count
     end
   end
 end
--- spec/components/admin/user_show/component_spec.rb
+++ spec/components/admin/user_show/component_spec.rb
@@ -6,11 +6,47 @@ RSpec.describe Admin::UserShow::Component, type: :component do
   let(:instance) { described_class.new(**options) }
   let(:component) { render_inline(instance) }
   let(:options) { {user:, bikes:, bikes_count:} }
-  let(:user) { nil }
-  let(:bikes) { nil }
-  let(:bikes_count) { nil }
+  let(:user) { FactoryBot.create(:user_confirmed, name: "Test User") }
+  let(:bikes) { [] }
+  let(:bikes_count) { 0 }
 
-  it "renders" do
-    expect(component).to have_css("div")
+  it "renders user display name" do
+    expect(component).to have_css("h1", text: /Test User/)
+  end
+
+  it "renders user email" do
+    expect(component).to have_content(user.email)
+  end
+
+  it "renders bikes count" do
+    expect(component).to have_css("h4", text: /Bikes/)
+    expect(component).to have_link(href: /user_id=#{user.id}/)
+  end
+
+  context "with deleted user" do
+    let(:user) { FactoryBot.create(:user_confirmed, deleted_at: Time.current) }
+
+    it "renders deleted alert" do
+      expect(component).to have_css(".alert-danger", text: /User deleted/)
+    end
+  end
+
+  context "with banned user" do
+    let(:user) { FactoryBot.create(:user_confirmed, banned: true) }
+    let!(:user_ban) { UserBan.create(user:, reason: :extortion) }
+
+    it "renders banned alert" do
+      expect(component).to have_css("h4.text-danger", text: /User banned/)
+      expect(component).to have_content("Extortion")
+    end
+  end
+
+  context "with superuser" do
+    let(:user) { FactoryBot.create(:superuser) }
+    let!(:superuser_ability) { SuperuserAbility.create(user:) }
+
+    it "renders superuser info" do
+      expect(component).to have_content("full superuser")
+    end
   end
 end
