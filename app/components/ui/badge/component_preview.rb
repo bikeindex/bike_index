715d4c69fdcf6188b2c172825db32ae23efc0f7a
Seth Herr <seth.william.herr@gmail.com>
2026-01-26 17:53:46 -0800

    Update preview

--- app/components/ui/badge/component_preview.rb
+++ app/components/ui/badge/component_preview.rb
@@ -7,24 +7,24 @@ module UI::Badge
       render(UI::Badge::Component.new(text: "Donor", color: :green))
     end
 
-    def blue_sm
-      render(UI::Badge::Component.new(text: "Organization", color: :blue, size: :sm))
+    def notice_sm
+      render(UI::Badge::Component.new(text: "Organization", color: :notice, size: :sm))
     end
 
     def purple_md
       render(UI::Badge::Component.new(text: "Superuser", color: :purple, size: :md))
     end
 
-    def amber_lg
-      render(UI::Badge::Component.new(text: "Recovery", color: :amber, size: :lg))
+    def warning_lg
+      render(UI::Badge::Component.new(text: "Recovery", color: :warning, size: :lg))
     end
 
     def gray_sm
       render(UI::Badge::Component.new(text: "Default", color: :gray, size: :sm))
     end
 
-    def red_md
-      render(UI::Badge::Component.new(text: "Banned", color: :red, size: :md))
+    def error_md
+      render(UI::Badge::Component.new(text: "Banned", color: :error, size: :md))
     end
 
     def cyan_lg
