083a8510a4661a52d0c0875d38230221f10ef7b9
Seth Herr <seth.william.herr@gmail.com>
2026-01-31 08:16:33 -0800

    Update where

--- spec/requests/info_request_spec.rb
+++ spec/requests/info_request_spec.rb
@@ -88,10 +88,10 @@ RSpec.describe InfoController, type: :request do
   end
 
   describe "where" do
-    let!(:organization1) { FactoryBot.create(:organization, :in_nyc, shown_on_map: true) }
-    let!(:organization2) { FactoryBot.create(:organization, :in_chicago, shown_on_map: false) }
-    let!(:organization3) { FactoryBot.create(:organization, :in_edmonton, shown_on_map: true) }
-    let!(:location_3) { FactoryBot.create(:location, :location_edmonton, organization: organization3) }
+    let!(:organization1) { FactoryBot.create(:organization, :in_nyc, show_on_map: true) }
+    let!(:organization2) { FactoryBot.create(:organization, :in_chicago, show_on_map: false) }
+    let!(:organization3) { FactoryBot.create(:organization, :in_edmonton, show_on_map: true) }
+    # let!(:location_3) { FactoryBot.create(:location_edmonton, organization: organization3) }
     it "renders" do
       get "/where"
       expect(response.status).to eq(200)
