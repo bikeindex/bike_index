d4abf3aef2119b2a88ff722d9232f23ba7b20b43
Seth Herr <seth.william.herr@gmail.com>
2026-01-31 07:06:15 -0800

    wip

--- app/components/search/form_organized/component.rb
+++ app/components/search/form_organized/component.rb
@@ -11,44 +11,12 @@ module Search::FormOrganized
 
     private
 
-    def query
-      @interpreted_params[:query]
-    end
-
-    def serial_value
-      @interpreted_params[:raw_serial]
-    end
-
-    def search_email_value
-      @interpreted_params[:search_email]
-    end
-
     def render_serial_field?
       !@skip_serial_field
     end
 
     def serial_looks_like_not_a_serial?
       @interpreted_params[:raw_serial].present? && @interpreted_params[:serial].blank?
-    end
-
-    def stolenness
-      @interpreted_params[:stolenness]
-    end
-
-    def search_stickers
-      @interpreted_params[:search_stickers]
-    end
-
-    def search_address
-      @interpreted_params[:search_address]
-    end
-
-    def search_secondary
-      @interpreted_params[:search_secondary]
-    end
-
-    def search_model_audit_id
-      @interpreted_params[:search_model_audit_id]
     end
   end
 end
