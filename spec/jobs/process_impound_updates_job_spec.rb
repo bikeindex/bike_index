bikeindex/bike_index

commit 72354635c7d70aed0b2bd5b445d8223acebd0644

Impound record address record migration (#3081)
After adding AddressRecords to Locations (#3077), we can add it to
ImpoundRecords.

app/models/bike.rb
@@ -729,7 +729,7 @@ class Bike < ApplicationRecord
 
   # NOTE! This will return different hashes - legacy hashes for stolen & impound
   def address_hash
-    current_stolen_record&.address_hash || current_impound_record&.address_hash ||
+    current_stolen_record&.address_hash || current_impound_record&.address_hash_legacy ||
       address_record&.address_hash
   end
 

app/models/impound_record.rb
@@ -19,6 +19,7 @@
 #  zipcode               :text
 #  created_at            :datetime         not null
 #  updated_at            :datetime         not null
+#  address_record_id     :bigint
 #  bike_id               :integer
 #  country_id            :bigint
 #  display_id            :string
@@ -29,16 +30,18 @@
 #
 # Indexes
 #
-#  index_impound_records_on_bike_id          (bike_id)
-#  index_impound_records_on_country_id       (country_id)
-#  index_impound_records_on_location_id      (location_id)
-#  index_impound_records_on_organization_id  (organization_id)
-#  index_impound_records_on_state_id         (state_id)
-#  index_impound_records_on_user_id          (user_id)
+#  index_impound_records_on_address_record_id  (address_record_id)
+#  index_impound_records_on_bike_id            (bike_id)
+#  index_impound_records_on_country_id         (country_id)
+#  index_impound_records_on_location_id        (location_id)
+#  index_impound_records_on_organization_id    (organization_id)
+#  index_impound_records_on_state_id           (state_id)
+#  index_impound_records_on_user_id            (user_id)
 #
 class ImpoundRecord < ApplicationRecord
   include Geocodeable
   include DefaultCurrencyable
+  include AddressRecorded
 
   belongs_to :bike, touch: true
   belongs_to :user
@@ -54,6 +57,7 @@ class ImpoundRecord < ApplicationRecord
   validates_uniqueness_of :bike_id, if: :current?, conditions: -> { current }
 
   before_validation :set_calculated_attributes
+  before_validation :sync_address_record_from_legacy_fields
   after_commit :update_associations
 
   enum :status, ImpoundRecordUpdate::KIND_ENUM
@@ -257,6 +261,9 @@ class ImpoundRecord < ApplicationRecord
     self.status = calculated_status
     self.resolved_at = resolving_update&.created_at
     self.location_id = calculated_location_id
+    # Only update address_record_id from location if there's an explicit move_location update
+    move_location_update = impound_record_updates.with_location.order(:id).last
+    self.address_record_id = move_location_update.location.address_record_id if move_location_update&.location&.address_record_id.present?
     self.user_id = calculated_user_id
     self.impounded_at ||= created_at || Time.current
     self.impounded_at = created_at if created_at.present? && created_at > impounded_at + 10.years
@@ -282,6 +289,13 @@ class ImpoundRecord < ApplicationRecord
     organization&.fetch_impound_configuration&.email ||
       organization&.auto_user&.email ||
       user&.email
+  end
+
+  # Override AddressRecorded delegation to fall back to legacy fields
+  def address_present?
+    return address_record.address_present? if address_record?
+
+    [street, city, zipcode].any?(&:present?)
   end
 
   private
@@ -339,5 +353,20 @@ class ImpoundRecord < ApplicationRecord
     end
     bike&.current_ownership&.status == "status_impounded" &&
       (created_at || Time.current).between?(b_created_at - 1.hour, b_created_at + 1.hour)
+  end
+
+  def sync_address_record_from_legacy_fields
+    return if skip_update || city.blank? && street.blank?
+    # Don't create address_record if move_location update set it
+    return if impound_record_updates.with_location.any?
+    # Don't overwrite if address_record was explicitly set via nested attributes
+    return if address_record.present? && address_record.changed?
+
+    legacy_attrs = AddressRecord.attrs_from_legacy(self).merge(kind: :impounded_from, organization_id:)
+    if address_record.present?
+      address_record.attributes = legacy_attrs
+    else
+      self.address_record = AddressRecord.new(legacy_attrs)
+    end
   end
 end

app/jobs/backfills/address_records_for_impound_records_job.rb
@@ -1,0 +1,54 @@
+# frozen_string_literal: true
+
+class Backfills::AddressRecordsForImpoundRecordsJob < ApplicationJob
+  include Sidekiq::IterableJob
+
+  sidekiq_options queue: "low_priority", retry: false
+
+  class << self
+    def iterable_scope
+      ImpoundRecord.where(address_record_id: nil).where.not(city: nil)
+        .or(ImpoundRecord.where(address_record_id: nil).where.not(street: nil))
+    end
+
+    def build_or_create_for(impound_record)
+      return impound_record.address_record if impound_record.address_record?
+
+      # Check if there's a move_location update - if so, use the location's address_record
+      move_location_update = impound_record.impound_record_updates.with_location.order(:id).last
+      if move_location_update&.location&.address_record_id.present?
+        # Create impounded_from address_record from legacy fields (for historical purposes)
+        create_impounded_from_address_record(impound_record)
+        # But set the impound_record's address_record to the location's address_record
+        impound_record.update(address_record_id: move_location_update.location.address_record_id)
+        return impound_record.address_record
+      end
+
+      existing_address_record = AddressRecord.where(kind: :impounded_from).find_by(user_id: impound_record.user_id)
+      if existing_address_record.present? && existing_address_record.internal_address_attrs == AddressRecord.new(AddressRecord.attrs_from_legacy(impound_record)).internal_address_attrs
+        impound_record.update(address_record: existing_address_record)
+        return existing_address_record
+      end
+
+      impound_record.address_record = AddressRecord.new(kind: :impounded_from)
+      impound_record.address_record.attributes = AddressRecord.attrs_from_legacy(impound_record)
+      impound_record.save
+      impound_record.address_record
+    end
+
+    def create_impounded_from_address_record(impound_record)
+      attrs = AddressRecord.attrs_from_legacy(impound_record).merge(kind: :impounded_from)
+      AddressRecord.create(attrs)
+    end
+  end
+
+  def build_enumerator(cursor:)
+    return if skip_job?
+
+    active_record_records_enumerator(self.class.iterable_scope, cursor:)
+  end
+
+  def each_iteration(impound_record)
+    self.class.build_or_create_for(impound_record)
+  end
+end

app/models/address_record.rb
@@ -31,7 +31,7 @@
 #  index_address_records_on_user_id           (user_id) WHERE (user_id IS NOT NULL)
 #
 class AddressRecord < ApplicationRecord
-  KIND_ENUM = {user: 0, bike: 1, marketplace_listing: 2, ownership: 3, organization: 4}.freeze
+  KIND_ENUM = {user: 0, bike: 1, marketplace_listing: 2, ownership: 3, organization: 4, impounded_from: 5}.freeze
   PUBLICLY_VISIBLE_ATTRIBUTE_ENUM = {postal_code: 1, street: 0, city: 2}.freeze
   RENDER_COUNTRY_OPTIONS = [:if_different, true, false].freeze
   ADDRESS_ATTRS = %i[street street_2 city region_record_id postal_code country_id latitude longitude]
@@ -208,7 +208,7 @@ class AddressRecord < ApplicationRecord
   private
 
   def update_associations
-    # Bikes, ownerships, and locations handle address assignment separately
+    # Bikes, ownerships, impound_records and locations/organizations handle address assignment separately
     return if skip_callback_job || bike? || ownership? || organization?
 
     CallbackJob::AddressRecordUpdateAssociationsJob.perform_async(id)

app/services/bike_services/calculate_location.rb
@@ -59,7 +59,7 @@ class BikeServices::CalculateLocation
     # prefer with street address, fallback to anything with a latitude, use hashes (not obj) because registration_address
     def location_record_coordinates(bike)
       l_hashes = [
-        bike.current_impound_record&.address_hash,
+        bike.current_impound_record&.address_hash_legacy,
         bike.current_parking_notification&.address_hash,
         bike.registration_address(true, address_record_id: true), # temporary cludge?
         bike.creation_organization&.default_location&.address_hash_legacy

db/migrate/20260130170531_add_address_record_to_impound_records.rb
@@ -1,0 +1,5 @@
+class AddAddressRecordToImpoundRecords < ActiveRecord::Migration[8.0]
+  def change
+    add_reference :impound_records, :address_record, index: true
+  end
+end

db/primary_replica_structure.sql
@@ -1718,7 +1718,8 @@ CREATE TABLE public.impound_records (
     display_id character varying,
     display_id_prefix character varying,
     impounded_description text,
-    unregistered_bike boolean DEFAULT false
+    unregistered_bike boolean DEFAULT false,
+    address_record_id bigint
 );
 
 
@@ -6208,6 +6209,13 @@ CREATE INDEX index_impound_record_updates_on_user_id ON public.impound_record_up
 
 
 --
+-- Name: index_impound_records_on_address_record_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_impound_records_on_address_record_id ON public.impound_records USING btree (address_record_id);
+
+
+--
 -- Name: index_impound_records_on_bike_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -7189,6 +7197,7 @@ ALTER TABLE ONLY public.ambassador_task_assignments
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20260130170531'),
 ('20260130162732'),
 ('20260129220857'),
 ('20260129122352'),

db/structure.sql
@@ -1718,7 +1718,8 @@ CREATE TABLE public.impound_records (
     display_id character varying,
     display_id_prefix character varying,
     impounded_description text,
-    unregistered_bike boolean DEFAULT false
+    unregistered_bike boolean DEFAULT false,
+    address_record_id bigint
 );
 
 
@@ -6208,6 +6209,13 @@ CREATE INDEX index_impound_record_updates_on_user_id ON public.impound_record_up
 
 
 --
+-- Name: index_impound_records_on_address_record_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_impound_records_on_address_record_id ON public.impound_records USING btree (address_record_id);
+
+
+--
 -- Name: index_impound_records_on_bike_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -7189,6 +7197,7 @@ ALTER TABLE ONLY public.ambassador_task_assignments
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20260130170531'),
 ('20260130162732'),
 ('20260129220857'),
 ('20260129122352'),

commit 1a9f73bf3c12e9856fc79275d94514ef30c67004

Fix ProcessImpoundUpdatesJob bugs (#3086)
Caused by #3081

app/models/bike.rb
@@ -926,7 +926,7 @@ class Bike < ApplicationRecord
     return if @deleted_address_record_id.blank?
 
     deleted_addy = AddressRecord.find_by_id(@deleted_address_record_id)
-    deleted_addy.destroy if deleted_addy.bike? && deleted_addy.bike_id == id
+    deleted_addy.destroy if deleted_addy&.bike? && deleted_addy.bike_id == id
   end
 
   def fetch_current_impound_record

app/models/impound_record.rb
@@ -170,7 +170,7 @@ class ImpoundRecord < ApplicationRecord
   end
 
   def organized?
-    organization_id.present?
+    organization.present?
   end
 
   def impound_claim_retrieved?

spec/jobs/process_impound_updates_job_spec.rb
@@ -82,6 +82,18 @@ RSpec.describe ProcessImpoundUpdatesJob, type: :job do
     end
   end
 
+  context "organization deleted" do
+    let(:organization) { impound_record.organization }
+    it "doesn't error" do
+      expect(impound_record.organized?).to be_truthy
+      organization.destroy
+      impound_record.reload
+      expect(impound_record.organization_id).to be_present
+      expect(impound_record.organization).to be_nil
+      described_class.new.perform(impound_record.id)
+    end
+  end
+
   context "display_id_next_integer" do
     let(:impound_configuration) { FactoryBot.create(:impound_configuration, display_id_prefix: "A", display_id_next_integer: 1212) }
     let(:organization) { impound_configuration.organization }

spec/services/credibility_scorer_spec.rb
@@ -161,7 +161,7 @@ RSpec.describe CredibilityScorer do
       end
       context "spam_registrations not embed" do
         let(:organization) { FactoryBot.create(:organization, approved: true, spam_registrations: true) }
-        it "returns without spam_registrations" do
+        it "returns without spam_registrations", :flaky do
           expect(subject.organization_trusted?(organization)).to be_falsey
           expect(instance.badges).to eq(%i[created_this_month long_time_user])
         end