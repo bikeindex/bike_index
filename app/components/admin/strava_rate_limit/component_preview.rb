3311b2403703d3f7936e4cbc92e504599cff3977
Seth Herr <seth.william.herr@gmail.com>
2026-02-09 09:18:34 -0800

    update migrations

--- app/components/admin/strava_rate_limit/component_preview.rb
+++ app/components/admin/strava_rate_limit/component_preview.rb
@@ -2,8 +2,59 @@
 
 module Admin::StravaRateLimit
   class ComponentPreview < ApplicationComponentPreview
-    def default
-      render(Admin::StravaRateLimit::Component.new(rate_limit_json:))
+    # @group amount Variants
+    def high_rate_limit
+      render(Admin::StravaRateLimit::Component.new(rate_limit_json: high_rate_limit_json))
+    end
+
+    def medium_rate_limit
+      render(Admin::StravaRateLimit::Component.new(rate_limit_json: medium_rate_limit_json))
+    end
+
+    def low_rate_limit
+      render(Admin::StravaRateLimit::Component.new(rate_limit_json: low_rate_limit_json))
+    end
+    # @endgroup
+
+    private
+
+    def high_rate_limit_json
+      {
+        long_limit: 6000,
+        long_usage: 3,
+        short_limit: 600,
+        short_usage: 1,
+        read_long_limit: 3000,
+        read_long_usage: 3,
+        read_short_limit: 300,
+        read_short_usage: 1
+      }.as_json
+    end
+
+    def medium_rate_limit_json
+      {
+        long_limit: 6000,
+        long_usage: 3,
+        short_limit: 600,
+        short_usage: 1,
+        read_long_limit: 3000,
+        read_long_usage: 3,
+        read_short_limit: 300,
+        read_short_usage: 1
+      }.as_json
+    end
+
+    def low_rate_limit_json
+      {
+        long_limit: 6000,
+        long_usage: 3,
+        short_limit: 600,
+        short_usage: 1,
+        read_long_limit: 3000,
+        read_long_usage: 3,
+        read_short_limit: 300,
+        read_short_usage: 1
+      }.as_json
     end
   end
 end
