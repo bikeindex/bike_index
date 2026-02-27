1f5b79b447540853448b2d95d7778184cb8db450
Seth Herr <seth.william.herr@gmail.com>
2026-02-27 10:22:38 -0800

    Higher batch size

--- spec/jobs/strava_jobs/request_runner_spec.rb
+++ spec/jobs/strava_jobs/request_runner_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe StravaJobs::RequestRunner, type: :job do
       end
 
       it "enqueues up to BATCH_SIZE requests" do
-        12.times.map do
+        30.times.map do
           StravaRequest.create!(user_id: strava_integration.user_id,
             strava_integration_id: strava_integration.id, request_type: :fetch_activity,
             parameters: {strava_id: "123"})
