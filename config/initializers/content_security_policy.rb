cf3f97fb28b90047e0ebd853b8a7013d4103043c
Seth Herr <seth.william.herr@gmail.com>
2026-03-07 12:03:40 -0800

    Add honeybadger csp key (#3220)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,60 +11,61 @@ Rails.application.configure do
     policy.img_src :self, "https://files.bikeindex.org",
       "https://uploads.bikeindex.org",
       "https://bikebook.s3.amazonaws.com",
-      "https://www.facebook.com",
-      "https://connect.facebook.net",
       "https://www.googletagmanager.com",
-      "https://pbs.twimg.com",
-      "https://syndication.twitter.com",
       "https://maps.googleapis.com",
       "https://maps.gstatic.com",
       # Google Translate injects images when users translate the page in Chrome
       "https://fonts.gstatic.com",
-      "https://translate.google.com", :data
+      "https://translate.google.com",
+      "https://www.facebook.com",
+      "https://connect.facebook.net",
+      "https://pbs.twimg.com",
+      "https://syndication.twitter.com", :data
     policy.object_src :none
     # unsafe_eval is required for application_revised.js jQuery - remove it when possible!
     policy.script_src :self, :unsafe_inline, :unsafe_eval,
+      "https://widget.bikeindex.org",
       "https://www.googletagmanager.com",
       "https://www.google-analytics.com",
       "https://www.googleadservices.com",
-      "https://cdn.jsdelivr.net",
-      "https://js.stripe.com",
-      "https://platform.twitter.com",
-      "https://connect.facebook.net",
       "https://maps.googleapis.com",
+      "https://connect.facebook.net",
+      "https://platform.twitter.com",
       "https://api.mapbox.com",
-      "https://widget.bikeindex.org"
+      "https://cdn.jsdelivr.net",
+      "https://js.stripe.com"
     policy.style_src :self, :unsafe_inline,
       "https://fonts.googleapis.com",
       "https://www.gstatic.com", # Google Translate styles
-      "https://api.mapbox.com",
-      "https://cdn.jsdelivr.net"
+      "https://cdn.jsdelivr.net",
+      "https://api.mapbox.com"
     policy.connect_src :self,
+      "https://bikebook.herokuapp.com",
       "https://www.google-analytics.com",
       "https://*.google-analytics.com",
       "https://www.googletagmanager.com",
-      "https://www.facebook.com",
       "https://maps.googleapis.com",
+      "https://translate.googleapis.com", # Google Translate API
+      "https://www.facebook.com",
       "https://api.mapbox.com",
       "https://events.mapbox.com",
       "https://*.tiles.mapbox.com",
       "https://cdn.jsdelivr.net",
-      "https://api.honeybadger.io",
-      "https://translate.googleapis.com" # Google Translate API
+      "https://api.honeybadger.io"
     policy.worker_src :self, :blob
     policy.frame_src :self,
-      "https://js.stripe.com",
-      "https://www.facebook.com",
-      "https://web.facebook.com",
-      "https://m.facebook.com",
       "https://www.google.com",
       "https://www.recaptcha.net",
       "https://googleads.g.doubleclick.net",
       "https://www.googletagmanager.com",
       "https://www.youtube.com",
-      "https://platform.twitter.com"
+      "https://www.facebook.com",
+      "https://web.facebook.com",
+      "https://m.facebook.com",
+      "https://platform.twitter.com",
+      "https://js.stripe.com"
     policy.report_uri -> do
-      "https://api.honeybadger.io/v1/browser/csp?api_key=#{ENV["HONEYBADGER_FRONTEND_API_KEY"]}&report_only=true&env=#{Rails.env}&context[user_id]=#{current_user&.id if respond_to?(:current_user)}"
+      "https://api.honeybadger.io/v1/browser/csp?api_key=#{ENV["HONEYBADGER_CSP_API_KEY"]}&report_only=true&env=#{Rails.env}&context[user_id]=#{current_user&.id if respond_to?(:current_user)}"
     end
   end
 
