322b344bb2509eb7dd7b1a5e5bf6cb0ca4315049
Seth Herr <seth.william.herr@gmail.com>
2026-03-07 13:01:07 -0800

    Revert "Revert SortableHelper defaults, stub sort_column/sort_direction for previews"
    This reverts commit 359ac95c1b7e63a68e6288b8012e9b2e5dbc69db.

--- config/initializers/lookbook.rb
+++ config/initializers/lookbook.rb
@@ -5,15 +5,3 @@ Lookbook.configure do |config|
   config.ui_theme_overrides = {header_bg: "#3498db"}
   config.preview_paths = ["#{Rails.root}/app/components/"]
 end
-
-# Provide sort_column/sort_direction defaults for preview rendering
-# (normally set by SortableTable concern in controllers)
-Rails.application.config.after_initialize do
-  [Lookbook::PreviewController, ViewComponentsController].each do |controller|
-    next unless defined?(controller)
-    controller.helper do
-      def sort_column = "id"
-      def sort_direction = "desc"
-    end
-  end
-end
