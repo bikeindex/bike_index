0538434c4c9e1e815b723a877816d0a66e9c46f4
Seth Herr <seth.william.herr@gmail.com>
2026-03-25 13:02:31 -0700

    Better strava requests info display

---
+++ app/components/admin/strava_requests_rate_limit_details/component.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Admin::StravaRequestsRateLimitDetails
+  class Component < ApplicationComponent
+    def initialize(now: Time.current.utc)
+      @now = now
+      @rate_limit_json = StravaRequest.estimated_current_rate_limit.as_json
+      @batch_size = StravaJobs::ScheduledRequestEnqueuer::BATCH_SIZE
+    end
+
+    private
+
+    def short_resets_in
+      (15 - @now.min % 15).minutes - @now.sec.seconds
+    end
+
+    def short_resets_display
+      if short_resets_in < 60
+        helpers.pluralize(short_resets_in.to_i, "second")
+      else
+        helpers.pluralize((short_resets_in / 60).to_i, "min")
+      end
+    end
+
+    def long_resets_at
+      @now.tomorrow.beginning_of_day
+    end
+  end
+end
