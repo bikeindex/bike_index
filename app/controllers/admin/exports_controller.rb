d7e6eb3c6ac2fa67f910c2ab6c92408480e48d46
Seth Herr <seth.william.herr@gmail.com>
2026-03-31 12:31:13 -0400

    view deleted

--- app/controllers/admin/exports_controller.rb
+++ app/controllers/admin/exports_controller.rb
@@ -1,12 +1,25 @@
 class Admin::ExportsController < Admin::BaseController
+  before_action :set_period, only: [:index]
+
   def index
     @per_page = permitted_per_page(default: 10)
+    @pagy, @collection = pagy(:countish,
+      matching_exports.includes(:organization, :user).order(created_at: :desc),
+      limit: @per_page, page: permitted_page)
+  end
+
+  helper_method :matching_exports
+
+  private
+
+  def matching_exports
     exports = if params[:organization_id].present?
       Export.where(organization_id: current_organization.id)
     else
       Export.all
     end
-    @pagy, @exports = pagy(:countish, exports.includes(:organization, :user).order(created_at: :desc),
-      limit: @per_page, page: permitted_page)
+    @deleted = Binxtils::InputNormalizer.boolean(params[:search_deleted])
+    exports = exports.deleted if @deleted
+    exports.where(created_at: @time_range)
   end
 end
