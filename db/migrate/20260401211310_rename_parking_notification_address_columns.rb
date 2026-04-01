80b209304f83ea0b6964767737150ec0aecfc046
Seth Herr <seth.william.herr@gmail.com>
2026-04-01 18:06:18 -0400

    column exists dance

--- db/migrate/20260401211310_rename_parking_notification_address_columns.rb
+++ db/migrate/20260401211310_rename_parking_notification_address_columns.rb
@@ -1,7 +1,7 @@
 class RenameParkingNotificationAddressColumns < ActiveRecord::Migration[8.1]
   def change
-    rename_column :parking_notifications, :zipcode, :postal_code
-    rename_column :parking_notifications, :state_id, :region_record_id
-    add_column :parking_notifications, :region_string, :string
+    rename_column :parking_notifications, :zipcode, :postal_code if column_exists?(:parking_notifications, :zipcode)
+    rename_column :parking_notifications, :state_id, :region_record_id if column_exists?(:parking_notifications, :state_id)
+    add_column :parking_notifications, :region_string, :string unless column_exists?(:parking_notifications, :region_string)
   end
 end
