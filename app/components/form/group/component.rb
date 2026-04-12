c2699d8546cadf5326fc7d698e37b47fb36f936f
Seth Herr <seth.william.herr@gmail.com>
2026-04-12 17:24:20 -0400

    Add Form::Input and Form::Group, move legacy_form_well into form/ (#3404)
    Pull in Form::Input and Form::Group components
    
    Move `legacy_form_well` into the form folder

---
+++ app/components/form/group/component.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Form
+  module Group
+    class Component < ApplicationComponent
+      def initialize(form_builder:, attribute:, kind: :text_field, label_text: nil, html_options: {})
+        @form_builder = form_builder
+        @attribute = attribute
+        @kind = kind
+        @label_text = label_text || attribute.to_s.humanize
+        @html_options = html_options
+      end
+    end
+  end
+end
