24b8c498101f2bb30dc5bfb4005db51e65a801fd
Seth Herr <seth.william.herr@gmail.com>
2026-04-12 17:06:41 -0400

    Update and improve UI::Dropdown component (#3395)
    Switch UI::Dropdown from accepting an `options:` array of `html_safe`
    strings to using ViewComponent's `renders_many :items` slots.
    
    Also:
    - Updates Admin IndexSkeleton to use dropdown component instead of
    bootstrap dropdown
    - Moves a bunch of additional default styles into the legacy layer in
    `admin.scss` (to fix overriding that was happening to the dropdown)

--- spec/components/ui/dropdown/component_system_spec.rb
+++ spec/components/ui/dropdown/component_system_spec.rb
@@ -3,16 +3,54 @@
 require "rails_helper"
 
 RSpec.describe UI::Dropdown::Component, :js, type: :system do
-  it "toggles menu on click" do
-    visit("/rails/view_components/ui/dropdown/component/default")
+  context "when interacting with the default dropdown" do
+    it "opens and closes" do
+      visit "/rails/view_components/ui/dropdown/component/default"
 
-    expect(page).to be_axe_clean.skipping(*SKIPPABLE_AXE_RULES)
-    expect(page).not_to have_css("[data-dropdown-target='menu']:not(.tw\\:hidden)")
+      expect(page).to have_css('[aria-expanded="false"]')
+      expect(page).to be_axe_clean.skipping(*SKIPPABLE_AXE_RULES)
 
-    click_button "menu"
+      click_button("Menu ▾")
 
-    expect(page).to have_text("Profile")
-    expect(page).to have_text("Settings")
-    expect(page).to be_axe_clean.skipping(*SKIPPABLE_AXE_RULES)
+      expect(page).to have_css('[aria-expanded="true"]')
+      expect(page).to have_text("Profile")
+      expect(page).to have_text("Settings")
+      expect(page).to have_text("Logout")
+      expect(page).to have_css('li[role="menuitem"]:nth-child(2) + li[role="separator"] + li[role="menuitem"]')
+      expect(page).to be_axe_clean.skipping(*SKIPPABLE_AXE_RULES)
+
+      send_keys(:escape)
+
+      expect(page).to have_css('[aria-expanded="false"]')
+
+      click_button("Menu ▾")
+
+      expect(page).to have_css('[aria-expanded="true"]')
+
+      page.find("body").click
+
+      expect(page).to have_css('[aria-expanded="false"]')
+    end
+  end
+
+  context "when interacting with the custom_button dropdown" do
+    it "opens with header and items" do
+      visit "/rails/view_components/ui/dropdown/component/custom_button"
+
+      expect(page).to have_css('[aria-expanded="false"]')
+      expect(page).to be_axe_clean.skipping(*SKIPPABLE_AXE_RULES)
+
+      click_button("seth herr ▾")
+
+      expect(page).to have_css('[aria-expanded="true"]')
+      expect(page).to have_text("Last synced: 2 minutes ago")
+      expect(page).to have_text("Settings")
+      expect(page).to have_text("Sync")
+      expect(page).to be_axe_clean.skipping(*SKIPPABLE_AXE_RULES)
+
+      send_keys(:escape)
+
+      expect(page).to have_css('[aria-expanded="false"]')
+    end
   end
 end
