3f17be8f4f45bb599d499bbef83fbafcac705a41
Seth Herr <seth.william.herr@gmail.com>
2026-04-14 14:13:46 -0700

    More

--- app/javascript/controllers/org/multi_serial_search_controller.js
+++ app/javascript/controllers/org/multi_serial_search_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from '@hotwired/stimulus'
 
+/* global Turbo */
+
 // Connects to data-controller='org--multi-serial-search'
 export default class extends Controller {
   static targets = ['textarea', 'button', 'serialChips', 'results']
@@ -37,7 +39,7 @@ export default class extends Controller {
     this.renderPlaceholderChips(serials)
     this.buttonTarget.disabled = true
 
-    await Promise.all(serials.map((serial, index) => this.searchSerial(serial, index)))
+    // await Promise.all(serials.map((serial, index) => this.searchSerial(serial, index)))
 
     this.buttonTarget.disabled = false
     this.searching = false
@@ -49,48 +51,31 @@ export default class extends Controller {
       const chip = document.createElement('span')
       chip.id = `chip_${index}`
       chip.className = this.emptyClassValue
-      chip.textContent = serial + ' '
+      chip.appendChild(this.serialSpan(serial))
       chip.insertAdjacentHTML('beforeend', this.spinnerValue)
       this.serialChipsTarget.appendChild(chip)
     })
   }
 
+  serialSpan (serial) {
+    const span = document.createElement('span')
+    span.className = 'serial-span tw:mr-3'
+    span.textContent = serial.toUpperCase()
+    return span
+  }
+
   async searchSerial (serial, index) {
     const url = new URL(this.urlValue, window.location.origin)
     url.searchParams.set('serial', serial)
+    url.searchParams.set('chip_id', `chip_${index}`)
 
     const response = await fetch(url, {
       headers: { Accept: 'text/vnd.turbo-stream.html' }
     })
 
     if (response.ok) {
-      const html = await response.text()
-      const template = document.createElement('template')
-      template.innerHTML = html
-      const streams = template.content.querySelectorAll('turbo-stream')
-      streams.forEach(stream => {
-        const target = document.getElementById(stream.getAttribute('target'))
-        const content = stream.querySelector('template').content
-        if (target && stream.getAttribute('action') === 'append') {
-          target.appendChild(content.cloneNode(true))
-        }
-      })
-      this.updateChip(index, serial)
+      Turbo.renderStreamMessage(await response.text())
       window.timeLocalizer?.localize()
     }
-  }
-
-  updateChip (index, serial) {
-    const chip = document.getElementById(`chip_${index}`)
-    if (!chip) return
-    const hasResults = this.resultsTarget.querySelector(`[data-serial="${serial}"] table`)
-    chip.className = hasResults ? this.successClassValue : this.grayClassValue
-    chip.textContent = serial
-  }
-
-  escapeHtml (text) {
-    const div = document.createElement('div')
-    div.textContent = text
-    return div.innerHTML
   }
 }
