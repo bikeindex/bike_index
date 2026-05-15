b6382d563baf2d0b0a9d1ed234bcc0e68eb45368
Seth Herr <seth.william.herr@gmail.com>
2026-05-15 09:47:26 -0700

    Rename table-multi-delete Stimulus controller to table-multi-select
    The controller is used for both deletion (admin bikes) and non-deletion
    flows (stolen bike approval, missing manufacturer update); the new name
    reflects its actual responsibility.

--- app/javascript/controllers/table_multi_select_controller.js
+++ app/javascript/controllers/table_multi_select_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from '@hotwired/stimulus'
 
-// Connects to data-controller="table-multi-delete"
+// Connects to data-controller="table-multi-select"
 export default class extends Controller {
   static targets = ['checkbox']
 
