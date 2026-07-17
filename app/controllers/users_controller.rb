1bf21b7bdb6cb003515fd4173a71fd297e739faa
Seth Herr <seth.william.herr@gmail.com>
2026-07-17 09:50:34 -0700

    Better language

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     return super unless action_name == "create"
 
     @user = User.new(email: params.dig(:user, :email))
-    flash.now[:error] = translation(:csrf_invalid, scope: [:controllers, :application, :handle_unverified_request])
+    flash.now[:error] = translation(:invalid_authenticity_token, scope: [:controllers, :application, :handle_unverified_request])
     render_partner_or_default_signin_layout(render_action: :new)
   end
 
