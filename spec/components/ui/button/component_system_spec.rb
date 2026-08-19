6c97427bddf32fcb775ea5591d6767cdfe1f0c67
Seth Herr <seth.william.herr@gmail.com>
2026-08-18 21:26:56 -0700

    Revert "TEMPORARY: touch a spec to time a CI run"
    This reverts commit cea503eeb431fb3610b12a56f7816b8da64bc098.

--- spec/components/ui/button/component_system_spec.rb
+++ spec/components/ui/button/component_system_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# TEMPORARY: touches a spec file to time a representative CI run. Reverted next commit.
-
 require "rails_helper"
 
 RSpec.describe UI::Button::Component, :js, type: :system do
