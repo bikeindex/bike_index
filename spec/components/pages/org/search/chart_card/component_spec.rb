5f8c719a0ec787b8266d7145d13aecf1bc124c08
Seth Herr <seth.william.herr@gmail.com>
2026-09-21 10:56:51 -0700

    Revert "Chart card delta badges are tooltips naming the previous window's count"
    This reverts commit a28304638bb076262d3c438e62034fa49a2cace7.

--- spec/components/pages/org/search/chart_card/component_spec.rb
+++ spec/components/pages/org/search/chart_card/component_spec.rb
@@ -17,8 +17,7 @@ RSpec.describe Pages::Org::Search::ChartCard::Component, type: :component do
     expect(component).to have_css("a[aria-current='true']", text: "Last year")
     expect(component).to have_link("Current search", href: scope_paths[:search])
     expect(component).to have_text("Total registrations")
-    expect(component).to have_css("button[aria-describedby]", exact_text: "+100%")
-    expect(component).to have_css("[role=tooltip]", exact_text: "6 last year", visible: :all)
+    expect(component).to have_text("+100%")
     expect(component).to have_text("Reported stolen")
     expect(component).to have_css("dl div", count: 2)
   end
@@ -29,7 +28,6 @@ RSpec.describe Pages::Org::Search::ChartCard::Component, type: :component do
     it "captions the search" do
       expect(component).to have_text("Chart · Current search", normalize_ws: true)
       expect(component).to have_css("a[aria-current='true']", text: "Current search")
-      expect(component).to have_css("[role=tooltip]", exact_text: "6 the period before", visible: :all)
     end
   end
 
